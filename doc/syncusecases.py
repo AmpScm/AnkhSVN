d993afbc9c01101295cc484cc71d94e23a3df394
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-02-09 13:24:58 +0000

    /doc/exportdiagrams.py - New script that uses the python COM bindings to access the Enterprise Architect automation model and save all of the non-empty diagrams to a .png file. It also creates an XML file suitable for inclusion into files.xml so that it is viewable in the file browser. /doc/filebrowserhelpers.py - Helper functions for creating those xml files that gets included into files.xml /doc/syncusecases.py - Factored out a lot of functionality into the above file

--- doc/syncusecases.py
+++ doc/syncusecases.py
@@ -2,33 +2,29 @@ import os, os.path
 from Ft.Xml.Domlette import NonvalidatingReader
 from Ft.Xml.XPath import Evaluate
 from Ft.Xml.Domlette import PrettyPrint
+import filebrowserhelpers
+import sys
 
 PATH="uml\\usecases"
 FILES="files.xml"
 HANDLER = "showusecase.html"
 
 def createNode( case, doc ):
-    fileNode = doc.createElementNS( None, "file" )
-    fileNode.setAttributeNS( None, 'path', case )
-    
-    handlerNode = doc.createElementNS( None, "handler" )
-    handlerNode.appendChild( doc.createTextNode( HANDLER ) )
-    fileNode.appendChild( handlerNode )
 
     realpath = os.path.join( PATH, case ).replace("\\", "/" )
-    realpathNode = doc.createElementNS( None, "realpath" )
-    realpathNode.appendChild( doc.createTextNode( realpath ) )
-    fileNode.appendChild( realpathNode )
 
     usecasedoc = NonvalidatingReader.parseUri( realpath )
     name = Evaluate( "/UseCase/Preface/Name/text()", usecasedoc.documentElement )[0].data
-    nameNode = doc.createElementNS( None, "name" )
-    nameNode.appendChild( doc.createTextNode( name ) )
-    fileNode.appendChild( nameNode )
-    
-    fileNode.appendChild( doc.createElementNS( None, "description" ) )
 
-    return fileNode
+    nodeObj = filebrowserhelpers.FileNode(  path = case, 
+                                            name=name, 
+                                            realpath=realpath,
+                                            description = "",
+                                            handler = HANDLER )
+    
+    
+
+    return filebrowserhelpers.createXmlFileNode( doc, nodeObj )
 
 
 def sync():
@@ -36,23 +32,21 @@ def sync():
         os.path.splitext( path )[1]==".xml"
     usecases = [ file for file in os.listdir( PATH ) if isuc( file ) ]
 
-    doc = NonvalidatingReader.parseUri( FILES )
-
-    usecaseRoot = Evaluate( "/root/directory[@path='design']/directory[@path='usecases']", \
-        doc.documentElement )
-    if not usecaseRoot:
-        print "Blah at files.xml. Aborting"
-        return
-    usecaseRoot = usecaseRoot[0]
+    
+    doc, useCaseRoot = filebrowserhelpers.createDocAndNode( 
+"""<directories>
+    <directory path="usecases">
+        <name>Use cases</name>
+    </directory>
+</directories>""")
     
     for case in usecases:
-        matches = Evaluate( "//file[@path='%s']" % case, doc.documentElement )
-        if not matches:
-            print "%s not found" % case
-            usecaseRoot.appendChild( createNode( case, doc ) )
+        print case
+        node = createNode( case, doc )
+        useCaseRoot.appendChild( createNode( case, doc ) )
 
 
-    out = open( FILES, "w" )
+    out = open( sys.argv[1], "w" )
     PrettyPrint( doc, out )
     out.close()
 
