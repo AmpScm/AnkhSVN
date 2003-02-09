d3de5e9694af5fd97666dcca8a5b619f1018a69f
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-02-09 23:54:18 +0000

    A    /src/NSvn.Core/StringHelper.h - A helper class to help with conversions between .NET strings and char*'s

---
+++ trunk/src/NSvn.Core/StringHelper.h
@@ -0,0 +1,74 @@
+#include "Stdafx.h"
+#include <vcclr.h>
+using namespace System;
+using namespace System::Runtime::InteropServices;
+
+namespace NSvn
+{
+    namespace Core
+    {
+        // Helper class to effect string conversions
+        class StringHelper
+        {
+        public:
+            // constructor that takes a System::String
+            StringHelper( String __gc* string )
+            {
+                this->string = string;
+                this->charPtr = 0;
+            }
+
+            // constructor that takes a const char*
+            StringHelper( const char* charPtr )
+            {
+                this->string = this->ConvertToSystemString( charPtr );
+                this->charPtr = 0;
+            }
+
+            // implicit conversion to a System::String
+            operator String*()
+            {
+                return this->string;
+            }
+
+            // implicit conversion to a const char*
+            operator const char* ()
+            {
+                // lazy creation of the char* - we might not need it
+                if ( this->charPtr == 0 )
+                    this->charPtr = ConvertToCharPtr( this->string );
+
+                return this->charPtr;
+            }
+
+            // dtor - get rid of the char ptr
+            ~StringHelper()
+            {
+                if ( this->charPtr != 0 )
+                    FreeCharPtr( this->charPtr );
+            }
+
+        private:
+            // convert a char ptr to a System::String
+            String __gc* ConvertToSystemString( const char* ptr )
+            {
+                return Marshal::PtrToStringAnsi( static_cast<IntPtr>(const_cast<char*>(ptr) ) );
+            }
+
+            // convert a System::String to a char*
+            char* ConvertToCharPtr( String __gc* string )
+            {
+                return static_cast<char*>( Marshal::StringToHGlobalAnsi( string ).ToPointer() );
+            }
+
+            // free the memory allocated by the StringToHGlobalAnsi call
+            void FreeCharPtr( char* ptr )
+            {
+                Marshal::FreeHGlobal( static_cast<IntPtr>( static_cast<void*>(ptr) ) );
+            }
+
+            gcroot<String*> string;
+            char* charPtr;
+        };
+    }
+}
