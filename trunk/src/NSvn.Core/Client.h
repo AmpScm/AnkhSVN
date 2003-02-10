0bdf6966664f1eb720a3973a7d2e8ffb1689d48d
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-02-10 17:30:50 +0000

    M    /src/NSvn.Core/Client.h - Changed the interface to match the new svn function signatures

--- trunk/src/NSvn.Core/Client.h
+++ trunk/src/NSvn.Core/Client.h
@@ -1,9 +1,8 @@
-#include "AuthenticationBaton.h"
-#include "AdminAccessBaton.h"
+
 #include "Revision.h"
 #include "CommitInfo.h"
 #include "PropListItem.h"
-#include "delegates.h"
+#include "ClientContext.h"
 
 #using <mscorlib.dll>
 #using <System.dll>
@@ -18,89 +17,77 @@ namespace NSvn
     {
         public __gc class Client
         {
-        public:
-            
-            static void Checkout( NotifyCallback* callback, AuthenticationBaton* authBaton, String* url, String* path, 
-                Revision* revision, bool recurse );
+        public:            
+            static void Checkout(String* url, String* path, Revision* revision, bool recurse, 
+                ClientContext* context);
 
-            static void Update( AuthenticationBaton* authBaton, String* path, Revision* revision, bool recurse,
-                NotifyCallback* callback );
+	        static void Update(String* path, Revision* revision, bool recurse, ClientContext* context);
 
-            static void Switch( AuthenticationBaton* authBaton, String* path, String* url, Revision* revision,
-                bool recurse, NotifyCallback* notifyCallback );
+	        static void Switch(String* path, String* url, Revision* revision, bool recurse, 
+                ClientContext* context);
 
-            static void Add( String* path, bool recursive, NotifyCallback* notifyCallback );
+	        static void Add(String* path, bool recursive, ClientContext* context);
 
-            static CommitInfo* MakeDir( String* path, AuthenticationBaton* authBaton, 
-                LogMessageCallback* logMessageCallback, NotifyCallback* notifyCallback );
+	        static CommitInfo* MakeDir(String* path, ClientContext* context);
 
-            static CommitInfo* Delete( String* path, AdminAccessBaton* adminAccessBaton, bool force,
-                AuthenticationBaton* authBaton, LogMessageCallback* logMessageCallback, 
-                NotifyCallback* callback );
+	        static CommitInfo* Delete(String* path, AdminAccessBaton* admAccessBaton, bool force, 
+                ClientContext* context);
 
-            static CommitInfo* Import( NotifyCallback* notifyCallback, AuthenticationBaton* authBaton, 
-                String* path, String* url, String* newEntry, LogMessageCallback* logMessageCallback,
-                bool nonRecursive );
+	        static CommitInfo* Import(String* path, String* url, String* newEntry, bool nonRecursive, 
+                ClientContext* context);
 
-            static CommitInfo* Commit( NotifyCallback* notifyCallback, AuthenticationBaton* authBaton,
-                String* targets[], LogMessageCallback* logMessageCallback, bool nonRecursive );
+	        static CommitInfo* Commit(String __gc* targets[], bool nonRecursive, ClientContext* context);
 
-            static StringDictionary* Status( Revision* youngest, AuthenticationBaton* authBaton, 
-                bool descend, bool getAll, bool update, bool noIgnore, NotifyCallback* notifyCallback );
+	        static StringDictionary* Status(Revision* youngest, String* path, bool descend, 
+                bool getAll, bool upDate,  bool noIgnore, ClientContext* context);
 
-            static void Log(AuthenticationBaton* authBaton, String* targets[], Revision* start, 
-                Revision* end, bool discoverChangePath, bool strictNodeHistory, 
-                LogMessageReceiver* receiver);
+	        static void Log(String* targets[], Revision* start, Revision* end, bool discoverChangePath, 
+                bool strictNodeHistory, LogMessageReceiver* receiver, ClientContext* context);
 
-            static void Diff( String* diffOptions[], AuthenticationBaton* authBaton, String* path1, 
-                Revision* revision1, String* path2, Revision* revision2, bool recurse, 
-                bool noDiffDeleted, Stream* outfile, Stream* errFile);
+	        static void Diff(String* diffOptions[], String* path1, Revision* revision1, 
+                String* path2, Revision* revision2, bool recurse, bool noDiffDeleted, 
+                Stream* outfile, Stream* errFile, ClientContext* context);
 
-            static void Merge( NotifyCallback* notifyCallback, AuthenticationBaton* authBaton, String* url1, 
-                Revision* revision1, String* url2, Revision* revision2, String* targetWcPath, 
-                bool recurse, bool force, bool dryRun);
+	        static void Merge(String* url1, Revision* revision1, String* url2, Revision* revision2, 
+                String* targetWcPath, bool recurse, bool force, bool dryRun, ClientContext* context);
 
-            static void Cleanup( String* dir);
+	        static void Cleanup(String* dir);
 
-            static void Revert( String* path, bool recursive, NotifyCallback* notifyCallback );
+	        static void Revert(String* path, bool recursive, ClientContext* context);
 
-            static void Resolve( String* path, NotifyCallback* notifyCallback, bool recursive );
+	        static void Resolve(String* path, bool recursive, ClientContext* context);
 
-            static CommitInfo* Copy(String* srcPath, Revision* srcRevision, String* dst, 
-                AdminAccessBaton* admAccess, AuthenticationBaton* authBatton, 
-                LogMessageCallback* logMessageCallback, NotifyCallback* notifyCallback);
+	        static CommitInfo* Copy(String* srcPath, Revision* srcRevision, String* dst, 
+                ClientContext* context);
 
-            static CommitInfo* Move( String* srcPath, Revision* srcRevision, String* dstPath, 
-                bool force, AuthenticationBaton* authBaton, LogMessageCallback* logMessageCallback, 
-                NotifyCallback* notifyCallback);
+	        static CommitInfo* Move(String* srcPath, Revision* srcRevision, String* dstPath, 
+                bool force, ClientContext* context);
 
-            static void PropSet(String* propName, Byte propval[], String* target, bool recurse);
+	        static void PropSet(String* propName, Byte propval[], String* target, bool recurse);
 
-            static void RevPropSet(String* propName, Byte propval[], String* url, Revision* revision, 
-                AuthenticationBaton* authBaton, RevisionNumber* setRev);
+	        static void RevPropSet(String* propName, Byte propval[], String* url, Revision* revision, 
+                RevisionNumber* setRev, ClientContext* context);
 
-            static StringDictionary* PropGet( String* propName, String* target, Revision* revision, 
-                AuthenticationBaton* authBaton, bool recurse);
+	        static StringDictionary* PropGet(String* propName, String* target, Revision* revision, 
+                bool recurse, ClientContext* context);
 
-            static Byte RevPropGet( String* propName, String* url, Revision* revision, 
-                AuthenticationBaton* authBaton, RevisionNumber* setRev) [];
+	        static Byte RevPropGet(String* propName, String* url, Revision* revision, 
+                RevisionNumber* setRev, ClientContext* context) [];
 
-            static PropListItem* PropList( String* target, Revision* revision, AuthenticationBaton* authBaton, 
-                bool recurse) [];
+	        static PropListItem* PropList(String* target, Revision* revision, 
+                AuthenticationBaton* authBaton, bool recurse, ClientContext* context)[];
 
-            static StringDictionary* RevPropList( String* url, Revision* revision, AuthenticationBaton* authBaton, 
-                RevisionNumber* setRev);
+	        static StringDictionary* RevPropList(String* url, Revision* revision, RevisionNumber* setRev, 
+                ClientContext* context);
 
-            static void Export( String* url, String* to, Revision* revision, 
-                AuthenticationBaton* authBaton, NotifyCallback* notifyCallback);
+	        static void Export(String* url, String* to, Revision* revision, ClientContext* context);
 
-            static String* Ls( String* url, Revision* revision, AuthenticationBaton* authBaton, bool recurse) [];
+	        static String* Ls(String* url, Revision* revision, bool recurse, ClientContext* context)[];
 
-            static void Cat( Stream* out, String* url, Revision* revision, AuthenticationBaton* authBaton);
-
+	        static void Cat(Stream* out, String* url, Revision* revision, ClientContext* context);
         private:
             // ctor made private to avoid instantiation of this class
-            Client();
+            Client(){;}
         };
     }
 }
