6d1cfd56150247b0aef7ea125d648b53ff33fbfa
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-02-12 11:23:19 +0000

    A    /src/NSvn.Core.Tests.MCpp/ManagedPointerTest.h - Test for the ManagedPointer class A    /src/NSvn.Core.Tests.MCpp/ManagedPointerTest.cpp - Ditto M    /src/NSvn.Core.Tests.MCpp/NSvn.Core.Tests.MCpp.vcproj - Added the above class

---
+++ src/NSvn.Core.Tests.MCpp/ManagedPointerTest.h
@@ -0,0 +1,25 @@
+#pragma once
+#using <nunit.framework.dll>
+using namespace NUnit::Framework;
+
+namespace NSvn
+{
+    namespace Core
+    {
+        namespace Tests
+        {
+            namespace MCpp
+            {
+                [TestFixture]
+                public __gc class ManagedPointerTest
+                {
+                public:
+                    /// <summary>Test that the thing works</summary>
+                    [Test]
+                    void TestBasic();
+
+                };
+            }
+        }
+    }
+}
