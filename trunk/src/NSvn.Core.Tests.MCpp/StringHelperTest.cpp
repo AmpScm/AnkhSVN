81020c6c9032b0f87cbd92800d4d743af4de018c
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-02-12 09:22:18 +0000

    Added new project for tests in Managed C++ A    /src/NSvn.Core.Tests.MCpp - Directory for the new projet A    /src/NSvn.Core.Tests.MCpp/Stdafx.h - Blah A    /src/NSvn.Core.Tests.MCpp/StringHelperTest.cpp - Test for the StringHelper class A    /src/NSvn.Core.Tests.MCpp/StringHelperTest.h - Ditto A    /src/NSvn.Core.Tests.MCpp/Stdafx.cpp - Blah A    /src/NSvn.Core.Tests.MCpp/ReadMe.txt - Blah A    /src/NSvn.Core.Tests.MCpp/AssemblyInfo.cpp - Blah A    /src/NSvn.Core.Tests.MCpp/NSvn.Core.Tests.MCpp.vcproj - Blah M    /src/src.sln - Blah

---
+++ trunk/src/NSvn.Core.Tests.MCpp/StringHelperTest.cpp
@@ -0,0 +1,53 @@
+#include "StdAfx.h"
+#include "../NSvn.Core/StringHelper.h"
+#include "stringhelpertest.h"
+#include <cstring>
+#using <mscorlib.dll>
+
+namespace NSvn
+{
+namespace Core
+{
+namespace Tests
+{
+namespace MCpp
+{
+    void StringHelperTest::TestFromConstChar()
+    {
+        char constChar[] = "Hello world";
+        StringHelper s( constChar );
+      
+        Assertion::AssertEquals( static_cast<String*>(s), S"Hello world" );
+    }
+
+    void StringHelperTest::TestFromSystemString()
+    {
+        String* sysString = S"Hello world";
+        StringHelper s( sysString );
+
+        Assertion::Assert( 
+            std::strcmp( static_cast<const char*>(s), "Hello world" ) == 0 );
+    }
+
+    void StringHelperTest::TestCopying()
+    {
+        StringHelper s( "Hello world" );
+
+        //make sure the const char* ptr is created before it is copied
+        const char* ptr = static_cast<const char*>(s);
+
+        StringHelper s2 = s;
+
+        Assertion::Assert( static_cast<String*>(s2) == static_cast<String*>(s) );
+
+        //make sure they're not the same string
+        const char* ptr1 = s;
+        const char* ptr2 = s2;
+        Assertion::Assert( ptr1 != ptr2 );
+
+    }
+}
+}
+}
+}
+
---
+++ trunk/src/NSvn.Core.Tests.MCpp/StringHelperTest.h
@@ -0,0 +1,29 @@
+#pragma once
+#using <mscorlib.dll>
+#using <NUnit.Framework.dll>
+using namespace NUnit::Framework;
+
+namespace NSvn{
+    namespace Core{
+        namespace Tests{
+            namespace MCpp{
+                [TestFixture]
+                public __gc class StringHelperTest
+                {
+                public:
+                    [Test]
+                    //Test that the implicit conversion to String* works
+                    void TestFromConstChar(); 
+
+                    //Test that the implicit conversion to char* works
+                    [Test]
+                    void TestFromSystemString();
+
+                    //Test that the string can be copied
+                    [Test]
+                    void TestCopying();
+                };
+            }
+        }
+    }
+}
