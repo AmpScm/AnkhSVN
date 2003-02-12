d99cdf5ef9a2cb243b68770a00ba3dfcf98c6465
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-02-12 09:22:18 +0000

    Added new project for tests in Managed C++ A    /src/NSvn.Core.Tests.MCpp - Directory for the new projet A    /src/NSvn.Core.Tests.MCpp/Stdafx.h - Blah A    /src/NSvn.Core.Tests.MCpp/StringHelperTest.cpp - Test for the StringHelper class A    /src/NSvn.Core.Tests.MCpp/StringHelperTest.h - Ditto A    /src/NSvn.Core.Tests.MCpp/Stdafx.cpp - Blah A    /src/NSvn.Core.Tests.MCpp/ReadMe.txt - Blah A    /src/NSvn.Core.Tests.MCpp/AssemblyInfo.cpp - Blah A    /src/NSvn.Core.Tests.MCpp/NSvn.Core.Tests.MCpp.vcproj - Blah M    /src/src.sln - Blah

---
+++ src/NSvn.Core.Tests.MCpp/Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+// NSvn.Core.Tests.MCpp.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
