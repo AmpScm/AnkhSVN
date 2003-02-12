57a2b9cea455bbb8c66b6b33e6259ca90964ba2e
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-02-12 10:33:01 +0000

    M    /src/NSvn.Core.Tests.MCpp/StringHelperTest.cpp - Unit test for the StringHelper class M    /src/NSvn.Core.Tests.MCpp/StringHelperTest.h - Ditto

--- trunk/src/NSvn.Core.Tests.MCpp/StringHelperTest.cpp
+++ trunk/src/NSvn.Core.Tests.MCpp/StringHelperTest.cpp
@@ -36,11 +36,27 @@ namespace MCpp
         //make sure the const char* ptr is created before it is copied
         const char* ptr = static_cast<const char*>(s);
 
-        StringHelper s2 = s;
+        StringHelper s2( s );
 
         Assertion::Assert( static_cast<String*>(s2) == static_cast<String*>(s) );
 
         //make sure they're not the same string
+        const char* ptr1 = s;
+        const char* ptr2 = s2;
+        Assertion::Assert( ptr1 != ptr2 );
+
+    }
+
+    void StringHelperTest::TestAssignment()
+    {
+        StringHelper s( "Hello world" );
+
+        StringHelper s2 = S"Moo world";
+
+        s2 = s;
+        Assertion::Assert( static_cast<String*>(s2) == static_cast<String*>(s) );
+
+        //make sure they're not the same char ptr
         const char* ptr1 = s;
         const char* ptr2 = s2;
         Assertion::Assert( ptr1 != ptr2 );
