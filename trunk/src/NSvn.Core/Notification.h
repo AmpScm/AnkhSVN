b537928b6a3c084fb83ec8cec3024f2adde0fd56
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-02-14 15:04:19 +0000

    A    /src/NSvn.Core/svnenums.h - Contains mappings of svn enums to .NET enums A    /src/NSvn.Core/Notification.h - An object containing the arguments to an notify callback function

---
+++ trunk/src/NSvn.Core/Notification.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "StringHelper.h"
+#include "svnenums.h"
+
+#using <mscorlib.dll>
+
+namespace NSvn
+{
+    namespace Core
+    {
+        using namespace System;
+
+        public __gc class Notification
+        {
+        public:
+            Notification( const char *path, svn_wc_notify_action_t action, 
+                svn_node_kind_t kind, const char *mime_type, 
+                svn_wc_notify_state_t content_state, 
+                svn_wc_notify_state_t prop_state, svn_revnum_t revision )
+            {
+                this->path = StringHelper( path );
+                this->action = static_cast<NSvn::Core::NotifyAction>(action);
+                this->nodeKind = static_cast<NSvn::Core::NodeKind>(kind);
+                this->mimeType = StringHelper( mime_type );
+                this->contentState = static_cast<NSvn::Core::NotifyState>(content_state);
+                this->propertyState = static_cast<NSvn::Core::NotifyState>(prop_state);
+                this->revisionNumber = revision;
+            }
+
+
+            ///<summary>The path affected in this notification</summary>
+            __property String* get_Path()
+            { return this->path; }
+
+            __property NotifyAction get_Action()
+            { return this->action; }
+
+            __property NodeKind get_NodeKind()
+            { return this->nodeKind; }
+
+            __property String* get_MimeType()
+            { return this->mimeType; }
+
+            __property NotifyState get_ContentState()
+            { return this->contentState; }
+
+            __property NotifyState get_PropertyState()
+            { return this->propertyState; }
+
+            __property int get_RevisionNumber()
+            { return this->revisionNumber; }
+
+        private:
+            String* path;
+            NSvn::Core::NotifyAction action;
+            NSvn::Core::NodeKind nodeKind;
+            String* mimeType;
+            NSvn::Core::NotifyState contentState;
+            NSvn::Core::NotifyState propertyState;
+            int revisionNumber;
+            
+        };
+
+    }
+}
---
+++ trunk/src/NSvn.Core/svnenums.h
@@ -0,0 +1,64 @@
+#pragma once
+#using <mscorlib.dll>
+#include <svn_types.h>
+#include <svn_wc.h>
+
+namespace NSvn
+{
+    namespace Core
+    {
+        public __value enum NodeKind
+        { 
+            None = svn_node_none, 
+            File = svn_node_file, 
+            Directory = svn_node_dir, 
+            Unknown = svn_node_unknown 
+         
+        };
+
+        public __value enum NotifyAction
+        {
+            Add = svn_wc_notify_add,
+            Copy = svn_wc_notify_copy,
+            Delete = svn_wc_notify_delete,
+            Restore = svn_wc_notify_restore,
+            Revert = svn_wc_notify_revert,
+            FailedRevert = svn_wc_notify_failed_revert,
+            Resolve = svn_wc_notify_resolve,
+            Status = svn_wc_notify_status,
+            Skip = svn_wc_notify_skip,
+            UpdateDelete = svn_wc_notify_update_delete,
+
+            /** Got an add in an update. */
+            UpdateAdd = svn_wc_notify_update_add,
+
+            /** Got any other action in an update. */
+            UpdateUpdate = svn_wc_notify_update_update,
+
+            /** The last notification in an update */
+            UpdateCompleted = svn_wc_notify_update_completed,
+
+            /** About to update an external module, use for checkouts and switches too,
+            * end with @c svn_wc_update_completed.
+            */
+            UpdateExternal = svn_wc_notify_update_external,
+
+            CommitModified = svn_wc_notify_commit_modified,
+            CommitAdded = svn_wc_notify_commit_added,
+            CommitDeleted = svn_wc_notify_commit_deleted,
+            CommitReplaced = svn_wc_notify_commit_replaced,
+            CommitPostfixTxDelta = svn_wc_notify_commit_postfix_txdelta
+        };
+
+        public __value enum NotifyState
+        {
+            Inapplicable = svn_wc_notify_state_inapplicable, 
+            Unknown = svn_wc_notify_state_unknown, 
+            Unchanged = svn_wc_notify_state_unchanged, 
+            Changed = svn_wc_notify_state_changed, 
+            Merged = svn_wc_notify_state_merged, 
+            Conflicted = svn_wc_notify_state_conflicted 
+        };
+
+    }
+}
