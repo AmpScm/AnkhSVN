111dfd4f39044f0b2ae98da99bbcbfe45014613f
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-02-26 22:23:44 +0000

    M    /src/NSvn.Core.Tests/ResolveTest.cs - Created a second test for resolving a whole directory recursively M    /src/NSvn.Core.Tests/RevertTest.cs - Ditto, plus factored out some of the common code to a new method, ModifyFile

--- src/NSvn.Core.Tests/RevertTest.cs
+++ src/NSvn.Core.Tests/RevertTest.cs
@@ -29,20 +29,44 @@ namespace NSvn.Core.Tests
             ClientContext ctx = new ClientContext( new NotifyCallback( this.NotifyCallback ));
 
             string oldContents ;
+            string newContents;
+            this.ModifyFile( out oldContents, out newContents, filePath, filePath, false );
+
+
+            Assertion.AssertEquals( "File not reverted", oldContents, newContents );
+
+        }  
+ 
+        /// <summary>
+        ///Attempts to revert the whole working copy 
+        /// </summary>
+        [Test]
+        public void TestRevertDirectory()
+        {
+            string oldContents;
+            string newContents;
+            this.ModifyFile( out oldContents, out newContents, Path.Combine( this.WcPath, "Form.cs" ), 
+                this.WcPath, true );
+
+            Assertion.AssertEquals( "File not reverted", oldContents, newContents );
+
+        }
+
+        private void ModifyFile(out string oldContents, out string newContents, string filePath, 
+            string revertPath, bool recursive )
+        {            
+
             using ( StreamReader reader = new StreamReader( filePath ))
                 oldContents = reader.ReadToEnd();
             using ( StreamWriter writer = new StreamWriter (filePath ))
                 writer.WriteLine( "mooooooo" );
 
-            Client.Revert( filePath, false, ctx);
+            ClientContext ctx = new ClientContext( new NotifyCallback( this.NotifyCallback ));
+            Client.Revert( revertPath, recursive, ctx);
 
-            string newContents;
             using ( StreamReader reader = new StreamReader( filePath ))
                 newContents = reader.ReadToEnd();
-
-            Assertion.AssertEquals( "File not reverted", oldContents, newContents );
-
-        }   
+        } 
 		    
 	}
 }
