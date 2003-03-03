97dcdbc5a2197051e3446294a0a8e130845f2d79
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-03-03 11:19:27 +0000

    Changed the implementation of the Revision class. M    /src/NSvn.Core/Revision.h - Made the special revisions static members of this class M    /src/NSvn.Core/svnenums.h - Removed the RevisionKind enum

--- trunk/src/NSvn.Core/Revision.h
+++ trunk/src/NSvn.Core/Revision.h
@@ -12,11 +12,17 @@ namespace NSvn
         public __gc class Revision
         {
         public:
+            static Revision* const Unspecified = new Revision( svn_opt_revision_unspecified );
+            static Revision* const Committed = new Revision( svn_opt_revision_committed );
+            static Revision* const Previous = new Revision( svn_opt_revision_previous );
+            static Revision* const Base = new Revision( svn_opt_revision_base );
+            static Revision* const Working = new Revision( svn_opt_revision_working );
+            static Revision* const Head = new Revision( svn_opt_revision_head );
+
             /// <summary>Creates a revision from a revision number</summary>
             static Revision* FromNumber( int revision )
             {
-                Revision* rev = new Revision();
-                rev->kind = svn_opt_revision_number;
+                Revision* rev = new Revision( svn_opt_revision_number );
                 rev->revision = revision;
                 return rev;
             }
@@ -24,31 +30,24 @@ namespace NSvn
             /// <summary>Creates a revision from a date</summary>
             static Revision* FromDate( DateTime date )
             {
-                Revision* rev = new Revision();
-                rev->kind = svn_opt_revision_date;
+                Revision* rev = new Revision( svn_opt_revision_date);
                 rev->date = date;
                 return rev;
             }
 
-            /// <summary>Creates a special revision</summary>
-            static Revision* Special( RevisionKind kind )
-            {
-                Revision* rev = new Revision();
-                rev->kind = static_cast<svn_opt_revision_kind>(kind);
-                return rev;
-            }
-
+            
+            // convert to an svn_opt_revision_t*
+            // allocate in pool
             svn_opt_revision_t* ToSvnOptRevision( const Pool& pool )
             {
                 svn_opt_revision_t* rev = static_cast<svn_opt_revision_t*>(
                     pool.PCalloc( sizeof(*rev) ) );
-                rev->kind = static_cast<svn_opt_revision_kind>(this->kind);
+                rev->kind = this->kind;
 
-                //what kind?
-                
-                switch( kind )
+                //what kind?                
+                switch( this->kind )
                 {
-                case RevisionKind::Date:
+                case svn_opt_revision_date:
                     {
                         DateTime epoch( 1970, 1, 1 );
                         TimeSpan t = this->date - epoch;
@@ -56,7 +55,7 @@ namespace NSvn
                         rev->value.date = static_cast<Int64>(t.TotalMilliseconds * 1000);
                         break;
                     }
-                case RevisionKind::Number:
+                case svn_opt_revision_number:
                     rev->value.number = this->revision;
                     break;
                 default:
@@ -67,17 +66,13 @@ namespace NSvn
             }
             
         private:
-            Revision()
+            Revision( svn_opt_revision_kind kind ) : kind( kind )
             {;}
             DateTime date;       
             int revision;
             svn_opt_revision_kind kind;
 
         };
-
-       
-
-        
     }
 }
 
