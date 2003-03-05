f4dbb9858ed12b9c9a155452ed542dcdd1ff4b8d
Kristin <Kristin@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-03-05 12:11:09 +0000

    Added dialogbox for login
    A    /src/Ankh.UI/LoginDialog.cs

---
+++ trunk/src/Ankh.UI/LoginDialog.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Ankh.UI
+{
+	/// <summary>
+	/// Summary description for LoginDialog.
+	/// </summary>
+	public class LoginDialog : System.Windows.Forms.Form
+	{
+       
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public LoginDialog()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+
+			//
+			// TODO: Add any constructor code after InitializeComponent call
+			//
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.savePasswordButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.Location = new System.Drawing.Point(50, 37);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "username:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Location = new System.Drawing.Point(50, 69);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "password:";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(140, 37);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(128, 20);
+            this.usernameTextBox.TabIndex = 2;
+            this.usernameTextBox.Text = "";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(140, 69);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(128, 20);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.Text = "";
+            // 
+            // savePasswordButton
+            // 
+            this.savePasswordButton.Anchor = (System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right);
+            this.savePasswordButton.Location = new System.Drawing.Point(48, 127);
+            this.savePasswordButton.Name = "savePasswordButton";
+            this.savePasswordButton.TabIndex = 4;
+            this.savePasswordButton.Text = "Save";
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+                | System.Windows.Forms.AnchorStyles.Right);
+            this.okButton.Location = new System.Drawing.Point(131, 128);
+            this.okButton.Name = "okButton";
+            this.okButton.TabIndex = 5;
+            this.okButton.Text = "Ok";
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+                | System.Windows.Forms.AnchorStyles.Right);
+            this.cancelButton.Location = new System.Drawing.Point(214, 128);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.TabIndex = 6;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // LoginDialog
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(292, 157);
+            this.Controls.AddRange(new System.Windows.Forms.Control[] {
+                                                                          this.cancelButton,
+                                                                          this.okButton,
+                                                                          this.savePasswordButton,
+                                                                          this.passwordTextBox,
+                                                                          this.usernameTextBox,
+                                                                          this.passwordLabel,
+                                                                          this.usernameLabel});
+            this.Name = "LoginDialog";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
+        }
+		#endregion
+
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button savePasswordButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+	}
+}
