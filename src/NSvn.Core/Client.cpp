add70320905d8cd26977a050c1a1a0262e31fa3b
Per <Per@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-03-06 16:14:24 +0000

    Implemented Client.Move and made tests.
    M    /src/NSvn.Core/Client.cpp - Implemented Move, Renamed ctx -> context
    M    /src/NSvn.Core/Client.h - Ditto. 
    A    /src/NSvn.Core.Tests/MoveTest.cs - Implemented Test
    M    /src/NSvn.Core.Tests/NSvn.Core.Tests.csproj - deleted StatusTest

--- src/NSvn.Core/Client.cpp
+++ src/NSvn.Core/Client.cpp
@@ -9,21 +9,21 @@
 
 
 // implementation of Client::Add
-void NSvn::Core::Client::Add( String* path, bool recursive, ClientContext* ctx )
+void NSvn::Core::Client::Add( String* path, bool recursive, ClientContext* context )
 {
     Pool pool;
 
     String* truePath = CanonicalizePath( path );
-    HandleError( svn_client_add( StringHelper( truePath ), recursive, ctx->ToSvnContext( pool ), pool ) );
+    HandleError( svn_client_add( StringHelper( truePath ), recursive, context->ToSvnContext( pool ), pool ) );
 }
 // implementation of Client::MakeDir
-NSvn::Core::CommitInfo* NSvn::Core::Client::MakeDir( String* path, ClientContext* ctx )
+NSvn::Core::CommitInfo* NSvn::Core::Client::MakeDir( String* path, ClientContext* context )
 {
     Pool pool;
 
     String* truePath = CanonicalizePath( path );
     svn_client_commit_info_t* commitInfo = 0;
-    HandleError( svn_client_mkdir( &commitInfo, StringHelper( truePath ), ctx->ToSvnContext( pool ), 
+    HandleError( svn_client_mkdir( &commitInfo, StringHelper( truePath ), context->ToSvnContext( pool ), 
         pool ) );
 
     if ( commitInfo != 0 )
@@ -40,37 +40,37 @@ void NSvn::Core::Client::Cleanup( String* directory )
 }
 
 // implementation of Client::Revert
-void NSvn::Core::Client::Revert(String* path, bool recursive, ClientContext* ctx )
+void NSvn::Core::Client::Revert(String* path, bool recursive, ClientContext* context )
 {
     Pool pool;
     String* truePath = CanonicalizePath( path );
 
     HandleError( svn_client_revert( StringHelper( truePath ), recursive, 
-        ctx->ToSvnContext (pool), pool));
+        context->ToSvnContext (pool), pool));
 }    
 
 // implementation of Client::Resolve
-void NSvn::Core::Client::Resolve(String* path, bool recursive, ClientContext* ctx )
+void NSvn::Core::Client::Resolve(String* path, bool recursive, ClientContext* context )
 {
     Pool pool;
     String* truePath = CanonicalizePath( path );
 
     HandleError( svn_client_resolve( StringHelper( truePath ), recursive, 
-        ctx->ToSvnContext (pool), pool));
+        context->ToSvnContext (pool), pool));
 }  
 
 
 // implementation of Client::Status
 //TO-DO:   Core::StringDictionary to be reconsidered
 StringDictionary* NSvn::Core::Client::Status( long* youngest, String* path, bool descend,
-                                bool getAll, bool update, bool noIgnore, ClientContext* ctx )
+                                bool getAll, bool update, bool noIgnore, ClientContext* context )
 {
     apr_hash_t* statushash = 0;
     Pool pool;
     String* truePath = CanonicalizePath( path );
     
     HandleError( svn_client_status( &statushash, youngest, StringHelper( truePath ), descend,
-        getAll, update, noIgnore, ctx->ToSvnContext( pool ), pool ) );
+        getAll, update, noIgnore, context->ToSvnContext( pool ), pool ) );
 
     if ( statushash != 0 )
         return new StringDictionary();
@@ -89,37 +89,58 @@ void NSvn::Core::Client::PropSet(String* propName, Byte propval[], String* targe
 
 // implementation of Client::Checkout
 void NSvn::Core::Client::Checkout( String* url, String* path, Revision* revision, 
-                                  bool recurse, ClientContext* ctx )
+                                  bool recurse, ClientContext* context )
 {
     Pool pool;
     String* truePath = CanonicalizePath( path );
     HandleError( svn_client_checkout( StringHelper( url ), StringHelper( truePath ), 
-        revision->ToSvnOptRevision( pool ), recurse, ctx->ToSvnContext( pool ), pool ) );
+        revision->ToSvnOptRevision( pool ), recurse, context->ToSvnContext( pool ), pool ) );
 }
 
 // implementation of Client::Update
 void NSvn::Core::Client::Update( String* path, Revision* revision, bool recurse, 
-                                ClientContext* ctx )
+                                ClientContext* context )
 {
     Pool pool;
     String* truePath = CanonicalizePath( path );
     HandleError( svn_client_update( StringHelper(truePath), revision->ToSvnOptRevision( pool ),
-        recurse, ctx->ToSvnContext( pool ), pool ) );
+        recurse, context->ToSvnContext( pool ), pool ) );
 }
 
 // implementation of Client::Commit
-NSvn::Core::CommitInfo* NSvn::Core::Client::Commit( String* targets[], bool nonRecursive, ClientContext* ctx )
+NSvn::Core::CommitInfo* NSvn::Core::Client::Commit( String* targets[], bool nonRecursive, ClientContext* context )
 {
     Pool pool;
     apr_array_header_t* aprArrayTargets = StringArrayToAprArray( targets, pool );
     svn_client_commit_info_t* commitInfoPtr;
 
     HandleError( svn_client_commit( &commitInfoPtr, aprArrayTargets, nonRecursive, 
-        ctx->ToSvnContext( pool ), pool ) );
+        context->ToSvnContext( pool ), pool ) );
 
     return new CommitInfo( commitInfoPtr );
 }
+// implementation of Client::Move
+NSvn::Core::CommitInfo* NSvn::Core::Client::Move( String* srcPath, 
+                                                         Revision* srcRevision, String* dstPath, 
+                                                         bool force, ClientContext* context )
+{
+    Pool pool;
+    String* trueSrcPath = CanonicalizePath( srcPath );
+    String* trueDstPath = CanonicalizePath( dstPath );
 
+    svn_client_commit_info_t* commitInfoPtr = 0;
+
+    HandleError( svn_client_move ( &commitInfoPtr, StringHelper( trueSrcPath ), 
+        srcRevision->ToSvnOptRevision( pool ), StringHelper( trueDstPath ), force, 
+        context->ToSvnContext( pool ), pool ) );
+
+     if ( commitInfoPtr != 0 )
+        return new CommitInfo( commitInfoPtr );
+    else
+        return CommitInfo::Invalid;
+}
+
+// Converts array of .NET strings to apr array of const char
 apr_array_header_t* NSvn::Core::Client::StringArrayToAprArray( String* strings[], Pool& pool )
 {
     apr_array_header_t* array = apr_array_make( pool, strings->Length, sizeof( char* ) );
@@ -133,13 +154,13 @@ apr_array_header_t* NSvn::Core::Client::StringArrayToAprArray( String* strings[]
     return array;
 }
 
-
-
+// Converts "\" to "/"
 String* NSvn::Core::Client::CanonicalizePath( String* path )
 {
     return path->Replace( "\\", "/" );
 }
 
+// Converts Byte array to svn_string_t
 void NSvn::Core::Client::ByteArrayToSvnString( svn_string_t* string, Byte array[], const Pool& pool  )
 {
     string->len = array.Length;
@@ -147,9 +168,3 @@ void NSvn::Core::Client::ByteArrayToSvnString( svn_string_t* string, Byte array[
     Marshal::Copy( array, 0, const_cast<char*>(string->data), array.Length );
 
 }
-
-
-
-
-
-
