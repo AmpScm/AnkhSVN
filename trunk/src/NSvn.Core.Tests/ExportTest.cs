2c2a423745fe46f6903498bb677d9bd23b9e0d32
Kristin <Kristin@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-03-07 11:24:11 +0000

    Added export
    M    /src/NSvn.Core/Client.cpp : Added Client::Export
    M    /src/NSvn.Core/Client.h : Added Client:Export + added new variable "optionalAdmAccess" in Client::Copy
    A    /src/NSvn.Core.Tests/ExportTest.cs : Test of export from WC and Repository
    M    /src/NSvn.Core.Tests/NSvn.Core.Tests.csproj : Added ExportTest.cs

---
+++ trunk/src/NSvn.Core.Tests/ExportTest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace NSvn.Core.Tests
+{
+    /// <summary>
+    /// Tests for the Client::Export method
+    /// </summary>
+    [TestFixture]
+    public class ExportTest : TestBase
+    {
+        [SetUp]
+        public override void SetUp() 
+        {
+            base.SetUp();
+            this.ExtractRepos();
+            this.ExtractWorkingCopy();
+            this.newWc = this.FindDirName( Path.Combine( Path.GetTempPath(), "moo" ) );
+        }
+
+        [TearDown]
+        public override void TearDown()
+        {
+            base.TearDown();
+            //Directory.Delete( this.newWc, true );
+        }
+
+        /// <summary>
+        /// Test export operation from a repository
+        /// </summary>
+        [Test]
+        public void TestExportRepos()
+        {
+            ClientContext ctx = new ClientContext();
+            Client.Export( this.ReposUrl, this.newWc, Revision.Head, ctx );
+
+            Assertion.Assert( "Exported file not there", 
+                File.Exists( Path.Combine( this.newWc, "Form.cs" ) ) );
+            Assertion.Assert( ".svn directory found", 
+                !Directory.Exists( Path.Combine( this.newWc, ".svn" ) ) );
+        }
+        /// <summary>
+        /// Test export operation from a working copy
+        /// </summary>
+        public void TestExportWc()
+        { 
+            ClientContext ctx = new ClientContext( new NotifyCallback( this.NotifyCallback ) );
+            
+            Client.Export( this.WcPath, this.newWc, Revision.Head, ctx );
+
+            Assertion.Assert( "Exported file not there", 
+                File.Exists( Path.Combine( this.newWc, "Form.cs" ) ) );
+            Assertion.Assert( ".svn directory found", 
+                !Directory.Exists( Path.Combine( this.newWc, ".svn" ) ) );
+        }
+
+        private string newWc;
+
+    }
+}
