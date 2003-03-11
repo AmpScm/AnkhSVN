d63eb91e27cdb0d9d4b8f6a2c2a0a655cc5e7327
Per <Per@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-03-11 15:09:21 +0000

    Changed the wc.zip and repos.zip. They now include 5 revisions and an new Doc folder containig text files. Needed this for testing merge and later tests.
    M   /src/NSvn.Core.Tests/wc.zip - added new 5 revisions
    M   /src/NSvn.Core.Tests/repos.zip - ditto
    M   /src/NSvn.Core.Tests/CommitTest.cs - changed test to look for latest revision. Before 1, now 5.

--- trunk/src/NSvn.Core.Tests/CommitTest.cs
+++ trunk/src/NSvn.Core.Tests/CommitTest.cs
@@ -59,7 +59,7 @@ namespace NSvn.Core.Tests
             Assertion.Assert( "Wrong path", items[0].Path.IndexOf( 
                 this.filepath.Replace( "\\", "/" ) ) >= 0 );
             Assertion.AssertEquals( "Wrong kind", NodeKind.File, items[0].Kind );
-            Assertion.AssertEquals( "Wrong revision", 1, items[0].Revision );
+            Assertion.AssertEquals( "Wrong revision", 5, items[0].Revision );
 
             return "Moo is the log message";
         }
