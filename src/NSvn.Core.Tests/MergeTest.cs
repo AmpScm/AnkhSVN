960261899c2d3e10ff019fd63692108f2be3a830
Per <Per@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-03-12 13:01:41 +0000

    Implemented the Core:Merge and added the MergeTest. The test is not fully implemented yet.
    M    /src/NSvn.Core/Client.cpp - Implemented Core:Merge
    M    /src/NSvn.Core/Client.h - ditto
    A    /src/NSvn.Core.Tests/MergeTest.cs - Implemented 1 of 2 tests
    M    /src/NSvn.Core.Tests/NSvn.Core.Tests.csproj - added MergeTest

---
+++ src/NSvn.Core.Tests/MergeTest.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+//TODO: Not possible to do the testing yet as the repository
+//      contains only one revision.
+namespace NSvn.Core.Tests 
+{
+    /// <summary>
+    /// Tests the NSvn.Client.MoveFile method
+    /// </summary>
+    [TestFixture]
+    public class MergeTest : TestBase 
+    {
+        [SetUp]
+        public override void SetUp() 
+        {
+            base.SetUp();
+
+            this.ExtractRepos();
+            this.ExtractWorkingCopy(); 
+        }
+
+        /// <summary>
+        /// Tests rolling back a change with merge
+        /// </summary>
+        //TODO: Not possible to do the testing yet as the repository
+        //      contains only one revision.
+        [Test]
+        public void TestMergeRollBack() 
+        {
+            string srcPath = this.ReposUrl;
+            string dstPath = this.WcPath;
+            string comparePath = Path.Combine (this.WcPath, @"doc/text_r5.txt" );            
+
+            ClientContext ctx = new ClientContext() ;
+            Client.Merge( srcPath, Revision.FromNumber(5) , srcPath, Revision.FromNumber(4) ,
+                 dstPath, true, false, false, ctx ); 
+
+            Assertion.AssertEquals( "Wrong status", 'D', this.GetSvnStatus( comparePath ) );
+       
+        }
+
+        /// <summary>
+        /// Tests moving changes with merge
+        /// </summary>
+        [Ignore( "Not implemented yet" )]
+        [Test]
+        public void TestMergeChanges() 
+        {
+                  
+        }   
+
+        
+    }
+}
