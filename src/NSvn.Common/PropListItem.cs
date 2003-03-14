0507d0181a846d2b0160a0260d25c74d76d71be2
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-03-14 15:05:33 +0000

    Implemented Client::PropList
    M    /src/NSvn.Core/Client.cpp - Implementation of Client::PropList. Factored ConvertToPropertyDictionary so that it could deal with both path and property names as hash key. Implemented ConvertPropListArray.
    M    /src/NSvn.Core/Client.h - See above
    A    /src/NSvn.Common/PropListItem.cs - Represents an item in a property list 
    M    /src/NSvn.Common/NSvn.Common.csproj - Added above file
    A    /src/NSvn.Core.Tests/PropListTest.cs - Test for Client::PropList
    M    /src/NSvn.Core.Tests/NSvn.Core.Tests.csproj - Added above file

---
+++ src/NSvn.Common/PropListItem.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace NSvn.Common
+{
+	/// <summary>
+	/// Represents an item in a property list
+	/// </summary>
+	public class PropListItem
+	{
+		public PropListItem( string nodeName, PropertyDictionary properties )
+		{
+            this.properties = properties;
+            this.nodeName = nodeName;			
+		}
+
+        public PropertyDictionary Properties
+        {
+            get{ return this.properties; }
+        }
+
+        public string NodeName
+        {
+            get{ return this.nodeName; }
+        }
+
+
+        private PropertyDictionary properties;
+        private string nodeName;
+	}
+}
