56b6cbe0b02e66ef0b026d10cd13f020b9fd3f8b
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-03-20 14:34:47 +0000

    Forgot to add this file in the previous commit. It currently contains just a dummy test - required for NUnit to run this testsuite

---
+++ trunk/src/NSvn.Tests/WorkingCopyItemTest.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+
+namespace NSvn.Tests
+{
+	/// <summary>
+	/// Summary description for WorkingCopyItemTest.
+	/// </summary>
+	[TestFixture]
+	public class WorkingCopyItemTest
+	{
+        [Test]
+        public void DummyTest()
+        {}
+	}
+}
