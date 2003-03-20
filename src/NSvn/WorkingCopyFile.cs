315f125fe8ab7752ab8ce9d02ff41c9ffcf04103
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-03-20 05:04:43 +0000

    Implemented/stubbed out the initial classes for the high-level wrapper.
    M    /src/NSvn/AssemblyInfo.cs - Removed the ClsCompliant(true) attribute, because the NSvn.Core assembly is not cls compliant at this time.
    A    /src/NSvn/WorkingCopyFile.cs - Represents a file in a working copy.
    M    /src/NSvn/NSvn.csproj - Added the files.
    A    /src/NSvn/WorkingCopyDirectory.cs - Represents a directory in a working copy.
    A    /src/NSvn/ILocalItem.cs - Represents all items(versioned or unversioned) under a working copy directory.
    A    /src/NSvn/ILogMessageProvider.cs - Represents an object that can provide a log message.
    A    /src/NSvn/WorkingCopyItem.cs - Base class for WorkingCopyFile and WorkingCpyItem
    M    /src/NSvn.Core/NSvn.Core.vcproj - Hm.

---
+++ src/NSvn/WorkingCopyFile.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace NSvn
+{
+	/// <summary>
+	/// Represents a versioned file in an SVN working copy.
+	/// </summary>
+	public class WorkingCopyFile : WorkingCopyItem
+	{
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+		public WorkingCopyFile( string path ) : base( path )
+		{
+			// empty for now
+		}
+	}
+}
