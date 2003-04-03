efd0be17d74c0833725698b3a3c27c1004cde455
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-04-03 19:59:11 +0000

    A    /src/Ankh/RepositoryExplorerMenuAttribute.cs - An attribute for repository explorer menu items. A    /src/Ankh/RepositoryExplorerMenuItem.cs - Repository explorer menu items. A    /src/Ankh/RepositoryExplorerMenuItem.resx - Blah

---
+++ trunk/src/Ankh/RepositoryExplorerMenuAttribute.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Ankh
+{
+	/// <summary>
+	/// Describes a menu item in the repository explorer.
+	/// </summary>
+    [AttributeUsage( AttributeTargets.Class )]
+	public class RepositoryExplorerMenuAttribute : Attribute
+	{
+		public RepositoryExplorerMenuAttribute( string text )
+		{
+            this.text = text;			
+		}
+
+        public string Text
+        {
+            get{ return this.text; }
+        }
+
+        public int Position
+        {
+            get{ return this.position; }
+            set{ this.position = value; }
+        }
+
+        private string text;       
+        private int position;
+	}
+}
