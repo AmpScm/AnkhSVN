2cdf9d26737bd204e9524ac88f2e1cde1e56587e
Per <Per@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-04-08 16:03:42 +0000

    Implemented Ankh.AddItemCommand. Added shaddow to the unversioned icon ('?'). Commented out QuerySendByWeb. SolutionExplorer.UpdateSelectionStatus doesn't update the status icons after adding a file yet.
    M    /src/Ankh/status_icons.bmp - added shaddow to the unversioned icon
    M    /src/Ankh/Connect.cs - Commented out the QuerySendByWeb
    M    /src/Ankh/resourcevisitors.cs - Implemented UnversionedVisitor
    M    /src/Ankh/Commands/AddItemCommand.cs - Implemeted AddItem.

--- trunk/src/Ankh/Commands/AddItemCommand.cs
+++ trunk/src/Ankh/Commands/AddItemCommand.cs
@@ -1,51 +1,70 @@
-//// $Id$
-//using System;
-//using NSvn;
-//using NSvn.Core;
-//using NSvn.Common;
-//using EnvDTE;
-//namespace Ankh.Commands
-//{
-//	/// <summary>
-//	/// Summary description for AddItem.
-//	/// </summary>
-//	[VSNetCommand( "AddItem", Text = "Add", Tooltip = "Add files to revision control"),
-//     VSNetControl( "Item", Position = 3)]
-//
-//    
-//	internal class AddItemCommand : CommandBase
-//	{
-//        #region Implementation of ICommand
-//
-//        public override EnvDTE.vsCommandStatus QueryStatus(Ankh.AnkhContext context)
-//        {
-//            ILocalResource[] resources = context.SolutionExplorer.GetSelectedItems();        
-//
-//            bool addAble = true;
-//            foreach( ILocalResource resource in resources )
-//            {
-//               if (resource.IsVersioned)
-//                   addAble = false;
-//            }
-//            if (addAble)
-//                return vsCommandStatus.vsCommandStatusEnabled |
-//                    vsCommandStatus.vsCommandStatusSupported;
-//            else return vsCommandStatus.vsCommandStatusUnsupported ;
-//        }
-//
-//        public override void Execute(Ankh.AnkhContext context)
-//        {
-//            ILocalResource[] resources = context.SolutionExplorer.GetSelectedItems();        
-//
-//            foreach( ILocalResource resource in resources )
-//            {
-//                context.SolutionExplorer.UpdateItem( resource, 
-//                    (( UnversionedResource) resource).Add( false ) );
-//            }
-//        }
-//        #endregion
-//    }
-//}
-//
-//
-//
+// $Id$
+using System;
+using NSvn;
+using NSvn.Core;
+using NSvn.Common;
+using EnvDTE;
+namespace Ankh.Commands
+{
+	/// <summary>
+	/// Adds an unversioned item to a working copy
+	/// </summary>
+    [VSNetCommand("AddItem", Text = "Add", Tooltip = "Adds selected item to a working copy"),
+    VSNetControl( "Item", Position = 3 ),
+    VSNetControl( "Project", Position = 3 ),
+    VSNetControl( "Folder", Position = 3 ),
+    VSNetControl( "Solution", Position = 3)]
+    
+	internal class AddItemCommand : CommandBase
+	{
+        #region Implementation of ICommand
+
+        public override EnvDTE.vsCommandStatus QueryStatus(Ankh.AnkhContext context)
+        {
+            AddCandidateVisitor a = new AddCandidateVisitor();
+            context.SolutionExplorer.VisitSelectedItems( a );
+            
+            if ( a.Addable )
+                return vsCommandStatus.vsCommandStatusEnabled |
+                    vsCommandStatus.vsCommandStatusSupported;
+            else
+                return vsCommandStatus.vsCommandStatusEnabled;
+        }
+
+        public override void Execute(Ankh.AnkhContext context)
+        {
+            UnversionedVisitor a = new UnversionedVisitor(); 
+            context.SolutionExplorer.VisitSelectedItems( new AddVisitor() );
+            context.SolutionExplorer.UpdateSelectionStatus();
+        }
+        #endregion
+        
+        /// <summary>
+        /// Checks if the visited item is versioned.
+        /// </summary>
+        private class AddCandidateVisitor : LocalResourceVisitorBase
+        {
+            public bool Addable = false;
+
+            public override void VisitUnversionedResource(NSvn.UnversionedResource resource)
+            {
+                if ( !resource.IsVersioned )
+                    Addable = true;
+            }
+        }
+        /// <summary>
+        /// Visitor that adds visited item to the Working copy
+        /// </summary>
+
+        private class AddVisitor : LocalResourceVisitorBase
+        {
+            public override void VisitUnversionedResource(NSvn.UnversionedResource resource)
+            {
+                resource.Add( true );
+            }
+        }
+    }
+}
+
+
+
