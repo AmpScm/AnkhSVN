75a5c62c4dfe71183bcc6bcf6e9f8a59d0457c4f
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-04-09 16:39:57 +0000

    M    /src/Ankh/Commands/UpdateItemCommand.cs - Now uses a more intelligent approach to updating multiple items - if one file is in a directory that is on the list of update targets, we won't bother calling update on that file.

--- src/Ankh/Commands/UpdateItemCommand.cs
+++ src/Ankh/Commands/UpdateItemCommand.cs
@@ -3,6 +3,7 @@ using System;
 using EnvDTE;
 using NSvn;
 using Ankh.UI;
+using System.Collections;
 
 namespace Ankh.Commands
 {
@@ -33,18 +34,58 @@ namespace Ankh.Commands
         public override void Execute(AnkhContext context)
         {
             // we assume by now that all items are working copy resources.
-            context.SolutionExplorer.VisitSelectedItems( new UpdateVisitor() );
+            UpdateVisitor v = new UpdateVisitor();
+            context.SolutionExplorer.VisitSelectedItems( v );
+            v.Update();
             context.SolutionExplorer.UpdateSelectionStatus();
         }    
         #endregion
 
+        #region UpdateVisitor
         private class UpdateVisitor : LocalResourceVisitorBase
         {
-            public override void VisitWorkingCopyResource(NSvn.WorkingCopyResource resource)
+            public override void VisitWorkingCopyResource(NSvn.WorkingCopyResource resource )
             {
-                resource.Update();
+                this.resources.Add( resource );
+            }           
+
+            public void Update()
+            {
+                IList list = this.Filter();
+                foreach( WorkingCopyResource resource in list )
+                    resource.Update();
             }
+
+            /// <summary>
+            /// Filter out paths that are subcomponents of other paths.
+            /// </summary>
+            /// <returns></returns>
+            private IList Filter()
+            {
+                ArrayList list = new ArrayList();
+                foreach( WorkingCopyResource outer in this.resources )
+                {
+                    bool found = false;
+                    foreach( WorkingCopyResource inner in this.resources )
+                    {
+                        if ( outer.Path.IndexOf( inner.Path ) == 0 && outer.Path != inner.Path )
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if ( !found )
+                        list.Add( outer );
+                }
+
+                return list;
+            }
+
+
+            private ArrayList resources = new ArrayList();
         }
+        #endregion
     }
 }
 
