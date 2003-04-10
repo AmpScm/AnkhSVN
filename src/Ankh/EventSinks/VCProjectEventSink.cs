38b1ce2c3a9111de015f7cb6ded9686980973f01
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-04-10 14:08:56 +0000

    Added an event sink for the VC++ project model. Lacks an implementation for remove. M    /src/Ankh/Ankh.csproj - Added VCProjectEventSink.cs M    /src/Ankh/EventSinks/EventSink.cs - Added support for creating VCProjectEventSink. A    /src/Ankh/EventSinks/VCProjectEventSink.cs - Event sink for the vc++ project model

---
+++ src/Ankh/EventSinks/VCProjectEventSink.cs
@@ -0,0 +1,44 @@
+using System;
+using Microsoft.VisualStudio.VCProjectEngine;
+using EnvDTE;
+
+namespace Ankh.EventSinks
+{
+	/// <summary>
+	/// Sink for VCProjectEngineEvents events.
+	/// </summary>
+	internal class VCProjectEventSink : EventSink
+	{
+		internal VCProjectEventSink( VCProjectEngineEvents events, AnkhContext context )
+            : base( context )
+		{
+            this.events = events;
+            this.events.ItemAdded += new _dispVCProjectEngineEvents_ItemAddedEventHandler(
+                this.ItemAdded );
+            this.events.ItemRemoved += new _dispVCProjectEngineEvents_ItemRemovedEventHandler(
+                this.ItemRemoved );
+		}
+
+        public override void Unhook()
+        {
+            this.events.ItemAdded += new _dispVCProjectEngineEvents_ItemAddedEventHandler(
+                this.ItemAdded );
+            this.events.ItemRemoved += new _dispVCProjectEngineEvents_ItemRemovedEventHandler(
+                this.ItemRemoved );
+        }
+
+        protected void ItemAdded( object item, object parent )
+        {
+            this.Context.SolutionExplorer.SyncWithTreeView();
+        }
+
+        protected void ItemRemoved( object item, object parent )
+        {
+            string s = item.ToString();
+            if ( parent is Project )
+                s = item.ToString();
+        }
+
+        private VCProjectEngineEvents events;
+	}
+}
