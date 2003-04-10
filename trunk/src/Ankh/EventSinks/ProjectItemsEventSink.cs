1a0b078ecaee38106f8015a1743ead4285130368
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-04-10 02:07:04 +0000

    Implemented a generic event sink mechanism
    M    /src/Ankh/Ankh.csproj - Added new files.
    M    /src/Ankh/AnkhContext.cs - Removed all event handling, except solution events. Event sinks are created when the solution is loaded, and they are unhooked when it is unloaded
    A    /src/Ankh/EventSinks - Namespace for event sink classes
    A    /src/Ankh/EventSinks/ProjectItemsEventSink.cs - Event sink
    A    /src/Ankh/EventSinks/ProjectsEventSink.cs - Event sink
    A    /src/Ankh/EventSinks/EventSink.cs - Base class for event sinks
    A    /src/Ankh/EventSinks/DocumentEventsSink.cs - Event sink

---
+++ trunk/src/Ankh/EventSinks/ProjectItemsEventSink.cs
@@ -0,0 +1,53 @@
+using System;
+using EnvDTE;
+using Extensibility;
+
+namespace Ankh.EventSinks
+{
+	/// <summary>
+	/// Event sink for the ProjectItemsEvent
+	/// </summary>
+	internal class ProjectItemsEventSink : EventSink
+	{
+		internal ProjectItemsEventSink( ProjectItemsEvents events, 
+            AnkhContext context )
+            : base( context  )
+		{
+            this.events = events;
+            events.ItemAdded += new _dispProjectItemsEvents_ItemAddedEventHandler(
+                this.ItemAdded );
+            events.ItemRemoved += new _dispProjectItemsEvents_ItemRemovedEventHandler(
+                this.ItemRemoved );
+            events.ItemRenamed += new _dispProjectItemsEvents_ItemRenamedEventHandler(
+                this.ItemRenamed );
+        }
+
+        public override void Unhook()
+        {
+            this.events.ItemAdded -= new _dispProjectItemsEvents_ItemAddedEventHandler(
+                this.ItemAdded );
+            this.events.ItemRemoved -= new _dispProjectItemsEvents_ItemRemovedEventHandler(
+                this.ItemRemoved );
+            this.events.ItemRenamed -= new _dispProjectItemsEvents_ItemRenamedEventHandler(
+                this.ItemRenamed );
+        }
+
+
+        protected void ItemAdded( ProjectItem item )
+        {
+            this.Context.SolutionExplorer.SyncWithTreeView();
+        }
+
+        protected void ItemRemoved( ProjectItem item )
+        {
+            string s = item.Kind;
+        }
+
+        protected void ItemRenamed( ProjectItem item, string oldName )
+        {
+            string s = item.Kind;
+        }
+
+        private ProjectItemsEvents events;
+	}
+}
---
+++ trunk/src/Ankh/EventSinks/ProjectsEventSink.cs
@@ -0,0 +1,50 @@
+using System;
+using EnvDTE;
+
+namespace Ankh.EventSinks
+{
+	/// <summary>
+	/// Event sink for the ProjectEvents events.
+	/// </summary>
+	internal class ProjectsEventSink : EventSink
+	{
+		internal ProjectsEventSink( ProjectsEvents events, AnkhContext context ) :
+            base( context )
+		{
+			this.events = events;
+            events.ItemAdded += new _dispProjectsEvents_ItemAddedEventHandler(
+                this.ItemAdded );
+            events.ItemRemoved += new _dispProjectsEvents_ItemRemovedEventHandler(
+                this.ItemRemoved );
+            events.ItemRenamed += new _dispProjectsEvents_ItemRenamedEventHandler(
+                this.ItemRenamed );           
+		}
+
+        public override void Unhook()
+        {
+            this.events.ItemAdded -= new _dispProjectsEvents_ItemAddedEventHandler(
+                this.ItemAdded );
+            this.events.ItemRemoved -= new _dispProjectsEvents_ItemRemovedEventHandler(
+                this.ItemRemoved );
+            this.events.ItemRenamed -= new _dispProjectsEvents_ItemRenamedEventHandler(
+                this.ItemRenamed );
+        }
+
+        protected void ItemAdded( Project item )
+        {
+            this.Context.SolutionExplorer.SyncWithTreeView();
+        }
+
+        protected void ItemRemoved( Project item )
+        {
+            string s = item.FileName;
+        }
+
+        protected void ItemRenamed( Project item, string oldName )
+        {
+            string s = item.FileName;
+        }
+
+        private ProjectsEvents events;
+	}
+}
