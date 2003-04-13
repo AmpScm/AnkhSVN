22057b8e30f5ae6f8967eb934cf6c62f6bbde34f
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-04-13 16:10:10 +0000

    Factored out the ProjectItemNode, SolutionNode and ProjectNode private classes into their own files.

---
+++ src/Ankh/Solution/ProjectItemNode.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using NSvn;
+using NSvn.Core;
+using EnvDTE;
+
+namespace Ankh.Solution
+{
+    /// <summary>
+    /// Represents a node containing a project item.
+    /// </summary>
+    internal class ProjectItemNode : TreeNode
+    {
+        public ProjectItemNode( UIHierarchyItem item, IntPtr hItem, Explorer explorer,
+            TreeNode parent ) :
+            base( item, hItem, explorer, parent )
+        {
+            ProjectItem pitem = (ProjectItem)item.Object;
+            this.resources = new ArrayList();
+            try
+            {
+                for( short i = 1; i <= pitem.FileCount; i++ ) 
+                {
+                    
+                    ILocalResource res = SvnResource.FromLocalPath( pitem.get_FileNames(i) );
+                    // does this resource exist?
+                    if ( res != null )
+                    {
+                        res.Context = explorer.Context;
+                        this.resources.Add( res );
+                    }
+                }
+                explorer.AddResource( pitem, this );                    
+            }
+            catch( NullReferenceException )
+            {
+                //swallow
+            }                
+        }
+
+        protected override StatusKind GetStatus()
+        {
+            // go through the resources belonging to this node
+            foreach( ILocalResource resource in this.resources )
+            {
+                StatusKind status = StatusFromResource( resource );
+                if ( status != StatusKind.Normal )
+                    return status;
+            }
+                                
+            return StatusKind.Normal;            
+        }
+
+        public override void VisitResources( ILocalResourceVisitor visitor )
+        {
+            foreach( ILocalResource resource in this.resources )
+                resource.Accept( visitor );
+
+            this.VisitChildren( visitor );
+        }
+
+        private IList resources;
+    }    
+
+}
---
+++ src/Ankh/Solution/SolutionNode.cs
@@ -0,0 +1,51 @@
+using System;
+using NSvn;
+using EnvDTE;
+using NSvn.Core;
+
+namespace Ankh.Solution
+{
+    /// <summary>
+    /// A node representing a solution.
+    /// </summary>
+    internal class SolutionNode : TreeNode
+    {
+        public SolutionNode( UIHierarchyItem item, IntPtr hItem, Explorer explorer )
+            : base( item, hItem, explorer, null )
+        {
+            EnvDTE.Solution solution = explorer.DTE.Solution;
+            this.solutionFile = SvnResource.FromLocalPath( solution.FullName );
+            this.solutionFile.Context = explorer.Context;
+
+            explorer.SetSolution( this );
+        }
+
+        public override void VisitResources( ILocalResourceVisitor visitor )
+        {
+            this.solutionFile.Accept( visitor );
+            this.VisitChildren( visitor );
+        } 
+
+        protected override StatusKind GetStatus()
+        {
+            if ( this.solutionFile == null )
+                return StatusKind.None;               
+            else
+            {
+                StatusKind status = StatusFromResource( this.solutionFile );
+                if ( status != StatusKind.Normal )
+                {
+                    // check the status on the projects
+                    ModifiedVisitor v = new ModifiedVisitor();
+                    this.VisitChildren( v );
+                    if ( v.Modified )
+                        status = StatusKind.Modified;
+                }
+
+                return status;
+            }
+        }
+
+        private ILocalResource solutionFile;
+    }
+}
