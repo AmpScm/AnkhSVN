0df13b40e32e35513effb0e4eafe2886b129568a
Per <Per@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-04-18 16:30:05 +0000

    Forgot to add some files, and set properties.
    A   /src/Ankh.UI/Loader.resx
    MM  /src/Ankh.UI/Loader.cs
    M   /src/Ankh.UI/Ankh.UI.csproj

--- src/Ankh.UI/Loader.cs
+++ src/Ankh.UI/Loader.cs
@@ -6,87 +6,84 @@ using System.Windows.Forms;
 
 namespace Ankh.UI
 {
-    /// <summary>
-    /// Summary description for Loader.
-    /// </summary>
-    public class Loader : System.Windows.Forms.Form
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.Container components = null;
+	/// <summary>
+	/// Summary description for Loader.
+	/// </summary>
+	public class Loader : System.Windows.Forms.Form
+	{
+        private System.Windows.Forms.Label LoaderText;
+        private System.Windows.Forms.PictureBox animatedGif;
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
 
         /// <summary>
-        /// Displays a Loader 
+        /// Loader Form
         /// </summary>
-        /// <param name="name">Name of the Form</param>
-        /// <param name="head">Text in the Form</param>
-        public Loader(string inText)
-        {
-            //
-            // Required for Windows Form Designer support
-            //
+        /// <param name="inText">Text to be printed in the form.</param>
+		public Loader(string inText)
+		{
+			//
+			// Required for Windows Form Designer support
+			//
             this.outText = inText;
-            InitializeComponent();
-             
+			InitializeComponent();
+		}
 
-            //
-            // TODO: Add any constructor code after InitializeComponent call
-            //
-        }
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        protected override void Dispose( bool disposing )
-        {
-            if( disposing )
-            {
-                if(components != null)
-                {
-                    components.Dispose();
-                }
-            }
-            base.Dispose( disposing );
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
 
 		#region Windows Form Designer generated code
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
             System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(Loader));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.loaderText = new System.Windows.Forms.Label();
+            this.LoaderText = new System.Windows.Forms.Label();
+            this.animatedGif = new System.Windows.Forms.PictureBox();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // LoaderText
             // 
-            this.pictureBox1.Image = ((System.Drawing.Bitmap)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(194, -3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(37, 49);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.LoaderText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+            this.LoaderText.Location = new System.Drawing.Point(0, 21);
+            this.LoaderText.Name = "LoaderText";
+            this.LoaderText.Text = this.outText;
+            this.LoaderText.Size = new System.Drawing.Size(176, 24);
+            this.LoaderText.TabIndex = 0;
             // 
-            // loaderText
+            // animatedGif
             // 
-            this.loaderText.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-            this.loaderText.Location = new System.Drawing.Point(6, 8);
-            this.loaderText.Name = "loaderText";
-            this.loaderText.Text = this.outText;
-            this.loaderText.Size = new System.Drawing.Size(191, 36);
+            this.animatedGif.Image = ((System.Drawing.Bitmap)(resources.GetObject("animatedGif.Image")));
+            this.animatedGif.Location = new System.Drawing.Point(184, 8);
+            this.animatedGif.Name = "animatedGif";
+            this.animatedGif.Size = new System.Drawing.Size(40, 40);
+            this.animatedGif.TabIndex = 1;
+            this.animatedGif.TabStop = false;
+            this.animatedGif.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Loader
             // 
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(235, 52);
+            this.ClientSize = new System.Drawing.Size(226, 56);
             this.Controls.AddRange(new System.Windows.Forms.Control[] {
-                                                                          this.loaderText,
-                                                                          this.pictureBox1});
-            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+                                                                          this.animatedGif,
+                                                                          this.LoaderText});
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Loader";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -96,19 +93,17 @@ namespace Ankh.UI
         }
 		#endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label loaderText;
-        private string outText;
-
         public static void Main()
         {
-            Loader load = new Loader("test");
-            load.ShowDialog();
-
+            Loader loader = new Loader(" Ankh er kult! ");
+            loader.ShowDialog();
         }
 
-        
-    }
-   
+        private string outText = null;
 
+        private void pictureBox1_Click(object sender, System.EventArgs e)
+        {
+        
+        }
+	}
 }
