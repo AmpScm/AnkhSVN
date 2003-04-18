6d6a6b2268aaa2b729eedc9a1234451785b5aafd
Per <Per@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-04-18 17:44:52 +0000

    Added icons.
    M   /src/Ankh/1033/BitmapDll.dll - Icons.
    M   /src/Ankh/Commands/AddItemCommand.cs - Added Bitmap
    M   /src/Ankh/Commands/CommitItemCommand.cs - ditto
    M   /src/Ankh/Commands/DiffLocalItemCommand.cs - ditto
    M   /src/Ankh/Commands/RefreshCommand.cs - ditto
    M   /src/Ankh/Commands/RevertItemCommand.cs - ditto
    M   /src/Ankh/ResourceBitmaps.cs - added icons

--- trunk/src/Ankh/ResourceBitmaps.cs
+++ trunk/src/Ankh/ResourceBitmaps.cs
@@ -12,6 +12,12 @@ namespace Ankh
 			// empty
 		}
 
-        public const int Default = 101;
-	}
+        public const int Default    = 101;
+        public const int Refresh    = 102;
+        public const int Add        = 103;
+        public const int Cleanup    = 104;
+        public const int Commit     = 105;
+        public const int Revert     = 106;
+        public const int Diff     = 107;
+    }
 }
