24f0f7537553b14bbc722a54be909695157e6ed6
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-05-07 20:04:33 +0000

    Macro update of AssemblyVersion.
    M    Ankh/AssemblyInfo.cs - Version update.
    M    NSvn/AssemblyInfo.cs - ditto
    M    NSvn.Tests/AssemblyInfo.cs - ditto

--- branches/alpha1-release/src/Ankh/AssemblyInfo.cs
+++ branches/alpha1-release/src/Ankh/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.1.0.429")]
+[assembly: AssemblyVersion("1.1.0.492")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
