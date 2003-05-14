9e295edaff458dfcccd3749cbada72b6704d0416
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-05-14 11:38:52 +0000

    (OnConnection) Changed the way we detect commandline builds

--- branches/alpha1-release/src/Ankh/Connect.cs
+++ branches/alpha1-release/src/Ankh/Connect.cs
@@ -7,6 +7,7 @@ namespace Ankh
 	using System.Runtime.InteropServices;
 	using EnvDTE;
     using System.Diagnostics;
+    using System.Text.RegularExpressions;
 
     using Ankh.Commands;
 
@@ -51,15 +52,9 @@ namespace Ankh
 		/// <seealso class='IDTExtensibility2' />
 		public void OnConnection(object application, Extensibility.ext_ConnectMode connectMode, object addInInst, ref System.Array custom)
 		{
-            try
-            {
-                // this will throw if we are invoked from the command line
-                object m = ((_DTE)application).MainWindow;
-            }
-            catch( Exception )
-            {
+            // we don't want to load on command line builds.
+            if ( Regex.IsMatch( Environment.CommandLine, "/build" ) )
                 return;
-            }
 
             try
             {
