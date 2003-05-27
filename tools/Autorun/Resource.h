f5c5e3eac80927413f45ae8d80542ff88154ec4c
Per <Per@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-05-27 22:10:12 +0000

    Added layout to the htm autorun page.
    M    Autorun/Autorun.aps - blae.
    M    Autorun/Autorun.htm - added buttons and bg img
    AM   Autorun/button.png - button
    M    Autorun/Autorun.vcproj - files
    M    Autorun/Autorun.rc - added images as a resource
    M    Autorun/AutorunDlg.cpp - removed cancel button
    AM   Autorun/bg.png - bg image
    M    Autorun/Resource.h -blae.

--- tools/Autorun/Resource.h
+++ tools/Autorun/Resource.h
@@ -1,20 +1,21 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by Autorun.RC
+// Used by Autorun.rc
 //
-#define IDR_MAINFRAME					128
-#define IDD_AUTORUN_DIALOG				102
-#define IDR_HTML_AUTORUN_DIALOG			104
-#define IDR_MANIFEST					CREATEPROCESS_MANIFEST_RESOURCE_ID
+#define IDR_MANIFEST                    1
+#define IDD_AUTORUN_DIALOG              102
+#define IDR_HTML_AUTORUN_DIALOG         104
+#define IDR_MAINFRAME                   128
+#define IDR_PNG1                        129
+#define IDR_PNG2                        130
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
