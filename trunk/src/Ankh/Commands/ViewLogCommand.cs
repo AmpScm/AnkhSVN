c613a0a52ee5bb50de40b4061aeb5367d9441436
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-06-11 20:26:58 +0000

    Moved the command to display the repository explorer window into a class of its own, from the ViewLogCommand(1) where it used to be.
    * \src\Ankh\Commands\ViewLogCommand.cs
    	Removed all the attributes. Commented out the class body.
    
    * \src\Ankh\Commands\ShowRepositoryExplorerCommand.cs
    	Moved the attributes from ViewLogCommand to here, as well as the class body. Changed the text from Repos Explorer to Repository Explorer.

--- trunk/src/Ankh/Commands/ViewLogCommand.cs
+++ trunk/src/Ankh/Commands/ViewLogCommand.cs
@@ -8,21 +8,19 @@ namespace Ankh.Commands
     /// <summary>
     /// Summary description for ViewLog.
     /// </summary>
-    [VSNetCommand("ViewLog", Text = "Repos Explorer", Tooltip = "Show the repository explorer",
-         Bitmap = ResourceBitmaps.ReposExplorer),
-    VSNetControl( "MenuBar.View", Position = 1 )]
-    internal class ViewLog : CommandBase
+    
+    internal class ViewLog
     {
-        public override EnvDTE.vsCommandStatus QueryStatus(Ankh.AnkhContext context)
-        {
-            return vsCommandStatus.vsCommandStatusEnabled |
-                vsCommandStatus.vsCommandStatusSupported;
-        }
-		
-        public override void Execute(Ankh.AnkhContext context)
-        {
-            context.RepositoryExplorerWindow.Visible = true;
-        }        
+//        public override EnvDTE.vsCommandStatus QueryStatus(Ankh.AnkhContext context)
+//        {
+//            return vsCommandStatus.vsCommandStatusEnabled |
+//                vsCommandStatus.vsCommandStatusSupported;
+//        }
+//		
+//        public override void Execute(Ankh.AnkhContext context)
+//        {
+//            context.RepositoryExplorerWindow.Visible = true;
+//        }        
     }
 }
 
