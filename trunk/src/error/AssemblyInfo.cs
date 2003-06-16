f38c649a36ae1aa1d13dfc17f582c59d5b60fedb
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-06-16 14:10:23 +0000

    Merged in changes to the error reporting form since rev 558

--- trunk/src/error/AssemblyInfo.cs
+++ trunk/src/error/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: 0.2.42.42]
+[assembly:AssemblyVersion("0.2.42.42")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
