f06f88f1a13fa9712ebb151e7b1a3a49681e4591
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-06-21 23:21:09 +0000

    Implemented svn cat functionality in the repository explorer, allowing the user to view the contents of a repository file in VS.NET.
    * \doc\faq.xml
    	Added a point about the dev list and the IRC channel.
    
    * \src\Ankh\resourcevisitors.cs
    	(IsRepositoryFileVisitor) - New class. Indicates whether only files are selected in the repos explorer.
    	
    * \src\Ankh\RepositoryExplorer\Controller.cs
    	(TreeView) - New property. Allows the setting of the treeview used.
    	(VisitSelectedNodes) - New method. Lets you pass in a visitor to visit the resources associated with the selected node in the repos explorer.
    	
    * \src\Ankh\Commands\MergeItemCommand.cs
    	Commented out - this was a dummy command so that we would have something on the repos explorer context menu.
    
    * \src\Ankh\Commands\ViewRepositoryFileCommand.cs
    	Implemented functionality for viewing a repository file by calling Cat on it.
    
    * \src\Ankh\Ankh.2003.csproj
    	Set the ALWAYSREGISTER preprocessor symbol.
    	
    * \src\NSvn\NSvn.2003.csproj
    	Added RepositoryResourceVisitorBase.cs
    	
    * \src\NSvn\RepositoryResourceVisitorBase.cs
    	New adapter class, containing empty implementations of the methods in IRepositoryResourceVisitor.
    
    * \src\Ankh.UI\Ankh.UI.2003.csproj
    	Added IRepositoryTreeView.cs
    	
    * \src\Ankh.UI\RepositoryTreeView.cs
    	(IRepositoryController.IRepositoryTreeView) - New property.
    	(RepositoryTreeView) - Now implements IRepositoryTreeView.
    	(SelectionCount) - New property.
    	(SelectedNodes) - New property.
    	Also added some doc comments.
    	
    * \src\Ankh.UI\RepositoryExplorerControl.cs
    	(set_Controller) - set the treeview of the controller at the same time the controller is being set.	
    	
    * \src\Ankh.UI\IRepositoryTreeView.cs
    	Defines an interface for repository treeviews.
    
    * \src\src.2003.sln
    	Added BitmapDLL.dll to the solution items.

--- trunk/src/Ankh/Commands/ViewRepositoryFileCommand.cs
+++ trunk/src/Ankh/Commands/ViewRepositoryFileCommand.cs
@@ -1,18 +1,52 @@
 // $Id$
-using System;
+using EnvDTE;
+using NSvn;
+using System.IO;
 
 namespace Ankh.Commands
 {
     /// <summary>
-    /// Summary description for ViewRepositoryFile.
+    /// A command that lets you view a repository file.
     /// </summary>
-    public class ViewRepositoryFile
+    [VSNetCommand("ViewRepositoryFile", Tooltip="View this file", Text = "In VS.NET" ),
+     VSNetControl( "ReposExplorer.View", Position = 1 ) ]
+    internal class ViewRepositoryFile : CommandBase
     {
-        public ViewRepositoryFile()
+       
+    
+        #region ICommand Members
+        public override EnvDTE.vsCommandStatus QueryStatus(AnkhContext context)
         {
-            //
-            // TODO: Add constructor logic here
-            //
+            IsRepositoryFileVisitor v = new IsRepositoryFileVisitor();
+            context.RepositoryController.VisitSelectedNodes( v );
+            return v.IsFile ? vsCommandStatus.vsCommandStatusSupported | vsCommandStatus.vsCommandStatusEnabled :
+                vsCommandStatus.vsCommandStatusSupported;
+        }
+
+        public override void Execute(AnkhContext context)
+        {
+            CatVisitor v = new CatVisitor( context.DTE );
+            context.RepositoryController.VisitSelectedNodes( v );
+        }
+        #endregion
+
+        private class CatVisitor : RepositoryResourceVisitorBase
+        {
+            public CatVisitor( _DTE dte )
+            {
+                this.dte = dte;
+            }
+
+            public override void VisitFile(RepositoryFile file)
+            {
+                string filename = Path.Combine( Path.GetTempPath(), file.Name );
+                using( FileStream fs = new FileStream( filename, FileMode.Create, FileAccess.Write ) )
+                    file.Cat( fs );                
+
+                this.dte.ItemOperations.OpenFile( filename, Constants.vsViewKindPrimary );
+            }
+
+            private _DTE dte;
         }
     }
 }
