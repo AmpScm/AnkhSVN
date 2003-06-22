de240b67b07e37524a6b278de5c4a616a79eee22
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-06-22 23:25:33 +0000

    More svn cat stuff. Added two commands to let the user either save the file to disk or have it opened by the default application in Windows.
    * \src\Ankh\Commands\ViewInWindowsCommand.cs
    	New class. ShellExecutes the file after it has been downloaded.
    
    * \src\Ankh\Commands\ViewInVSNetCommand.cs
    	New class. Contains functionality factored out of ViewRepositoryFileCommand
    
    * \src\Ankh\Commands\SaveToFileCommand.cs
    	New class. Lets the user save a catted file to disk.
    
    * \src\Ankh\Commands\ViewRepositoryFileCommand.cs
    	Made abstract.
    	(Execute) - Removed. Must now be overriden by subclasses.
    	(CatVisitor) - Made protected instead of private.
    	(CatVisitor.ctor) Removed
    	(CatVisitor.FileNames) - New. Returns the filenames of the catted files.
    	(GetPath) - New method. Constructs the path to the catted file based on its filename. Can be overriden by subclasses who want more control over where the file is saved.
    
    * \src\Ankh\Ankh.2003.csproj
    	Added ViewInWindowsCommand.cs, ViewInVSNetCommand.cs and SaveToFileCommand.cs

---
+++ src/Ankh/Commands/SaveToFileCommand.cs
@@ -0,0 +1,36 @@
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Ankh.Commands
+{
+    /// <summary>
+    /// Lets the user cat a file from a repos and have Windows open it.
+    /// </summary>
+    [VSNetCommand("SaveToFile", Tooltip="Save the file to disk.", Text = "Save to file" ),
+    VSNetControl( "ReposExplorer.View", Position = 1 ) ]
+    internal class SaveToFileCommand : ViewRepositoryFileCommand
+    {
+        public override void Execute(AnkhContext context)
+        {
+            SaveCatVisitor v = new SaveCatVisitor( );     
+            context.RepositoryController.VisitSelectedNodes( v );
+        }        
+
+        protected class SaveCatVisitor : CatVisitor
+        {
+            protected override string GetPath(string filename)
+            {
+                using( SaveFileDialog sfd = new SaveFileDialog() )
+                {
+                    sfd.FileName = filename;
+                    if ( sfd.ShowDialog() == DialogResult.OK )
+                        return sfd.FileName;
+                    else
+                        return null;
+                }
+            }
+        }
+    }
+}
