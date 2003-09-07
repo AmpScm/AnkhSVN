b0b65d149e4d7f81f0836f3854776a56b6b12ed0
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-09-07 19:47:43 +0000

    Add a progress dialog to be shown when lengthy operations are performed.
    * \src\Ankh\AnkhContext.cs
    	(StartOperation) - Set caption and show the progress dialog
    	(EndOperation) -- Hide progress dialog
    
    * \src\Ankh.UI\ProgressDialog.cs
    	New class - displays a progress dialog.
    	
    * \src\Ankh.UI\Ankh.UI.2003.csproj
    	Added ProgressDialog.cs

---
+++ trunk/src/Ankh.UI/ProgressDialog.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Ankh.UI
+{
+    /// <summary>
+    /// Summary description for Loader.
+    /// </summary>
+    public class ProgressDialog : System.Windows.Forms.Form
+    {
+        
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
+
+        /// <summary>
+        /// Loader Form
+        /// </summary>
+        /// <param name="inText">Text to be printed in the form.</param>
+        public ProgressDialog()
+        {
+            //
+            // Required for Windows Form Designer support
+            //
+            InitializeComponent();
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return textLabel.Text;
+            }
+            set
+            {
+                textLabel.Text = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing )
+            {
+                if(components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose( disposing );
+        }
+
+		#region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(ProgressDialog));
+            this.textLabel = new System.Windows.Forms.Label();
+            this.animatedGif = new System.Windows.Forms.PictureBox();
+            this.SuspendLayout();
+            // 
+            // textLabel
+            // 
+            this.textLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+            this.textLabel.Location = new System.Drawing.Point(0, 64);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(368, 56);
+            this.textLabel.TabIndex = 0;
+            this.textLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // animatedGif
+            // 
+            this.animatedGif.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+                | System.Windows.Forms.AnchorStyles.Right)));
+            this.animatedGif.Image = ((System.Drawing.Image)(resources.GetObject("animatedGif.Image")));
+            this.animatedGif.Location = new System.Drawing.Point(152, 8);
+            this.animatedGif.Name = "animatedGif";
+            this.animatedGif.Size = new System.Drawing.Size(64, 40);
+            this.animatedGif.TabIndex = 1;
+            this.animatedGif.TabStop = false;
+            // 
+            // ProgressDialog
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(368, 120);
+            this.ControlBox = false;
+            this.Controls.Add(this.animatedGif);
+            this.Controls.Add(this.textLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ProgressDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ankh";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+		#endregion
+
+
+        private System.Windows.Forms.PictureBox animatedGif;
+        private System.Windows.Forms.Label textLabel;
+    }
+}
