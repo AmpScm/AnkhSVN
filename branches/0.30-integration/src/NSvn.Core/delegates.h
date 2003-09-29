5bbba294fa773e8f97d985f3766078487783047f
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-09-29 21:30:09 +0000

    Made NSvn build against Subversion 0.30. Updated to use new streamy svn_client_status. Updated tests.
    * \src\NSvn.Core\Client.cpp
    	(StatusBaton) - Used to pass a pool and the callback delegate to the status callback.
    	(svn_log_message_receiver) - Declaration moved here from Client.h, to reduce scope
    	(svn_status_func) - Ditto
    	(Status) - Changed signature and implementation to match the new svn_client_status signature. 
    	(svn_status_func) - New function. Marshalls the status callback into managed space.
    
    * \src\NSvn.Core\Client.h
    	(Client::Status) - Updated signature
    	(svn_log_message_receiver) - Moved from here into Client.cpp
    
    * \src\NSvn.Core\delegates.h
    	(StatusCallback) - New delegate to use for status callbacks.
    	Added forward decl of the Status class.
    	
    * \src\NSvn.Core\StringHelper.h
    	Removed param from call to svn_utf_cstring_to_utf8.
    
    * \src\NSvn.Core\svnenums.h
    	Removed the Status->svn_wc_notify_status enumerated value.
    
    * \src\NSvn.Core.Tests\StatusTest.cs
    	(TestLocalStatus) - Updated to use new callback API.
    	(TestEntry) - Ditto
    	(TestRepositoryStatus) - Put in a try/finally to ensure additional test wc gets deleted.
    	(StatusFunc) - Callback for calls to Client::Status

--- branches/0.30-integration/src/NSvn.Core/delegates.h
+++ branches/0.30-integration/src/NSvn.Core/delegates.h
@@ -2,6 +2,7 @@
 #pragma once
 #using <mscorlib.dll>
 
+
 namespace NSvn
 {
     namespace Core
@@ -9,10 +10,13 @@ namespace NSvn
         public __gc class Notification;
         public __gc class CommitItem;
         public __gc class LogMessage;
+        public __gc class Status;
 
         public __delegate void NotifyCallback( Notification* notification );
         public __delegate System::String* LogMessageCallback( CommitItem* items[] );
         public __delegate void LogMessageReceiver( LogMessage* logMessage );
         public __delegate void PromptCallback();
+
+        public __delegate void StatusCallback( System::String* path, Status* status );
     }
 }
