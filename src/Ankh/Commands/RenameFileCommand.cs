01ba6921b3880364f8014f8628724b1591ae4a54
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-10-13 17:39:02 +0000

    Implemented rename functionality.
    * \src\Ankh\Commands\RenameFileCommand.cs
    	New class - performs the rename.
    
    * \src\Ankh\EventSinks\ItemEventSink.cs
    	(RemoveVisitor.VisitWorkingCopyResource) - Added comment
    
    * \src\Ankh\Solution\ProjectItemNode.cs
    	(ProjectItem) - New property
    
    * \src\Ankh\resourcevisitors.cs
    	(RenamableVisitor) - New visitor - used to determine whether a file can be renamed.
    
    * \src\Ankh\Ankh.2003.csproj
    	Added RenameFileCommand.cs	
    
    * \src\Ankh.UI\RenameDialog.resx
    	Resx file - fun stuff.
    
    * \src\Ankh.UI\RenameDialog.cs
    	Dialog for choosing a new name.
    
    * \src\Ankh.UI\Ankh.UI.2003.csproj
    	Added RenameDialog.[cs|resx]

---
+++ src/Ankh/Commands/RenameFileCommand.cs
@@ -0,0 +1,80 @@
+using System;
+using EnvDTE;
+using Ankh.UI;
+using NSvn;
+using System.IO;
+using System.Windows.Forms;
+using Ankh.Solution;
+
+namespace Ankh.Commands
+{
+    /// <summary>
+    /// Summary description for RenameCommand.
+    /// </summary>
+    [VSNetCommand("RenameFile", Text = "Rename file...", Tooltip = "Rename this file...", 
+         Bitmap = ResourceBitmaps.Refresh),
+    VSNetControl( "Item.Ankh", Position = 1 )]
+    internal class RenameFileCommand : CommandBase
+    {
+        public override EnvDTE.vsCommandStatus QueryStatus(AnkhContext context)
+        {
+            RenamableVisitor v = new RenamableVisitor();
+            context.SolutionExplorer.VisitSelectedItems( v, false );
+            return v.Renamable ? 
+                (   vsCommandStatus.vsCommandStatusEnabled | 
+                    vsCommandStatus.vsCommandStatusSupported ) 
+                : vsCommandStatus.vsCommandStatusUnsupported;
+        }
+
+        public override void Execute(AnkhContext context)
+        {
+            RenameVisitor v = new RenameVisitor( context );
+            context.SolutionExplorer.VisitSelectedNodes( v );
+        }
+
+        private class RenameVisitor : INodeVisitor
+        {
+            public RenameVisitor( AnkhContext context )
+            {
+                this.context = context;
+            }
+
+            public void VisitProjectItem( ProjectItemNode node )
+            {
+                // we can only rename a single item
+                if ( node.Resources.Count > 1 || node.Resources.Count == 0 )
+                    return;
+
+                WorkingCopyFile file = (WorkingCopyFile)node.Resources[0];
+
+                string filename = Path.GetFileName( file.Path );
+                string dirname = Path.GetDirectoryName( file.Path );
+                using( RenameDialog dialog = new RenameDialog( filename ) )
+                {
+                    if ( dialog.ShowDialog() == DialogResult.OK )
+                    {
+                        string newPath = Path.Combine( dirname, dialog.NewName );
+                        file.Move( newPath, true );
+
+                        // remove the old file and add the new to the project
+                        Project project = node.ProjectItem.ContainingProject;
+                        node.ProjectItem.Remove();
+                        project.ProjectItems.AddFromFile( newPath );
+                    }
+                }
+            }
+
+            public void VisitProject( ProjectNode node )
+            {
+                // empty
+            }
+
+            public void VisitSolutionNode( SolutionNode node )
+            {
+                // empty
+            }
+
+            private AnkhContext context;
+        }
+    }
+}
