d64e3d17d435d33b219045f098cbbf5eb1e1135d
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-10-14 18:32:24 +0000

    Added an uninstall action to the installer that removes the VS.NET commandbars left behind by Ankh.
    *    Ankh/AnkhSetup/AnkhSetup.vdproj
        Added the uninstall action
        
    *    ReposInstaller/ReposInstaller.csproj
        ?
    *    ReposInstaller/ReposInstaller.2003.csproj
        ?
        
    *    ReposInstaller/ReposInstaller.cs
        (Uninstall) - New method

--- trunk/src/ReposInstaller/ReposInstaller.cs
+++ trunk/src/ReposInstaller/ReposInstaller.cs
@@ -8,86 +8,159 @@ using NSvn;
 using System.IO;
 using System.Windows.Forms;
 using System.Web;
+using EnvDTE;
 
 namespace ReposInstaller
 {
-    /// <summary>
-    /// Installs a repos and extracts a working copy from it.
-    /// </summary>
-    [RunInstaller(true)]
-    public class ReposInstaller : Installer
-    {
-        public ReposInstaller()
-        {			
-        }
+	/// <summary>
+	/// Installs a repos and extracts a working copy from it.
+	/// </summary>
+	[RunInstaller(true)]
+	public class ReposInstaller : Installer
+	{
+		public ReposInstaller()
+		{			
+		}
 
-        public override void Install( IDictionary state )
-        {
+		public override void Install( IDictionary state )
+		{
 
-            if ( MessageBox.Show( "Do you want to extract a test repository?", "Test repository",
-                MessageBoxButtons.YesNo ) == DialogResult.No )
-                return;
+			if ( MessageBox.Show( "Do you want to extract a test repository?", "Test repository",
+				MessageBoxButtons.YesNo ) == DialogResult.No )
+				return;
 
-            FolderBrowser folderBrowser = new FolderBrowser();
-            folderBrowser.StartLocation = FolderBrowser.FolderID.MyComputer;
-            folderBrowser.OnlyFilesystem = true;
+			FolderBrowser folderBrowser = new FolderBrowser();
+			folderBrowser.StartLocation = FolderBrowser.FolderID.MyComputer;
+			folderBrowser.OnlyFilesystem = true;
 
-            if ( folderBrowser.ShowDialog() == DialogResult.Cancel )
-                return;
+			if ( folderBrowser.ShowDialog() == DialogResult.Cancel )
+				return;
 
-            string reposPath = Path.Combine( folderBrowser.DirectoryPath, "repos" );
+			string reposPath = Path.Combine( folderBrowser.DirectoryPath, "repos" );
             
-            if ( Directory.Exists( reposPath  ) )
-                this.RecursiveDelete( reposPath );
+			if ( Directory.Exists( reposPath  ) )
+				this.RecursiveDelete( reposPath );
 
-            Zip.ExtractZipResource( reposPath, this.GetType(), "ReposInstaller.repos.zip" );
-            state["repospath"] = reposPath;
+			Zip.ExtractZipResource( reposPath, this.GetType(), "ReposInstaller.repos.zip" );
+			state["repospath"] = reposPath;
 
-            if ( MessageBox.Show( "Do you want to check out a test working copy from the " + 
-                "repository?", "Working copy", MessageBoxButtons.YesNo ) == DialogResult.No )
-                return;
+			if ( MessageBox.Show( "Do you want to check out a test working copy from the " + 
+				"repository?", "Working copy", MessageBoxButtons.YesNo ) == DialogResult.No )
+				return;
 
-            if ( folderBrowser.ShowDialog() == DialogResult.Cancel )
-                return;
+			if ( folderBrowser.ShowDialog() == DialogResult.Cancel )
+				return;
 
-            string wcPath = Path.Combine( folderBrowser.DirectoryPath, "wc" );
-            if ( Directory.Exists( wcPath ) )
-                this.RecursiveDelete( wcPath );
+			string wcPath = Path.Combine( folderBrowser.DirectoryPath, "wc" );
+			if ( Directory.Exists( wcPath ) )
+				this.RecursiveDelete( wcPath );
 
-            string reposUrl = Path.Combine( reposPath, "trunk" );
-            reposUrl = "file:///" + reposUrl.Replace( "\\", "/" ).Replace(" ", "%20" );
+			string reposUrl = Path.Combine( reposPath, "trunk" );
+			reposUrl = "file:///" + reposUrl.Replace( "\\", "/" ).Replace(" ", "%20" );
 
-            RepositoryDirectory dir = new RepositoryDirectory( reposUrl );           
-            dir.Checkout( wcPath, true );
+			RepositoryDirectory dir = new RepositoryDirectory( reposUrl );           
+			dir.Checkout( wcPath, true );
 
-            state[ "wcpath" ] = wcPath;
-        }
+			state[ "wcpath" ] = wcPath;
+		}
 
-        public override void Rollback(System.Collections.IDictionary savedState)
-        {
-            base.Rollback( savedState );
+		public override void Rollback(System.Collections.IDictionary savedState)
+		{
+			base.Rollback( savedState );
 
-            string reposPath = (string)savedState[ "repospath" ];
-            string wcPath = (string)savedState[ "wcpath" ];
+			string reposPath = (string)savedState[ "repospath" ];
+			string wcPath = (string)savedState[ "wcpath" ];
 
-            if ( reposPath != null && Directory.Exists( reposPath ) )
-                this.RecursiveDelete( reposPath );
-            if ( wcPath != null && Directory.Exists( wcPath ) )
-                this.RecursiveDelete( wcPath );        
-        }   
+			if ( reposPath != null && Directory.Exists( reposPath ) )
+				this.RecursiveDelete( reposPath );
+			if ( wcPath != null && Directory.Exists( wcPath ) )
+				this.RecursiveDelete( wcPath );        
+		}   
+
+		public override void Uninstall(IDictionary savedState)
+		{
+			
+			//base.Uninstall( savedState );
+
+			// make sure VS.NET is closed down
+			while( this.VSIsRunningRunning() )
+			{
+				MessageBox.Show( "One or more instances of VS.NET are running. " + 
+					"Please close these before continuing", "VS.NET is running", MessageBoxButtons.OK,
+					MessageBoxIcon.Warning );
+			} 
+          
+			MessageBox.Show( "Hello 2" );
+
+			// delete the commands
+			this.DeleteAnkhCommands( "VisualStudio.DTE.7" );
+			this.DeleteAnkhCommands( "VisualStudio.DTE.7.1" );
+            
+		}
+
+		/// <summary>
+		/// Deletes the Ankh commands associated with a specific VS.NET version
+		/// </summary>
+		/// <param name="progid"></param>
+		private void DeleteAnkhCommands( string progid )
+		{
+			Type t = Type.GetTypeFromProgID( progid );
+			if ( t == null )
+				return;
+
+			_DTE dte = (_DTE)Activator.CreateInstance( t );
+
+			// find our commands and delete them           
+			foreach( Command cmd in dte.Commands )
+			{
+				try
+				{
+					if ( cmd.Name.StartsWith( PROGID ) )
+						cmd.Delete();
+				}
+				catch( Exception ex )
+				{
+					// HACK: swallow
+				}
+			}
+		}
+
+		private bool VSIsRunningRunning()
+		{ 
+			System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
+			foreach( System.Diagnostics.Process process in processes )
+			{
+				try
+				{
+					if ( process != null && process.MainModule != null )
+					{
+						if ( process.MainModule.FileName.EndsWith( "devenv.exe" ) )
+							return true;
+					}
+				}
+				catch( Exception )
+				{
+					// HACK: swallow
+				}
+			}
+			return false;
+		}
+
         
-        private void RecursiveDelete( string path )
-        {
-            foreach( string dir in Directory.GetDirectories( path ) )
-            {
-                this.RecursiveDelete( dir );
-            }
+		private void RecursiveDelete( string path )
+		{
+			foreach( string dir in Directory.GetDirectories( path ) )
+			{
+				this.RecursiveDelete( dir );
+			}
 
-            foreach( string file in Directory.GetFiles( path ) )
-                File.SetAttributes( file, FileAttributes.Normal );
+			foreach( string file in Directory.GetFiles( path ) )
+				File.SetAttributes( file, FileAttributes.Normal );
 
-            File.SetAttributes( path, FileAttributes.Normal );
-            Directory.Delete( path, true );
-        }
-    }
+			File.SetAttributes( path, FileAttributes.Normal );
+			Directory.Delete( path, true );
+		}
+
+		private string PROGID="Ankh";
+	}
 }
