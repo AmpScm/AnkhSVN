5402f0ad40a62adff1c69400a22b53e33b33afac
vorlon <vorlon@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-10-17 17:46:30 +0000

    Reverted to FolderBrowser from Utils Changed command name and text
     \Ankh\Commands\CheckoutFolderCommand.cs

--- src/Ankh/Commands/CheckoutFolderCommand.cs
+++ src/Ankh/Commands/CheckoutFolderCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Windows.Forms;
+using Utils;
 using EnvDTE;
 using NSvn;
 
@@ -9,23 +10,22 @@ namespace Ankh.Commands
 	/// <summary>
 	/// Summary description for ViewInVSNetCommand.
 	/// </summary>
-    [VSNetCommand("Checkout", Tooltip="Checkout this folder", Text = "Checkout" ),
-    VSNetControl( "ReposExplorer", Position = 2 ) ]
-	internal class CheckoutFolderCommand : CheckoutCommand
+    [VSNetCommand("CheckoutFolder", Tooltip="Checkout this folder", Text = "Checkout Folder ..." ),
+    VSNetControl( "ReposExplorer", Position = 1 ) ]
+	internal class CheckoutFolderCommand : 
+        CheckoutCommand
 	{
         public override void Execute(AnkhContext context)
         {
             /// first get the parent folder
-            FolderBrowserDialog browser = new FolderBrowserDialog();
-            browser.Description = "Select the location where to checkout";
-            browser.ShowNewFolderButton = true;
+            FolderBrowser browser = new FolderBrowser();
 
             /// give a chance to the user to bail
             if ( browser.ShowDialog() != DialogResult.OK) 
                 return;
 
             /// save parent folder path
-            this.parentPath = browser.SelectedPath;
+            this.parentPath = browser.DirectoryPath;
 
             CheckoutVisitor v = new CheckoutVisitor( );
             context.RepositoryController.VisitSelectedNodes( v );
