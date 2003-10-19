c7c40e66841d92b89d25fc9b4055dbdf224cb9a5
Arild Fines <63673+ArildF@users.noreply.github.com>
2003-10-19 21:41:33 +0000

    Created 0.3 release tag

--- src/NSvn/AssemblyInfo.cs
+++ src/NSvn/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersionAttribute( "0.3.0.925")]
+[assembly: AssemblyVersionAttribute( "0.2.1.833")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
