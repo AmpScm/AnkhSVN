3eedf610f84c5745645b22d6c8432fd3cfb1de95
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-10-20 21:12:56 +0000

    Create 0.3.2 release branch

---
+++ branches/0.3.2-release/src/Ankh/Commands/MakePatchCommand.cs
@@ -0,0 +1,21 @@
+// $Id$
+using System;
+
+namespace Ankh.Commands
+{
+    /// <summary>
+    /// Summary description for MakePatch.
+    /// </summary>
+    internal class MakePatchCommand
+    {
+        internal MakePatchCommand()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
+
+
+
