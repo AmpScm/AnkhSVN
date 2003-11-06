6b05d1ee1330be3070a7d67bbb069dff4f527d35
vorlon <vorlon@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-11-06 19:25:36 +0000

    Created Ankh-0.3.3 branch
    A    http://arild.no-ip.com:8088/svn/finalproject/tags/Ankh-0.3.3

---
+++ tags/Ankh-0.3.3/src/Ankh/Commands/SwitchItemCommand.cs
@@ -0,0 +1,21 @@
+// $Id$
+using System;
+
+namespace Ankh.Commands
+{
+    /// <summary>
+    /// Summary description for SwitchItem.
+    /// </summary>
+    internal class SwitchItemCommand
+    {
+        public SwitchItemCommand()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
+
+
+
