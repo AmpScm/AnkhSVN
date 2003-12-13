05475f096bb47dffaa1a008d2ff3a6e39a6e2d66
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2003-12-13 18:39:38 +0000

    Updated NSvn.Tests to account for the API changes.
    * \src\NSvn.Tests\AuthenticationTest.cs
    	(TestSimpleProvider) - Use an AuthenticationProviderObject.
    	(TestCustomProvider) - Removed, since we no longer have that option.
    	(AuthProvider) - Removed - see above.
    	(SuccessPrompt) - New callback.
    
    * \src\NSvn.Tests\RepositoryDirectoryTest.cs
    * \src\NSvn.Tests\RepositoryFileTest.cs
    * \src\NSvn.Tests\RepositoryResourceTest.cs
    * \src\NSvn.Tests\WorkingCopyResourceTest.cs
    	Add authentication providers where applicable.

--- branches/newauth2/src/NSvn.Tests/WorkingCopyResourceTest.cs
+++ branches/newauth2/src/NSvn.Tests/WorkingCopyResourceTest.cs
@@ -29,6 +29,7 @@ namespace NSvn.Tests
         public void TestCommit()
         {
             WorkingCopyResource item  = new WorkingCopyFile( Path.Combine( this.WcPath, "Form.cs" ) );
+            item.Context.AddAuthenticationProvider( Authentication.GetUsernameProvider() );
             
             Assertion.AssertEquals( "Wrong status. Should be normal", StatusKind.Normal,
                 item.Status.TextStatus  );
@@ -64,6 +65,10 @@ namespace NSvn.Tests
             using( StreamWriter w2 = new StreamWriter( item2.Path, true ) )
                 w2.Write( "Moo" );
 
+            AuthenticationProviderObject provider = Authentication.GetUsernameProvider();
+            item1.Context.AddAuthenticationProvider( provider );
+            item2.Context.AddAuthenticationProvider( provider );
+
             WorkingCopyResource.Commit( new WorkingCopyResource[]{ item1, item2 }, false );
 
             Assertion.AssertEquals( "Wrong status", StatusKind.Normal, 
@@ -84,6 +89,7 @@ namespace NSvn.Tests
             Assertion.AssertEquals( "Wrong status. Cant copy an unversioned file", StatusKind.Normal,
                 itemWcSrc.Status.TextStatus  );
           
+            itemWcSrc.Context.AddAuthenticationProvider( Authentication.GetUsernameProvider() );
             WorkingCopyResource itemWcDst = itemWcSrc.CopyTo( new WorkingCopyFile( Path.Combine( this.WcPath, @"obj" )), Revision.Head);
 
             Assertion.AssertEquals( "Wrong type returned. Should be working copy file",
@@ -140,10 +146,12 @@ namespace NSvn.Tests
                 WorkingCopyResource wc2item = new WorkingCopyFile( Path.Combine( wc2, "Form.cs" ) );
                 using( StreamWriter w = new StreamWriter( wc2item.Path, false ) )
                     w.Write( "MOO" );
+                wc2item.Context.AddAuthenticationProvider( Authentication.GetUsernameProvider() );
                 wc2item.Commit( false );
 
                 // try to update the primary wc
                 WorkingCopyResource item = new WorkingCopyFile( Path.Combine( this.WcPath, "Form.cs" ) );
+                item.Context.AddAuthenticationProvider( Authentication.GetUsernameProvider() );
                 using( StreamWriter w2 = new StreamWriter( item.Path ) )
                     w2.Write( "BLAH" );
                 item.Update();
@@ -154,6 +162,7 @@ namespace NSvn.Tests
                 // make sure you cannot commit a conflicted wc
                 try
                 {
+                    item.Context.AddAuthenticationProvider( Authentication.GetUsernameProvider() );
                     item.Commit( false );
                     Assertion.Fail( "Should not be able to commit a conflicted file" );
                 }
@@ -163,6 +172,8 @@ namespace NSvn.Tests
                 WorkingCopyResource folderItem = new WorkingCopyDirectory( this.WcPath );
                 try
                 {
+                    folderItem.Context.AddAuthenticationProvider( Authentication.GetUsernameProvider() );
+
                     folderItem.Commit(  true );
                     Assertion.Fail( "Should not be able to commit a conflicted directory" );
                 }
