b958ceef05312734958563c36fac98416e1af124
Arild Fines <63673+ArildF@users.noreply.github.com>
2004-01-26 15:53:57 +0000

    Moved the private ProcessReader class in TestBase to a public class in the Utils project. This will allow us to use it from other classes as well.
    * \src\NSvn.Core.Tests\TestBase.cs
    	(ProcessReader) - Moved from  here.
    
    * \src\Utils\ProcessReader.cs
    	Moved here.
    
    * \src\Utils\Utils.2003.csproj
    	Added ProcessReader.cs

--- src/NSvn.Core.Tests/TestBase.cs
+++ src/NSvn.Core.Tests/TestBase.cs
@@ -249,40 +249,7 @@ namespace NSvn.Core.Tests
         }
 
 
-        private class ProcessReader
-        {
-            public ProcessReader( StreamReader reader )
-            {
-                this.reader = reader;
-            }
-            
-            public void Start()
-            {
-                this.thread = new Thread( new ThreadStart( this.Read ) );
-                this.thread.Start();
-            }
-
-            public void Wait()
-            {
-                this.thread.Join();
-            }
-
-            public string Output
-            {
-                get{ return this.output; }
-            }
-
-            private void Read()
-            {
-                this.output = reader.ReadToEnd();
-            }
-
-            
-
-            private StreamReader reader;
-            private Thread thread;
-            private string output;
-        }
+        
         
        
 
