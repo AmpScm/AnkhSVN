f0404b74b72f4e7b25cc7aefc7afcbfa7eb99886
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-02-08 19:11:30 +0000

    Some renames that got lost in the reconstruction effort after the repository crash.
    * \src\NSvn.Core\CancelEventArgs.h
    	Added.
    
    * \src\NSvn.Core\LogMessageEventArgs.h
    	Added.
    
    * \src\NSvn.Core\NotificationEventArgs.h
    	Renamed from Notification.h

---
+++ branches/nsvnredesign/src/NSvn.Core/CancelEventArgs.h
@@ -0,0 +1,25 @@
+// $Id$
+
+#include "stdafx.h"
+#include "svnenums.h"
+
+namespace NSvn
+{
+    namespace Core
+    {
+
+        public __gc class CancelEventArgs : public System::EventArgs
+        {
+        public:
+           
+            __property bool get_Cancel()
+            { return this->cancel; }
+
+            __property void set_Cancel( bool cancel )
+            { this->cancel = cancel; }
+
+        private:
+            bool cancel;
+        };
+    }
+}
