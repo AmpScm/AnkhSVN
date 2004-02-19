817f04eb851b54ca3983b0847c990ea997fa6f4b
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-02-19 21:46:46 +0000

    Restored the SaveToFile command. Use new API.
    * \src\ankh\commands\savetofilecommand.cs
    	Restored. New API.

--- branches/nsvnredesign/src/Ankh/Commands/SaveToFileCommand.cs
+++ branches/nsvnredesign/src/Ankh/Commands/SaveToFileCommand.cs
@@ -1,48 +1,43 @@
-//using System;
-//using System.ComponentModel;
-//using System.Diagnostics;
-//using System.Windows.Forms;
-//
-//namespace Ankh.Commands
-//{
-//    /// <summary>
-//    /// Lets the user cat a file from a repos and have Windows open it.
-//    /// </summary>
-//    [VSNetCommand("SaveToFile", Tooltip="Save the file to disk.", Text = "Save to file" ),
-//    VSNetControl( "ReposExplorer.View", Position = 1 ) ]
-//    internal class SaveToFileCommand : ViewRepositoryFileCommand
-//    {
-//        public override void Execute(AnkhContext context, string parameters)
-//        {
-//            context.StartOperation( "Saving" );
-//            try
-//            {
-//                SaveCatVisitor v = new SaveCatVisitor( context );     
-//                context.RepositoryController.VisitSelectedNodes( v );
-//            }
-//            finally
-//            {
-//                context.EndOperation();
-//            }
-//        }        
-//
-//        // override the CatVisitor class so we can pop up a save file dialog.
-//        protected class SaveCatVisitor : CatVisitor
-//        {
-//            public SaveCatVisitor( AnkhContext context ) : base( context )
-//            {}
-//
-//            protected override string GetPath(string filename)
-//            {
-//                using( SaveFileDialog sfd = new SaveFileDialog() )
-//                {
-//                    sfd.FileName = filename;
-//                    if ( sfd.ShowDialog() == DialogResult.OK )
-//                        return sfd.FileName;
-//                    else
-//                        return null;
-//                }
-//            }
-//        }
-//    }
-//}
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Ankh.RepositoryExplorer;
+
+namespace Ankh.Commands
+{
+    /// <summary>
+    /// Lets the user cat a file from a repos and have Windows open it.
+    /// </summary>
+    [VSNetCommand("SaveToFile", Tooltip="Save the file to disk.", Text = "Save to file" ),
+    VSNetControl( "ReposExplorer.View", Position = 1 ) ]
+    internal class SaveToFileCommand : ViewRepositoryFileCommand
+    {
+        public override void Execute(AnkhContext context, string parameters)
+        {
+            context.StartOperation( "Saving" );
+            try
+            {
+                INode node = (INode)context.RepositoryExplorer.SelectedNode;
+                string filename = null;
+                using( SaveFileDialog sfd = new SaveFileDialog() )
+                {
+                    sfd.FileName = node.Name;
+                    if ( sfd.ShowDialog() == DialogResult.OK )
+                        filename = sfd.FileName;
+                    else
+                        return;
+                }
+
+                
+                CatRunner runner = new CatRunner( context, node.Revision, node.Url, 
+                    filename );
+                runner.Start( "Retrieving file" );
+            }
+            finally
+            {
+                context.EndOperation();
+            }
+        } 
+    }
+}
