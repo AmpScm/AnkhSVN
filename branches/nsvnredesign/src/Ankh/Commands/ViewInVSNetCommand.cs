9e0385389e15dda28eaf081ac505822db6975ef1
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-02-19 21:31:21 +0000

    Restored the ViewInVSNet command.
    * \src\ankh\commands\viewinvsnetcommand.cs
    	Restored. Use new API.

--- branches/nsvnredesign/src/Ankh/Commands/ViewInVSNetCommand.cs
+++ branches/nsvnredesign/src/Ankh/Commands/ViewInVSNetCommand.cs
@@ -1,32 +1,35 @@
-//using System;
-//using EnvDTE;
-//
-//namespace Ankh.Commands
-//{
-//	/// <summary>
-//	/// A command that opens a file from the server in VS.NET
-//	/// </summary>
-//    [VSNetCommand("ViewInVsNet", Tooltip="View this file in VS.NET", Text = "In VS.NET" ),
-//    VSNetControl( "ReposExplorer.View", Position = 1 ) ]
-//	internal class ViewInVSNetCommand : ViewRepositoryFileCommand
-//	{
-//        public override void Execute(AnkhContext context, string parameters)
-//        {
-//            try
-//            {
-//                context.StartOperation( "Opening" );
-//
-//                CatVisitor v = new CatVisitor( context );
-//                context.RepositoryController.VisitSelectedNodes( v );
-//
-//                foreach( string filename in v.FileNames )
-//                    context.DTE.ItemOperations.OpenFile( filename, Constants.vsViewKindPrimary );
-//
-//            }
-//            finally
-//            {
-//                context.EndOperation();
-//            }
-//        }        
-//	}
-//}
+using System;
+using EnvDTE;
+using Ankh.RepositoryExplorer;
+
+namespace Ankh.Commands
+{
+	/// <summary>
+	/// A command that opens a file from the server in VS.NET
+	/// </summary>
+    [VSNetCommand("ViewInVsNet", Tooltip="View this file in VS.NET", Text = "In VS.NET" ),
+    VSNetControl( "ReposExplorer.View", Position = 1 ) ]
+	internal class ViewInVSNetCommand : ViewRepositoryFileCommand
+	{
+        public override void Execute(AnkhContext context, string parameters)
+        {
+            try
+            {
+                context.StartOperation( "Opening" );
+
+                INode node = (INode)context.RepositoryExplorer.SelectedNode;
+
+                CatRunner runner = new CatRunner(context, node.Name, node.Revision, node.Url);
+                runner.Start( "Retrieving file" );
+
+                context.DTE.ItemOperations.OpenFile( runner.Path, 
+                    Constants.vsViewKindPrimary );
+
+            }
+            finally
+            {
+                context.EndOperation();
+            }
+        }        
+	}
+}
