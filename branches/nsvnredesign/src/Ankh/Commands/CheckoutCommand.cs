643bce572c4652d0ee208cbbdfc3b48b4ce48d3e
vorlon <vorlon@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-02-20 22:30:14 +0000

    Rewrote CheckoutCommand base class and CheckoutFolderCommand and enabled checkout in the Repository Explorer
    * \ankh\commands\checkoutfoldercommand.cs
    * \ankh\commands\checkoutcommand.cs

--- branches/nsvnredesign/src/Ankh/Commands/CheckoutCommand.cs
+++ branches/nsvnredesign/src/Ankh/Commands/CheckoutCommand.cs
@@ -1,44 +1,56 @@
-//// $Id: ViewRepositoryFileCommand.cs 738 2003-06-22 23:25:33Z Arild $
-//using EnvDTE;
-//
-//using System.IO;
-//using System.Collections;
-//
-//namespace Ankh.Commands
-//{
-//    /// <summary>
-//    /// A command that lets you view a repository file.
-//    /// </summary>
-//    internal abstract class CheckoutCommand : CommandBase
-//    {
-//        #region ICommand Members
-//        public override EnvDTE.vsCommandStatus QueryStatus(AnkhContext context)
-//        {
-//            IsRepositoryFileVisitor v = new IsRepositoryFileVisitor();
-//            context.RepositoryController.VisitSelectedNodes( v );
-//            return v.IsFile ? vsCommandStatus.vsCommandStatusSupported :
-//                vsCommandStatus.vsCommandStatusSupported | vsCommandStatus.vsCommandStatusEnabled;
-//        }
-//        #endregion
-//
-//        #region CheckoutVisitor
-//        protected class CheckoutVisitor : RepositoryResourceVisitorBase
-//        {          
-//            public virtual IEnumerable Directories
-//            {
-//                get{ return this.directories; }
-//            }
-//
-//            public override void VisitDirectory(RepositoryDirectory directory)
-//            {
-//                this.directories.Add( directory );
-//            }
-//
-//            private ArrayList directories = new ArrayList();
-//        }
-//        #endregion
-//    }
-//}
-//
-//
-//
+// $Id: ViewRepositoryFileCommand.cs 738 2003-06-22 23:25:33Z Arild $
+using EnvDTE;
+
+using System.IO;
+using System.Collections;
+using NSvn.Core;
+using Ankh.RepositoryExplorer;
+
+namespace Ankh.Commands
+{
+    /// <summary>
+    /// A command that lets you view a repository file.
+    /// </summary>
+    internal abstract class CheckoutCommand : CommandBase
+    {
+        #region ICommand Members
+        public override EnvDTE.vsCommandStatus QueryStatus(AnkhContext context)
+        {
+            return context.RepositoryExplorer.SelectedNode.IsDirectory ? 
+                vsCommandStatus.vsCommandStatusSupported | vsCommandStatus.vsCommandStatusEnabled :
+                vsCommandStatus.vsCommandStatusSupported;
+
+        }
+        #endregion
+
+
+        #region CheckoutRunner class
+        /// <summary>
+        /// For running checkouts on a separate thread.
+        /// </summary>
+        protected class CheckoutRunner : ProgressRunner
+        {
+            public CheckoutRunner( AnkhContext context, string path, Revision revision, string url ) 
+                : base( context )
+            { 
+                this.path = path;
+                this.url = url;
+                this.revision = revision;
+            }
+
+            protected override void DoRun()
+            {
+                this.Context.Client.Checkout( this.url, this.path, this.revision, true );
+            }
+
+            private Revision revision;
+            private string url;
+            private string path;
+
+        }
+        #endregion
+    }
+}
+
+
+
