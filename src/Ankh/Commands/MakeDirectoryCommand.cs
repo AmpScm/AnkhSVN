87b7e76dff8444431de9ced9f9630c64e6baf810
Arild Fines <63673+ArildF@users.noreply.github.com>
2004-02-27 09:33:28 +0000

    Implement the MakeDir command.
    * \src\ankh\commands\makedirectorycommand.cs
    	Implemented
    
    * \src\ankh\repositoryexplorer\controller.cs
    	(MakeDir) - New. Starts the make directory label editing operation.
    
    * \src\ankh.ui\repositoryexplorercontrol.cs
    	(MakeDir) - Creates a new subnode of the currently selected node and starts a label edit on it.
    	(MakeDirAfterEdit) - Called when the label edit finishes. Delegates to the INewDirectoryHandler
    		to actually create the directory.
    	(INewDirectoryHandler) - Represents an object that can perform the directory creation.	
    
    * \src\ankh\svnclient.cs
    	(ShowLogMessageDialog) - Add a new argument, urlPaths, indicating whether the items
    		are URLs.
    
    * \src\ankh\commands\commititemcommand.cs
    	(Execute) - Call ShowLogMessageDialog with new argument urlPaths set to false.
    
    * \src\ankh\repositoryexplorer\node.cs
    	(UriCombine) - Made public.

--- src/Ankh/Commands/MakeDirectoryCommand.cs
+++ src/Ankh/Commands/MakeDirectoryCommand.cs
@@ -1,18 +1,80 @@
 // $Id$
 using System;
+using System.Windows.Forms;
+using System.Collections;
+using Ankh.RepositoryExplorer;
+using Ankh.UI;
 
 namespace Ankh.Commands
 {
     /// <summary>
     /// Summary description for MakeDirectory.
     /// </summary>
-    internal class MakeDirectoryCommand
+    [VSNetCommand("MakeDirectoryCommand", 
+         Tooltip="Create new directory here", Text = "New directory..." ),
+    VSNetControl( "ReposExplorer", Position = 1 ) ]
+    internal class MakeDirectoryCommand : CommandBase
     {
-        public MakeDirectoryCommand()
+        public override EnvDTE.vsCommandStatus QueryStatus(AnkhContext context)
         {
-            //
-            // TODO: Add constructor logic here
-            //
+            // we only want directories
+            if ( context.RepositoryExplorer.SelectedNode != null &&
+                context.RepositoryExplorer.SelectedNode.IsDirectory )
+            {
+                return Enabled;
+            }
+            else
+                return Disabled;
+        }
+
+        public override void Execute(AnkhContext context, string parameters)
+        {
+            context.RepositoryExplorer.MakeDir( new NewDirHandler(context) );           
+        }
+
+        /// <summary>
+        /// This class handles the actual creation of the new dir.
+        /// </summary>
+        private class NewDirHandler : ProgressRunner, INewDirectoryHandler
+        {
+            public NewDirHandler( AnkhContext context ) : base(context)
+            {}
+
+            public bool MakeDir(IRepositoryTreeNode parent, string dirname)
+            {
+                // first show the log message dialog
+                this.url = Node.UriCombine( ((INode)parent).Url, dirname );
+                IList list = this.Context.Client.ShowLogMessageDialog( 
+                    new string[]{this.url}, true );
+                if ( list == null || list.Count == 0 )
+                    return false;
+
+                this.Context.StartOperation( "Creating directory " + url );
+                try
+                {
+                    this.Start( "Creating directory" );
+                    this.Context.Client.CommitCompleted();
+                    return !this.Cancelled;
+                }
+                catch( Exception ex )
+                {
+                    Error.Handle( ex );
+                    return false;
+                }
+                finally
+                {
+                    this.Context.EndOperation();
+                }
+            }
+
+            protected override void DoRun()
+            {
+                // create the dir.
+                this.Context.Client.MakeDir( new string[]{this.url} );
+            }
+
+
+            private string url;
         }
     }
 }
