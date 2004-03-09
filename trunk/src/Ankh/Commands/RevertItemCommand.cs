e1d44bf817703ab3cba882fad699f3db13bd951a
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-03-09 20:57:40 +0000

    Put the project menu entries back on the Project command bar, in addition to the "Project Node" command bar. The comment to r1219, indicating that "Project Node" covers both regular projects and S&D projects, is wrong.

--- trunk/src/Ankh/Commands/RevertItemCommand.cs
+++ trunk/src/Ankh/Commands/RevertItemCommand.cs
@@ -19,6 +19,7 @@ namespace Ankh.Commands
          Bitmap = ResourceBitmaps.Revert),
     VSNetControl( "Item.Ankh", Position = 1 ),
     VSNetControl( "Project Node.Ankh", Position = 1 ),
+    VSNetControl( "Project.Ankh", Position = 1 ),
     VSNetControl( "Folder.Ankh", Position = 1 ),
     VSNetControl( "Solution.Ankh", Position = 1)]
     internal class RevertItemCommand : CommandBase
