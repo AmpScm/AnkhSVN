1b673e6825b9399976382dc0f9acee5428caacf7
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-04-08 04:35:18 +0000

    Updated version numbers

--- trunk/src/NSvn.Core.Tests.MCpp/AssemblyInfo.cpp
+++ trunk/src/NSvn.Core.Tests.MCpp/AssemblyInfo.cpp
@@ -29,7 +29,7 @@ using namespace System::Runtime::CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute( "0.4.0.1074")];
+[assembly:AssemblyVersionAttribute( "0.4.99.1296")];
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
