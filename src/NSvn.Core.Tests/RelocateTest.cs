72a790d074cbcb2c68507b0ceea18a4b1ed6e7bf
Arild Fines <63673+ArildF@users.noreply.github.com>
2004-06-19 00:24:52 +0000

    Write slightly less braindead test for relocate. Start an svnserve instance on the repos, relocate to an svn://localhost URL:
    * \src\NSvn.Core.Tests\RelocateTest.cs
    	(Test) - Call StartSvnServe, kill off the process when done
    	(StartSvnServe) - New method.

--- src/NSvn.Core.Tests/RelocateTest.cs
+++ src/NSvn.Core.Tests/RelocateTest.cs
@@ -1,5 +1,7 @@
 using System;
 using NUnit.Framework;
+using System.Diagnostics;
+using System.IO;
 
 namespace NSvn.Core.Tests
 {
@@ -20,9 +22,40 @@ namespace NSvn.Core.Tests
         [Test]
         public void Test()
         {
-            this.Client.Relocate( this.WcPath, "file", "file", true );
-            string url = this.Client.SingleStatus( this.WcPath ).Entry.Url;
-            Assertion.Assert( url.StartsWith( "file" ) );
+            // start a svnserve process on this repos
+            Process svnserve = this.StartSvnServe( this.ReposPath );
+            try
+            {
+                this.Client.Relocate( 
+                    this.WcPath, 
+                    "file:///tmp/repos", 
+                    String.Format("svn://localhost:{0}/", PortNumber), 
+                    true );
+                string url = this.Client.SingleStatus( this.WcPath ).Entry.Url;
+                Assertion.Assert( url.StartsWith( "svn://localhost" ) );
+            }
+            finally
+            {
+                svnserve.Kill();
+            }
         }
+
+        /// <summary>
+        /// Starts a svnserve instance.
+        /// </summary>
+        /// <param name="root">The root directory to use for svnserve.</param>
+        /// <returns></returns>
+        private Process StartSvnServe( string root )
+        {
+            ProcessStartInfo psi = new ProcessStartInfo( "svnserve", 
+                String.Format( "--daemon --root {0} --listen-port {1}", root, 
+                PortNumber ) );
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+            return p;
+        }
+
+        private const int PortNumber = 7777;
 	}
 }
