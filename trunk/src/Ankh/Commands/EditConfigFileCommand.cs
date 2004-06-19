d4952e80d4582dcfa00de2fe44d2c19ee0ff516b
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-06-19 05:20:44 +0000

    Add a command to let you open the AnkhSVN config file directly in VS.
    * \src\Ankh\Ankh.2003.csproj
    	Added EditConfigFileCommand.cs
    
    * \src\Ankh\Commands\EditConfigFileCommand.cs
    	The command itself.

---
+++ trunk/src/Ankh/Commands/EditConfigFileCommand.cs
@@ -0,0 +1,26 @@
+using System;
+using Ankh.Config;
+using EnvDTE;
+
+namespace Ankh.Commands
+{
+	/// <summary>
+	/// A command that lets you edit the AnkhSVN config file.
+	/// </summary>
+    [VSNetCommand("EditConfigFIle", Tooltip="Edit the AnkhSVN config file", 
+         Text = "Edit the AnkhSVN config file" ),
+    VSNetControl( "MenuBar.Tools.AnkhSVN", Position = 1 ) ]
+	internal class EditConfigFileCommand : CommandBase
+	{
+        public override EnvDTE.vsCommandStatus QueryStatus(AnkhContext context)
+        {
+            return Enabled;
+        }
+
+        public override void Execute(AnkhContext context, string parameters)
+        {
+            context.DTE.ItemOperations.OpenFile( context.ConfigLoader.ConfigPath,
+                Constants.vsViewKindPrimary );
+        }
+	}
+}
