902cd62c79244bec442d7619c1d495173eeb84c3
meadow <meadow@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-07-04 04:45:47 +0000

    Fix dialog to have autocomplete on textbox, check that the working copy to export from is a valid wc,  adjust appearance.
    * \Ankh.UI\ExportDialog.cs
    	Positional adjustments of controls on form. 
    	(*ctor) - Add call to Win32.SHAutoComplete so exportFromDirTextBox has autocomplete.
    	(ControlChanged) - Add call to SvnUtils.IsWorkingCopyPath for checking validity of 
    	exportFromDirTextBox.Text
    * \Ankh.UI\ExportDialog.resx

--- src/Ankh.UI/ExportDialog.cs
+++ src/Ankh.UI/ExportDialog.cs
@@ -25,6 +25,8 @@ namespace Ankh.UI
                 Shacf.UrlAll );
             Win32.SHAutoComplete( this.localDirTextBox.Handle, 
                 Shacf.Filesystem );
+            Win32.SHAutoComplete( this.exportFromDirTextBox.Handle, 
+                Shacf.Filesystem );
 	
             this.ControlsChanged( this, EventArgs.Empty );
         }
@@ -117,9 +119,9 @@ namespace Ankh.UI
             // revisionGroupBox
             // 
             this.revisionGroupBox.Controls.Add(this.revisionPicker);
-            this.revisionGroupBox.Location = new System.Drawing.Point(8, 88);
+            this.revisionGroupBox.Location = new System.Drawing.Point(8, 82);
             this.revisionGroupBox.Name = "revisionGroupBox";
-            this.revisionGroupBox.Size = new System.Drawing.Size(416, 64);
+            this.revisionGroupBox.Size = new System.Drawing.Size(408, 56);
             this.revisionGroupBox.TabIndex = 0;
             this.revisionGroupBox.TabStop = false;
             this.revisionGroupBox.Text = "Revision";
@@ -127,18 +129,18 @@ namespace Ankh.UI
             // revisionPicker
             // 
             this.revisionPicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.revisionPicker.Location = new System.Drawing.Point(16, 28);
+            this.revisionPicker.Location = new System.Drawing.Point(16, 24);
             this.revisionPicker.Name = "revisionPicker";
-            this.revisionPicker.Size = new System.Drawing.Size(392, 20);
+            this.revisionPicker.Size = new System.Drawing.Size(384, 20);
             this.revisionPicker.TabIndex = 0;
             this.revisionPicker.Changed += new System.EventHandler(this.ControlsChanged);
             // 
             // urlGroupBox
             // 
             this.urlGroupBox.Controls.Add(this.urlTextBox);
-            this.urlGroupBox.Location = new System.Drawing.Point(8, 160);
+            this.urlGroupBox.Location = new System.Drawing.Point(8, 156);
             this.urlGroupBox.Name = "urlGroupBox";
-            this.urlGroupBox.Size = new System.Drawing.Size(416, 64);
+            this.urlGroupBox.Size = new System.Drawing.Size(408, 56);
             this.urlGroupBox.TabIndex = 1;
             this.urlGroupBox.TabStop = false;
             this.urlGroupBox.Text = "URL";
@@ -147,9 +149,9 @@ namespace Ankh.UI
             // urlTextBox
             // 
             this.urlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.urlTextBox.Location = new System.Drawing.Point(16, 24);
+            this.urlTextBox.Location = new System.Drawing.Point(16, 20);
             this.urlTextBox.Name = "urlTextBox";
-            this.urlTextBox.Size = new System.Drawing.Size(384, 20);
+            this.urlTextBox.Size = new System.Drawing.Size(376, 20);
             this.urlTextBox.TabIndex = 0;
             this.urlTextBox.Text = "";
             // 
@@ -157,9 +159,9 @@ namespace Ankh.UI
             // 
             this.localDirGroupBox.Controls.Add(this.toDirBrowseButton);
             this.localDirGroupBox.Controls.Add(this.localDirTextBox);
-            this.localDirGroupBox.Location = new System.Drawing.Point(8, 232);
+            this.localDirGroupBox.Location = new System.Drawing.Point(8, 230);
             this.localDirGroupBox.Name = "localDirGroupBox";
-            this.localDirGroupBox.Size = new System.Drawing.Size(416, 80);
+            this.localDirGroupBox.Size = new System.Drawing.Size(408, 56);
             this.localDirGroupBox.TabIndex = 2;
             this.localDirGroupBox.TabStop = false;
             this.localDirGroupBox.Text = "Local Directory";
@@ -167,7 +169,7 @@ namespace Ankh.UI
             // toDirBrowseButton
             // 
             this.toDirBrowseButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.toDirBrowseButton.Location = new System.Drawing.Point(384, 32);
+            this.toDirBrowseButton.Location = new System.Drawing.Point(376, 20);
             this.toDirBrowseButton.Name = "toDirBrowseButton";
             this.toDirBrowseButton.Size = new System.Drawing.Size(24, 23);
             this.toDirBrowseButton.TabIndex = 1;
@@ -177,9 +179,9 @@ namespace Ankh.UI
             // localDirTextBox
             // 
             this.localDirTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.localDirTextBox.Location = new System.Drawing.Point(16, 32);
+            this.localDirTextBox.Location = new System.Drawing.Point(16, 20);
             this.localDirTextBox.Name = "localDirTextBox";
-            this.localDirTextBox.Size = new System.Drawing.Size(360, 20);
+            this.localDirTextBox.Size = new System.Drawing.Size(352, 20);
             this.localDirTextBox.TabIndex = 0;
             this.localDirTextBox.Text = "";
             this.localDirTextBox.TextChanged += new System.EventHandler(this.ControlsChanged);
@@ -187,7 +189,7 @@ namespace Ankh.UI
             // nonRecursiveCheckBox
             // 
             this.nonRecursiveCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.nonRecursiveCheckBox.Location = new System.Drawing.Point(8, 354);
+            this.nonRecursiveCheckBox.Location = new System.Drawing.Point(16, 304);
             this.nonRecursiveCheckBox.Name = "nonRecursiveCheckBox";
             this.nonRecursiveCheckBox.TabIndex = 3;
             this.nonRecursiveCheckBox.Text = "Non-recursive";
@@ -197,7 +199,7 @@ namespace Ankh.UI
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.okButton.Enabled = false;
-            this.okButton.Location = new System.Drawing.Point(266, 378);
+            this.okButton.Location = new System.Drawing.Point(256, 304);
             this.okButton.Name = "okButton";
             this.okButton.TabIndex = 4;
             this.okButton.Text = "OK";
@@ -206,7 +208,7 @@ namespace Ankh.UI
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(354, 378);
+            this.cancelButton.Location = new System.Drawing.Point(344, 304);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.TabIndex = 5;
             this.cancelButton.Text = "Cancel";
@@ -215,16 +217,16 @@ namespace Ankh.UI
             // 
             this.radioButtonGroupbox.Controls.Add(this.radioButtonFromDir);
             this.radioButtonGroupbox.Controls.Add(this.radioButtonFromURL);
-            this.radioButtonGroupbox.Location = new System.Drawing.Point(8, 16);
+            this.radioButtonGroupbox.Location = new System.Drawing.Point(8, 8);
             this.radioButtonGroupbox.Name = "radioButtonGroupbox";
-            this.radioButtonGroupbox.Size = new System.Drawing.Size(416, 64);
+            this.radioButtonGroupbox.Size = new System.Drawing.Size(408, 56);
             this.radioButtonGroupbox.TabIndex = 6;
             this.radioButtonGroupbox.TabStop = false;
             this.radioButtonGroupbox.Text = "Export Source";
             // 
             // radioButtonFromDir
             // 
-            this.radioButtonFromDir.Location = new System.Drawing.Point(200, 24);
+            this.radioButtonFromDir.Location = new System.Drawing.Point(216, 24);
             this.radioButtonFromDir.Name = "radioButtonFromDir";
             this.radioButtonFromDir.Size = new System.Drawing.Size(176, 24);
             this.radioButtonFromDir.TabIndex = 1;
@@ -246,9 +248,9 @@ namespace Ankh.UI
             // 
             this.exportFromDirGroupbox.Controls.Add(this.exportFromDirButton);
             this.exportFromDirGroupbox.Controls.Add(this.exportFromDirTextBox);
-            this.exportFromDirGroupbox.Location = new System.Drawing.Point(8, 160);
+            this.exportFromDirGroupbox.Location = new System.Drawing.Point(8, 156);
             this.exportFromDirGroupbox.Name = "exportFromDirGroupbox";
-            this.exportFromDirGroupbox.Size = new System.Drawing.Size(416, 65);
+            this.exportFromDirGroupbox.Size = new System.Drawing.Size(408, 56);
             this.exportFromDirGroupbox.TabIndex = 7;
             this.exportFromDirGroupbox.TabStop = false;
             this.exportFromDirGroupbox.Text = "Export from Local Directory";
@@ -257,7 +259,7 @@ namespace Ankh.UI
             // exportFromDirButton
             // 
             this.exportFromDirButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.exportFromDirButton.Location = new System.Drawing.Point(384, 25);
+            this.exportFromDirButton.Location = new System.Drawing.Point(376, 20);
             this.exportFromDirButton.Name = "exportFromDirButton";
             this.exportFromDirButton.Size = new System.Drawing.Size(24, 23);
             this.exportFromDirButton.TabIndex = 1;
@@ -267,25 +269,26 @@ namespace Ankh.UI
             // exportFromDirTextBox
             // 
             this.exportFromDirTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.exportFromDirTextBox.Location = new System.Drawing.Point(16, 25);
+            this.exportFromDirTextBox.Location = new System.Drawing.Point(16, 20);
             this.exportFromDirTextBox.Name = "exportFromDirTextBox";
-            this.exportFromDirTextBox.Size = new System.Drawing.Size(360, 20);
+            this.exportFromDirTextBox.Size = new System.Drawing.Size(352, 20);
             this.exportFromDirTextBox.TabIndex = 0;
             this.exportFromDirTextBox.Text = "";
+            this.exportFromDirTextBox.TextChanged += new System.EventHandler(this.ControlsChanged);
             // 
             // ExportDialog
             // 
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(442, 415);
+            this.ClientSize = new System.Drawing.Size(426, 343);
             this.ControlBox = false;
-            this.Controls.Add(this.exportFromDirGroupbox);
             this.Controls.Add(this.radioButtonGroupbox);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.nonRecursiveCheckBox);
             this.Controls.Add(this.localDirGroupBox);
-            this.Controls.Add(this.urlGroupBox);
             this.Controls.Add(this.revisionGroupBox);
+            this.Controls.Add(this.exportFromDirGroupbox);
+            this.Controls.Add(this.urlGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "ExportDialog";
             this.ShowInTaskbar = false;
@@ -319,7 +322,7 @@ namespace Ankh.UI
                 if(this.radioButtonFromURL.Checked)
                     this.okButton.Enabled = UriUtils.ValidUrl.IsMatch( this.urlTextBox.Text ) ;
                 else 
-                    this.okButton.Enabled=(this.exportFromDirTextBox.Text.Length > 0);
+                    this.okButton.Enabled=SvnUtils.IsWorkingCopyPath(this.exportFromDirTextBox.Text);
             }
         }
 
