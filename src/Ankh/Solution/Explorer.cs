2c6a37a91fb4c87746c2a39865322b902d95f40e
Arild Fines <63673+ArildF@users.noreply.github.com>
2004-07-12 23:44:44 +0000

    Extract some of the functionality from Solution.Explorer into an interface ISelectionContainer.
    * \src\Ankh\Ankh.2003.csproj
    	Add ISelectionContainer.cs
    
    * \src\Ankh\Solution\Explorer.cs
    	Implement ISelectionContainer.
    
    * \src\Ankh\ISelectionContainer.cs
    	New interface.

--- src/Ankh/Solution/Explorer.cs
+++ src/Ankh/Solution/Explorer.cs
@@ -22,7 +22,7 @@ namespace Ankh.Solution
     /// <summary>
     /// Represents the Solution Explorer window in the VS.NET IDE
     /// </summary>
-    public class Explorer 
+    public class Explorer : ISelectionContainer
     {
         public Explorer( _DTE dte, IContext context )
         {
