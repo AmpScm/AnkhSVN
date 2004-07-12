a6c2b31dfd5bd40185e3faaa253f0fa58aaa5ded
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-07-12 23:44:44 +0000

    Extract some of the functionality from Solution.Explorer into an interface ISelectionContainer.
    * \src\Ankh\Ankh.2003.csproj
    	Add ISelectionContainer.cs
    
    * \src\Ankh\Solution\Explorer.cs
    	Implement ISelectionContainer.
    
    * \src\Ankh\ISelectionContainer.cs
    	New interface.

---
+++ trunk/src/Ankh/ISelectionContainer.cs
@@ -0,0 +1,64 @@
+// $Id$
+using System.Collections;
+using EnvDTE;
+
+namespace Ankh.Solution
+{
+    public interface ISelectionContainer
+    {
+        /// <summary>
+        /// Updates the status of selected items.
+        /// </summary>
+        void UpdateSelectionStatus();
+
+        /// <summary>
+        /// Refreshes the parents of the selected items.
+        /// </summary>
+        void RefreshSelectionParents();
+
+        /// <summary>
+        /// Refreshes the current selection.
+        /// </summary>
+        void RefreshSelection();
+
+        void SyncAll();
+
+        /// <summary>
+        /// Returns the SvnItem resources associated with the selected items
+        /// in the solution explorer.
+        /// </summary>
+        /// <param name="getChildItems">Whether children of the items in 
+        /// question should be included.</param>        /// 
+        /// <returns>A list of SvnItem instances.</returns>
+        IList GetSelectionResources( bool getChildItems );
+
+        /// <summary>	 	
+        /// Visits all the selected nodes.	 	
+        /// </summary>	 	
+        /// <param name="visitor"></param>	 	
+        void VisitSelectedNodes( INodeVisitor visitor );
+
+        /// <summary>
+        /// Returns the SvnItem resources associated with the selected items
+        /// in the solution explorer.
+        /// </summary>
+        /// <param name="getChildItems">Whether children of the items in 
+        /// question should be included.</param>
+        /// <param name="filter">A callback used to filter the items
+        /// that are added.</param>
+        /// <returns>A list of SvnItem instances.</returns>
+        IList GetSelectionResources( bool getChildItems, 
+            ResourceFilterCallback filter );
+
+        /// <summary>
+        /// Returns all  the SvnItem resources from root
+        /// </summary>
+        /// <param name="getChildItems">Whether children of the items in 
+        /// question should be included.</param>
+        /// <param name="filter">A callback used to filter the items
+        /// that are added.</param>
+        /// <returns>A list of SvnItem instances.</returns>
+        IList GetAllResources( bool getChildItems, 
+            ResourceFilterCallback filter );
+    }
+}
