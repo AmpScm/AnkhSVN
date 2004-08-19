35624ec133411e27b39a7d7bc5cbf61c05636161
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-08-19 11:31:39 +0000

    Add a ShowMessageBox method to IUIShell and implementor.
    * \src\Ankh\UIShell.cs
    * \src\Ankh\IUIShell.cs
    	(ShowMessageBox * 2) - New methods.

--- trunk/src/Ankh/IUIShell.cs
+++ trunk/src/Ankh/IUIShell.cs
@@ -45,5 +45,25 @@ namespace Ankh
         /// </summary>
         /// <param name="selection"></param>
         void SetRepositoryExplorerSelection( object[] selection );
+
+        /// <summary>
+        /// Display a message box.
+        /// </summary>
+        /// <param name="caption"></param>
+        /// <param name="text"></param>
+        /// <param name="buttons"></param>
+        /// <returns></returns>
+        DialogResult ShowMessageBox( string text, string caption, 
+            MessageBoxButtons buttons );
+
+        /// <summary>
+        /// Display a message box.
+        /// </summary>
+        /// <param name="caption"></param>
+        /// <param name="text"></param>
+        /// <param name="buttons"></param>
+        /// <returns></returns>
+        DialogResult ShowMessageBox( string text, string caption, 
+            MessageBoxButtons buttons, MessageBoxIcon icon );
 	}
 }
