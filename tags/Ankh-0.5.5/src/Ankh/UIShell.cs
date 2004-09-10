1723c256a0ac12cf1b244d48004307751f2a649c
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-09-10 20:12:59 +0000

    Ooops, seems we forgot to tag this release

---
+++ tags/Ankh-0.5.5/src/Ankh/UIShell.cs
@@ -0,0 +1,101 @@
+// $Id$
+using System;
+using Ankh.UI;
+using EnvDTE;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Ankh
+{
+	/// <summary>
+	/// Summary description for UIShell.
+	/// </summary>
+	public class UIShell : IUIShell
+	{
+		public UIShell()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+        }
+        #region IUIShell Members
+        public RepositoryExplorerControl RepositoryExplorer
+        {
+            get
+            { 
+                if( this.repositoryExplorerControl == null )
+                    this.CreateRepositoryExplorer();
+                return this.repositoryExplorerControl;
+            }
+        }
+
+        public IContext Context
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get{ return this.context; }
+
+            [System.Diagnostics.DebuggerStepThrough]
+            set{ this.context = value; }
+        }
+
+        public DialogResult QueryWhetherAnkhShouldLoad()
+        {
+            string nl = Environment.NewLine;
+            string msg = "Ankh has detected that the solution file for this solution " + 
+                "is in a Subversion working copy." + nl + 
+                "Do you want to enable Ankh for this solution?" + nl +
+                "(If you select Cancel, Ankh will not be enabled, " + nl +
+                "but you will " +
+                "be asked this question again the next time you open the solution)";
+
+            //TODO: The UIShell should be responsible for maintaining the hostwindow
+            return MessageBox.Show( 
+                this.context.HostWindow, msg, "Ankh", 
+                MessageBoxButtons.YesNoCancel );
+        }
+
+        public void ShowRepositoryExplorer(bool show)
+        {
+            this.repositoryExplorerWindow.Visible = show;
+
+        }
+    
+        public void SetRepositoryExplorerSelection(object[] selection)
+        {
+            this.repositoryExplorerWindow.SetSelectionContainer( ref selection );
+        }
+
+
+        #endregion
+
+        private void CreateRepositoryExplorer()
+        {   
+            Debug.WriteLine( "Creating repository explorer", "Ankh" );
+            object control = null;
+            this.repositoryExplorerWindow = this.context.DTE.Windows.CreateToolWindow( 
+                this.context.AddIn, "AnkhUserControlHost.AnkhUserControlHostCtl", 
+                "Repository Explorer", REPOSEXPLORERGUID, ref control );
+            
+            this.repositoryExplorerWindow.Visible = true;
+            this.repositoryExplorerWindow.Caption = "Repository Explorer";
+            
+            AnkhUserControlHostLib.IAnkhUserControlHostCtlCtl 
+                objControl = (AnkhUserControlHostLib.IAnkhUserControlHostCtlCtl)control;
+            
+            this.repositoryExplorerControl = new RepositoryExplorerControl();
+            objControl.HostUserControl( this.repositoryExplorerControl );
+            
+            System.Diagnostics.Debug.Assert( this.repositoryExplorerControl != null, 
+                "Could not create tool window" );
+        }
+
+        private RepositoryExplorerControl repositoryExplorerControl;
+        private Window repositoryExplorerWindow;
+        private IContext context;
+        
+        public const string REPOSEXPLORERGUID = 
+            "{1C5A739C-448C-4401-9076-5990300B0E1B}";
+    
+        
+    }
+}
---
+++ tags/Ankh-0.5.5/src/Ankh/VSNetProjectNodeControlAttribute.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.Office.Core;
+
+namespace Ankh
+{
+    /// <summary>
+    /// An attribute that lets you add controls to *all* of the VS Project bars.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class VSNetProjectNodeControlAttribute : VSNetControlAttribute
+    {
+        public VSNetProjectNodeControlAttribute( string commandBar ) : 
+            base( commandBar )
+        {
+            // empty
+        }
+        
+        public override void AddControl(ICommand cmd, IContext context, string tag)
+        {
+            this.AddControls( Bars, cmd, context, tag );
+        }
+
+        private static readonly string[] Bars = new string[]{ 
+            "Project Node", "Cab Project Node", "Project" };
+
+
+    }
+}
