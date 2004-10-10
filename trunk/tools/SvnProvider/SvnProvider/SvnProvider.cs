5726cf7c356d013b4906ed7aaa54a1a6b0bebede
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2004-10-10 15:58:56 +0000

    Created a VS2005 solution for implementing an MSH commandlet provider that accesses a Subversion repository

---
+++ trunk/tools/SvnProvider/SvnProvider/SvnProvider.cs
@@ -0,0 +1,18 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Rogue.Monad
+{
+	public class SvnProvider
+	{
+		public SvnProvider()
+		{
+
+		}
+	}
+}
