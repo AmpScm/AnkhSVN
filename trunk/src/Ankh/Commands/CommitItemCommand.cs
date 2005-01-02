3b709b7b9eb899094e9452d2b90f1949fbe1c079
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2005-01-02 02:09:18 +0000

    Allow commits on solutions composed of projects from multiple repositories(different repository UUIDs).
    * \src\Ankh\Commands\CommitItemCommand.cs
    	(Execute) - Sort items by repository first, then invoke commit once for each individual repository.
    	(SortByRepository) - Sort SvnItems by repository UUID.

--- trunk/src/Ankh/Commands/CommitItemCommand.cs
+++ trunk/src/Ankh/Commands/CommitItemCommand.cs
@@ -44,45 +44,78 @@ namespace Ankh.Commands
             if ( resources == null ) 
                 return;
 
+            // we need to commit to each repository separately
+            ICollection repositories = this.SortByRepository( resources );           
+
             this.commitInfo = null;
-
-            try
+            
+            foreach( IList items in repositories )
             {
+                string startText = "Committing ";
+                if ( repositories.Count > 1 && items.Count > 0 )
+                    startText += "to repository " + ((SvnItem)items[0]).Status.Entry.Uuid;
+                context.StartOperation( startText );
 
-                context.StartOperation( "Committing" );
-                this.paths = SvnItem.GetPaths( resources );
-
-                ProgressRunner runner = new ProgressRunner( context, 
-                    new ProgressRunnerCallback( this.DoCommit ) );
-                runner.Start( "Committing" );
-
-                if(!runner.Cancelled)
+                try
                 {
-                    foreach( SvnItem item in resources )
-                        item.Refresh( context.Client );
+                    this.paths = SvnItem.GetPaths( items );
 
-                    context.Client.CommitCompleted();
+                    ProgressRunner runner = new ProgressRunner( context, 
+                        new ProgressRunnerCallback( this.DoCommit ) );
+                    runner.Start( "Committing" );
+
+                    if(!runner.Cancelled)
+                    {
+                        foreach( SvnItem item in resources )
+                            item.Refresh( context.Client );                        
+                    }
+                }
+           
+                catch( NSvn.Common.SvnException )
+                {
+                    context.OutputPane.WriteLine( "Commit aborted" );
+                    throw;
+                }
+                finally
+                {
+                    if (this.commitInfo != null)
+                        context.OutputPane.WriteLine("\nCommitted revision {0}.", 
+                            this.commitInfo.Revision);
+
+                    context.EndOperation();
                 }
             }
-            catch( NSvn.Common.SvnException )
-            {
-                context.OutputPane.WriteLine( "Commit aborted" );
-                throw;
-            }
-            finally
-            {
-                if (this.commitInfo != null)
-                    context.OutputPane.WriteLine("\nCommitted revision {0}.", 
-                        this.commitInfo.Revision);
 
-                context.EndOperation();
-            }
+            // not in the finally, because we want to preserve the message for a 
+            // non-successful commit
+            context.Client.CommitCompleted();
         }        
         #endregion
 
         private void DoCommit( IContext context )
         {
             this.commitInfo = context.Client.Commit( this.paths, true );
+        }
+
+        /// <summary>
+        /// Sort the items by repository.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        private ICollection SortByRepository( IList items )
+        {
+            Hashtable repositories = new Hashtable();
+            foreach( SvnItem item in items )
+            {
+                string uuid = item.Status.Entry.Uuid;
+                if ( !repositories.ContainsKey(uuid) )
+                {
+                    repositories.Add( uuid, new ArrayList() ); 
+                }
+                ((IList)repositories[uuid]).Add( item );
+            }
+
+            return repositories.Values;
         }
 
         private string[] paths;
