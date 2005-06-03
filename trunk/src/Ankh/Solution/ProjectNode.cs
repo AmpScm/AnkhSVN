c9b99dd4f1ab8a1e61bb876e4bb24608c87fc755
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2005-06-03 04:26:34 +0000

    Make sure "web projects" in VS 2005 get treated right - they don't have a project file and are represented solely through their directories. Make ProjectNode deal with this scenario.
    * \src\Ankh\Solution\ProjectNode.cs
    	(FindProjectResources) - Check for the case of Project.FullName representing a directory, and 
    	assume it's a web project.

--- trunk/src/Ankh/Solution/ProjectNode.cs
+++ trunk/src/Ankh/Solution/ProjectNode.cs
@@ -61,26 +61,36 @@ namespace Ankh.Solution
             this.additionalResources = new ArrayList();
 
             // special treatment for VDs
-            if ( String.Compare( project.Kind, ProjectNode.VDPROJKIND, true ) == 0 )
+            if (String.Compare(project.Kind, ProjectNode.VDPROJKIND, true) == 0)
                 fullname += ".vdproj";
 
+            StatusChanged del = new StatusChanged(this.ChildOrResourceChanged);
             // the Solution Items project has no path
-            if ( fullname != string.Empty && File.Exists( fullname ) )
+            if (fullname != string.Empty && File.Exists(fullname))
             {
-                string parentPath = Path.GetDirectoryName( fullname );
-                this.projectFolder = this.Explorer.Context.StatusCache[ parentPath ];                
-                this.projectFile = this.Explorer.Context.StatusCache[ fullname ];
+                string parentPath = Path.GetDirectoryName(fullname);
+                this.projectFolder = this.Explorer.Context.StatusCache[parentPath];
+                this.projectFile = this.Explorer.Context.StatusCache[fullname];
 
-                this.Explorer.AddResource( project, this, fullname ); 
+                this.Explorer.AddResource(project, this, fullname);
 
-                // attach event handlers
-                StatusChanged del = new StatusChanged( this.ChildOrResourceChanged );
+                // attach event handlers                
                 this.projectFolder.Changed += del;
-                this.projectFile.Changed += del;  
-                
+                this.projectFile.Changed += del;
+
                 // we also want deleted items in this folder
-                this.AddDeletions( this.projectFolder.Path, 
-                    this.additionalResources, del );
+                this.AddDeletions(this.projectFolder.Path,
+                    this.additionalResources, del);
+            }
+            // web projects in VS 2005 have no project files
+            else if (System.IO.Directory.Exists(fullname))
+            {
+                this.projectFolder = this.Explorer.Context.StatusCache[fullname];
+                this.projectFile = SvnItem.Unversionable;
+                this.Explorer.AddResource(project, this, fullname);
+
+                this.projectFolder.Changed += del;
+                this.AddDeletions(this.projectFolder.Path, this.additionalResources, del);
             }
             else
             {
@@ -104,11 +114,12 @@ namespace Ankh.Solution
         /// </summary>
         /// <returns></returns>
         protected override NodeStatus ThisNodeStatus()
-        {            
+        {
             // check status on the project folder
-            return this.MergeStatuses( 
-                this.MergeStatuses(this.projectFolder, this.projectFile), 
-                this.MergeStatuses(this.additionalResources) );
+            return this.MergeStatuses(
+                this.MergeStatuses(this.projectFolder, this.projectFile),
+                this.MergeStatuses(this.additionalResources));
+
                
         }                    
 
