2a8ba6bad07db2e82c5454df1a1814ff87eefe06
Sander Rijken <1292085+srijken@users.noreply.github.com>
2005-06-13 22:34:41 +0000

    Implement the UI for the lock command.
    * /src/Ankh.UI/Ankh.UI.2003.csproj
    	Add LockDialog.cs
    * /src/Ankh.UI/LockDialog.cs
    * /src/Ankh.UI/LockDialog.resx
    	Add the actual dialog
    * /src/Ankh/Ankh.2003.csproj
    	Add Commands/LockCommand.cs and LockDialogInfo.cs
    * /src/Ankh/Commands/LockCommand.cs
    	Add a new file implementing the lock command.
    * /src/Ankh/LockDialogInfo.cs
    	Add new file
    * /src/Ankh/IUIShell.cs
    * /src/Ankh/UIShell.cs
    	(ShowLockDialog) - Add new method that creates the LockDialog

---
+++ src/Ankh/Commands/LockCommand.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using NSvn.Core;
+
+namespace Ankh.Commands
+{
+    /// <summary>
+    /// A command class to support the svn lock command
+    /// </summary>
+    [VSNetCommand("Lock", Text = "Lock...", Tooltip = "Locks the selected item",
+         Bitmap = ResourceBitmaps.Default),
+    VSNetProjectItemControl( "Ankh", Position = 1 ),
+    VSNetProjectNodeControl( "Ankh", Position = 1 ),
+    VSNetControl( "Solution.Ankh", Position = 1)]    
+    public class LockCommand : CommandBase
+	{
+        public override void Execute(IContext context, string parameters)
+        {
+            IList resources = context.SolutionExplorer.GetSelectionResources(true, 
+                new ResourceFilterCallback( CommandBase.VersionedFilter) );
+
+            this.info = new LockDialogInfo( resources, resources );
+            
+            // is Shift down?
+            if ( !CommandBase.Shift )
+            {
+                this.info = context.UIShell.ShowLockDialog( this.info );
+                if( this.info == null)
+                    return;
+            }
+
+            context.UIShell.RunWithProgressDialog( new SimpleProgressWorker( 
+                new SimpleProgressWorkerCallback( this.ProgressCallback ) ), "Locking files" );
+        }
+
+        private void ProgressCallback( IContext context )
+        {
+            string[] paths = SvnItem.GetPaths( info.CheckedItems );
+            
+            context.Client.Lock( paths, this.info.Message, this.info.StealLocks );
+        }
+
+        public override EnvDTE.vsCommandStatus QueryStatus(IContext context)
+        {
+            return Enabled;
+        }
+        
+        private LockDialogInfo info;
+	}
+}
