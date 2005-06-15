e1cfd129430d154d57e572f82c392b7afc534c9f
Arild Fines <63673+ArildF@users.noreply.github.com>
2005-06-15 12:27:03 +0000

    * \src\NSvn.Core.Tests.MCpp\Stdafx.h 	Added comment describing why the #if _MSC_VER test is necessary.

--- src/NSvn.Core.Tests.MCpp/Stdafx.h
+++ src/NSvn.Core.Tests.MCpp/Stdafx.h
@@ -7,6 +7,8 @@
 
 #using <mscorlib.dll>
 #using <NSvn.Common.dll>
+
+// In VS2003, we add a reference to the project, in 2002, the #using is needed
 #if _MSC_VER < 1310
 #using <NUnit.Framework.dll>
 #endif
