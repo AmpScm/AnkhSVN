351efc11768d1d9f93d73ac048627c5b6e0cf46e
Arild Fines <63673+ArildF@users.noreply.github.com>
2005-06-20 15:16:50 +0000

    Add an Unlock command.
    * \src\Ankh\Ankh.2003.csproj
    	Add UnlockCommand.cs
    
    * \src\Ankh\Commands\UnlockCommand.cs
    	New command class.

---
+++ src/Ankh/Commands/UnlockCommand.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+
+namespace Ankh.Commands
+{
+    [VSNetCommand("Unlock", Text = "Unlock...", Tooltip = "Unlocks the selected item(s)",
+         Bitmap = ResourceBitmaps.Default),
+    VSNetProjectItemControl( "Ankh", Position = 1 ),
+    VSNetProjectNodeControl( "Ankh", Position = 1 ),
+    VSNetControl( "Solution.Ankh", Position = 1)] 
+	public class UnlockCommand : CommandBase
+	{
+        public override EnvDTE.vsCommandStatus QueryStatus(IContext context)
+        {
+            IList resources = context.SolutionExplorer.GetSelectionResources(true, 
+                new ResourceFilterCallback( CommandBase.LockedFilter ) );
+            return resources.Count > 0 ? CommandBase.Enabled : CommandBase.Disabled;
+        }
+
+        public override void Execute(IContext context, string parameters)
+        {
+            IList resources = context.SolutionExplorer.GetSelectionResources(true, 
+                new ResourceFilterCallback( CommandBase.LockedFilter ) );
+
+            this.info = new PathSelectorInfo( "Unlock files", resources, resources );
+
+            if ( !CommandBase.Shift )
+            {
+                this.info = context.UIShell.ShowPathSelector( this.info );
+
+                if ( this.info == null )
+                    return;
+            }
+
+            context.UIShell.RunWithProgressDialog( new SimpleProgressWorker(
+                new SimpleProgressWorkerCallback( this.DoUnlock )), "Unlocking files" );
+            
+            foreach( SvnItem item in this.info.CheckedItems )
+                item.Refresh( context.Client );
+        }
+
+        private void DoUnlock( IContext context )
+        {
+            string[] paths = SvnItem.GetPaths( this.info.CheckedItems );
+            context.Client.Unlock( paths, false );
+        }
+
+        private PathSelectorInfo info;
+
+	}
+}
