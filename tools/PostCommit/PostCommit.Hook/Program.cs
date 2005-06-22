0e96ee425206236affbc0fce32caff1afe45b6d9
Arild Fines <63673+ArildF@users.noreply.github.com>
2005-06-22 03:29:04 +0000

    Created initial solution layout for a distributed app that will deliver post commit messages to ankhbot.

---
+++ tools/PostCommit/PostCommit.Hook/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostCommit.Hook
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+        }
+    }
+}
