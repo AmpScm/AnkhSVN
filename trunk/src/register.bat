2ce032a5bd3837f9b0d12c5237312036362212c5
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2005-07-06 14:39:21 +0000

    Patch from Yury Korolev: Synhronize 2003 and 2005 projects
    * /Ankh/Ankh.2005.csproj
    	Added CommitOperation.cs, DteUtils.cs, VSNetProjectItemControlAttribute.cs
    
    * /Ankh.Tests/Ankh.Tests.2005.csproj
    	Added LockDialog.cs, NewDirectoryDialog.cs, LockDialog.resx, NewDirectoryDialog.resx
    
    * /InstallLib/InstallLib.2005.vcproj
    	Removed customdetails.h, ReadMe.txt
    
    * /NSvn.Core.Tests/NSvn.Core.Tests.2005.csproj
    	Added LockTest.cs, UnlockTest.cs

--- trunk/src/register.bat
+++ trunk/src/register.bat
@@ -1,3 +1,3 @@
-regedit /s N:\removeankh.reg
-regedit /s N:\ankh.reg 
-start "bleh" "I:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.exe"
+regedit /s D:\src\removeankh.reg
+regedit /s D:\src\ankh.reg 
+start "bleh" "C:\Programfiler\Microsoft Visual Studio 8\Common7\IDE\devenv.exe"
