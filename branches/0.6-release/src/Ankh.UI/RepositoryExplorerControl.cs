f1100441479d68fede6bc7d4c5c5398f542c8922
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2005-07-16 13:34:36 +0000

    Branch for 0.6 release

---
+++ branches/0.6-release/src/Ankh.UI/RepositoryExplorerControl.cs
@@ -0,0 +1,383 @@
+// $Id$
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Windows.Forms;
+using NSvn.Core;
+using Utils;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using Utils.Win32;
+using System.Reflection;
+
+namespace Ankh.UI
+{
+    
+    /// <summary>
+    /// Gives a tree view of the repository based on revision.
+    /// </summary>
+    public class RepositoryExplorerControl : System.Windows.Forms.UserControl
+    {
+        public RepositoryExplorerControl()
+        {
+            // This call is required by the Windows.Forms Form Designer.
+            InitializeComponent();
+ 
+
+            this.components = new Container();
+
+            this.AddToolBarImages();
+           
+        }
+
+        /// <summary>
+        /// The command bar to be used for a context menu.
+        /// </summary>
+        public object CommandBar
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get{ return this.commandBar; }
+
+            [System.Diagnostics.DebuggerStepThrough]
+            set{ this.commandBar = value; }
+        }     
+
+        
+        
+        /// <summary>
+        /// Fired if the background listing checkbox' state is changed.
+        /// </summary>
+        public event EventHandler EnableBackgroundListingChanged;
+
+        /// <summary>
+        /// Fired when the selection changes.
+        /// </summary>
+        public event EventHandler SelectionChanged;
+
+        /// <summary>
+        /// Fired whenever a directory node is expanded.
+        /// </summary>
+        public event NodeExpandingDelegate NodeExpanding;
+
+        
+
+        /// <summary>
+        /// The selected node. Will be null if there is no selection.
+        /// </summary>
+        public IRepositoryTreeNode SelectedNode
+        {
+            get
+            { 
+                return 
+                    this.treeView.SelectedNode != null ? 
+                    (IRepositoryTreeNode)this.treeView.SelectedNode.Tag :
+                    null;
+            }
+        }
+
+        /// <summary>
+        /// The current roots in the treeview.
+        /// </summary>
+        public IRepositoryTreeNode[] Roots
+        {
+            get
+            {
+                ArrayList list = new ArrayList();
+                foreach( TreeNode node in this.treeView.Nodes )
+                    list.Add( node.Tag );
+                return (IRepositoryTreeNode[])
+                    list.ToArray(typeof(IRepositoryTreeNode));
+            }
+        }
+
+        /// <summary>
+        /// Whether the "Enable background listing" checkbox is checked.
+        /// </summary>
+        public bool EnableBackgroundListing
+        {
+            get{ return this.enableBackgroundListingButton.Pushed; }
+        }
+
+        /// <summary>
+        /// Add a new URL root to the tree.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="node"></param>
+        public void AddRoot( string text, IRepositoryTreeNode node)
+        {
+            if ( !node.IsDirectory )
+                throw new ArgumentException( "The root needs to be a directory.", "node" );
+
+            this.treeView.AddRoot( node, text );
+        }
+
+        /// <summary>
+        /// Removes the root of a given node.
+        /// </summary>
+        /// <param name="node"></param>
+        public void RemoveRoot( IRepositoryTreeNode node )
+        {
+            TreeNode root = (TreeNode)node.Tag;
+            while( root.Parent != null )
+                root = root.Parent;
+            root.Remove();
+        }
+
+        /// <summary>
+        /// Refreshes the node.
+        /// </summary>
+        /// <param name="node"></param>
+        public void RefreshNode( IRepositoryTreeNode node )
+        {
+            this.treeView.RefreshNode( node );
+        }
+
+        /// <summary>
+        /// Start the "create a new dir" operation under the current node.
+        /// </summary>
+        /// <param name="handler"></param>
+        public void MakeDir( INewDirectoryHandler handler )
+        {
+            if ( this.treeView.SelectedNode == null )
+                return;
+
+            // store the handler so we have it in the callback
+            this.newDirHandler = handler;
+            this.treeView.AfterLabelEdit += new NodeLabelEditEventHandler(MakeDirAfterEdit);
+
+            // create a new node.
+            TreeNode node = new TreeNode( "Newdir" );
+            node.SelectedImageIndex = this.treeView.ClosedFolderIndex;
+            node.ImageIndex = this.treeView.ClosedFolderIndex;
+            this.treeView.SelectedNode.Nodes.Add( node );
+
+            // start editing it
+            this.treeView.LabelEdit = true;
+            node.BeginEdit();
+        }
+
+        private void AddToolBarImages()
+        {
+            this.toolbarImageList.Images.Add(
+                new Icon( this.GetType().Assembly.GetManifestResourceStream(
+                "Ankh.UI.Graphics.EnableBackgroundListing.ico" ) ) );
+        }
+
+        
+
+        /// <summary>
+        /// This will be called when the user finishes editing the new node.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MakeDirAfterEdit(object sender, NodeLabelEditEventArgs e)
+        {
+            // disconnect the event handler - we don't want it fired more than once.
+            this.treeView.AfterLabelEdit -= new NodeLabelEditEventHandler(
+                this.MakeDirAfterEdit );
+            this.treeView.LabelEdit = false;
+
+            bool cancel = true;
+            try
+            {
+                // did the user actually enter a name?
+                if ( this.newDirHandler != null && e.Label != null )
+                {  
+                    // create the new dir and refresh the parent dir.
+                    cancel = !this.newDirHandler.MakeDir(
+                        (IRepositoryTreeNode)e.Node.Parent.Tag, e.Label );
+                    if ( !cancel )
+                        this.RefreshNode( (IRepositoryTreeNode)e.Node.Parent.Tag );
+                }
+                // if the user cancelled, just get rid of the new node.
+                if ( cancel ) 
+                    e.Node.Remove();
+            }
+            finally
+            {
+                this.newDirHandler = null;
+                e.CancelEdit = cancel;
+            }
+        }
+
+
+
+        /// <summary>
+        /// The user wants to expand a node. Let him, but we have to list whats under it
+        /// first.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            // don't bother going to the server unless it's the dummy child
+            if ( e.Node.Nodes.Count > 0 &&
+                e.Node.Nodes[0].Tag != RepositoryTreeView.DUMMY_NODE )
+                return;
+
+            // now see if any event handlers want to provide the children
+            IRepositoryTreeNode node = (IRepositoryTreeNode)e.Node.Tag;
+            NodeExpandingEventArgs args = new NodeExpandingEventArgs( node );
+
+            try
+            {
+                if ( this.NodeExpanding != null )
+                    this.NodeExpanding( this, args );
+                this.treeView.AddChildren( node, args.Children );
+            }
+            catch( SvnClientException )
+            {
+                // don't open it - the user can click on the plus to try again
+                e.Cancel = true;
+            }
+        }
+        
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing )
+            {
+                if(components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose( disposing );
+        }	
+        
+
+        private void TreeViewMouseDown( object sender, MouseEventArgs args )
+        {
+            Debug.Assert( this.commandBar != null, "commandBar is null" );
+            
+            if ( args.Button != MouseButtons.Right )
+                return;
+
+            // make sure right click causes a selection
+            this.treeView.SelectedNode = this.treeView.GetNodeAt( args.X, args.Y );
+
+            Point screen = this.treeView.PointToScreen( new Point(args.X, args.Y) );
+            this.commandBar.GetType().InvokeMember(
+                "ShowPopup", BindingFlags.InvokeMethod, null, 
+                this.commandBar, new object[]{ screen.X, screen.Y } );
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if ( this.SelectionChanged != null )
+                this.SelectionChanged( this, EventArgs.Empty );
+        }
+
+        private void EnableBackgroundListingClicked(object sender, 
+            System.Windows.Forms.ToolBarButtonClickEventArgs e)
+        {
+            Debug.Assert( e.Button == this.enableBackgroundListingButton, 
+                "Need to change this event handler when we get more buttons" );
+            if ( this.EnableBackgroundListingChanged != null )
+                this.EnableBackgroundListingChanged( this, EventArgs.Empty );
+        }
+
+
+
+        #region Component Designer generated code
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundListingCheck = new System.Windows.Forms.CheckBox();
+            this.treeView = new Ankh.UI.RepositoryTreeView();
+            this.toolBar = new System.Windows.Forms.ToolBar();
+            this.enableBackgroundListingButton = new System.Windows.Forms.ToolBarButton();
+            this.toolbarImageList = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // backgroundListingCheck
+            // 
+            this.backgroundListingCheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.backgroundListingCheck.Location = new System.Drawing.Point(80, 160);
+            this.backgroundListingCheck.Name = "backgroundListingCheck";
+            this.backgroundListingCheck.Size = new System.Drawing.Size(184, 16);
+            this.backgroundListingCheck.TabIndex = 6;
+            this.backgroundListingCheck.Text = "Enable background listing";
+            // 
+            // treeView
+            // 
+            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+                | System.Windows.Forms.AnchorStyles.Left) 
+                | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView.ImageIndex = -1;
+            this.treeView.Location = new System.Drawing.Point(0, 28);
+            this.treeView.Name = "treeView";
+            this.treeView.SelectedImageIndex = -1;
+            this.treeView.Size = new System.Drawing.Size(296, 268);
+            this.treeView.TabIndex = 9;
+            this.treeView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TreeViewMouseDown);
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            this.treeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_BeforeExpand);
+            // 
+            // toolBar
+            // 
+            this.toolBar.Appearance = System.Windows.Forms.ToolBarAppearance.Flat;
+            this.toolBar.Buttons.AddRange(new System.Windows.Forms.ToolBarButton[] {
+                                                                                       this.enableBackgroundListingButton});
+            this.toolBar.ButtonSize = new System.Drawing.Size(16, 16);
+            this.toolBar.DropDownArrows = true;
+            this.toolBar.ImageList = this.toolbarImageList;
+            this.toolBar.Location = new System.Drawing.Point(0, 0);
+            this.toolBar.Name = "toolBar";
+            this.toolBar.ShowToolTips = true;
+            this.toolBar.Size = new System.Drawing.Size(296, 28);
+            this.toolBar.TabIndex = 10;
+            this.toolBar.ButtonClick += new System.Windows.Forms.ToolBarButtonClickEventHandler(this.EnableBackgroundListingClicked);
+            // 
+            // enableBackgroundListingButton
+            // 
+            this.enableBackgroundListingButton.ImageIndex = 0;
+            this.enableBackgroundListingButton.Style = System.Windows.Forms.ToolBarButtonStyle.ToggleButton;
+            this.enableBackgroundListingButton.ToolTipText = "Enable background listing";
+            // 
+            // toolbarImageList
+            // 
+            this.toolbarImageList.ImageSize = new System.Drawing.Size(16, 16);
+            this.toolbarImageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // RepositoryExplorerControl
+            // 
+            this.Controls.Add(this.toolBar);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.backgroundListingCheck);
+            this.Name = "RepositoryExplorerControl";
+            this.Size = new System.Drawing.Size(296, 296);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private object commandBar;
+        private System.Windows.Forms.CheckBox backgroundListingCheck;
+        private Ankh.UI.RepositoryTreeView treeView;
+        private System.ComponentModel.IContainer components;
+        private System.Windows.Forms.ToolBar toolBar;
+        private System.Windows.Forms.ToolBarButton enableBackgroundListingButton;
+        private System.Windows.Forms.ImageList toolbarImageList;
+
+        private INewDirectoryHandler newDirHandler;
+
+        
+    }
+
+    /// <summary>
+    /// An object that handles the actual creation of a directory.
+    /// </summary>
+    public interface INewDirectoryHandler
+    {
+        bool MakeDir( IRepositoryTreeNode parent, string dirname );
+    }
+}
