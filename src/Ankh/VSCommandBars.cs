65d75570bf736d2f1d1f5aef100c333ab8f3001d
Sander Rijken <1292085+srijken@users.noreply.github.com>
2005-12-17 15:25:28 +0000

    Fix the icons on commandBars in VS2005
    * /src/Ankh/1033/BitmapDll.dll
    	Replace the (managed) dll with an unmanaged one containing the same resources.
    * /src/Ankh/VSCommandBars.cs
    	(VSCommandBars2005.AddNamedCommand) - Call AddNamedCommand2 with the correct parameters

--- src/Ankh/VSCommandBars.cs
+++ src/Ankh/VSCommandBars.cs
@@ -124,8 +124,8 @@ namespace Ankh
 
             public override Command AddNamedCommand(string commandName, string text, string tooltip, int bitmapId, int status)
             {
-                ParameterModifier pm = new ParameterModifier( 8 );
-                for ( int i = 0; i < 8; i++ )
+                ParameterModifier pm = new ParameterModifier( 7 );
+                for ( int i = 0; i < 7; i++ )
                     pm[i] = false;
 
                 // the 7th argument is a ref parameter
@@ -138,8 +138,8 @@ namespace Ankh
                                                           commandName, 
                                                           text, 
                                                           tooltip, 
-                                                          true,
-                                                          null,
+                                                          false,
+                                                          bitmapId,
                                                           contextGuids
                                                       }, 
                     new ParameterModifier[]{pm}, null, null);
