edf2159a13a585331c19453760e4df5714ec602e
Arild Fines <63673+ArildF@users.noreply.github.com>
2005-12-28 04:29:46 +0000

    Merge all revisions from the extension branch (2234-2295) to the trunk

--- src/Ankh/AnkhContext.cs
+++ src/Ankh/AnkhContext.cs
@@ -12,6 +12,7 @@ using Ankh.Solution;
 using System.Collections;
 using System.IO;
 using System.Diagnostics;
+using System.ComponentModel;
 
 namespace Ankh
 {
@@ -254,7 +255,16 @@ namespace Ankh
         public VSCommandBars CommandBars
         {
             [System.Diagnostics.DebuggerStepThrough]
-            get{ return this.commandBars; }
+            get { return this.commandBars; }
+        }
+
+        public IServiceProvider ServiceProvider
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get 
+            {
+                return this.dte as IServiceProvider;
+            }
         }
 
 
@@ -262,37 +272,16 @@ namespace Ankh
         /// Event handler for the SolutionOpened event. Can also be called at
         /// addin load time, or if Ankh is enabled for a solution.
         /// </summary>
-        public void SolutionOpened()
+        public bool EnableAnkhForLoadedSolution()
         {
-            try
+            if ( this.SolutionLoaded() )
             {
-                if ( !this.CheckWhetherAnkhShouldLoad() )
-                    return;
-
-                System.Diagnostics.Trace.WriteLine( "Solution opening", "Ankh" );
-
-                Utils.DebugTimer timer = DebugTimer.Start();
-                
-
-                this.statusCache = new StatusCache( this.Client );
-
-                this.solutionExplorer.Load();
-                this.eventSinks = EventSinks.EventSink.CreateEventSinks( this );
-
-                timer.End( "Solution opened", "Ankh" );
-                
-                //MessageBox.Show( timer.ToString() );
-
-                // Add Conflict tasks for all conflicts in solution
-                this.conflictManager.CreateTaskItems(); 
+                this.solutionEvents.InitializeForLoadedSolution();
+                return true;
             }
-            catch( Exception ex )
+            else
             {
-                ErrorHandler.Handle( ex );
-            }
-            finally
-            {
-                this.EndOperation();   
+                return false;
             }
         }
 
@@ -306,13 +295,7 @@ namespace Ankh
             this.conflictManager.RemoveAllTaskItems();
             this.SolutionExplorer.Unload();
 
-            // unhook events.
-            if ( this.eventSinks != null )
-            {
-                foreach( EventSinks.EventSink sink in this.eventSinks )
-                    sink.Unhook();
-            }
-
+            
         }
 
         /// <summary>
@@ -376,6 +359,10 @@ namespace Ankh
         {  
             if ( this.Unloading != null )
                 this.Unloading( this, EventArgs.Empty );
+
+            if ( this.solutionEvents != null )
+                this.solutionEvents.Unhook();
+
         }
 
 
@@ -385,15 +372,60 @@ namespace Ankh
         /// </summary>
         private void SetUpEvents()
         {
-            // apparently necessary to avoid the SolutionEvents object being
-            // gc'd :-/
-            this.solutionEvents = this.DTE.Events.SolutionEvents;
-            this.solutionEvents.Opened += new 
-                _dispSolutionEvents_OpenedEventHandler( this.SolutionOpened );
-            this.solutionEvents.BeforeClosing += new 
-                _dispSolutionEvents_BeforeClosingEventHandler( this.SolutionClosing);
+            this.solutionEvents = new Ankh.EventSinks.SolutionEventsSink( this );
+            this.solutionEvents.SolutionLoaded += new CancelEventHandler( this.HandleSolutionLoaded );
+            this.solutionEvents.SolutionBeforeClosing  += new EventHandler( this.HandleSolutionClosing );
         }
         #endregion        
+
+        private void HandleSolutionLoaded( object sender, System.ComponentModel.CancelEventArgs args )
+        {
+            args.Cancel = !SolutionLoaded();
+        }
+
+        private bool SolutionLoaded( )
+        {
+            try
+            {
+                if ( !this.CheckWhetherAnkhShouldLoad() )
+                {
+                    return false;
+                }
+
+
+                System.Diagnostics.Trace.WriteLine( "Solution opening", "Ankh" );
+
+                Utils.DebugTimer timer = DebugTimer.Start();
+
+
+                this.statusCache = new StatusCache( this.Client );
+
+                this.solutionExplorer.Load();
+
+                timer.End( "Solution opened", "Ankh" );
+
+                //MessageBox.Show( timer.ToString() );
+
+                // Add Conflict tasks for all conflicts in solution
+                this.conflictManager.CreateTaskItems();
+
+                return true;
+            }
+            catch ( Exception ex )
+            {
+                ErrorHandler.Handle( ex );
+                return false;
+            }
+            finally
+            {
+                this.EndOperation();
+            }
+        }
+
+        private void HandleSolutionClosing( object sender, EventArgs args )
+        {
+            this.SolutionClosing();
+        }
 
         private void HandleSolutionFinishedLoading( object sender, EventArgs args )
         {
@@ -529,14 +561,12 @@ namespace Ankh
         private EnvDTE.AddIn addin;
         private IWin32Window hostWindow;
 
-        private IList eventSinks;
-
         private RepositoryExplorer.Controller repositoryController;
 
         private OutputPaneWriter outputPane;
 
         //required to ensure events will still fire
-        private SolutionEvents solutionEvents;
+        private Ankh.EventSinks.SolutionEventsSink solutionEvents;
         private Explorer solutionExplorer = null;
 
         private Ankh.Config.Config config;
@@ -559,5 +589,11 @@ namespace Ankh
         private Ankh.Config.ConfigLoader configLoader;
 
         private VSCommandBars commandBars;
-    }
+
+        #region IContext Members
+
+        
+
+        #endregion
+}
 }
--- src/Ankh/EventSinks/SolutionEventsSink.cs
+++ src/Ankh/EventSinks/SolutionEventsSink.cs
@@ -1,84 +1,194 @@
 // $Id$
 using System;
 using EnvDTE;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
+using System.ComponentModel;
 
 namespace Ankh.EventSinks
 {
     /// <summary>
     /// Event sink for solution related events.
     /// </summary>
-    public class SolutionEventsSink : ItemEventSink
+    public class SolutionEventsSink : EventSink, IVsSolutionEvents
     {
-        public SolutionEventsSink( IContext context ) : base( context )
+        public event CancelEventHandler SolutionLoaded;
+        public event EventHandler SolutionBeforeClosing;
+
+        public SolutionEventsSink( IContext context )
+            : base( context )
         {
-            this.solutionEvents = context.DTE.Events.SolutionEvents;
-            this.solutionEvents.ProjectAdded += new _dispSolutionEvents_ProjectAddedEventHandler(
-                this.ProjectAdded );
-            this.solutionEvents.ProjectRemoved += new _dispSolutionEvents_ProjectRemovedEventHandler(
-                this.ProjectRemoved );
+            this.hierarchyEvents = new HierarchyEventsSink( this.Context );
+            this.AdviseSolutionEvents();
 
-            this.solutionItemsEvents = this.Context.DTE.Events.SolutionItemsEvents;
-            this.solutionItemsEvents.ItemAdded += new _dispProjectItemsEvents_ItemAddedEventHandler(
-                this.ItemAdded );
-            this.solutionItemsEvents.ItemRemoved += new _dispProjectItemsEvents_ItemRemovedEventHandler(
-                this.ItemRemoved );
-            this.solutionItemsEvents.ItemRenamed += new _dispProjectItemsEvents_ItemRenamedEventHandler(
-                this.ItemRenamed );
+        }
 
-            this.miscFilesEvents = this.Context.DTE.Events.MiscFilesEvents;
-            this.miscFilesEvents.ItemAdded += new _dispProjectItemsEvents_ItemAddedEventHandler(
-                this.ItemAdded );
-            this.miscFilesEvents.ItemRemoved += new _dispProjectItemsEvents_ItemRemovedEventHandler(
-                this.ItemRemoved );
-            this.miscFilesEvents.ItemRenamed += new _dispProjectItemsEvents_ItemRenamedEventHandler(
-                this.ItemRenamed );
+        private void AdviseSolutionEvents()
+        {
+            IServiceProvider sp = this.Context.ServiceProvider;
+            Guid serviceGuid = typeof(SVsSolution).GUID;
+            Guid interfaceGuid = typeof(IVsSolution).GUID;
 
-
+            IntPtr ptr;
+            Marshal.ThrowExceptionForHR(sp.QueryService( ref serviceGuid, ref interfaceGuid, out ptr ));
+            this.solution = (IVsSolution)Marshal.GetObjectForIUnknown( ptr );
+            Marshal.ThrowExceptionForHR( this.solution.AdviseSolutionEvents( this, out this.cookie ) );
         }
 
         public override void Unhook()
         {
-            this.solutionEvents.ProjectAdded -= new _dispSolutionEvents_ProjectAddedEventHandler(
-                this.ProjectAdded );
-            this.solutionEvents.ProjectRemoved -= new _dispSolutionEvents_ProjectRemovedEventHandler(
-                this.ProjectRemoved );
-            this.solutionItemsEvents.ItemAdded -= new _dispProjectItemsEvents_ItemAddedEventHandler(
-                this.ItemAdded );
-            this.solutionItemsEvents.ItemRemoved -= new _dispProjectItemsEvents_ItemRemovedEventHandler(
-                this.ItemRemoved );
-            this.solutionItemsEvents.ItemRenamed -= new _dispProjectItemsEvents_ItemRenamedEventHandler(
-                this.ItemRenamed );
-
-            this.miscFilesEvents.ItemAdded -= new _dispProjectItemsEvents_ItemAddedEventHandler(
-                this.ItemAdded );
-            this.miscFilesEvents.ItemRemoved -= new _dispProjectItemsEvents_ItemRemovedEventHandler(
-                this.ItemRemoved );
-            this.miscFilesEvents.ItemRenamed -= new _dispProjectItemsEvents_ItemRenamedEventHandler(
-                this.ItemRenamed );
+            this.UnhookEventsForSolution();
+            this.solution.UnadviseSolutionEvents( this.cookie );
         }
 
-        protected void ProjectAdded( Project project )
+        public void InitializeForLoadedSolution()
         {
+            this.SetupEventsForSolution();
+        }
+
+        #region IVsSolutionEvents Members
+
+        int IVsSolutionEvents.OnAfterOpenProject( IVsHierarchy pHierarchy, int fAdded )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnAfterOpenProject() of {0}", this.ToString() ) );
+
             try
             {
-                this.Context.SolutionExplorer.SyncAll();
-            }   
-            catch( Exception ex )
+                if ( this.Context.AnkhLoadedForSolution )
+                    this.hierarchyEvents.AddHierarchy( pHierarchy );
+            }
+            catch ( Exception ex )
             {
                 this.Context.ErrorHandler.Handle( ex );
             }
-            finally
+
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnQueryCloseProject( IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnQueryCloseProject() of {0}", this.ToString() ) );
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnBeforeCloseProject( IVsHierarchy pHierarchy, int fRemoved )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnBeforeCloseProject() of {0}", this.ToString() ) );
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnAfterLoadProject( IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnAfterLoadProject() of {0}", this.ToString() ) );
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnQueryUnloadProject( IVsHierarchy pRealHierarchy, ref int pfCancel )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnQueryUnloadProject() of {0}", this.ToString() ) );
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnBeforeUnloadProject( IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnBeforeUnloadProject() of {0}", this.ToString() ) );
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnAfterOpenSolution( object pUnkReserved, int fNewSolution )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnAfterOpenSolution() of {0}", this.ToString() ) );
+            CancelEventArgs args = new CancelEventArgs( true );
+
+            try
             {
-                EventSink.AddingProject = false;
+                if ( this.SolutionLoaded != null )
+                    this.SolutionLoaded( this, args );
+                if ( !args.Cancel )
+                {
+                    this.SetupEventsForSolution();
+                }
+            }
+            catch ( Exception ex )
+            {
+                this.Context.ErrorHandler.Handle( ex );
+                return VSConstants.E_FAIL;
+            }
+            
+            return VSConstants.S_OK;
+        }
+
+       
+
+        int IVsSolutionEvents.OnQueryCloseSolution( object pUnkReserved, ref int pfCancel )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnQueryCloseSolution() of {0}", this.ToString() ) );
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnBeforeCloseSolution( object pUnkReserved )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnBeforeCloseSolution() of {0}", this.ToString() ) );
+
+            try
+            {
+                this.UnhookEventsForSolution();
+
+                if ( this.SolutionBeforeClosing != null )
+                    this.SolutionBeforeClosing( this, EventArgs.Empty );
+            }
+            catch ( Exception ex )
+            {
+                this.Context.ErrorHandler.Handle( ex );
+                return VSConstants.E_FAIL;
+            }
+            return VSConstants.S_OK;
+        }
+
+        int IVsSolutionEvents.OnAfterCloseSolution( object pUnkReserved )
+        {
+            Trace.WriteLine( string.Format( CultureInfo.CurrentCulture, "Entering OnAfterCloseSolution() of {0}", this.ToString() ) );
+
+            return VSConstants.S_OK;
+        }
+
+       
+
+        #endregion
+
+        private void SetupEventsForSolution()
+        {
+            this.hierarchyEvents = new HierarchyEventsSink( this.Context );
+
+            this.eventSinks = new ArrayList();
+
+            this.eventSinks.Add( new TrackProjectDocumentsEventSink( this.Context ) );
+            this.eventSinks.Add( new ProjectFilesEventSink( this.Context ) );
+            this.eventSinks.Add( new DocumentEventsSink( this.Context ) );
+            this.eventSinks.Add( new CommandsEventSink( this.Context ) );
+        }
+
+        private void UnhookEventsForSolution()
+        {
+            if ( this.Context.AnkhLoadedForSolution )
+            {
+                this.hierarchyEvents.Unhook();
+                this.hierarchyEvents = null;
+
+                foreach ( EventSink sink in this.eventSinks )
+                    sink.Unhook();
+
+                this.eventSinks = null;
             }
         }
 
-        protected void ProjectRemoved( Project project )
-        {           
-        }
+        private HierarchyEventsSink hierarchyEvents;
+        private uint cookie;
+        private IVsSolution solution;
+        private IList eventSinks;
 
-        private SolutionEvents solutionEvents;
-        private ProjectItemsEvents solutionItemsEvents;
-        private ProjectItemsEvents miscFilesEvents;
+        
     }
 }
