419c46de58f178c88e2f46617d50ae412dbab6c7
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2006-04-20 16:23:37 +0000

    Have Ankh perform a refresh of the selected project if the VS "Refresh" button is pressed.
    * \src\Ankh\EventSinks\CommandsEventSink.cs
    	(RegisterCommandEvents) - Hook up to the command event fired when that button is pressed.
    		Reuse the same event handler we use for the "Show all" button.
    	(Unhook) - Unregister from the Refresh command event.

--- trunk/src/Ankh/EventSinks/CommandsEventSink.cs
+++ trunk/src/Ankh/EventSinks/CommandsEventSink.cs
@@ -22,6 +22,8 @@ namespace Ankh.EventSinks
             {
                 this.projectShowAll.AfterExecute -= new _dispCommandEvents_AfterExecuteEventHandler(
                     this.AfterProjectShowAll );
+                this.refresh.AfterExecute -= new _dispCommandEvents_AfterExecuteEventHandler(
+                    this.AfterProjectShowAll );
             }
         }
 
@@ -32,18 +34,49 @@ namespace Ankh.EventSinks
             // Project.ShowAll
             try
             {
-                this.projectShowAll = dte.Events.get_CommandEvents( PROJECTSHOWALLGUID, 600 );
+                this.projectShowAll = dte.Events.get_CommandEvents( PROJECTSHOWALLGUID, PROJECTSHOWALLID );
                 this.projectShowAll.AfterExecute += new _dispCommandEvents_AfterExecuteEventHandler(
                     this.AfterProjectShowAll );
             }
-            catch( NullReferenceException )
+            catch ( NullReferenceException )
             {
                 // happens if Visual Assist X is installed
-                this.Context.OutputPane.WriteLine( "Unable to attach to the Project.ShowAll event." + 
+                this.Context.OutputPane.WriteLine( "Unable to attach to the Project.ShowAll event." +
                     Environment.NewLine + "Possible conflict with other installed plugin." );
             }
+            try
+            {
+
+                //this.allEvents = (EnvDTE.CommandEvents)dte.Events.get_CommandEvents("{00000000-0000-0000-0000-000000000000}", 0);
+                //this.allEvents.AfterExecute += new _dispCommandEvents_AfterExecuteEventHandler( allEvents_AfterExecute );
+
+                this.refresh = dte.Events.get_CommandEvents( REFRESHGUID, REFRESHID );
+                this.refresh.AfterExecute += new _dispCommandEvents_AfterExecuteEventHandler(
+                    this.AfterProjectShowAll );
+            }
+            catch ( NullReferenceException )
+            {
+                // happens if Visual Assist X is installed
+                this.Context.OutputPane.WriteLine( "Unable to attach to the Project.ShowAll event." +
+                    Environment.NewLine + "Possible conflict with other installed plugin." );
+
+            }
+           
             
         }
+
+        //void allEvents_AfterExecute( string Guid, int ID, object CustomIn, object CustomOut )
+        //{
+        //    string name = "";
+        //    try
+        //    {
+        //        name = this.Context.DTE.Commands.Item( Guid, ID ).Name;
+        //    }
+        //    catch ( Exception )
+        //    {
+        //    }
+        //    this.Context.OutputPane.WriteLine( "Command {0} invoked - Guid: {1}, ID: {2}", name, Guid, ID );
+        //}
 
 
         private void AfterProjectShowAll( string guid,
@@ -53,7 +86,7 @@ namespace Ankh.EventSinks
         {
             try
             {
-                // if the "Show all" button is pressed, we need to refresh the whole project
+                // if the "Show all" or "Refresh" buttons are pressed, we need to refresh the whole project
                 foreach( Project proj in (Array)this.Context.DTE.ActiveSolutionProjects )
                     this.Context.SolutionExplorer.Refresh( proj );
             }
@@ -66,7 +99,12 @@ namespace Ankh.EventSinks
 
 
         private CommandEvents projectShowAll;
+        //private CommandEvents allEvents;
+        private CommandEvents refresh;
         private const string PROJECTSHOWALLGUID = "{1496A755-94DE-11D0-8C3F-00C04FC2AAE2}";
+        private const string REFRESHGUID = "{1496A755-94DE-11D0-8C3F-00C04FC2AAE2}";
+        private const int REFRESHID = 222;
+        private const int PROJECTSHOWALLID = 600;
 
     }
 }
