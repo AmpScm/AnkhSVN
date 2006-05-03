AmpScm/AnkhSVN

commit b802bb08573cd7c9dc1b81c679f7884481e70efa

merge r2396 to r2399 to trunk
git-svn-id: https://ctf.open.collab.net/svn/repos/ankhsvn/trunk@2403 ecf7f2a6-7b29-9143-b1c6-df2d17ec8657

src/Ankh/Solution/ProjectNode.cs
@@ -53,7 +53,14 @@ namespace Ankh.Solution
                 new StatusChanged(this.ChildOrResourceChanged) );
         }
 
-
+        public override void Refresh(bool rescan)
+        {
+            if (rescan)
+            {
+                ((SolutionNode)this.Parent).Parser.Refresh();
+            }
+            base.Refresh(rescan);
+        }
 
 
         private void FindProjectResources(Explorer explorer)
@@ -126,9 +133,9 @@ namespace Ankh.Solution
             get { return this.projectFolder.Path; }
         }
 
-		/// <summary>
-		/// True if this project is supported via Visual Studio Automation
-		/// </summary>
+        /// <summary>
+        /// True if this project is supported via Visual Studio Automation
+        /// </summary>
         public bool Modeled
         {
             [System.Diagnostics.DebuggerStepThrough()]
@@ -136,17 +143,17 @@ namespace Ankh.Solution
         }
 
         /// <summary>
-		/// Name of the project
-		/// </summary>
+        /// Name of the project
+        /// </summary>
         public string Name
         {
             [System.Diagnostics.DebuggerStepThrough()]
             get{ return this.project.Name; }
         }
 
-		/// <summary>
-		/// The Visual Studio Automation project object
-		/// </summary>
+        /// <summary>
+        /// The Visual Studio Automation project object
+        /// </summary>
         public Project Project
         {
             [System.Diagnostics.DebuggerStepThrough()]

src/Ankh/Solution/ParsedSolution.cs
@@ -1,13 +1,14 @@
 using System;
 using System.IO;
 using System.Collections;
+using System.Globalization;
 
 namespace Ankh.Solution
 {
     /// <summary>
     /// Parser for Visual Studio Solution & Project files
     /// </summary>
-    public class ParsedSolution
+    public sealed class ParsedSolution
     {
         /// <summary>
         /// Constructor
@@ -22,6 +23,12 @@ namespace Ankh.Solution
             this.context=context;
             this.projects=new Hashtable();
             this.solutionFile=solutionFile;
+        }
+
+        public void Refresh()
+        {
+            this.solutionContents = null;
+            this.projects.Clear();
         }
 
         /// <summary>
@@ -77,9 +84,7 @@ namespace Ankh.Solution
         {
             ParsedSolutionItem project=this.GetProjectItems(projectName);
             if(project==null)
-            {
                 return null;
-            }
 
             foreach(ParsedSolutionItem item in project.Children)
             {
@@ -151,33 +156,33 @@ namespace Ankh.Solution
         /// <param name="reader">Open file being parsed</param>
         private void ParseDatabaseItemChildren(ParsedSolutionItem item, StreamReader reader)
         {
-            for(string line=reader.ReadLine(); line.Trim().ToLower()!="end"; line=reader.ReadLine())
+            for(string line=reader.ReadLine(); string.Compare("end", 0, line, line.Length - 3, 3, true, CultureInfo.InvariantCulture) != 0; line=reader.ReadLine())
             {
-                ParsedSolutionItem child=new ParsedSolutionItem();
-                child.Parent=item;
+                ParsedSolutionItem child = new ParsedSolutionItem(item);
 
-                if(line.Trim().ToLower().StartsWith("begin folder"))
+                if (string.Compare(DbStrings.FolderBegin, 0, line, 3, DbStrings.FolderBegin.Length, true, CultureInfo.InvariantCulture) == 0)
                 {
-                    int startIndex=line.IndexOf("=")+1;
-                    child.Name=line.Substring(startIndex, line.Length-startIndex-1).Replace("\"", "").Trim();
-                    child.FileName=Path.GetDirectoryName(item.FileName)+"\\"+child.Name+"\\";
+                    int startIndex = line.IndexOf("= \"") + 3;
+                    child.Name = line.Substring(startIndex, line.Length-startIndex-1);
+                    child.FileName = Path.Combine(Path.GetDirectoryName(item.FileName), child.Name);
                     item.Children.Add(child);
 
                     this.ParseDatabaseItemChildren(child, reader);
                 }
-                else if(line.Trim().ToLower().StartsWith("script"))
+                else if(string.Compare(DbStrings.Script, 0, line, 6, DbStrings.Script.Length, true, CultureInfo.InvariantCulture) == 0 ||
+                    string.Compare(DbStrings.Query, 0, line, 6, DbStrings.Query.Length, true, CultureInfo.InvariantCulture) == 0)
                 {
-                    int startIndex=line.IndexOf("=")+1;
-                    child.Name=line.Substring(startIndex, line.Length-startIndex-1).Replace("\"", "").Trim();
-                    child.FileName=Path.GetDirectoryName(item.FileName)+"\\"+child.Name;
+                    int startIndex=line.IndexOf("= \"") + 3;
+                    child.Name = line.Substring(startIndex, line.Length-startIndex-1);
+                    child.FileName = Path.Combine(item.FileName, child.Name);
                     item.Children.Add(child);
                 }
-                else if(line.Trim().ToLower().StartsWith("begin dbreffolder"))
+                else if (string.Compare(DbStrings.DbRefFolderBegin, 0, line, 3, DbStrings.DbRefFolderBegin.Length, true, CultureInfo.InvariantCulture) == 0)
                 {
                     //get the folder
-                    int startIndex=line.IndexOf("=")+1;
-                    child.Name=line.Substring(startIndex, line.Length-startIndex-1).Replace("\"", "").Trim();
-                    child.FileName=Path.GetDirectoryName(item.FileName)+"\\"+child.Name;
+                    int startIndex=line.IndexOf("= \"") + 3;
+                    child.Name = line.Substring(startIndex, line.Length-startIndex-1);
+                    child.FileName = Path.Combine(Path.GetDirectoryName(item.FileName), child.Name);
                     item.Children.Add(child);
 
                     //slurp db references
@@ -194,21 +199,26 @@ namespace Ankh.Solution
         /// <param name="startIndent">Number of section endings necessary to finish slurping</param>
         private void SlurpSection(StreamReader reader,int startIndent)
         {
-            int indent=startIndent;
-            while(indent>0)
+            if (startIndent < 0)
+                throw new ArgumentOutOfRangeException("startIndent");
+
+            
+            string begin = "begin ";
+
+            while(startIndent > 0)
             {
                 string line=reader.ReadLine();
-                if(line==null)
+                if(line == null)
                 {
-                    indent=-1;
+                    break;
                 }
-                else if(line.Trim().ToLower()=="end")
+                else if(string.Compare(line.Trim(), "end", true, CultureInfo.InvariantCulture) == 0)
                 {
-                    indent--;
+                    startIndent--;
                 }
-                else if(line.Trim().ToLower().StartsWith("begin "))
+                else if(string.Compare(line.Trim(), 0, begin, 0, begin.Length, true, CultureInfo.InvariantCulture) == 0)
                 {
-                    indent++;
+                    startIndent++;
                 }
             }
         }
@@ -233,6 +243,14 @@ namespace Ankh.Solution
             }
 
             this.ParseDatabaseItemChildren(project, reader);
+        }
+
+        class DbStrings
+        {
+            internal const string FolderBegin  = "begin folder";
+            internal const string Script = "script ";
+            internal const string Query = "query ";
+            internal const string DbRefFolderBegin = "begin dbreffolder";
         }
 
         private IContext context;

src/Ankh/Solution/ParsedSolutionItem.cs
@@ -16,6 +16,15 @@ namespace Ankh.Solution
             this.children=new ArrayList();
         }
 
+        public ParsedSolutionItem(ParsedSolutionItem parent)
+            : this()
+        {
+            if (parent == null)
+                throw new ArgumentNullException("parent");
+
+            this.parent = parent;
+        }
+
         /// <summary>
         /// Find the direct child with the given name
         /// </summary>
@@ -72,8 +81,8 @@ namespace Ankh.Solution
         {
             [System.Diagnostics.DebuggerStepThrough()]
             get{ return this.parent; }
-            [System.Diagnostics.DebuggerStepThrough()]
-            set{ this.parent=value; }
+            //[System.Diagnostics.DebuggerStepThrough()]
+            //set{ this.parent=value; }
         }
         
         private ArrayList children;

commit 1a864a14168d9ca1b625a97cc323a925d8e76873

Fix issue #268 by avoiding the illegal cast, introduced in r2403
* \src\Ankh\Solution\TreeNode.cs
	(Solution) - Add new property to return the SolutionNode. 
* \src\Ankh\Solution\SolutionNode.cs
	(Solution) - Override above property to make the SolutionNode return itself
* \src\Ankh\Solution\ProjectNode.cs
	(Refresh) - Use TreeNode.Solution instead of a cast to reach the SolutionNode

src/Ankh/Solution/ProjectNode.cs
@@ -66,7 +66,8 @@ namespace Ankh.Solution
         {
             if (rescan)
             {
-                ((SolutionNode)this.Parent).Parser.Refresh();
+                if (this.Solution != null)
+                    this.Solution.Parser.Refresh();
             }
             base.Refresh(rescan);
         }

src/Ankh/Solution/SolutionNode.cs
@@ -34,7 +34,13 @@ namespace Ankh.Solution
             explorer.SetSolution( this );
                 
             this.FindChildren();  
-        }   
+        }
+
+        public override SolutionNode Solution
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get { return this; }
+        }
 
         public override void Accept(INodeVisitor visitor)
         {

src/Ankh/Solution/TreeNode.cs
@@ -36,7 +36,13 @@ namespace Ankh.Solution
         {
             [System.Diagnostics.DebuggerStepThrough]
             get{ return this.explorer; }
-        }     
+        }
+
+        public virtual SolutionNode Solution
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get { return this.Parent.Solution; }
+        }
         
         public static TreeNode CreateNode( UIHierarchyItem item, IntPtr hItem,
             Explorer explorer, TreeNode parent )