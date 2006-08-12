5cc9256a3bada8e5a59641500abb1adfc42a62df
Arild <Arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2006-08-12 15:08:09 +0000

    Create branch to integrate our build process with that of Subversion 1.4. A    http://ankhsvn.com/svn/finalproject/branches/subversion-1.4-integration

---
+++ branches/subversion-1.4-integration/tools/ErrorReport/ErrorReportExtractor/IReportContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ErrorReportExtractor
+{
+    public interface IReportContainer : IService
+    {
+        IEnumerable<IErrorReport> GetItems( );
+        IEnumerable<IMailItem> GetPotentialReplies();
+    }
+}
