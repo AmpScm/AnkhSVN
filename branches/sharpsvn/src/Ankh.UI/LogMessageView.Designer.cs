f49e03edd9ba8558c43d8228ab44a918aa128006
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-02-24 18:30:34 +0000

    Mark control as Localizable=true
    * \ankh.ui\LogMessageView.Designer.cs
    * \ankh.ui\LogMessageView.resx
    	Mark Localizable=true

--- branches/sharpsvn/src/Ankh.UI/LogMessageView.Designer.cs
+++ branches/sharpsvn/src/Ankh.UI/LogMessageView.Designer.cs
@@ -28,38 +28,30 @@ namespace Ankh.UI
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogMessageView));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(0, 16);
-            this.textBox1.Multiline = true;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(403, 131);
-            this.textBox1.TabIndex = 0;
+            this.textBox1.ReadOnly = true;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(-3, 0);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Log message";
             // 
             // LogMessageView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "LogMessageView";
-            this.Size = new System.Drawing.Size(406, 150);
             this.ResumeLayout(false);
             this.PerformLayout();
 
