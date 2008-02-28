4c623bf55598e2a20a2919a9d5965493e9a0cc5a
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-02-28 16:58:06 +0000

    Added AnkhSvn and AnkhSvn.Ids projects for both VS2005 and VS2008.
    AnkhSvn is a very minimal VS package containing nothing more than a few 
    command definitions. All command definitions are compiled to a ctc file
    which is embedded in the managed resources. (MS framework).
    
    In the Gui subdirectory I added a conversion script from a much friendlier
    xml file format to define the UI structure, which reads the definitions 
    in  then new AnkhSvn.Ids assembly to get the command values used within 
    the code. (This part abstracts all the VS menu and toolbar integration 
    from the csharp code.)
    
    The .csproj file is completely altered to work with both the VS2005 
    and VS2008 SDK. It's dependency assemblies change between the two SDK 
    releases. To make sure you don't accidently build code which is not
    compatible with the other SDK, most code must not be embedded directly
    within this assembly.
    
    * AnkhSvn
    * AnkhSvn/Guids.cs
    * AnkhSvn/AnkhSvn.snk
    * AnkhSvn/GlobalSuppressions.cs
    * AnkhSvn/Key.snk
    * AnkhSvn/Gui
    * AnkhSvn/Gui/Gui-To-Ctc.xsl
    * AnkhSvn/Gui/Gui.xsd
    * AnkhSvn/Gui/Gui.xsx
    * AnkhSvn/Gui/AnkhSvn.xml
    * AnkhSvn/VSPackage.resx
    * AnkhSvn/AnkhSvn.vsct
    * AnkhSvn/AnkhSvn.csproj
    * AnkhSvn/AnkhSvnPackage.cs
    * AnkhSvn/Resources
    * AnkhSvn/Resources/Package.ico
    * AnkhSvn/Resources/Images_32bit.bmp
    * AnkhSvn/Resources.resx
    * AnkhSvn/Properties
    * AnkhSvn/Properties/AssemblyInfo.cs
    * AnkhSvn/Resources.Designer.cs
      See above
    
    * AnkhSvn/Gui/ctc
      Directory containing the generated ctc file. You will probably see
      errors refering to files here if you mis-edit the AnkhSvn.xml file
    
    * AnkhSvn.Ids
    * AnkhSvn.Ids/AnkhSvn.Ids.snk
    * AnkhSvn.Ids/AnkhCommandGroup.cs
    * AnkhSvn.Ids/AnkhId.cs
    * AnkhSvn.Ids/AnkhCommand.cs
    * AnkhSvn.Ids/AnkhSvn.Ids.csproj
    * AnkhSvn.Ids/AnkhCommandMenu.cs
    * AnkhSvn.Ids/Properties
    * AnkhSvn.Ids/Properties/AssemblyInfo.cs
      Added assembly containing a few enum and constant classes. These
      values are read by the Gui-To-Ctc convertion script and embedded
      in the compiled resources. The same values are send back from VS
      when updating. This assembly stays in memory within VS after 
      updating. (VS does not lock it)

---
+++ trunk/src/AnkhSvn.Ids/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("AnkhSvn.Ids")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnkhSvn.Ids")]
+[assembly: AssemblyCopyright("Copyright © Ankh Team 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f09b3087-2501-4d00-98f5-1267bdeeb18b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.0.*")]
