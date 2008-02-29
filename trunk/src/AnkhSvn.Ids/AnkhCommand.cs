be9091a27443a636f30ba8bc6c631fc3c2c61a5b
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-02-29 16:57:50 +0000

    Added all existing top level commands to the gui test menu (Will move them to the correct places) and hook everything together to make the commands actually work, including updating status, etc.
    * src/AnkhSvn.Ids/AnkhCommand.cs
    * src/AnkhSvn.Ids/AnkhSvn.Ids.csproj
    * src/AnkhSvn/Gui/AnkhSvn.xml
    * src/AnkhSvn/AnkhSvn.csproj
    * src/AnkhSvn/AnkhSvnPackage.cs

--- trunk/src/AnkhSvn.Ids/AnkhCommand.cs
+++ trunk/src/AnkhSvn.Ids/AnkhCommand.cs
@@ -16,18 +16,60 @@ namespace AnkhSvn.Ids
 	public enum AnkhCommand // : int
 	{
 		[Obsolete]
-		None=0,
+		None = 0,
 
 		// These values live in the same numberspace as the other values within 
 		// the command set. So we start countin at this number to make sure we
 		// do not reuse values
 		CommandFirst = 0x1FFFFFFF,
-		
+
+		// Two test commands
 		FileSccMenuUpdateLatest,
 		FileSccMenuUpdateSpecific,
 
+		// Raw dump of old commands; to be sorted out
 		AddItem,
 		AddRepositoryRoot,
-		AddSolutionToRepository
+		AddSolutionToRepository,
+		AddWorkingCopyExplorerRoot,
+		Blame,
+		ChangeAdminDirName,
+		Checkout,
+		CheckoutFolder,
+		CheckoutSolution,
+		Cleanup,
+		CommitItem,
+		CopyReposExplorerUrl,
+		CreatePatch,
+		DiffExternalLocalItem,
+		DiffLocalItem,
+		EditConfigFile,
+		Export,
+		ExportFolder,
+		Lock,
+		Log,
+		MakeDirectory,
+		Refresh,
+		RefreshRepositoryItem,
+		Relocate,
+		RemoveRepositoryRoot,
+		RemoveWorkingCopyExplorerRoot,
+		ResolveConflict,
+		ResolveConflictExternal,
+		ReverseMerge,
+		RevertItem,
+		SaveToFile,
+		SendErrorReport,
+		ShowCommitDialog,
+		ShowRepositoryExplorer,
+		ShowWorkingCopyExplorer,
+		SwitchItem,
+		ToggleAnkh,
+		Unlock,
+		UpdateItem,
+		ViewInVsNet,
+		ViewInWindows,
+		ViewRepositoryFile,
+		CheckForOrphanedTreeNodes,
 	}
 }
