f4fef9cc6c82f5da045e75e1bc5ddf4ccbb2a8e7
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-03-01 20:45:57 +0000

    Make log dialog more testable, add some (incomplete) test-cases.
    * /src/Ankh.Tests/Ankh.Tests.2005.csproj
    * /src/Ankh.Tests/Presenters
    * /src/Ankh.Tests/Presenters/SvnLogPresenterTest.cs
    * /src/Ankh.Tests/Services
    * /src/Ankh.Tests/Services/LogServicetest.cs
    	add tests for the presenter and provider for the log control
    * /src/Ankh.UI/Ankh.UI.2005.csproj
    * /src/Ankh.UI/LogDialogView.cs
    * /src/Ankh.UI/LogDialogView.Designer.cs
    * /src/Ankh.UI/LogDialogView.resx
    * /src/Ankh.UI/Presenters
    * /src/Ankh.UI/Presenters/SvnLogPresenter.cs
    * /src/Ankh.UI/Services
    * /src/Ankh.UI/Services/ISvnLogService.cs
    * /src/Ankh.UI/Services/SvnLogService.cs
    * /src/Ankh.UI/Views
    * /src/Ankh.UI/Views/ISvnLogView.cs
    	Add new files, logservice, logview, logpresenter and logdialog (that encapsulated the 3 different controls that were present in the testdialog)
    * /src/Ankh.UI/LogChangedPaths.Designer.cs
    * /src/Ankh.UI/LogChangedPaths.resx
    	Design actions, hide the row-headers
    * /src/Ankh.UI/LogDialogTest.cs
    * /src/Ankh.UI/LogDialogTest.Designer.cs
    * /src/Ankh.UI/LogDialogTest.resx
    	Remove all controls, and just host LogDialogView for testing purposed. UI moved to LogDialogView
    * /src/Ankh.UI/LogRevisionControl.cs
    * /src/Ankh.UI/LogRevisionControl.Designer.cs
    * /src/Ankh.UI/LogRevisionControl.resx
    	Remove code for fetching log information, moved to SvnLogService

--- trunk/src/Ankh.UI/LogDialogTest.cs
+++ trunk/src/Ankh.UI/LogDialogTest.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using SharpSvn;
+using Ankh.UI.Services;
+using Ankh.UI.Presenters;
 
 namespace Ankh.UI
 {
@@ -14,48 +16,55 @@ namespace Ankh.UI
         public LogDialogTest()
         {
             InitializeComponent();
+
+			SvnLogService logSvc = new SvnLogService();
+
+			SvnLogPresenter presenter = new SvnLogPresenter(this.logDialogView1, logSvc);
+			logSvc.RemoteTarget = new Uri("http://ankhsvn.open.collab.net/svn/ankhsvn/");
+
+			presenter.Start();
         }
 
-        public Uri RemoteTarget
-        {
-            get { return logRevisionControl.RemoteTarget; }
-            set { logRevisionControl.RemoteTarget = value; }
-        }
+		//public Uri RemoteTarget
+		//{
+		//    get { return logRevisionControl.RemoteTarget; }
+		//    set { logRevisionControl.RemoteTarget = value; }
+		//}
 
-        public string LocalTarget
-        {
-            get { return logRevisionControl.LocalTarget; }
-            set { logRevisionControl.LocalTarget = value; }
-        }
+		//public string LocalTarget
+		//{
+		//    get { return logRevisionControl.LocalTarget; }
+		//    set { logRevisionControl.LocalTarget = value; }
+		//}
 
         public static void Main(string[] args)
         {
-            LogDialogTest dialog = new LogDialogTest();
-            dialog.RemoteTarget = new Uri("http://ankhsvn.open.collab.net/svn/ankhsvn/");
-            dialog.ShowDialog();
+			LogDialogTest dialog = new LogDialogTest();
+			//dialog.RemoteTarget = new Uri("http://ankhsvn.open.collab.net/svn/ankhsvn/");
+			dialog.ShowDialog();
             
         }
 
-        private void toolStripButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            splitContainer2.Panel2Collapsed = !cbLogMessageViewer.Checked;
-            splitContainer1.Panel2Collapsed = !(cbLogMessageViewer.Checked || cbChangedPathsViewer.Checked);
-        }
+		private void toolStripButton1_CheckedChanged(object sender, EventArgs e)
+		{
+			//splitContainer2.Panel2Collapsed = !cbLogMessageViewer.Checked;
+			//splitContainer1.Panel2Collapsed = !(cbLogMessageViewer.Checked || cbChangedPathsViewer.Checked);
+		}
 
-        private void cbChangedPathsViewer_Click(object sender, EventArgs e)
-        {
-            splitContainer2.Panel1Collapsed = !cbChangedPathsViewer.Checked;
-            splitContainer1.Panel2Collapsed = !(cbLogMessageViewer.Checked || cbChangedPathsViewer.Checked);
-        }
+		private void cbChangedPathsViewer_Click(object sender, EventArgs e)
+		{
+			//splitContainer2.Panel1Collapsed = !cbChangedPathsViewer.Checked;
+			//splitContainer1.Panel2Collapsed = !(cbLogMessageViewer.Checked || cbChangedPathsViewer.Checked);
+		}
 
-        private void cbIncludeMerged_Click(object sender, EventArgs e)
-        {
-            logRevisionControl.IncludeMergedRevisions = cbIncludeMerged.Checked;
-        }
+		private void cbIncludeMerged_Click(object sender, EventArgs e)
+		{
+			//logRevisionControl.IncludeMergedRevisions = cbIncludeMerged.Checked;
+		}
 
-        private void cbStopOnCopy_Click(object sender, EventArgs e)
-        {
-            logRevisionControl.StrictNodeHistory = cbStopOnCopy.Checked;
-        }
+		private void cbStopOnCopy_Click(object sender, EventArgs e)
+		{
+			//logRevisionControl.StrictNodeHistory = cbStopOnCopy.Checked;
+		}
     }
 }
---
+++ trunk/src/Ankh.UI/Presenters/SvnLogPresenter.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+using SharpSvn;
+using System.Threading;
+
+namespace Ankh.UI.Presenters
+{
+	public class SvnLogPresenter
+	{
+		readonly ISvnLogView logView;
+		readonly ISvnLogService logService;
+		bool operationRunning;
+
+		const int initialBatch = 30;
+		const int extraItems = 50;
+		const int hysteresis = 20;
+		long lastRevision;
+		SendOrPostCallback invokeUpdateRowCount;
+		
+		SynchronizationContext sync;
+
+		public SvnLogPresenter(ISvnLogView logView, ISvnLogService logService)
+		{
+			if (logView == null)
+				throw new ArgumentNullException("logView");
+			if (logService == null)
+				throw new ArgumentNullException("logService");
+
+			this.logView = logView;
+			this.logService = logService;
+
+			this.logView.IncludeMergedRevisionsChanged += logView_IncludeMergedRevisionsChanged;
+			this.logView.StrictNodeHistoryChanged += logView_StrictNodeHistoryChanged;
+			this.logView.ScrollPositionChanged += logView_ItemsRequestedChanged;
+
+			this.logService.Started += logService_Started;
+			this.logService.Completed += logService_Completed;
+			this.logService.LogItemReceived += logService_LogItemReceived;
+
+			sync = SynchronizationContext.Current;
+			invokeUpdateRowCount = new SendOrPostCallback(InvokeUpdateRowCount);
+		}
+
+		public void Start()
+		{
+			this.logService.RequiredItemCount = initialBatch;
+			logService.Start();
+		}
+
+		void logService_Started(object sender, EventArgs e)
+		{
+			operationRunning = true;
+			Trace.WriteLine("Logservice_Started");
+		}
+
+		void logView_ItemsRequestedChanged(object sender, ScrollEventArgs e)
+		{
+			if (operationRunning)
+				return;
+
+			if (e.ScrollOrientation != ScrollOrientation.VerticalScroll)
+				return;
+			if (e.NewValue < e.OldValue)
+				return;
+			
+			int itemsToFetch = extraItems - (logView.ItemCount - e.NewValue);
+			if (itemsToFetch < hysteresis)
+				return;
+
+			this.logService.RequiredItemCount = itemsToFetch;
+
+			this.logService.StartRevision = lastRevision > 1 ? lastRevision - 1 : SvnRevision.None;
+			Trace.WriteLine("Starting log");
+			this.logService.Start();
+		}
+
+		void logView_StrictNodeHistoryChanged(object sender, EventArgs e)
+		{
+			this.logService.Cancel();
+			this.logService.RequiredItemCount = initialBatch;
+			this.logService.StartRevision = SvnRevision.None;
+			this.logView.Reset();
+
+			this.logService.StrictNodeHistory = this.logView.StrictNodeHistory;
+			
+			this.logService.Start();
+		}
+
+		void logView_IncludeMergedRevisionsChanged(object sender, EventArgs e)
+		{
+			this.logService.Cancel();
+			this.logService.RequiredItemCount = initialBatch;
+			this.logService.StartRevision = SvnRevision.None;
+			this.logView.Reset();
+			
+			this.logService.IncludeMergedRevisions = this.logView.IncludeMergedRevisions;
+
+			this.logService.Start();
+		}
+
+		void logService_LogItemReceived(object sender, EventArgs e)
+		{
+			foreach (SvnLogEventArgs arg in this.logService.RetrieveAndFlushLogItems())
+			{
+				this.logView.Add(arg);
+				lastRevision = arg.Revision;
+			}
+			if (sync != null)
+				sync.Post(invokeUpdateRowCount, null);
+			else
+				InvokeUpdateRowCount(null);
+		}
+
+		void logService_Completed(object sender, EventArgs e)
+		{
+			
+			
+			operationRunning = false;
+		}
+
+		void InvokeUpdateRowCount(object state)
+		{
+			logView.UpdateRowCount();
+		}
+	}
+}
