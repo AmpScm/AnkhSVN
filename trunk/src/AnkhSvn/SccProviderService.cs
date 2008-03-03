c4a86c0a04b136958d32d9c5b6e152f68fe2b288
arild <arild@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-03-03 22:20:16 +0000

    Implement enough of IVsSccProvider that we provide correct status images with a custom glyph list.
    *  \collab\src\Ankh\Solution\Explorer.cs
    	(SyncAll) - Remove call to SolutionExplorerLoadStrategy.Load.
    	(SolutionExplorerLoadStrategy) - Delete these classes.
    
    * \collab\src\Ankh\StatusImages.cs
    	Make the class public.
    
    * \collab\src\AnkhSvn\AnkhSvnPackage.cs
    	(Initialize) - Pass this to SccProviderService. Make sure that IAnkhPackage is registered 
    	before that.
    
    * \collab\src\AnkhSvn\SccProviderService.cs
    	Removed throwing of exceptions.
    	(SetActive, SetInActive, AnyItemsUnderSourceControl) - Implemented properly.
    	(GetSccGlyph) - Implemented to get the correct glyph.
    	(GetCustomGlyphList) - Return StatusImages.StatusImageList.
    	(GenerateStatus) - Helper method for GetSccGlyph.

--- trunk/src/AnkhSvn/SccProviderService.cs
+++ trunk/src/AnkhSvn/SccProviderService.cs
@@ -3,65 +3,128 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.Shell.Interop;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
+using SharpSvn;
 
 namespace Ankh.VSPackage
 {
     [GuidAttribute( GuidList.guidAnkhSccProviderServiceString )]
     class SccProviderService : IVsSccProvider, IVsSccManager2, IVsSccGlyphs
     {
+        public SccProviderService( IContext context )
+        {
+            this.context = context;
+        }
+
         public int AnyItemsUnderSourceControl( out int pfResult )
         {
-            throw new Exception( "The method or operation is not implemented." );
+            Trace.WriteLine( "In AnyItemsUnderSourceControl" );
+            pfResult = active ? 1 : 0;
+            return VSConstants.S_OK;
         }
 
         public int SetActive()
         {
-            throw new Exception( "The method or operation is not implemented." );
+            Trace.WriteLine( "In SetActive" );
+            this.active = true;
+            return VSConstants.S_OK;
         }
 
         public int SetInactive()
         {
-            throw new Exception( "The method or operation is not implemented." );
+            Trace.WriteLine( "In SetInActive" );
+            this.active = false;
+            return VSConstants.S_OK;
         }
+
 
         public int BrowseForProject( out string pbstrDirectory, out int pfOK )
         {
-            throw new Exception( "The method or operation is not implemented." );
+            pbstrDirectory = null;
+            pfOK = 0;
+
+            return VSConstants.E_NOTIMPL;
         }
 
         public int CancelAfterBrowseForProject()
         {
-            throw new Exception( "The method or operation is not implemented." );
+            return VSConstants.E_NOTIMPL;
         }
 
         public int GetSccGlyph( int cFiles, string[] rgpszFullPaths, VsStateIcon[] rgsiGlyphs, uint[] rgdwSccStatus )
         {
-            throw new Exception( "The method or operation is not implemented." );
+            for ( int i = 0; i < cFiles; i++ )
+            {
+                SvnItem item = this.context.StatusCache[ rgpszFullPaths[ i ] ];
+                NodeStatus nodeStatus = GenerateStatus( item );
+                rgsiGlyphs[ i ] = (VsStateIcon)
+                    StatusImages.GetStatusImageForNodeStatus( nodeStatus );
+                rgdwSccStatus[ i ] = 21;
+            }
+
+            return VSConstants.S_OK;
         }
 
         public int GetSccGlyphFromStatus( uint dwSccStatus, VsStateIcon[] psiGlyph )
         {
-            throw new Exception( "The method or operation is not implemented." );
+            psiGlyph[ 0 ] = VsStateIcon.STATEICON_CHECKEDIN;
+
+            return VSConstants.S_OK;
         }
+
 
         public int IsInstalled( out int pbInstalled )
         {
-            throw new Exception( "The method or operation is not implemented." );
+            pbInstalled = 1;
+
+            return VSConstants.S_OK;
         }
 
         public int RegisterSccProject( IVsSccProject2 pscp2Project, string pszSccProjectName, string pszSccAuxPath, string pszSccLocalPath, string pszProvider )
         {
-            throw new Exception( "The method or operation is not implemented." );
+            return VSConstants.S_OK;
         }
 
         public int UnregisterSccProject( IVsSccProject2 pscp2Project )
         {
-            throw new Exception( "The method or operation is not implemented." );
+            return VSConstants.S_OK;
+
         }
 
         public int GetCustomGlyphList( uint BaseIndex, out uint pdwImageListHandle )
         {
-            throw new Exception( "The method or operation is not implemented." );
+            pdwImageListHandle = (uint)StatusImages.StatusImageList.Handle.ToInt32();
+
+            this.baseIndex = BaseIndex;
+
+            return VSConstants.S_OK;
         }
+
+        protected static NodeStatus GenerateStatus( SvnItem item )
+        {
+            AnkhStatus status = item.Status;
+            NodeStatusKind kind;
+            if ( status.LocalContentStatus != SvnStatus.Normal )
+            {
+                kind = (NodeStatusKind)status.LocalContentStatus;
+            }
+            else if ( status.LocalPropertyStatus != SvnStatus.Normal &&
+                status.LocalPropertyStatus != SvnStatus.None )
+            {
+                kind = (NodeStatusKind)status.LocalPropertyStatus;
+            }
+            else
+            {
+                kind = NodeStatusKind.Normal;
+            }
+
+            return new NodeStatus( kind, item.IsReadOnly, item.IsLocked );
+        }
+
+
+        private uint baseIndex;
+        private bool active;
+        private IContext context;
+
     }
 }
