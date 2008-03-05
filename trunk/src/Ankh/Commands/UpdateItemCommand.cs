e291c64a45362e59ada8c0e03c853e92d3254ee8
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-03-05 17:23:58 +0000

    Updated the UpdateItem command to use the new selection api

--- trunk/src/Ankh/Commands/UpdateItemCommand.cs
+++ trunk/src/Ankh/Commands/UpdateItemCommand.cs
@@ -10,154 +10,159 @@ using Ankh.Solution;
 
 using SharpSvn;
 using AnkhSvn.Ids;
+using System.Collections.Generic;
 
 namespace Ankh.Commands
 {
-    /// <summary>
-    /// A command that updates an item.
-    /// </summary>
-    [VSNetCommand(AnkhCommand.UpdateItem,
+	/// <summary>
+	/// A command that updates an item.
+	/// </summary>
+	[VSNetCommand(AnkhCommand.UpdateItem,
 		"UpdateItem",
-         Text = "&Update...",
-         Tooltip = "Updates local items with changes from the Subversion repository.",
-         Bitmap = ResourceBitmaps.Update),
-         VSNetItemControl( "", Position = 3 )]
-    public class UpdateItem : CommandBase
-    {
-        #region Implementation of ICommand
+		 Text = "&Update...",
+		 Tooltip = "Updates local items with changes from the Subversion repository.",
+		 Bitmap = ResourceBitmaps.Update),
+		 VSNetItemControl("", Position = 3)]
+	public class UpdateItem : CommandBase
+	{
+		#region Implementation of ICommand
 
-        public override EnvDTE.vsCommandStatus QueryStatus(IContext context)
-        {
-            // all items must be versioned if we are going to run update.
-            IList resources = context.Selection.GetSelectionResources( true,
-                new ResourceFilterCallback(SvnItem.VersionedFilter) );
-            if ( resources.Count > 0 )
-                return Enabled;
-            else
-                return Disabled;
-        }
+		public override void OnUpdate(CommandUpdateEventArgs e)
+		{
+			ICollection<string> files = e.Selection.GetSelectedFiles();
 
-        public override void Execute(IContext context, string parameters)
-        {
-            try
-            {
-                // save all files
-                this.SaveAllDirtyDocuments( context );
-                context.StartOperation( "Updating" );
+			if (files == null)
+			{
+				e.Enabled = false;
+				return;
+			}
 
-                IList resources = context.Selection.GetSelectionResources( true,
-                    new ResourceFilterCallback(SvnItem.VersionedFilter) );
+			// TODO: Filter files which are not managed by subversion
 
-                // we assume by now that all items are working copy resources.                
-                UpdateRunner runner = new UpdateRunner( context, resources );
-                if ( !runner.MaybeShowUpdateDialog() )
-                    return;
+			if (files.Count == 0)
+				e.Enabled = false;
+		}
 
-                // run the actual update on another thread
-                context.ProjectFileWatcher.StartWatchingForChanges();
-                bool completed = context.UIShell.RunWithProgressDialog( runner, "Updating" );
+		public override void OnExecute(CommandEventArgs e)
+		{
+			try
+			{
+				// save all files
+				this.SaveAllDirtyDocuments(e.Context);
+				e.Context.StartOperation("Updating");
 
-                // this *must* happen on the primary thread.
-                if ( completed )
-                {
-                    if ( !context.ReloadSolutionIfNecessary() )
-                        context.Selection.RefreshSelection();
-                }
-            }
-            finally
-            {
-                context.EndOperation();
-            }
-        }
+				List<SvnItem> files = new List<SvnItem>(e.Selection.GetSelectedSvnItems());
 
-        #endregion
+				// we assume by now that all items are working copy resources.                
+				UpdateRunner runner = new UpdateRunner(e.Context, files);
+				if (!runner.MaybeShowUpdateDialog())
+					return;
 
-        #region UpdateVisitor
-        private class UpdateRunner : IProgressWorker
-        {
-            public UpdateRunner( IContext context, IList resources ) 
-            {
-                this.context = context;
-                this.resources = resources;
-            }
+				// run the actual update on another thread
+				e.Context.ProjectFileWatcher.StartWatchingForChanges();
+				bool completed = e.Context.UIShell.RunWithProgressDialog(runner, "Updating");
 
-            public IContext Context
-            {
-                get{ return this.context; }
-            }
+				// this *must* happen on the primary thread.
+				if (completed)
+				{
+					if (!e.Context.ReloadSolutionIfNecessary())
+						e.Context.Selection.RefreshSelection();
+				}
+			}
+			finally
+			{
+				e.Context.EndOperation();
+			}
+		}
 
-            /// <summary>
-            /// Show the update dialog if wanted.
-            /// </summary>
-            /// <returns></returns>
-            public bool MaybeShowUpdateDialog()
-            {
-                this.depth = SvnDepth.Empty;
-                this.revision = SvnRevision.Head;
+		#endregion
 
-                // We're using the update dialog no matter what to
-                // take advantage of it's path processing capabilities.
-                // This is the best way to ensure holding down Shift is
-                // equivalent to accepting the default in the dialog.
-                using(UpdateDialog d = new UpdateDialog())
-                {
-                    d.GetPathInfo += new ResolvingPathInfoHandler(SvnItem.GetPathInfo);
-                    d.Items = this.resources;
-                    d.CheckedItems = this.resources;
-                    d.Recursive = true;
+		#region UpdateVisitor
+		private class UpdateRunner : IProgressWorker
+		{
+			public UpdateRunner(IContext context, IList resources)
+			{
+				this.context = context;
+				this.resources = resources;
+			}
 
-                    if ( !CommandBase.Shift )
-                    {
-                        if ( d.ShowDialog( this.Context.HostWindow ) != DialogResult.OK )
-                            return false;
-                    }
+			public IContext Context
+			{
+				get { return this.context; }
+			}
 
-                    depth = d.Recursive ? SvnDepth.Infinity : SvnDepth.Empty;
-                    this.resources = d.CheckedItems;
-                    this.revision = d.Revision;
-                }
+			/// <summary>
+			/// Show the update dialog if wanted.
+			/// </summary>
+			/// <returns></returns>
+			public bool MaybeShowUpdateDialog()
+			{
+				this.depth = SvnDepth.Empty;
+				this.revision = SvnRevision.Head;
 
-                // the user hasn't cancelled the update
-                return true;
-            }
+				// We're using the update dialog no matter what to
+				// take advantage of it's path processing capabilities.
+				// This is the best way to ensure holding down Shift is
+				// equivalent to accepting the default in the dialog.
+				using (UpdateDialog d = new UpdateDialog())
+				{
+					d.GetPathInfo += new ResolvingPathInfoHandler(SvnItem.GetPathInfo);
+					d.Items = this.resources;
+					d.CheckedItems = this.resources;
+					d.Recursive = true;
 
-            /// <summary>
-            /// The actual updating happens here.
-            /// </summary>
-            public void Work(IContext context)
-            {
-                string[] paths = SvnItem.GetPaths(this.resources);
-                SvnUpdateArgs args = new SvnUpdateArgs();
-                args.Notify += new EventHandler<SvnNotifyEventArgs>(OnNotificationEventHandler);
-                args.Revision = revision;
-                args.Depth = depth;
-                args.IgnoreExternals = false;
-                context.Client.Update(paths, args);
+					if (!CommandBase.Shift)
+					{
+						if (d.ShowDialog(this.Context.HostWindow) != DialogResult.OK)
+							return false;
+					}
 
-                if (this.conflictsOccurred)
-                    context.ConflictManager.NavigateTaskList();
-            }
-            
-            /// <summary>
-            ///  Handlke event for onNotification that conflicts occurred from update
-            /// </summary>
-            /// <param name="taskItem"></param>
-            /// <param name="navigateHandled"></param>
-            private void  OnNotificationEventHandler(Object sender, SvnNotifyEventArgs args)
-            {
-                if ( args.ContentState == SvnNotifyState.Conflicted)
-                {
-                    this.Context.ConflictManager.AddTask(args.Path);
-                    this.conflictsOccurred = true;
-                }
-            }
+					depth = d.Recursive ? SvnDepth.Infinity : SvnDepth.Empty;
+					this.resources = d.CheckedItems;
+					this.revision = d.Revision;
+				}
 
-            private IList resources = new ArrayList();
-            private SvnRevision revision;
-            private SvnDepth depth;
-            private bool conflictsOccurred = false; 
-            private IContext context;
-        }            
-        #endregion
-    }
+				// the user hasn't cancelled the update
+				return true;
+			}
+
+			/// <summary>
+			/// The actual updating happens here.
+			/// </summary>
+			public void Work(IContext context)
+			{
+				string[] paths = SvnItem.GetPaths(this.resources);
+				SvnUpdateArgs args = new SvnUpdateArgs();
+				args.Notify += new EventHandler<SvnNotifyEventArgs>(OnNotificationEventHandler);
+				args.Revision = revision;
+				args.Depth = depth;
+				args.IgnoreExternals = false;
+				context.Client.Update(paths, args);
+
+				if (this.conflictsOccurred)
+					context.ConflictManager.NavigateTaskList();
+			}
+
+			/// <summary>
+			///  Handlke event for onNotification that conflicts occurred from update
+			/// </summary>
+			/// <param name="taskItem"></param>
+			/// <param name="navigateHandled"></param>
+			private void OnNotificationEventHandler(Object sender, SvnNotifyEventArgs args)
+			{
+				if (args.ContentState == SvnNotifyState.Conflicted)
+				{
+					this.Context.ConflictManager.AddTask(args.Path);
+					this.conflictsOccurred = true;
+				}
+			}
+
+			private IList resources;
+			private SvnRevision revision;
+			private SvnDepth depth;
+			private bool conflictsOccurred = false;
+			private IContext context;
+		}
+		#endregion
+	}
 }
