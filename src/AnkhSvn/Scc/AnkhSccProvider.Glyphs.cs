e40f6f6b8a7fb86e9c2855c4eb542c48ecf879dc
Bert Huijben <rhuijben@users.noreply.github.com>
2008-03-07 23:23:34 +0000

    Implemented a few extra Scc interfaces and separated the code over a few partial class files
    * AnkhSvn/Resources.resx
    * AnkhSvn/AnkhSvn.csproj
    * AnkhSvn/SccProviderService.cs
    * AnkhSvn/Scc
    * AnkhSvn/Scc/AnkhSccProvider.QueryEditSave.cs
    * AnkhSvn/Scc/AnkhSccProvider.Solution.cs
    * AnkhSvn/Scc/AnkhSccProvider.cs
    * AnkhSvn/Scc/AnkhSccProvider.Glyphs.cs
    * AnkhSvn/Resources.Designer.cs
      Updated api to catch more hooks from VS. Implemented the
      required interface IVsQueryEditQuerySave2 and several optional
      interfaces which enable extra UI for SCC providers in Visual Studio.

---
+++ src/AnkhSvn/Scc/AnkhSccProvider.Glyphs.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Ankh.VSPackage.Scc
+{
+    partial class AnkhSccProvider : IVsSccManager2, IVsSccManagerTooltip
+    {
+        /// <summary>
+        /// This method is called by projects to discover the source control glyphs 
+        /// to use on files and the files' source control status; this is the only way to get status.
+        /// </summary>
+        /// <param name="cFiles">The c files.</param>
+        /// <param name="rgpszFullPaths">The RGPSZ full paths.</param>
+        /// <param name="rgsiGlyphs">The rgsi glyphs.</param>
+        /// <param name="rgdwSccStatus">The RGDW SCC status.</param>
+        /// <returns></returns>
+        public int GetSccGlyph(int cFiles, string[] rgpszFullPaths, VsStateIcon[] rgsiGlyphs, uint[] rgdwSccStatus)
+        {
+            for (int i = 0; i < cFiles; i++)
+            {
+                SvnItem item = this.context.StatusCache[rgpszFullPaths[i]];
+                NodeStatus nodeStatus = GenerateStatus(item);
+                rgsiGlyphs[i] = (VsStateIcon)
+                    StatusImages.GetStatusImageForNodeStatus(nodeStatus);
+                rgdwSccStatus[i] = 21;
+            }
+
+            return VSConstants.S_OK;
+        }
+
+        /// <summary>
+        /// This function determines which glyph to display, given a combination of status flags.
+        /// </summary>
+        /// <param name="dwSccStatus">The dw SCC status.</param>
+        /// <param name="psiGlyph">The psi glyph.</param>
+        /// <returns></returns>
+        public int GetSccGlyphFromStatus(uint dwSccStatus, VsStateIcon[] psiGlyph)
+        {
+            psiGlyph[0] = VsStateIcon.STATEICON_CHECKEDIN;
+
+            return VSConstants.S_OK;
+        }
+
+        /*public int GetGlyphTipText(IVsHierarchy phierHierarchy, uint itemidNode, out string pbstrTooltipText)
+        {
+            // Todo find file via hierarchy, reusing code from SelectionContext 
+        }*/
+
+        public int GetCustomGlyphList(uint BaseIndex, out uint pdwImageListHandle)
+        {
+            pdwImageListHandle = (uint)StatusImages.StatusImageList.Handle.ToInt32();
+
+            this.baseIndex = BaseIndex;
+
+            return VSConstants.S_OK;
+        }
+
+        protected static NodeStatus GenerateStatus(SvnItem item)
+        {
+            AnkhStatus status = item.Status;
+            NodeStatusKind kind;
+            if (status.LocalContentStatus != SvnStatus.Normal)
+            {
+                kind = (NodeStatusKind)status.LocalContentStatus;
+            }
+            else if (status.LocalPropertyStatus != SvnStatus.Normal &&
+                status.LocalPropertyStatus != SvnStatus.None)
+            {
+                kind = (NodeStatusKind)status.LocalPropertyStatus;
+            }
+            else
+            {
+                kind = NodeStatusKind.Normal;
+            }
+
+            return new NodeStatus(kind, item.IsReadOnly, item.IsLocked);
+        }
+    }
+}
