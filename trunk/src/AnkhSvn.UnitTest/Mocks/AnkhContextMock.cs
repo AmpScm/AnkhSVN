cf4d521ba73cf8da863111889ea2888efe510865
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-03-10 17:30:08 +0000

    Add tests that invoke a few commands
    * /src/AnkhSvn.UnitTest/AnkhSvn.UnitTest.csproj
    	New files
    * /src/AnkhSvn.UnitTest/CommandRouting/CommandRoutingTest.cs
    	New file containing testcases
    * /src/AnkhSvn.UnitTest/Mocks/SelectionContext.cs
    	New file containing a test imlementation of an ISelectionContext
    * /src/AnkhSvn.UnitTest/Mocks/AnkhContextMock.cs
    	Allow mocking of both IUIShell and ISelectionContext on IContext

--- trunk/src/AnkhSvn.UnitTest/Mocks/AnkhContextMock.cs
+++ trunk/src/AnkhSvn.UnitTest/Mocks/AnkhContextMock.cs
@@ -11,12 +11,29 @@ namespace AnkhSvn_UnitTestProject.Mocks
     {
         public static IContext GetInstance(MockRepository mocks)
         {
+            return GetInstance(mocks, null, mocks.DynamicMock<ISelectionContext>());
+        }
+
+        public static IContext GetInstance(MockRepository mocks, ISelectionContext selContext)
+        {
+            return GetInstance(mocks, null, selContext);
+        }
+
+        public static IContext GetInstance(MockRepository mocks, IUIShell uiShell)
+        {
+            return GetInstance(mocks, uiShell, mocks.DynamicMock<ISelectionContext>());
+        }
+
+        static IContext GetInstance(MockRepository mocks, IUIShell uiShell, ISelectionContext selContext)
+        {
             IContext context = mocks.DynamicMock<IContext>();
-            ISelectionContext selectionContext = mocks.DynamicMock<ISelectionContext>();
+            
 
             using (mocks.Record())
             {
-                Expect.Call(context.SelectionContext).Return(selectionContext).Repeat.Any();
+                if (uiShell != null)
+                    Expect.Call(context.UIShell).Return(uiShell).Repeat.Any();
+                Expect.Call(context.SelectionContext).Return(selContext).Repeat.Any();
             }
 
             return context;
