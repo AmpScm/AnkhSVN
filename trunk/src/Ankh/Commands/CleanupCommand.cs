e4461fc1c841b0453ff312ca41931e04c835ddc5
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-03-10 17:33:44 +0000

    (Partially) re-implement commands to use ISelectionContext
    * /Ankh/Commands/BlameCommand.cs
    	(OnExecute, OnUpdate) - See above
    * /Ankh/Commands/CleanupCommand.cs
    	(OnUpdate) - See above

--- trunk/src/Ankh/Commands/CleanupCommand.cs
+++ trunk/src/Ankh/Commands/CleanupCommand.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections;
 using AnkhSvn.Ids;
+using System.Collections.Generic;
+using System.IO;
+using SharpSvn;
 
 namespace Ankh.Commands
 {
@@ -16,16 +19,45 @@ namespace Ankh.Commands
          VSNetItemControl( VSNetControlAttribute.AnkhSubMenu, Position = 11)]
     public class Cleanup : CommandBase
     {
+        public override void OnUpdate(CommandUpdateEventArgs e)
+        {
+            foreach (SvnItem item in e.Selection.GetSelectedSvnItems(true))
+            {
+                if (item.IsVersioned)
+                    return;
+            }
+            e.Enabled = false;
+        }
         public override void OnExecute(CommandEventArgs e)
         {
             IContext context = e.Context;
-
+            SvnClient client = context.Client;
             using (context.StartOperation("Running cleanup"))
             {
-                IList resources = context.Selection.GetSelectionResources(false,
-                    new ResourceFilterCallback(SvnItem.DirectoryFilter));
-                foreach (SvnItem item in resources)
-                    context.Client.CleanUp(item.Path);
+                SortedList<string, SvnItem> list = new SortedList<string, SvnItem>(StringComparer.OrdinalIgnoreCase);
+                foreach (SvnItem item in e.Selection.GetSelectedSvnItems(true))
+                {
+                    if (!item.IsVersioned)
+                        continue;
+
+                    string path = item.IsDirectory ? item.Path : Path.GetDirectoryName(item.Path);
+                    if (list.ContainsKey(path))
+                        continue;
+
+                    list.Add(path, item);
+                }
+
+                foreach (string path in new List<string>(list.Keys))
+                {
+                    string parentPath = Path.GetDirectoryName(path);
+                    if (list.ContainsKey(parentPath) && parentPath != path)
+                        list.Remove(path);
+                }
+
+                SvnCleanUpArgs args = new SvnCleanUpArgs();
+                args.ThrowOnError = false;
+                foreach (string path in list.Keys)
+                    client.CleanUp(path, args);
             }
         }
     }
