6ff4ece710f97b586a7cc1f3882eb1f041fbb569
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-03-10 11:47:35 +0000

    Add several helper methods to extract code away from test-cases to prevent code duplication, rework MenuItemCallback to use this and call existing methods through our command-routing
    * /src/AnkhSvn.UnitTest/AnkhSvn.UnitTest.csproj
    	New files
    * /src/AnkhSvn.UnitTest/Helpers/CommandExecutor.cs
    * /src/AnkhSvn.UnitTest/Helpers/ReflectionHelper.cs
    * /src/AnkhSvn.UnitTest/Helpers/ServiceProviderHelper.cs
    	New helpers
    * /src/AnkhSvn.UnitTest/Mocks/AnkhContextMock.cs
    	New class mocking IContext
    * /src/AnkhSvn.UnitTest/MenuItemTests/MenuItemCallback.cs
    	(InitializeMenuCommand) - Ensure the menu service is able to find our commands
    	(MenuItemCallback, TestRefreshCommand) - Rename and rework test-case to invoke the Refresh command

---
+++ trunk/src/AnkhSvn.UnitTest/Helpers/ServiceProviderHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VsSDK.UnitTestLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace AnkhSvn_UnitTestProject.Helpers
+{
+    class ServiceProviderHelper : IDisposable
+    {
+        static OleServiceProvider serviceProvider;
+        static ServiceProviderHelper()
+        {
+            serviceProvider = OleServiceProvider.CreateOleServiceProviderWithBasicServices();
+        }
+
+        Type type;
+
+        private ServiceProviderHelper(Type t, object instance)
+        {
+            type = t;
+            serviceProvider.AddService(t, instance, true);
+        }
+
+        private ServiceProviderHelper()
+        {
+        }
+
+        public void Dispose()
+        {
+            if (type != null && serviceProvider.GetService(type) != null)
+                serviceProvider.RemoveService(type);
+        }
+
+        public static IDisposable AddService(Type t, object instance)
+        {
+            return new ServiceProviderHelper(t, instance);
+        }
+
+        public static IDisposable SetSite(IVsPackage package)
+        {
+            Assert.AreEqual(0, package.SetSite(serviceProvider), "SetSite did not return S_OK");
+            return new ServiceProviderHelper();
+        }
+    }
+}
---
+++ trunk/src/AnkhSvn.UnitTest/Mocks/AnkhContextMock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ankh;
+using Rhino.Mocks;
+using Ankh.Selection;
+
+namespace AnkhSvn_UnitTestProject.Mocks
+{
+    static class AnkhContextMock
+    {
+        public static IContext GetInstance(MockRepository mocks)
+        {
+            IContext context = mocks.DynamicMock<IContext>();
+            ISelectionContext selectionContext = mocks.DynamicMock<ISelectionContext>();
+
+            using (mocks.Record())
+            {
+                Expect.Call(context.SelectionContext).Return(selectionContext).Repeat.Any();
+            }
+
+            return context;
+        }
+    }
+}
