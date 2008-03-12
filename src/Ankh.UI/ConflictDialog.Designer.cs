b57c548deee917be7611510f70181a4d39f6d57d
Bert Huijben <rhuijben@users.noreply.github.com>
2008-03-12 21:19:41 +0000

    Moved designable user interface text to the designer
    * ConflictDialog.cs
    * ConflictDialog.Designer.cs
    * ConflictDialog.resx
      Moved sourcecode for tooltips and default to the designer
      to allow localization

--- src/Ankh.UI/ConflictDialog.Designer.cs
+++ src/Ankh.UI/ConflictDialog.Designer.cs
@@ -13,6 +13,7 @@ namespace Ankh.UI
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.mineFileRadioButton = new System.Windows.Forms.RadioButton();
             this.oldRevRadioButton = new System.Windows.Forms.RadioButton();
             this.newRevRadioButton = new System.Windows.Forms.RadioButton();
@@ -20,55 +21,58 @@ namespace Ankh.UI
             this.cancelButton = new System.Windows.Forms.Button();
             this.okButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mineFileRadioButton
             // 
             this.mineFileRadioButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                | System.Windows.Forms.AnchorStyles.Right)));
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.mineFileRadioButton.Checked = true;
             this.mineFileRadioButton.Location = new System.Drawing.Point(8, 96);
             this.mineFileRadioButton.Name = "mineFileRadioButton";
             this.mineFileRadioButton.Size = new System.Drawing.Size(280, 24);
             this.mineFileRadioButton.TabIndex = 0;
             this.mineFileRadioButton.TabStop = true;
             this.mineFileRadioButton.Text = "test.mine";
+            this.toolTip.SetToolTip(this.mineFileRadioButton, "Latest local file");
             this.mineFileRadioButton.Click += new System.EventHandler(this.selectedButton);
             // 
             // oldRevRadioButton
             // 
             this.oldRevRadioButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                | System.Windows.Forms.AnchorStyles.Right)));
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.oldRevRadioButton.Location = new System.Drawing.Point(8, 48);
             this.oldRevRadioButton.Name = "oldRevRadioButton";
             this.oldRevRadioButton.Size = new System.Drawing.Size(280, 24);
             this.oldRevRadioButton.TabIndex = 1;
-            this.oldRevRadioButton.TabStop = true;
             this.oldRevRadioButton.Text = "test.r1";
+            this.toolTip.SetToolTip(this.oldRevRadioButton, "Latest updated revision");
             this.oldRevRadioButton.Click += new System.EventHandler(this.selectedButton);
             // 
             // newRevRadioButton
             // 
             this.newRevRadioButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                | System.Windows.Forms.AnchorStyles.Right)));
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.newRevRadioButton.Location = new System.Drawing.Point(8, 72);
             this.newRevRadioButton.Name = "newRevRadioButton";
             this.newRevRadioButton.Size = new System.Drawing.Size(280, 24);
             this.newRevRadioButton.TabIndex = 2;
-            this.newRevRadioButton.TabStop = true;
             this.newRevRadioButton.Text = "test.r2";
+            this.toolTip.SetToolTip(this.newRevRadioButton, "Latest version in repository");
             this.newRevRadioButton.Click += new System.EventHandler(this.selectedButton);
             // 
             // fileRadioButton
             // 
             this.fileRadioButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                | System.Windows.Forms.AnchorStyles.Right)));
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.fileRadioButton.Location = new System.Drawing.Point(8, 24);
             this.fileRadioButton.Name = "fileRadioButton";
             this.fileRadioButton.Size = new System.Drawing.Size(280, 24);
             this.fileRadioButton.TabIndex = 3;
-            this.fileRadioButton.TabStop = true;
             this.fileRadioButton.Text = "test.txt";
+            this.toolTip.SetToolTip(this.fileRadioButton, "File with conflict markers");
             this.fileRadioButton.Click += new System.EventHandler(this.selectedButton);
             // 
             // cancelButton
@@ -77,8 +81,10 @@ namespace Ankh.UI
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.cancelButton.Location = new System.Drawing.Point(228, 145);
             this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 6;
             this.cancelButton.Text = "Cancel";
+            this.toolTip.SetToolTip(this.cancelButton, "Cancel this dialog.");
             // 
             // okButton
             // 
@@ -86,13 +92,15 @@ namespace Ankh.UI
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.okButton.Location = new System.Drawing.Point(148, 145);
             this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 0;
             this.okButton.Text = "Resolve";
+            this.toolTip.SetToolTip(this.okButton, "Resolve conflict and delete the three files that are not selected.");
             // 
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                | System.Windows.Forms.AnchorStyles.Right)));
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.oldRevRadioButton);
             this.groupBox1.Controls.Add(this.newRevRadioButton);
             this.groupBox1.Controls.Add(this.fileRadioButton);
@@ -134,5 +142,6 @@ namespace Ankh.UI
         private System.Windows.Forms.GroupBox groupBox1;
 
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ToolTip toolTip;
     }
 }
