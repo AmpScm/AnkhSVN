72f6e67d2c20c55992d9850d0fa95da1b133dbfc
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-03-13 18:37:20 +0000

    Added tracking project member-file event routing from the ProjectDocumentTracker to the AnkhSccProvider.
    * Ankh.Scc.csproj
    * AnkhSccProvider.FileMap.cs
      Added file for managing the file <-> project mapping
    
    * AnkhSccProvider.Projects.cs
      Removed the old handlers which were accidently committed
    
    * ProjectDocumentTracker.Add.cs
    * ProjectDocumentTracker.Remove.cs
    * ProjectDocumentTracker.Rename.cs
      Parsed the variants of add, remove, rename to a few helpers on
      the AnkhSccProvider

---
+++ trunk/src/Ankh.Scc/AnkhSccProvider.FileMap.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Ankh.Scc
+{
+    /// <summary>
+    /// 
+    /// </summary>
+	partial class AnkhSccProvider
+	{
+        /// <summary>
+        /// Called when a file is added to a project
+        /// </summary>
+        /// <param name="project">The project.</param>
+        /// <param name="filename">The filename.</param>
+        /// <param name="flags">The flags.</param>
+        internal void OnProjectFileAdded(IVsSccProject2 project, string filename, VSADDFILEFLAGS flags)
+        {
+        }
+
+        /// <summary>
+        /// Called when a file is removed from a project
+        /// </summary>
+        /// <param name="project">The project.</param>
+        /// <param name="filename">The filename.</param>
+        /// <param name="flags">The flags.</param>
+        internal void OnProjectFileRemoved(IVsSccProject2 project, string filename, VSREMOVEFILEFLAGS flags)
+        {
+        }
+
+        /// <summary>
+        /// Called when a directory is added to a project
+        /// </summary>
+        /// <param name="project">The project.</param>
+        /// <param name="directoryname">The directoryname.</param>
+        /// <param name="flags">The flags.</param>
+        internal void OnProjectDirectoryAdded(IVsSccProject2 project, string directoryname, VSADDDIRECTORYFLAGS flags)
+        {
+        }
+
+        /// <summary>
+        /// Called when a directory is removed from a project
+        /// </summary>
+        /// <param name="sccProject">The SCC project.</param>
+        /// <param name="directoryname">The directoryname.</param>
+        /// <param name="flags">The flags.</param>
+        internal void OnProjectDirectoryRemoved(IVsSccProject2 sccProject, string directoryname, VSREMOVEDIRECTORYFLAGS flags)
+        {
+        }
+
+        /// <summary>
+        /// Called just before a file in a project is renamed
+        /// </summary>
+        /// <param name="sccProject">The SCC project.</param>
+        /// <param name="oldName">The old name.</param>
+        /// <param name="newName">The new name.</param>
+        /// <param name="vSQUERYRENAMEFILEFLAGS">The v SQUERYRENAMEFILEFLAGS.</param>
+        /// <param name="ok">if set to <c>true</c> [ok].</param>
+        internal void OnBeforeProjectRenameFile(IVsSccProject2 sccProject, string oldName, string newName, VSQUERYRENAMEFILEFLAGS vSQUERYRENAMEFILEFLAGS, out bool ok)
+        {
+            ok = true;
+        }
+
+        /// <summary>
+        /// Called when a file in a project is renamed
+        /// </summary>
+        /// <param name="sccProject">The SCC project.</param>
+        /// <param name="oldName">The old name.</param>
+        /// <param name="newName">The new name.</param>
+        /// <param name="flags">The flags.</param>
+        internal void OnProjectRenameFile(IVsSccProject2 sccProject, string oldName, string newName, VSRENAMEFILEFLAGS flags)
+        {
+        }
+
+        /// <summary>
+        /// Called just before a directory in a project is renamed
+        /// </summary>
+        /// <param name="sccProject"></param>
+        /// <param name="oldName"></param>
+        /// <param name="newName"></param>
+        /// <param name="vSQUERYRENAMEDIRECTORYFLAGS"></param>
+        /// <param name="ok"></param>
+        internal void OnBeforeProjectDirectoryRename(IVsSccProject2 sccProject, string oldName, string newName, VSQUERYRENAMEDIRECTORYFLAGS vSQUERYRENAMEDIRECTORYFLAGS, out bool ok)
+        {
+            ok = true;
+        }
+
+        /// <summary>
+        /// Called when a directory in a project is renamed
+        /// </summary>
+        /// <param name="sccProject">The SCC project.</param>
+        /// <param name="oldName">The old name.</param>
+        /// <param name="newName">The new name.</param>
+        /// <param name="vSRENAMEDIRECTORYFLAGS">The v SRENAMEDIRECTORYFLAGS.</param>
+        internal void OnProjectDirectoryRename(IVsSccProject2 sccProject, string oldName, string newName, VSRENAMEDIRECTORYFLAGS vSRENAMEDIRECTORYFLAGS)
+        {
+        }
+	}
+}
--- trunk/src/Ankh.Scc/ProjectDocumentTracker.Add.cs
+++ trunk/src/Ankh.Scc/ProjectDocumentTracker.Add.cs
@@ -9,6 +9,31 @@ namespace Ankh.Scc
     partial class ProjectDocumentTracker
     {
         /// <summary>
+        /// This method notifies the client when a project has requested to add files.
+        /// </summary>
+        /// <param name="pProject"></param>
+        /// <param name="cFiles"></param>
+        /// <param name="rgpszMkDocuments"></param>
+        /// <param name="rgFlags"></param>
+        /// <param name="pSummaryResult"></param>
+        /// <param name="rgResults"></param>
+        /// <returns></returns>
+        public int OnQueryAddFiles(IVsProject pProject, int cFiles, string[] rgpszMkDocuments, VSQUERYADDFILEFLAGS[] rgFlags, VSQUERYADDFILERESULTS[] pSummaryResult, VSQUERYADDFILERESULTS[] rgResults)
+        {
+            // For now, we allow adding all files as is
+            // We might propose moving files to within a managed root
+
+            for (int i = 0; i < cFiles; i++)
+            {
+                rgResults[i] = VSQUERYADDFILERESULTS.VSQUERYADDFILERESULTS_AddOK;
+            }
+
+            pSummaryResult[0] = VSQUERYADDFILERESULTS.VSQUERYADDFILERESULTS_AddOK; // All ok
+
+            return VSConstants.S_OK;
+        }
+
+        /// <summary>
         /// Determines if it is okay to add a collection of files (possibly from source control) whose final destination may be different from a source location.
         /// </summary>
         /// <param name="pProject">[in] Project making the request about adding files.</param>
@@ -21,28 +46,109 @@ namespace Ankh.Scc
         /// <returns>
         /// If the method succeeds, it returns <see cref="F:Microsoft.VisualStudio.VSConstants.S_OK"></see>. If it fails, it returns an error code.
         /// </returns>
+        /// <remarks>Deny a query only if allowing the operation would compromise your stable state</remarks>
         public int OnQueryAddFilesEx(IVsProject pProject, int cFiles, string[] rgpszNewMkDocuments, string[] rgpszSrcMkDocuments, VSQUERYADDFILEFLAGS[] rgFlags, VSQUERYADDFILERESULTS[] pSummaryResult, VSQUERYADDFILERESULTS[] rgResults)
         {
-            return VSConstants.S_OK;
-        }    
+            // For now, we allow adding all files as is
+            // We might propose moving files to within a managed root
 
-        public int OnQueryAddFiles(IVsProject pProject, int cFiles, string[] rgpszMkDocuments, VSQUERYADDFILEFLAGS[] rgFlags, VSQUERYADDFILERESULTS[] pSummaryResult, VSQUERYADDFILERESULTS[] rgResults)
-        {
+            for (int i = 0; i < cFiles; i++)
+            {
+                rgResults[i] = VSQUERYADDFILERESULTS.VSQUERYADDFILERESULTS_AddOK;
+            }
+
+            pSummaryResult[0] = VSQUERYADDFILERESULTS.VSQUERYADDFILERESULTS_AddOK; // All ok
+            
             return VSConstants.S_OK;
         }
 
+        /// <summary>
+        /// This method notifies the client after a project has added files.
+        /// </summary>
+        /// <param name="cProjects">[in] Number of projects to which files were added.</param>
+        /// <param name="cFiles">[in] Number of files that were added.</param>
+        /// <param name="rgpProjects">[in] Array of projects to which files were added.</param>
+        /// <param name="rgFirstIndices">[in] Array of first indices identifying which project each file belongs to. For more information, see IVsTrackProjectDocumentsEvents2.</param>
+        /// <param name="rgpszMkDocuments">[in] Array of paths for the files that were processed. This is the same size as cFiles.</param>
+        /// <param name="rgFlags">[in] Array of flags. For a list of rgFlags values, see <see cref="VSADDFILEFLAGS" />.</param>
+        /// <returns></returns>
         public int OnAfterAddFilesEx(int cProjects, int cFiles, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSADDFILEFLAGS[] rgFlags)
         {
-            return VSConstants.S_OK;
-        }
+            int iFile = 0;
 
+            for (int iProject = 0; (iProject < cProjects) && (iFile < cFiles); iProject++)
+            {
+                int iLastFileThisProject = (iProject < cProjects - 1) ? rgFirstIndices[iProject + 1] : cFiles;
+
+                IVsProject project = rgpProjects[iProject];
+                IVsSccProject2 sccProject = project as IVsSccProject2;
+
+                for (; iFile < iLastFileThisProject; iFile++)
+                {
+                    if(sccProject == null)
+                        continue; // Not handled by our provider
+
+                    _sccProvider.OnProjectFileAdded(sccProject, rgpszMkDocuments[iFile], rgFlags[iFile]);
+                }
+            }
+            return VSConstants.S_OK;
+        }        
+
+        /// <summary>
+        /// This method notifies the client when a project has requested to add directories.
+        /// </summary>
+        /// <param name="pProject">[in] Project to which the directories will be added.</param>
+        /// <param name="cDirectories">[in] Number of directories to add.</param>
+        /// <param name="rgpszMkDocuments">[in] Array of paths for the directories to add.</param>
+        /// <param name="rgFlags">[in] Flags identifying information about each directory. For a list of rgFlags values, see <see cref=">VSQUERYADDDIRECTORYFLAGS"/.</param>
+        /// <param name="pSummaryResult">[out] Summary result object. This object is a summation of the yes and no results for the array of directories passed in rgpszMkDocuments.
+        /// If the result for a single directory is no, then pSummaryResult is equal to VSQUERYADDDIRECTORYRESULTS_AddNotOK; if the results for all directories are yes, then pSummaryResult is equal to VSQUERYADDDIRECTORYRESULTS_AddOK. For a list of pSummaryResult values, see VSQUERYADDDIRECTORYRESULTS.</param>
+        /// <param name="rgResults">[out] Array of results. For a list of rgResults values, see VSQUERYADDDIRECTORYRESULTS.</param>
+        /// <returns></returns>
+        /// <remarks>Deny a query only if allowing the operation would compromise your stable state</remarks>
         public int OnQueryAddDirectories(IVsProject pProject, int cDirectories, string[] rgpszMkDocuments, VSQUERYADDDIRECTORYFLAGS[] rgFlags, VSQUERYADDDIRECTORYRESULTS[] pSummaryResult, VSQUERYADDDIRECTORYRESULTS[] rgResults)
         {
+            for (int i = 0; i < cDirectories; i++)
+            {
+                rgResults[i] = VSQUERYADDDIRECTORYRESULTS.VSQUERYADDDIRECTORYRESULTS_AddOK;
+            }
+            
+            pSummaryResult[0] = VSQUERYADDDIRECTORYRESULTS.VSQUERYADDDIRECTORYRESULTS_AddOK; // All ok
+
             return VSConstants.S_OK;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cProjects"></param>
+        /// <param name="cDirectories"></param>
+        /// <param name="rgpProjects"></param>
+        /// <param name="rgFirstIndices"></param>
+        /// <param name="rgpszMkDocuments"></param>
+        /// <param name="rgFlags"></param>
+        /// <returns></returns>
+        /// <remarks>Deny a query only if allowing the operation would compromise your stable state</remarks>
         public int OnAfterAddDirectoriesEx(int cProjects, int cDirectories, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSADDDIRECTORYFLAGS[] rgFlags)
         {
+            int iDirectory = 0;
+
+            for (int iProject = 0; (iProject < cProjects) && (iDirectory < cDirectories); iProject++)
+            {
+                int iLastDirectoryThisProject = (iProject < cProjects - 1) ? rgFirstIndices[iProject + 1] : cDirectories;
+
+                IVsProject project = rgpProjects[iProject];
+                IVsSccProject2 sccProject = project as IVsSccProject2;
+
+                for (; iDirectory < iLastDirectoryThisProject; iDirectory++)
+                {
+                    if (sccProject == null)
+                        continue; // Not handled by our provider
+
+                    _sccProvider.OnProjectDirectoryAdded(sccProject, rgpszMkDocuments[iDirectory], rgFlags[iDirectory]);
+                }
+            }
+
             return VSConstants.S_OK;
         }        
     }
