cc10ebda1a0f7dd7e153f2ea6f7e34503b0d074c
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-03-13 17:50:17 +0000

    Manage solution folders as part of the solution. (Copy the scc data from the solution on opening and updating)
    * SccProjectData.cs
      Added property IsSolutionFolder which tells whether the IVsSccProject2 
      instance is in fact a solution folder.
    
    * AnkhSccProvider.Projects.cs
      Copy Solution Scc data to solution folders on opening, creating
      and adding/removing scc state

--- trunk/src/Ankh.Scc/SccProjectData.cs
+++ trunk/src/Ankh.Scc/SccProjectData.cs
@@ -4,12 +4,14 @@ using System.Text;
 using Microsoft.VisualStudio.Shell.Interop;
 using System.Runtime.InteropServices;
 using AnkhSvn.Ids;
+using Microsoft.VisualStudio;
 
 namespace Ankh.Scc
 {
     class SccProjectData
     {
         readonly IVsSccProject2 _project;
+        readonly bool _isSolutionFolder;
         bool _isManaged;
         bool _isRegistered;
 
@@ -19,6 +21,7 @@ namespace Ankh.Scc
                 throw new ArgumentNullException("project");
 
             _project = project;
+            _isSolutionFolder = IsSolutionFolderProject(project);
         }
 
         public IVsSccProject2 Project
@@ -47,6 +50,11 @@ namespace Ankh.Scc
             internal set { _isRegistered = value; }
         }
 
+        public bool IsSolutionFolder
+        {
+            get { return _isSolutionFolder; }
+        }
+
         internal void SetManaged(bool managed)
         {
             if (managed == IsManaged)
@@ -62,5 +70,28 @@ namespace Ankh.Scc
 
             IsManaged = managed;
         }
+
+
+        #region Helper code
+        /// <summary>
+        /// Checks whether the specified project is a solution folder
+        /// </summary>
+        private static readonly Guid _solutionFolderProjectId = new Guid("2150e333-8fdc-42a3-9474-1a3956d46de8");
+        static bool IsSolutionFolderProject(IVsSccProject2 project)
+        {
+            IPersistFileFormat pFileFormat = project as IPersistFileFormat;
+            if (pFileFormat != null)
+            {
+                Guid guidClassID;
+                if (VSConstants.S_OK == pFileFormat.GetClassID(out guidClassID)
+                    && guidClassID == _solutionFolderProjectId)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        #endregion
     }
 }
