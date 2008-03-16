941cbc8a57e5159990d780de028ca8704aaf8315
Bert Huijben <rhuijben@users.noreply.github.com>
2008-03-16 22:51:57 +0000

    Cleaned up MarkProjectDirty code to only mark the selected project dirty
    * src/Ankh/Ankh.csproj
    * src/Ankh/Commands/MarkProjectDirty.cs
      Moved the MarkProjectDirty code to Ankh.Scc as it depends om Scc internal
      knowledge.
    
    * src/Ankh.Scc/Ankh.Scc.csproj
    * src/Ankh.Scc/MarkProjectDirty.cs
      Resolve the passed project (when .RawObject is null) and then
      only refresh that project.

---
+++ src/Ankh.Scc/MarkProjectDirty.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio;
+using Ankh.UI;
+using Ankh.Selection;
+using Ankh.Commands;
+
+namespace Ankh.Scc
+{
+    [Command(AnkhSvn.Ids.AnkhCommand.MarkProjectDirty)]
+    public class MarkProjectDirty : ICommandHandler
+    {
+        public void OnUpdate(CommandUpdateEventArgs e)
+        {
+        }
+
+        public void OnExecute(CommandEventArgs e)
+        {
+            // Mark Scc Glyphs dirty, to force reload of glyphs from StatusCache
+            IEnumerable<SvnProject> projects = e.Argument as IEnumerable<SvnProject>;
+
+            IProjectFileMapper mapper = e.Context.GetService<IProjectFileMapper>();
+
+            if (projects == null || mapper == null)
+                return;
+
+            foreach (SvnProject p in projects)
+            {
+                SvnProject project = mapper.ResolveRawProject(p);
+
+                IVsSccProject2 scc = project.RawHandle as IVsSccProject2;
+
+                if(scc != null)
+                    scc.SccGlyphChanged(0, null, null, null);
+            }
+        }
+    }
+}
