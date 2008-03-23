8de350782a258a20ff7501787dfc2d678af0aa0c
Bert Huijben <rhuijben@users.noreply.github.com>
2008-03-23 12:07:29 +0000

    Commit files here before moving to the right place (WC issue workaround)
    Should have been committed by my previous commit

---
+++ Ankh.Configuration/ArrayOfStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Ankh.Configuration
+{
+    [Serializable]
+    [XmlRoot("ArrayOfString")]
+    public class ArrayOfStrings
+    {
+        [XmlElement("string")]
+        public string[] Strings = new string[0];
+    }
+}
