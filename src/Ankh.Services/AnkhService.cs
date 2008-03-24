1b8127d914ed65d89fcc5280b2a2f9388b98fe51
Bert Huijben <rhuijben@users.noreply.github.com>
2008-03-24 22:39:23 +0000

    Updated SharpSvn reference to r498.
    * src/Ankh.Services/Ankh.Services.csproj
      Updated SharpSvn reference
      This fixes the svn cp wc wc issues.
    
    * src/AnkhServices/AnkhService.cs  
      Added a new service baseclass which can be used as a start for new services

---
+++ src/Ankh.Services/AnkhService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.Design;
+
+namespace Ankh
+{
+    /// <summary>
+    /// Generic service baseclass
+    /// </summary>
+    public abstract class AnkhService : IAnkhServiceProvider
+    {
+        readonly IAnkhServiceProvider _context;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AnkhService"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        protected AnkhService(IAnkhServiceProvider context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            _context = context;
+
+        }
+
+        /// <summary>
+        /// Gets the context.
+        /// </summary>
+        /// <value>The context.</value>
+        protected IAnkhServiceProvider Context
+        {
+            get { return _context; }
+        }
+
+        #region IAnkhServiceProvider Members
+
+        /// <summary>
+        /// Gets the service object of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of service to get</typeparam>
+        /// <returns>
+        /// A service object of type <paramref name="serviceType"/>.-or- null if there is no service object of type <paramref name="serviceType"/>.
+        /// </returns>
+        T IAnkhServiceProvider.GetService<T>()
+        {
+            return _context.GetService<T>();
+        }
+
+        /// <summary>
+        /// Gets the service object of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of service to get</typeparam>
+        /// <returns>
+        /// A service object of type <paramref name="serviceType"/>.-or- null if there is no service object of type <paramref name="serviceType"/>.
+        /// </returns>
+        protected T GetService<T>()
+        {
+            return _context.GetService<T>();
+        }
+
+        /// <summary>
+        /// Gets the service object of the specified type.
+        /// </summary>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <returns>
+        /// A service object of type <paramref name="serviceType"/>.-or- null if there is no service object of type <paramref name="serviceType"/>.
+        /// </returns>
+        object IServiceProvider.GetService(Type serviceType)
+        {
+            return _context.GetService<IServiceContainer>();
+        }
+
+        /// <summary>
+        /// Gets the service object of the specified type.
+        /// </summary>
+        /// <param name="serviceType">An object that specifies the type of service object to get.</param>
+        /// <returns>
+        /// A service object of type <paramref name="serviceType"/>.-or- null if there is no service object of type <paramref name="serviceType"/>.
+        /// </returns>
+        protected object GetService(Type serviceType)
+        {
+            return _context.GetService<IServiceContainer>();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets the service container.
+        /// </summary>
+        /// <value>The service container.</value>
+        protected IServiceContainer ServiceContainer
+        {
+            get { return _context.GetService<IServiceContainer>(); }
+        }
+    }
+}
