ca8da659703a6b6ae8a486bf343b00fde039030e
Bert Huijben <rhuijben@users.noreply.github.com>
2008-04-01 12:16:09 +0000

    Added an Open in Visual Studio command handler.
    * src/Ankh/Ankh.csproj
    * src/Ankh/Commands/OpenInVisualStudio.cs
      Added handler for the open command in the pendingchanges window
    
    * src/Ankh.UI/PendingChanges/PendingCommitItem.cs
      More textual changes on the pendingchanges box

---
+++ src/Ankh/Commands/OpenInVisualStudio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AnkhSvn.Ids;
+using Microsoft.VisualStudio.Shell;
+
+namespace Ankh.Commands
+{
+    [Command(AnkhCommand.ItemOpenVisualStudio)]
+    class OpenInVisualStudio : CommandBase
+    {
+        public override void OnUpdate(CommandUpdateEventArgs e)
+        {
+            foreach (SvnItem item in e.Selection.GetSelectedSvnItems())
+            {
+                if (item.Exists)
+                    return;
+            }
+
+            e.Enabled = false;
+        }
+        public override void OnExecute(CommandEventArgs e)
+        {
+            foreach (SvnItem item in e.Selection.GetSelectedSvnItems())
+            {
+                if (item.Exists)
+                    VsShellUtilities.OpenDocument(e.Context, item.FullPath);
+            }            
+        }
+    }
+}
