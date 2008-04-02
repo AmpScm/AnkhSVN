739ac9d79309883026789028a6bc743321ac0a42
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-04-02 16:37:10 +0000

    Added Solution update command implementation and prepared a ProjectUpdate command.
    * src/Ankh/Ankh.csproj
    * src/Ankh.Ids/AnkhCommand.cs
    * src/Ankh/Commands/SolutionUpdateCommand.cs
      Added simple solution+project update command which look for the solution/project directories
      via our mapping api. (No error handling at all at this time).

--- trunk/src/Ankh.Ids/AnkhCommand.cs
+++ trunk/src/Ankh.Ids/AnkhCommand.cs
@@ -5,75 +5,75 @@ using System.Runtime.InteropServices;
 
 namespace AnkhSvn.Ids
 {
-	/// <summary>
-	/// List of ankh commands
-	/// </summary>
-	/// <remarks>
+    /// <summary>
+    /// List of ankh commands
+    /// </summary>
+    /// <remarks>
     /// <para>New items should be added at the end. Items should only be obsoleted between releases.</para>
     /// <para>The values of this enum are part of our interaction with other packages within Visual Studio</para>
-	/// </remarks>
-	[Guid(AnkhId.CommandSet)]
-	public enum AnkhCommand // : int
-	{
-		None = 0,
+    /// </remarks>
+    [Guid(AnkhId.CommandSet)]
+    public enum AnkhCommand // : int
+    {
+        None = 0,
 
         // Special task commands not used in menu's
         MarkProjectDirty,
         FileCacheFinishTasks,
-        SccFinishTasks,        
+        SccFinishTasks,
 
-		// These values live in the same numberspace as the other values within 
-		// the command set. So we start countin at this number to make sure we
-		// do not reuse values
-		CommandFirst = 0x1FFFFFFF,
+        // These values live in the same numberspace as the other values within 
+        // the command set. So we start countin at this number to make sure we
+        // do not reuse values
+        CommandFirst = 0x1FFFFFFF,
 
-		// Two test commands
-		FileSccMenuUpdateLatest,
-		FileSccMenuUpdateSpecific,
+        // Two test commands
+        FileSccMenuUpdateLatest,
+        FileSccMenuUpdateSpecific,
 
-		// Raw dump of old commands; to be sorted out
-		AddItem,
-		AddRepositoryRoot,
-		AddSolutionToRepository,
-		AddWorkingCopyExplorerRoot,
-		Blame,
-		ChangeAdminDirName,
-		Checkout,
-		CheckoutFolder,
-		CheckoutSolution,
-		Cleanup,
-		CommitItem,
-		CopyReposExplorerUrl,
-		CreatePatch,
-		DiffExternalLocalItem,
-		DiffLocalItem,
-		EditConfigFile,
-		Export,
-		ExportFolder,
-		Lock,
-		Log,
-		NewDirectory,
-		Refresh,
-		RefreshRepositoryItem,
-		Relocate,
-		RemoveRepositoryRoot,
-		RemoveWorkingCopyExplorerRoot,
-		ResolveConflict,
-		ResolveConflictExternal,
-		RevertToRevision,
-		RevertItem,
-		SaveToFile,
-		SendFeedback,
-		ShowPendingChanges,
-		ShowRepositoryExplorer,
-		ShowWorkingCopyExplorer,
-		SwitchItem,
-		ToggleAnkh,
-		Unlock,
-		UpdateItem,
-		ViewInVsNet,
-		ViewInWindows,
-		CheckForOrphanedTreeNodes,
+        // Raw dump of old commands; to be sorted out
+        AddItem,
+        AddRepositoryRoot,
+        AddSolutionToRepository,
+        AddWorkingCopyExplorerRoot,
+        Blame,
+        ChangeAdminDirName,
+        Checkout,
+        CheckoutFolder,
+        CheckoutSolution,
+        Cleanup,
+        CommitItem,
+        CopyReposExplorerUrl,
+        CreatePatch,
+        DiffExternalLocalItem,
+        DiffLocalItem,
+        EditConfigFile,
+        Export,
+        ExportFolder,
+        Lock,
+        Log,
+        NewDirectory,
+        Refresh,
+        RefreshRepositoryItem,
+        Relocate,
+        RemoveRepositoryRoot,
+        RemoveWorkingCopyExplorerRoot,
+        ResolveConflict,
+        ResolveConflictExternal,
+        RevertToRevision,
+        RevertItem,
+        SaveToFile,
+        SendFeedback,
+        ShowPendingChanges,
+        ShowRepositoryExplorer,
+        ShowWorkingCopyExplorer,
+        SwitchItem,
+        ToggleAnkh,
+        Unlock,
+        UpdateItem,
+        ViewInVsNet,
+        ViewInWindows,
+        CheckForOrphanedTreeNodes,
 
         PendingChangesSpacer, // Whitespace command to move all buttons a bit
 
@@ -81,12 +81,15 @@ namespace AnkhSvn.Ids
         CommitPendingChangesKeepingLocks,
 
         SolutionUpdateHead,
-        SolutionUpdateSpecific,        
+        SolutionUpdateSpecific,
+
+        ProjectUpdateHead,
+        ProjectUpdateSpecific,
 
         PendingChangesViewFlat,
         PendingChangesViewProject,
         PendingChangesViewChangeList,
-        PendingChangesViewFolder,                
+        PendingChangesViewFolder,
 
         PendingChangesViewAll,
         PendingChangesViewLogMessage,
@@ -102,7 +105,7 @@ namespace AnkhSvn.Ids
         ItemCompareCommitted,
         ItemComparePrevious,
         ItemCompareSpecific,
-        
+
         ItemOpenVisualStudio,
         ItemOpenWindows,
         ItemOpenTextEditor,
@@ -115,13 +118,13 @@ namespace AnkhSvn.Ids
         ItemIgnoreFile,
         ItemIgnoreFileType,
         ItemIgnoreFilesInFolder,
-        
+
         RefreshPendingChanges,
 
         SolutionSwitchCombo,
         SolutionSwitchComboFill,
-        SolutionSwitchDialog,        
+        SolutionSwitchDialog,
 
-		RunSvnCommand,
-	}
+        RunSvnCommand,
+    }
 }
