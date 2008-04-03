899dcd07013d194b57bc0ab62437a4ddb591b74f
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-04-03 15:08:00 +0000

    Added a dummy setting pages 'Subversion' below 'Source Control' in Tools->Options which currently contains a button showing the old dialog.
    * Ankh.Package/Ankh.Package.csproj
    * Ankh.Package/AnkhSourceControlSettingsPage.cs
      Added page wrapper
    
    * Ankh.Package/AnkhSvnPackage.cs
      Register the page with the ProvideOptionPage attribute
    
    * Ankh.Package/Gui/PendingChanges/AddToSourceControl.ico
    * Ankh.Package/Gui/PendingChanges/ChangeSourceControl.ico
      Added a few new icons which will be used in a future commit
    
    * Ankh.Package/VSPackage.resx
      Register localizable resources of the settings page
    
    * Ankh.UI/Ankh.UI.csproj
    * Ankh.UI/AnkhSettingsControl.cs
    * Ankh.UI/AnkhSettingsControl.Designer.cs
    * Ankh.UI/AnkhSettingsControl.resx
      Created dummy settings page with one button.

---
+++ trunk/src/Ankh.Package/AnkhSourceControlSettingsPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Shell;
+using Ankh.UI;
+
+namespace Ankh.VSPackage
+{
+    class AnkhSourceControlSettingsPage : DialogPage
+    {
+        AnkhSettingsControl _control;
+        protected override System.Windows.Forms.IWin32Window Window
+        {
+            get
+            {
+                return _control ?? (_control = CreateControl());
+            }
+        }
+
+        private AnkhSettingsControl CreateControl()
+        {
+            AnkhSettingsControl control = new AnkhSettingsControl();
+            IAnkhServiceProvider sp = (IAnkhServiceProvider)GetService(typeof(IAnkhServiceProvider));
+            
+            if (sp != null)
+                control.Context = sp;
+
+            return control;
+        }
+    }
+}
---
+++ trunk/src/Ankh.UI/AnkhSettingsControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Ankh.Commands;
+using AnkhSvn.Ids;
+
+namespace Ankh.UI
+{
+    public partial class AnkhSettingsControl : UserControl
+    {
+        IAnkhServiceProvider _context;
+        public AnkhSettingsControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the context.
+        /// </summary>
+        /// <value>The context.</value>
+        public IAnkhServiceProvider Context
+        {
+            get { return _context; }
+            set { _context = value; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IAnkhCommandService cs = Context.GetService<IAnkhCommandService>();
+
+            cs.DirectlyExecCommand(AnkhCommand.EditConfigFile, null);
+        }
+    }
+}
