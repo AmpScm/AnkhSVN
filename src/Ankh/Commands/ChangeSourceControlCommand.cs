3a453802f9eaae73a00c0c8c20b1ee0a0dfc5145
Bert Huijben <rhuijben@users.noreply.github.com>
2008-04-03 21:31:45 +0000

    Added a ChangeSourceControl dialog as required by the SCC specifications. Not that usefull for Subversion, but it allows unbreaking the solution from the SCC provider to allow adding it to an other SCC provider; and that part is required
    * src/Ankh.UI/Ankh.UI.csproj
    * src/Ankh.UI/SccManagement
    * src/Ankh.UI/SccManagement/ChangeSourceControl.cs
    * src/Ankh.UI/SccManagement/ChangeSourceControl.Designer.cs
    * src/Ankh.UI/SccManagement/ChangeSourceControl.resx
      Added ChangeSourceControl dialog with a simple gridview containing
      the current bindings. And a combobox with the solution root directory
    
    * src/Ankh/Ankh.csproj
    * src/Ankh/Commands/ChangeSourceControlCommand.cs
       Handle the File->Subversion->ChangeSourceControl command

---
+++ src/Ankh/Commands/ChangeSourceControlCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ankh.UI.SccManagement;
+using AnkhSvn.Ids;
+
+namespace Ankh.Commands
+{
+    [Command(AnkhCommand.FileSccChangeSourceControl)]
+    sealed class ChangeSourceControlCommand : CommandBase
+    {
+        public override void OnUpdate(CommandUpdateEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.Selection.SolutionFilename))
+                e.Enabled = e.Visible = false;
+        }
+
+        public override void OnExecute(CommandEventArgs e)
+        {
+            using (ChangeSourceControl csc = new ChangeSourceControl())
+            {
+                csc.Context = e.Context;
+
+                csc.ShowDialog(e.Context.DialogOwner);
+            }
+        }
+    }
+}
