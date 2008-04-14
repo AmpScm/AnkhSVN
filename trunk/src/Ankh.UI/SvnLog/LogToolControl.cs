884908ffe067c015a31753023ff1e8cde2448dbc
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-04-14 12:54:37 +0000

    Add files that should have been in my previous commit
    * src/Ankh.UI/SvnLog/LogToolControl.cs
    * src/Ankh.UI/SvnLog/LogToolControl.Designer.cs
    * src/Ankh.UI/SvnLog/LogToolControl.resx
    	New file, toolwindow containing the proof of concept log control(s)

---
+++ trunk/src/Ankh.UI/SvnLog/LogToolControl.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Ankh.UI.Presenters;
+using Ankh.UI.Services;
+using Microsoft.VisualStudio.Shell.Interop;
+using Ankh.Selection;
+
+namespace Ankh.UI.SvnLog
+{
+    public partial class LogToolControl : UserControl
+    {
+        IAnkhUISite _site;
+        SvnLogPresenter _presenter;
+
+        public LogToolControl()
+        {
+            InitializeComponent();
+        }
+
+        public override ISite Site
+        {
+            get { return base.Site; }
+            set
+            {
+                base.Site = value;
+
+                IAnkhUISite site = value as IAnkhUISite;
+
+                if (site != null)
+                {
+                    _site = site;
+                    
+                    if (_site.GetService<LogToolControl>() == null)
+                        _site.Package.AddService(typeof(LogToolControl), this);
+                    CreatePresenter(_site);
+                    //foreach (PendingChangesPage page in panel1.Controls)
+                    //{
+                    //    page.UISite = site;
+                    //}
+
+                    //UpdateColors();
+                    //UpdateCaption();
+                }
+            }
+        }
+
+        ISvnLogService LogService
+        {
+            get { return _site.GetService<ISvnLogService>();}
+        }
+
+        public ICollection<string> Target
+        {
+            get { return LogService.LocalTargets;}
+            set 
+            {
+                LogService.Cancel();
+                LogService.LocalTargets = value;
+                logDialogView1.Reset();
+
+                _presenter.Start(value);
+            }
+        }
+
+        void CreatePresenter(IAnkhServiceProvider context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+            ISelectionContext selection = context.GetService<ISelectionContext>();
+
+            List<string> selected = new List<string>(selection.GetSelectedFiles(false));
+            if(selected.Count == 0)
+                return;
+
+            ISvnLogService svc = context.GetService<ISvnLogService>();
+            _presenter = new SvnLogPresenter(logDialogView1, svc);
+            _presenter.Start(selected);
+        }
+    }
+}
