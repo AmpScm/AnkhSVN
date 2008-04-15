c945335b35cd6700ed4e3de79193fbedda3cefdb
Bert Huijben <rhuijben@users.noreply.github.com>
2008-04-15 13:52:07 +0000

    Handle project upgraded event as reason to mark the project file dirty
    * src/Ankh.Scc/ProjectTracker.Solution.cs
      Implemented IVsSolutionEventsProjectUpgrade interface

--- src/Ankh.Scc/ProjectTracker.Solution.cs
+++ src/Ankh.Scc/ProjectTracker.Solution.cs
@@ -8,7 +8,7 @@ using Ankh.Selection;
 
 namespace Ankh.Scc
 {
-    partial class ProjectTracker : IVsSolutionEvents, IVsSolutionEvents2, IVsSolutionEvents3, IVsSolutionEvents4
+    partial class ProjectTracker : IVsSolutionEvents, IVsSolutionEvents2, IVsSolutionEvents3, IVsSolutionEvents4, IVsSolutionEventsProjectUpgrade
     {
         #region IVsSolutionEvents Members
 
@@ -58,6 +58,10 @@ namespace Ankh.Scc
             {
                 _sccProvider.OnProjectOpened(project, fAdded != 0);
             }
+            //else
+            //{
+            //  IVsSccVirtualFolders vf = pHierarchy as IVsSccVirtualFolders; // Available for webprojects on a server
+            //}
 
             return VSConstants.S_OK;
         }
@@ -159,6 +163,45 @@ namespace Ankh.Scc
         public int OnQueryChangeProjectParent(IVsHierarchy pHierarchy, IVsHierarchy pNewParentHier, ref int pfCancel)
         {
             pfCancel = 0;
+            return VSConstants.S_OK;
+        }
+
+        #endregion
+
+        #region IVsSolutionEventsProjectUpgrade Members
+
+        /// <summary>
+        /// Defines a method to call after a project upgrade.
+        /// </summary>
+        /// <param name="pHierarchy">[in] Pointer to the <see cref="T:Microsoft.VisualStudio.Shell.Interop.IVsHierarchy"></see> interface of the project.</param>
+        /// <param name="fUpgradeFlag">[in] Integer. Flag indicating the nature of the upgrade. Values taken from the <see cref="T:Microsoft.VisualStudio.Shell.Interop.__VSPPROJECTUPGRADEVIAFACTORYFLAGS"></see> enumeration. Will only be PUVFF_COPYUPGRADE, PUVFF_SXSBACKUP, or PUVFF_COPYBACKUP.</param>
+        /// <param name="bstrCopyLocation">[in] String containing the location of the copy upgrade (PUVFF_COPYUPGRADE) or back up copy (PUVFF_COPYBACKUP).</param>
+        /// <param name="stUpgradeTime">[in] A <see cref="T:Microsoft.VisualStudio.Shell.Interop.SYSTEMTIME"></see> value. The time the upgrade was done.</param>
+        /// <param name="pLogger">[in] Pointer to an <see cref="T:Microsoft.VisualStudio.Shell.Interop.IVsUpgradeLogger"></see> interface to use for logging upgrade messages.</param>
+        /// <returns>
+        /// If the method succeeds, it returns <see cref="F:Microsoft.VisualStudio.VSConstants.S_OK"></see>. If it fails, it returns an error code.
+        /// </returns>
+        public int OnAfterUpgradeProject(IVsHierarchy pHierarchy, uint fUpgradeFlag, string bstrCopyLocation, SYSTEMTIME stUpgradeTime, IVsUpgradeLogger pLogger)
+        {
+            IProjectFileMapper mapper = GetService<IProjectFileMapper>();
+            IFileStatusMonitor monitor = GetService<IFileStatusMonitor>();
+
+            if(monitor == null || mapper == null)
+                return VSConstants.S_OK;
+
+            if (!string.IsNullOrEmpty(bstrCopyLocation))
+                monitor.ScheduleSvnStatus(bstrCopyLocation);
+                
+            IVsSccProject2 project = pHierarchy as IVsSccProject2;
+
+            if (project != null)
+            {
+                ISvnProjectInfo info = mapper.GetProjectInfo(new SvnProject(null, project));
+
+                if (info != null && !string.IsNullOrEmpty(info.ProjectFile))
+                    monitor.ScheduleSvnStatus(info.ProjectFile);
+            }
+
             return VSConstants.S_OK;
         }
 
