4e6debf575e38cc508479569052373d800672dc0
Bert Huijben <rhuijben@users.noreply.github.com>
2008-04-21 16:04:07 +0000

    Use the live selection for the extender provider instead of a sometimes invalid cached value
    * src/Ankh/Ankh.csproj
    * src/Ankh/Extenders/ResourceExtender.cs
    * src/Ankh/Extenders/SvnItemExtender.cs
      Renamed file to match class name and use the selection context from the SvnItem

--- src/Ankh/Extenders/SvnItemExtender.cs
+++ src/Ankh/Extenders/SvnItemExtender.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 
 
 using SharpSvn;
+using Ankh.Selection;
 
 
 namespace Ankh.Extenders
@@ -14,6 +15,7 @@ namespace Ankh.Extenders
     [ComVisible(true)]
     public class SvnItemExtender
     {
+        readonly ISelectionContext _selContext;
         readonly IAnkhServiceProvider _context;
         readonly SvnItem _item;
         public SvnItemExtender(SvnItem item, IAnkhServiceProvider context)
@@ -25,60 +27,82 @@ namespace Ankh.Extenders
 
             _item = item;
             _context = context;
+            _selContext = context.GetService<ISelectionContext>();
         }
 
-        [Category("Subversion"), Description("URL")]
+        [Browsable(false)]
+        internal SvnItem SvnItem
+        {
+            get 
+            {
+                foreach (SvnItem item in _selContext.GetSelectedSvnItems(false))
+                {
+                    return item;
+                }
+
+                return _item; 
+            }
+        }
+
+        [Category("Subversion"), Description("Url"), DisplayName("Url")]
         public Uri Url
         {
-            get { return _item.Status.Uri; }
+            get { return SvnItem.Status.Uri; }
         }
 
-        [Category("Subversion"), Description("Repository UUID")]
+        [Category("Subversion"), DisplayName("Change List"), Description("Change List")]
+        public string ChangeList
+        {
+            get { return SvnItem.Status.RepositoryId; }
+        }
+
+
+        [Category("Subversion"), Description("Repository UUID"), DisplayName("Repository UUID")]
         public string RepositoryUuid
         {
-            get { return _item.Status.RepositoryId; }
+            get { return SvnItem.Status.RepositoryId; }
         }
 
         [Category("Subversion"), Description("Last committed author")]
         public string LastCommittedAuthor
         {
-            get { return _item.Status.LastChangeAuthor; }
+            get { return SvnItem.Status.LastChangeAuthor; }
         }
 
         [Category("Subversion"), Description("Revision")]
         public long Revision
         {
-            get { return _item.Status.Revision; }
+            get { return SvnItem.Status.Revision; }
         }
 
         [Category("Subversion"), Description("Last committed date")]
         public DateTime LastCommittedDate
         {
-            get { return _item.Status.LastChangeTime.ToLocalTime(); }
+            get { return SvnItem.Status.LastChangeTime.ToLocalTime(); }
         }
 
         [Category("Subversion"), Description("Last committed revision")]
         public long LastCommittedRevision
         {
-            get { return _item.Status.LastChangeRevision; }
+            get { return SvnItem.Status.LastChangeRevision; }
         }
 
         [Category("Subversion"), Description("Text status")]
         public string TextStatus
         {
-            get { return _item.Status.LocalContentStatus.ToString(); }
+            get { return SvnItem.Status.LocalContentStatus.ToString(); }
         }
 
         [Category("Subversion"), Description("Property status")]
         public string PropertyStatus
         {
-            get { return _item.Status.LocalPropertyStatus.ToString(); }
+            get { return SvnItem.Status.LocalPropertyStatus.ToString(); }
         }
 
         [Category("Subversion"), Description("Locked")]
         public bool Locked
         {
-            get { return _item.Status.IsLockedLocal; }
+            get { return SvnItem.Status.IsLockedLocal; }
         }
     }
 }
