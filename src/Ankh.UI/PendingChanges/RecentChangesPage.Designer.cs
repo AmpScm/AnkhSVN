896e68a5e2c1108881497101e07ab94f7d4eabf7
Bert Huijben <rhuijben@users.noreply.github.com>
2008-04-24 13:31:43 +0000

    Fixed stupid typo in placeholder text
    * src/Ankh.UI/PendingChanges/RecentChangesPage.Designer.cs
      Fixed stupid typo in placeholder text

--- src/Ankh.UI/PendingChanges/RecentChangesPage.Designer.cs
+++ src/Ankh.UI/PendingChanges/RecentChangesPage.Designer.cs
@@ -38,7 +38,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(168, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Placeholder for recent pages view";
+            this.label1.Text = "Placeholder for recent changes view";
             // 
             // RecentChangesPage
             // 
