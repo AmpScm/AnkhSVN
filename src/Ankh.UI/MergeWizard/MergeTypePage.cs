3b4d2e5a46cefcc569563b08220a8bf1bcbf500b
Jeremy Whitlock <jwhitlock@apache.org>
2008-05-07 20:51:34 +0000

    Initial work on the merge wizard for AnkhSVN's merge capabilities.  This commit contains the necessary components for the wizard to work and the initial implementation of the merge type page, the first page of the merge wizard. (WORK IN PROGRESS)
    * src/Ankh.UI/Ankh.UI.csproj: Updated to locate the merge wizard sources.
    
    * src/Ankh.UI/MergeWizard/images: Added all images necessary for the merge
      wizard to this point.
    
    * src/Ankh.UI/MergeWizard/MergeTypePage.cs: The first page in the merge wizard.
    
    * src/Ankh.UI/MergeWizard/MergeTypePageControl.*: Resources for the merge type wizard
      page.
    
    * src/Ankh.UI/MergeWizard/MergeWizard.cs: The wizard implementation source.
    
    * src/Ankh.UI/MergeWizard/MergeWizardDialog.cs: The wizard dialog implementation.
    
    * src/Ankh.UI/MergeWizard/Resources.*: The localization resources for the merge wizard.
    
    * src/Ankh.UI/MergeWizard/TestMergeWizardUI.cs: Simple script for displaying (testing) the
      merge wizard.
    
    * src/lib/WizardFramework.dll: Updated to support newer version of the wizard framework
      which fixed a few sizing issues.

---
+++ src/Ankh.UI/MergeWizard/MergeTypePage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Resources;
+using WizardFramework;
+
+namespace Ankh.UI.MergeWizard
+{
+    /// <summary>
+    /// Implementation of a wizard page for handling the merge type
+    /// selection of AnkhSVN's merge capabilities
+    /// </summary>
+    class MergeTypePage : WizardPage
+    {
+        public MergeTypePage() : base("Merge Type")
+        {
+            IsPageComplete = true;
+
+            Title = resman.GetString("MergeTypePageHeaderTitle");
+            this.Message = new WizardMessage(resman.GetString("MergeTypePageHeaderMessage"));
+            this.Image = (System.Drawing.Image)resman.GetObject("MergeWizardHeaderImage");
+        }
+
+        /// <see cref="WizardFramework.WizardPage.Control" />
+        public override System.Windows.Forms.UserControl Control
+        {
+            get { return control_prop; }
+        }
+
+        private System.Windows.Forms.UserControl control_prop = new MergeTypePageControl();
+        private ResourceManager resman = new ResourceManager("Ankh.UI.MergeWizard.Resources", Assembly.GetExecutingAssembly());
+    }
+}
---
+++ src/Ankh.UI/MergeWizard/MergeTypePageControl.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Resources;
+using System.Reflection;
+
+namespace Ankh.UI.MergeWizard
+{
+    public partial class MergeTypePageControl : UserControl
+    {
+        public MergeTypePageControl()
+        {
+            InitializeComponent();
+        }
+
+        #region UI Events
+        private void rangeofRevisionsRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            ToggleMergeTypeInformation(this, (RadioButton)sender);
+        }
+
+        private void reintegrateABranchRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            ToggleMergeTypeInformation(this, (RadioButton)sender);
+        }
+
+        private void changesetBasedRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            ToggleMergeTypeInformation(this, (RadioButton)sender);
+        }
+
+        private void twoDifferentTreesRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            ToggleMergeTypeInformation(this, (RadioButton)sender);
+        }
+
+        private void manuallyRecordMergeRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            ToggleMergeTypeInformation(this, (RadioButton)sender);
+        }
+
+        private void manuallyRemoveRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            ToggleMergeTypeInformation(this, (RadioButton)sender);
+        }
+        #endregion
+
+        private void ToggleMergeTypeInformation(MergeTypePageControl page, RadioButton radioButton)
+        {
+            if (radioButton.Name == "rangeOfRevisionsRadioButton")
+            {
+                page.mergeTypeDescriptionLabel.Text = resman.GetString("RangeOfRevisionsDescription");
+                page.mergeTypePictureBox.Image = (System.Drawing.Image)resman.GetObject("RangeOfRevisionsImage");
+            }
+            else if (radioButton.Name == "reintegrateABranchRadioButton")
+            {
+                page.mergeTypeDescriptionLabel.Text = resman.GetString("ReintegrateABranchDescription");
+                page.mergeTypePictureBox.Image = (System.Drawing.Image)resman.GetObject("ReintegrateABranchImage");
+            }
+            else if (radioButton.Name == "changesetBasedRadioButton")
+            {
+                page.mergeTypeDescriptionLabel.Text = resman.GetString("ChangesetBasedDescription");
+                page.mergeTypePictureBox.Image = (System.Drawing.Image)resman.GetObject("ChangesetBasedImage");
+            }
+            else if (radioButton.Name == "twoDifferentTreesRadioButton")
+            {
+                page.mergeTypeDescriptionLabel.Text = resman.GetString("TwoDifferentTreesDescription");
+                page.mergeTypePictureBox.Image = (System.Drawing.Image)resman.GetObject("TwoDifferentTreesImage");
+            }
+            else if (radioButton.Name == "manuallyRecordRadioButton")
+            {
+                page.mergeTypeDescriptionLabel.Text = resman.GetString("ManuallyRecordDescription");
+                page.mergeTypePictureBox.Image = (System.Drawing.Image)resman.GetObject("ManuallyRecordImage");
+            }
+            else if (radioButton.Name == "manuallyRemoveRadioButton")
+            {
+                page.mergeTypeDescriptionLabel.Text = resman.GetString("ManuallyRemoveDescription");
+                page.mergeTypePictureBox.Image = (System.Drawing.Image)resman.GetObject("ManuallyRemoveImage");
+            }
+        }
+
+        private ResourceManager resman = new ResourceManager("Ankh.UI.MergeWizard.Resources", Assembly.GetExecutingAssembly());
+    }
+}
