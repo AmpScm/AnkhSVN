a05c57e2bd0b548c7523cbd90a8c32124352246a
jwhitlock <jwhitlock@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-05-07 20:51:34 +0000

    Initial work on the merge wizard for AnkhSVN's merge capabilities.  This commit contains the necessary components for the wizard to work and the initial implementation of the merge type page, the first page of the merge wizard. (WORK IN PROGRESS)
    * src/Ankh.UI/Ankh.UI.csproj: Updated to locate the merge wizard sources.
    
    * src/Ankh.UI/MergeWizard/images: Added all images necessary for the merge
      wizard to this point.
    
    * src/Ankh.UI/MergeWizard/MergeTypePage.cs: The first page in the merge wizard.
    
    * src/Ankh.UI/MergeWizard/MergeTypePageControl.*: Resources for the merge type wizard
      page.
    
    * src/Ankh.UI/MergeWizard/MergeWizard.cs: The wizard implementation source.
    
    * src/Ankh.UI/MergeWizard/MergeWizardDialog.cs: The wizard dialog implementation.
    
    * src/Ankh.UI/MergeWizard/Resources.*: The localization resources for the merge wizard.
    
    * src/Ankh.UI/MergeWizard/TestMergeWizardUI.cs: Simple script for displaying (testing) the
      merge wizard.
    
    * src/lib/WizardFramework.dll: Updated to support newer version of the wizard framework
      which fixed a few sizing issues.

---
+++ trunk/src/Ankh.UI/MergeWizard/MergeWizard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WizardFramework;
+using System.Resources;
+using System.Reflection;
+
+namespace Ankh.UI.MergeWizard
+{
+    /// <summary>
+    /// This is the wizard implementation for AnkhSVN's merge capability.
+    /// </summary>
+    class MergeWizard : Wizard
+    {
+        public MergeWizard()
+            : base()
+        {
+            this.WindowTitle = resman.GetString("MergeWizardTitle");
+        }
+
+        public override void AddPages()
+        {
+            WizardPage mergeType = new MergeTypePage();
+            AddPage(mergeType);
+        }
+
+        /// <see cref="WizardFramework.Wizard.PerformFinish" />
+        public override bool PerformFinish()
+        {
+            // TODO: Implement
+            return true;
+        }
+
+        private ResourceManager resman = new ResourceManager("Ankh.UI.MergeWizard.Resources", Assembly.GetExecutingAssembly());
+    }
+}
---
+++ trunk/src/Ankh.UI/MergeWizard/MergeWizardDialog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WizardFramework;
+using System.Resources;
+using System.Reflection;
+
+namespace Ankh.UI.MergeWizard
+{
+    /// <summary>
+    /// Implementation of a wizard dialog for AnkhSVN's merge capabilities.
+    /// </summary>
+    class MergeWizardDialog : WizardDialog
+    {
+        public MergeWizardDialog() : base()
+        {
+            this.Text = "Merge Wizard";
+            this.Icon = (System.Drawing.Icon)resman.GetObject("MergeWizardIcon");
+            this.Wizard = wizard;
+        }
+
+        private Wizard wizard = new MergeWizard();
+        private ResourceManager resman = new ResourceManager("Ankh.UI.MergeWizard.Resources", Assembly.GetExecutingAssembly());
+    }
+}
