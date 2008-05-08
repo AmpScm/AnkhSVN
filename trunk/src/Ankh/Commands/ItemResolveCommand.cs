6a12558df7e7729ae618653923fd9f725a68c566
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-05-08 13:42:37 +0000

    Implemented several of the resolve commands
    * src/Ankh/Commands/ItemResolveCommand.cs
      Added simple implementation of the non-UI resolve commands. Some confirmation should be added

--- trunk/src/Ankh/Commands/ItemResolveCommand.cs
+++ trunk/src/Ankh/Commands/ItemResolveCommand.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Ankh.Ids;
+using SharpSvn;
 
 namespace Ankh.Commands
 {
@@ -37,15 +38,52 @@ namespace Ankh.Commands
                 e.Visible = e.Enabled = false;
             else if (!canDiff && (e.Command == AnkhCommand.ItemResolveTheirsConflict || e.Command == AnkhCommand.ItemResolveMineConflict))
                 e.Visible = e.Enabled = false;
+            else if(/* Subversion 1.5 && */ (e.Command == AnkhCommand.ItemResolveTheirsConflict || e.Command == AnkhCommand.ItemResolveMineConflict))
+                e.Visible = e.Enabled = false;
             else
             {               
-                //e.Enabled = false; // Not supported yet
             }
         }
 
         public override void OnExecute(CommandEventArgs e)
         {
-            throw new NotImplementedException();
+            SvnAccept accept = SvnAccept.Postpone;
+            switch (e.Command)
+            {
+                case AnkhCommand.ItemResolveMineFull:
+                    accept = SvnAccept.MineFull;
+                    break;
+                case AnkhCommand.ItemResolveTheirsFull:
+                    accept = SvnAccept.TheirsFull;
+                    break;
+                case AnkhCommand.ItemResolveWorking:
+                    accept = SvnAccept.Merged;
+                    break;
+                case AnkhCommand.ItemResolveBase:
+                    accept = SvnAccept.Base;
+                    break;
+                case AnkhCommand.ItemResolveMineConflict:
+                case AnkhCommand.ItemResolveTheirsConflict:
+                default:
+                    throw new NotImplementedException();                
+            }
+            if(accept == SvnAccept.Postpone)
+                throw new NotImplementedException();
+
+            using (SvnClient client = e.GetService<ISvnClientPool>().GetNoUIClient())
+            {
+                SvnResolveArgs a = new SvnResolveArgs();
+                a.Depth = SvnDepth.Empty;
+
+                foreach(SvnItem item in e.Selection.GetSelectedSvnItems(true))
+                {
+                    if(!item.IsConflicted)
+                        continue;
+
+                    // Let the command throw exceptions for now
+                    client.Resolve(item.FullPath, accept, a);
+                }                
+            }
         }
     }
 }
