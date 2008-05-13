b81f09c1856aa9c062558ac285ed6cbb35487eb4
Jeremy Whitlock <jwhitlock@apache.org>
2008-05-13 23:01:27 +0000

    Added the "Range of Revisions" merge source page.
    * src/Ankh.UI/MergeWizard/MergeSourceRangeOfRevisionsPageControl.Designer.cs
    * src/Ankh.UI/MergeWizard/MergeSourceRangeOfRevisionsPageControl.resx
    * src/Ankh.UI/MergeWizard/MergeSourceRangeOfRevisionsPageControl.cs
    * src/Ankh.UI/MergeWizard/MergeSourceRangeOfRevisionsPage.cs
      Added.
    
    * src/Ankh.UI/MergeWizard/Resources.resx
    
    * src/Ankh.UI/Ankh.UI.csproj
      Updated for new wizard page.
    
    * src/Ankh.UI/MergeWizard/MergeWizard.cs
      Updated the display logic for the new page.
    
    * src/Ankh.UI/MergeWizard/MergeBestPracticesPage.cs
      Added logic that will eventually be used to decide whether or not to display the best practices page as needed.

---
+++ src/Ankh.UI/MergeWizard/MergeSourceRangeOfRevisionsPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Resources;
+using WizardFramework;
+
+namespace Ankh.UI.MergeWizard
+{
+    class MergeSourceRangeOfRevisionsPage : WizardPage
+    {
+        public MergeSourceRangeOfRevisionsPage() : base("Merge Source Range Of Revisions")
+        {
+            IsPageComplete = false;
+            Title = resman.GetString("MergeSourceHeaderTitle");
+            this.Message = new WizardMessage(resman.GetString("MergeSourceRangeOfRevisionsPageHeaderMessage"));
+        }
+
+        /// <see cref="WizardFramework.IWizardPage.Control" />
+        public override System.Windows.Forms.UserControl Control
+        {
+            get { return control_prop; }
+        }
+
+        private System.Windows.Forms.UserControl control_prop = new MergeSourceRangeOfRevisionsPageControl();
+        private ResourceManager resman = new ResourceManager("Ankh.UI.MergeWizard.Resources", Assembly.GetExecutingAssembly());
+    }
+}
---
+++ src/Ankh.UI/MergeWizard/MergeSourceRangeOfRevisionsPageControl.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Ankh.UI.MergeWizard
+{
+    partial class MergeSourceRangeOfRevisionsPageControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MergeSourceRangeOfRevisionsPageControl));
+            this.mergeFromLabel = new System.Windows.Forms.Label();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.mergeFromTextBox = new System.Windows.Forms.TextBox();
+            this.revisionsGroupBox = new System.Windows.Forms.GroupBox();
+            this.allRevisionsRadioButton = new System.Windows.Forms.RadioButton();
+            this.selectRevisionsRadioButton = new System.Windows.Forms.RadioButton();
+            this.revisionsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mergeFromLabel
+            // 
+            resources.ApplyResources(this.mergeFromLabel, "mergeFromLabel");
+            this.mergeFromLabel.Name = "mergeFromLabel";
+            // 
+            // selectButton
+            // 
+            resources.ApplyResources(this.selectButton, "selectButton");
+            this.selectButton.Name = "selectButton";
+            this.selectButton.UseVisualStyleBackColor = true;
+            // 
+            // mergeFromTextBox
+            // 
+            resources.ApplyResources(this.mergeFromTextBox, "mergeFromTextBox");
+            this.mergeFromTextBox.Name = "mergeFromTextBox";
+            // 
+            // revisionsGroupBox
+            // 
+            resources.ApplyResources(this.revisionsGroupBox, "revisionsGroupBox");
+            this.revisionsGroupBox.Controls.Add(this.selectRevisionsRadioButton);
+            this.revisionsGroupBox.Controls.Add(this.allRevisionsRadioButton);
+            this.revisionsGroupBox.Name = "revisionsGroupBox";
+            this.revisionsGroupBox.TabStop = false;
+            // 
+            // allRevisionsRadioButton
+            // 
+            resources.ApplyResources(this.allRevisionsRadioButton, "allRevisionsRadioButton");
+            this.allRevisionsRadioButton.Checked = true;
+            this.allRevisionsRadioButton.Name = "allRevisionsRadioButton";
+            this.allRevisionsRadioButton.TabStop = true;
+            this.allRevisionsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // selectRevisionsRadioButton
+            // 
+            resources.ApplyResources(this.selectRevisionsRadioButton, "selectRevisionsRadioButton");
+            this.selectRevisionsRadioButton.Name = "selectRevisionsRadioButton";
+            this.selectRevisionsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // MergeSourceRangeOfRevisionsPageControl
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.revisionsGroupBox);
+            this.Controls.Add(this.mergeFromTextBox);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.mergeFromLabel);
+            this.Name = "MergeSourceRangeOfRevisionsPageControl";
+            this.revisionsGroupBox.ResumeLayout(false);
+            this.revisionsGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mergeFromLabel;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.TextBox mergeFromTextBox;
+        private System.Windows.Forms.GroupBox revisionsGroupBox;
+        private System.Windows.Forms.RadioButton allRevisionsRadioButton;
+        private System.Windows.Forms.RadioButton selectRevisionsRadioButton;
+    }
+}
