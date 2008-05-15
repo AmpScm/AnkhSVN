0127beb105c226de96d073bea472ac4eda40cd58
jwhitlock <jwhitlock@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-05-15 21:30:51 +0000

    Added the "Manually Record" merge source page.
    * src/Ankh.UI/MergeWizard/MergeSourceManuallyRecordPageControl.Designer.cs
    * src/Ankh.UI/MergeWizard/MergeSourceManuallyRecordPageControl.cs
    * src/Ankh.UI/MergeWizard/MergeSourceManuallyRecordPage.cs
    * src/Ankh.UI/MergeWizard/MergeSourceManuallyRecordPageControl.resx
    * src/Ankh.UI/MergeWizard/MergeSourceReintegratePageControl.resx
      Added.
    
    * src/Ankh.UI/Ankh.UI.csproj
    * src/Ankh.UI/MergeWizard/Resources.resx
    * src/Ankh.UI/MergeWizard/MergeWizard.cs
      Updated for the new merge source page.

---
+++ trunk/src/Ankh.UI/MergeWizard/MergeSourceManuallyRecordPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Resources;
+using WizardFramework;
+
+namespace Ankh.UI.MergeWizard
+{
+    /// <summary>
+    /// Implementation of a wizard page for retrieving the merge source
+    /// information for a merge where you manually record revision changes.
+    /// </summary>
+    class MergeSourceManuallyRecordPage : WizardPage
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MergeSourceManuallyRecordPage()
+            : base("Merge Source Manually Record")
+        {
+            IsPageComplete = false;
+            Title = resman.GetString("MergeSourceHeaderTitle");
+            Description = resman.GetString("MergeSourceManuallyRecordPageHeaderMessage");
+        }
+
+        /// <see cref="WizardFramework.IWizardPage.Control" />
+        public override System.Windows.Forms.UserControl Control
+        {
+            get { return control_prop; }
+        }
+
+        private System.Windows.Forms.UserControl control_prop = new MergeSourceManuallyRecordPageControl();
+        private ResourceManager resman = new ResourceManager("Ankh.UI.MergeWizard.Resources", Assembly.GetExecutingAssembly());
+    }
+}
