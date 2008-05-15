6be27be4a3d5a5f3634e23cdfb8ea85e1ddabd5c
Jeremy Whitlock <jwhitlock@apache.org>
2008-05-15 23:29:55 +0000

    Added the "Manually Remove" merge source page.
    * src/Ankh.UI/MergeWizard/MergeSourceManuallyRemovePageControl.resx
    * src/Ankh.UI/MergeWizard/MergeSourceManuallyRemovePageControl.Designer.cs
    * src/Ankh.UI/MergeWizard/MergeSourceManuallyRemovePageControl.cs
    * src/Ankh.UI/MergeWizard/MergeSourceManuallyRemovePage.cs
      Added.
    
    * src/Ankh.UI/Ankh.UI.csproj
    * src/Ankh.UI/MergeWizard/MergeWizard.cs
    * src/Ankh.UI/MergeWizard/Resources.resx
      Updated for the new merge source page.

---
+++ src/Ankh.UI/MergeWizard/MergeSourceManuallyRemovePage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Resources;
+using WizardFramework;
+
+namespace Ankh.UI.MergeWizard
+{
+    /// <summary>
+    /// Implementation of a wizard page for retrieving the merge source
+    /// information for a merge where you manually record revision changes.
+    /// </summary>
+    class MergeSourceManuallyRemovePage : WizardPage
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MergeSourceManuallyRemovePage()
+            : base("Merge Source Manually Remove")
+        {
+            IsPageComplete = false;
+            Title = resman.GetString("MergeSourceHeaderTitle");
+            Description = resman.GetString("MergeSourceManuallyRemovePageHeaderMessage");
+        }
+
+        /// <see cref="WizardFramework.IWizardPage.Control" />
+        public override System.Windows.Forms.UserControl Control
+        {
+            get { return control_prop; }
+        }
+
+        private System.Windows.Forms.UserControl control_prop = new MergeSourceManuallyRemovePageControl();
+        private ResourceManager resman = new ResourceManager("Ankh.UI.MergeWizard.Resources", Assembly.GetExecutingAssembly());
+    }
+}
---
+++ src/Ankh.UI/MergeWizard/MergeSourceManuallyRemovePageControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ankh.UI.MergeWizard
+{
+    public partial class MergeSourceManuallyRemovePageControl : UserControl
+    {
+        public MergeSourceManuallyRemovePageControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
