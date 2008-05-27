5de5cdbdaaf8a5dfe491d67174621cf7683691fc
jwhitlock <jwhitlock@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-05-27 22:15:43 +0000

    Updated the "Two Different Trees" merge source so that the UI works, other than the repository browser.
    * src/Ankh.UI/MergeWizard/MergeSourceTwoDifferentTreesPageControl.cs
    * src/Ankh.UI/MergeWizard/MergeSourceTwoDifferentTreesPageControl.resx
    * src/Ankh.UI/MergeWizard/MergeSourceTwoDifferentTreesPageControl.Designer.cs
    * src/Ankh.UI/MergeWizard/MergeSourceTwoDifferentTreesPage.cs
      Updated as mentioned above.

--- trunk/src/Ankh.UI/MergeWizard/MergeSourceTwoDifferentTreesPageControl.cs
+++ trunk/src/Ankh.UI/MergeWizard/MergeSourceTwoDifferentTreesPageControl.cs
@@ -5,14 +5,57 @@ using System.Drawing;
 using System.Data;
 using System.Text;
 using System.Windows.Forms;
+using WizardFramework;
 
 namespace Ankh.UI.MergeWizard
 {
     public partial class MergeSourceTwoDifferentTreesPageControl : UserControl
     {
+        private WizardPage _wizardPage;
+
         public MergeSourceTwoDifferentTreesPageControl()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Gets/Sets the wizard page associated with this control.
+        /// </summary>
+        public WizardPage WizardPage
+        {
+            set { _wizardPage = value; }
+            get { return _wizardPage; }
+        }
+
+        #region UI Events
+        private void useFromURLCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((CheckBox)sender).Checked)
+            {
+                toURLSelectButton.Enabled = false;
+                toURLSelectButton.Visible = false;
+
+                toURLTextBox.Enabled = false;
+                toURLTextBox.Visible = false;
+                toURLTextBox.Text = "";
+            }
+            else
+            {
+                toURLSelectButton.Enabled = true;
+                toURLSelectButton.Visible = true;
+
+                toURLTextBox.Enabled = true;
+                toURLTextBox.Visible = true;
+                toURLTextBox.Text = fromURLTextBox.Text;
+                toURLTextBox.SelectAll();
+            }
+        }
+
+        private void MergeSourceTwoDifferentTreesPageControl_Load(object sender, EventArgs e)
+        {
+            fromURLTextBox.Text = ((MergeWizard)WizardPage.Wizard).MergeTarget.Status.Uri.ToString();
+            fromURLTextBox.SelectAll();
+        }
+        #endregion
     }
 }
