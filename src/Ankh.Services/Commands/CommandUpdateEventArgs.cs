71fe44487e5cfa789628546b4cc32f0b9a7f8de1
Bert Huijben <rhuijben@users.noreply.github.com>
2008-05-27 20:54:38 +0000

    Updated SharpSvnReference and added some comments on CommandUpdateEventArgs
    * src/Ankh.Services/Ankh.Services.csproj
      Updated SharpSvn reference
      
    * src/Ankh.Services/Commands/CommandUpdateEventArgs.cs
      Documented several properties

--- src/Ankh.Services/Commands/CommandUpdateEventArgs.cs
+++ src/Ankh.Services/Commands/CommandUpdateEventArgs.cs
@@ -36,42 +36,72 @@ namespace Ankh.Commands
         {
         }
 
+        /// <summary>
+        /// The command is enabled
+        /// </summary>
         public bool Enabled
         {
             get { return !_disabled; }
             set { _disabled = !value; }
         }
 
+        /// <summary>
+        /// The command is visible (Requires dynamicVisibility)
+        /// </summary>
         public bool Visible
         {
             get { return !_invisible; }
             set { _invisible = !value; }
         }
 
+        /// <summary>
+        /// The command is an on-off toggle and is currently on.
+        /// </summary>
         public bool Latched
         {
             get { return _latched; }
             set { _latched = value; }
         }
 
+        /// <summary>
+        /// Visual Studio SDK: Reserved for future usage
+        /// </summary>
         public bool Ninched
         {
             get { return _ninched; }
             set { _ninched = value; }
         }
 
+        /// <summary>
+        /// Allows updating the text (Use <see cref="TextQueryType" /> to determine what text to update
+        /// </summary>
         public string Text
         {
             get { return _text ?? _originalText; }
             set { _text = value; }
         }
 
+        /// <summary>
+        /// Gets the type of the text query.
+        /// </summary>
+        /// <value>The type of the text query.</value>
+        public TextQueryType TextQueryType
+        {
+            get { return _queryType; }
+        }
+
+        /// <summary>
+        /// Marks the end of a dynamic menu range if set
+        /// </summary>
         public bool DynamicMenuEnd
         {
             get { return _dynamicMenuEnd; }
             set { _dynamicMenuEnd = value; }
         }
 
+        /// <summary>
+        /// Gets the original text
+        /// </summary>
         public string OriginalText
         {
             get { return _originalText; }
