0d291b43e5f1659a383e4076675e655e8186d659
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-05-29 16:13:02 +0000

    Add initial 'change log message' dialog
    * src/Ankh.UI/Ankh.UI.csproj
    * src/Ankh.UI/SvnLog/EditLogMessageDialog.cs
    * src/Ankh.UI/SvnLog/EditLogMessageDialog.Designer.cs
    * src/Ankh.UI/SvnLog/EditLogMessageDialog.resx
        Add dialog
    * src/Ankh.UI/SvnLog/Commands/ChangeLogMessage.cs
        Change command to show above dialog

--- trunk/src/Ankh.UI/SvnLog/Commands/ChangeLogMessage.cs
+++ trunk/src/Ankh.UI/SvnLog/Commands/ChangeLogMessage.cs
@@ -5,11 +5,15 @@ using Ankh.Commands;
 using Ankh.Ids;
 using SharpSvn;
 using SharpSvn.Implementation;
+using System.Windows.Forms.Design;
+using System.Windows.Forms;
+using Ankh.VS;
+using System.ComponentModel;
 
 namespace Ankh.UI.SvnLog.Commands
 {
-    [Command(AnkhCommand.LogChangeLogMessage)]
-    class ChangeLogMessage : ICommandHandler
+    [Command(AnkhCommand.LogChangeLogMessage, HideWhenDisabled = true)]
+    class ChangeLogMessage : ICommandHandler, IComponent
     {
         public void OnUpdate(CommandUpdateEventArgs e)
         {
@@ -21,18 +25,51 @@ namespace Ankh.UI.SvnLog.Commands
                 if (count > 1)
                     break;
             }
-            if(count != 1)
-                e.Enabled = e.Visible = false;
+            if (count != 1)
+                e.Enabled = false;
         }
 
         public void OnExecute(CommandEventArgs e)
         {
-            string newValue = null;
-            using (SvnClient client = e.GetService<ISvnClientPool>().GetClient())
+            IUIService uiService = e.GetService<IUIService>();
+            IAnkhSolutionSettings slnSettings = e.GetService<IAnkhSolutionSettings>();
+            List<LogItem> logItems = new List<LogItem>(e.Selection.GetSelection<LogItem>());
+            if (logItems.Count != 1)
+                return;
+
+            using (EditLogMessageDialog dialog = new EditLogMessageDialog())
             {
-                SvnSetRevisionPropertyArgs args = new SvnSetRevisionPropertyArgs();
-                client.SetRevisionProperty(null, SvnPropertyNames.SvnLog, args, newValue);
+                dialog.Context = e.Context;
+                dialog.LogMessage = logItems[0].RawData.LogMessage;
+
+                if (uiService.ShowDialog(dialog) == DialogResult.OK)
+                {
+                    using (SvnClient client = e.GetService<ISvnClientPool>().GetClient())
+                    {
+                        client.SetRevisionProperty(new SvnUriTarget(slnSettings.ProjectRootUri, logItems[0].RawData.Revision), SvnPropertyNames.SvnLog, dialog.LogMessage);
+                    }
+                }
             }
+        }
+
+        public event EventHandler Disposed;
+
+        ISite _site;
+        public ISite Site
+        {
+            get
+            {
+                return _site;
+            }
+            set
+            {
+                _site = value;
+            }
+        }
+
+
+        public void Dispose()
+        {
         }
     }
 }
