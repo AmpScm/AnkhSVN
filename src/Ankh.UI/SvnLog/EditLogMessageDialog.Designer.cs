bd5d3084c8cb4111da8d683c37bb6796148f6986
Sander Rijken <1292085+srijken@users.noreply.github.com>
2008-05-29 16:13:02 +0000

    Add initial 'change log message' dialog
    * src/Ankh.UI/Ankh.UI.csproj
    * src/Ankh.UI/SvnLog/EditLogMessageDialog.cs
    * src/Ankh.UI/SvnLog/EditLogMessageDialog.Designer.cs
    * src/Ankh.UI/SvnLog/EditLogMessageDialog.resx
        Add dialog
    * src/Ankh.UI/SvnLog/Commands/ChangeLogMessage.cs
        Change command to show above dialog

---
+++ src/Ankh.UI/SvnLog/EditLogMessageDialog.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Ankh.UI.SvnLog
+{
+    partial class EditLogMessageDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.logMessageEditor1 = new Ankh.UI.PendingChanges.LogMessageEditor();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(409, 258);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(328, 257);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // logMessageEditor1
+            // 
+            this.logMessageEditor1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.logMessageEditor1.Location = new System.Drawing.Point(12, 12);
+            this.logMessageEditor1.Name = "logMessageEditor1";
+            this.logMessageEditor1.Size = new System.Drawing.Size(472, 225);
+            this.logMessageEditor1.TabIndex = 0;
+            this.logMessageEditor1.Text = null;
+            // 
+            // EditLogMessageDialog
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(496, 293);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.logMessageEditor1);
+            this.Name = "EditLogMessageDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Edit log message";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Ankh.UI.PendingChanges.LogMessageEditor logMessageEditor1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
---
+++ src/Ankh.UI/SvnLog/EditLogMessageDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ankh.UI.Services;
+
+namespace Ankh.UI.SvnLog
+{
+    public partial class EditLogMessageDialog : Form
+    {
+        public EditLogMessageDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string LogMessage
+        {
+            get { return logMessageEditor1.Text; }
+            set { logMessageEditor1.Text = value; }
+        }
+
+        IAnkhServiceProvider _context;
+        public IAnkhServiceProvider Context
+        {
+            get { return _context; }
+            set
+            {
+                _context = value;
+                logMessageEditor1.Init(_context);
+            }
+        }
+    }
+}
