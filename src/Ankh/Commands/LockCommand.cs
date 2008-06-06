4faa2ac36c8106ddd03434356019a86497648e33
Bert Huijben <rhuijben@users.noreply.github.com>
2008-06-06 09:40:28 +0000

    Fixed the unlock command and disabled the relocate command (Locking will require a new dialog; which will probably not happen before 2.0)
    * src/Ankh/Commands/UnlockCommand.cs
      Handle unlocking via the standard pathselector dialog
      
    * src/Ankh/Commands/LockCommand.cs
      Removed obsolete code
    
    * src/Ankh.Package/Gui/AnkhSvn.xml
      Re-enabled unlocking

--- src/Ankh/Commands/LockCommand.cs
+++ src/Ankh/Commands/LockCommand.cs
@@ -14,104 +14,22 @@ namespace Ankh.Commands
     [Command(AnkhCommand.Lock)]
     public class LockCommand : CommandBase
     {
+        public override void OnUpdate(CommandUpdateEventArgs e)
+        { 
+            e.Enabled = false;
+        }
+
         #region Implementation of ICommand
 
 
         public override void OnExecute(CommandEventArgs e)
         {
-            /*IContext context = e.Context.GetService<IContext>();
-
-            IList resources = context.Selection.GetSelectionResources(true, 
-                new ResourceFilterCallback( SvnItem.NotLockedAndLockableFilter ) );
-
-            this.info = new LockDialogInfo( resources, resources );
-            
-            // is Shift down?
-            if ( !CommandBase.Shift )
-            {
-                this.info = context.UIShell.ShowLockDialog( this.info );
-                if( this.info == null)
-                    return;
-            }
-
-            this.lockFailedFiles = new ArrayList();
-            context.Client.Notify += new EventHandler<SvnNotifyEventArgs>(OnClientNotification);
-            try
-            {
-                context.UIShell.RunWithProgressDialog( new SimpleProgressWorker(
-                    new SimpleProgressWorkerCallback( this.ProgressCallback ) ), "Locking files" );
-                foreach ( SvnItem item in info.CheckedItems )
-                    item.MarkDirty();
-            }
-            finally
-            {
-                context.Client.Notify -= new EventHandler<SvnNotifyEventArgs>( OnClientNotification );
-            }
-
-            if ( this.lockFailedFiles.Count > 0 )
-            {
-                this.ShowLockFailedMessage( context );
-            }*/
+            // TODO: Use path selector based dialog to select files
+            // and lock thise
         }
 
         #endregion
 
-        private void ShowLockFailedMessage( IContext context )
-        {
-            StringBuilder sb = new StringBuilder();
-            bool onlyOne = this.lockFailedFiles.Count == 1;
-
-            sb.AppendFormat("The following file{0} {1} out of date and could not be locked:", 
-                onlyOne ? "" : "s", 
-                onlyOne ? "was" : "were");
-            sb.Append( Environment.NewLine );
-            sb.Append( Environment.NewLine );
-
-            foreach ( string path in this.lockFailedFiles )
-            {
-                sb.Append( path );
-                sb.Append( Environment.NewLine );
-            }
-
-            context.UIShell.ShowMessageBox( sb.ToString(), "Lock failed", System.Windows.Forms.MessageBoxButtons.OK );
-        }
-
-        void OnClientNotification( object sender, SvnNotifyEventArgs args )
-        {
-            if ( args.Action == SvnNotifyAction.LockFailedLock )
-            {
-                this.lockFailedFiles.Add( args.Path );
-            }
-        }
-
-        private void ProgressCallback(IContext context)
-        {
-            ICollection<string> paths = SvnItem.GetPaths(info.CheckedItems);
-            SvnLockArgs args = new SvnLockArgs();
-            args.StealLock = info.StealLocks;
-            args.Comment = info.Message;
-            using (SvnClient client = context.ClientPool.GetClient())
-            {
-                client.Lock(paths, args);
-            }
-        }
-
-        public override void OnUpdate(CommandUpdateEventArgs e)
-        {
-            if (!e.State.SccProviderActive)
-            {
-                e.Visible = e.Enabled = false;
-                return;
-            }
-            foreach (SvnItem item in e.Selection.GetSelectedSvnItems(false))
-            {
-                if (!item.IsLocked && item.IsVersioned && item.IsFile)
-                    return;
-            }
-            e.Enabled = false;
-        }
         
-        private LockDialogInfo info = null;
-        private ArrayList lockFailedFiles = null;
     }
 }
--- src/Ankh/Commands/UnlockCommand.cs
+++ src/Ankh/Commands/UnlockCommand.cs
@@ -4,66 +4,70 @@ using SharpSvn;
 using Ankh.Ids;
 using Ankh.VS;
 using System.Collections.Generic;
+using Ankh.UI;
 
 namespace Ankh.Commands
 {
     /// <summary>
     /// Command to unlock the selected items.
     /// </summary>
-    [Command(AnkhCommand.Unlock)] 
+    [Command(AnkhCommand.Unlock, HideWhenDisabled=true)] 
 	public class UnlockCommand : CommandBase
     {
-        #region Implementation of ICommand
-
         public override void OnUpdate(CommandUpdateEventArgs e)
         {
-            if (!e.State.SccProviderActive)
-            {
-                e.Visible = e.Enabled = false;
-                return;
-            }
             foreach (SvnItem item in e.Selection.GetSelectedSvnItems(true))
             {
-                if (item.IsLocked)
+                if (item.IsVersioned && item.IsLocked)
                     return;
+
             }
             e.Enabled = false;
         }
 
         public override void OnExecute(CommandEventArgs e)
         {
-            IContext context = e.Context.GetService<IContext>();
+            PathSelectorInfo psi = new PathSelectorInfo("Select Files to Unlock", e.Selection.GetSelectedSvnItems(true));
 
-            /*IList resources = context.Selection.GetSelectionResources(true, 
-                new ResourceFilterCallback( SvnItem.LockedFilter ) );
-
-            this.info = new PathSelectorInfo( "Unlock files", resources, resources );
-
-            if ( !CommandBase.Shift )
+            psi.VisibleFilter += delegate(SvnItem item)
             {
-                this.info = context.UIShell.ShowPathSelector( this.info );
+                return item.IsLocked;
+            };
 
-                if ( this.info == null )
-                    return;
+            psi.CheckedFilter += delegate(SvnItem item)
+            {
+                return item.IsLocked;
+            };
+            
+            PathSelectorResult psr;
+            if (!CommandBase.Shift)
+            {
+                IUIShell uiShell = e.GetService<IUIShell>();
+
+                psr = uiShell.ShowPathSelector(psi);
+            }
+            else
+                psr = psi.DefaultResult;
+
+            if (!psr.Succeeded)
+                return;
+
+            List<string> files = new List<string>();
+
+            foreach (SvnItem item in psr.Selection)
+            {
+                files.Add(item.FullPath);
             }
 
-            context.UIShell.RunWithProgressDialog( new SimpleProgressWorker(
-                new SimpleProgressWorkerCallback( this.DoUnlock )), "Unlocking files" );
-            
-            foreach( SvnItem item in this.info.CheckedItems )
-                item.MarkDirty();*/
+            if(files.Count == 0)
+                return;
+
+            e.GetService<IProgressRunner>().Run("Unlocking",
+                delegate(object sender, ProgressWorkerArgs ee)
+                {
+                    SvnUnlockArgs ua = new SvnUnlockArgs();
+                    ee.Client.Unlock(files, ua);
+                });
         }
-
-        #endregion
-
-        private void DoUnlock(ProgressWorkerArgs e)
-        {
-            ICollection<string> paths = SvnItem.GetPaths(this.info.CheckedItems);
-            SvnUnlockArgs args = new SvnUnlockArgs();
-            args.BreakLock = false;
-            e.Client.Unlock(paths, args);
-        }
-
-        private PathSelectorInfo info=null;
 	}
 }
