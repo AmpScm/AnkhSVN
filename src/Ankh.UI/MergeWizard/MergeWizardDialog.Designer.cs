54f28d1f7066e7f7fe6c99e662e36d2ef945caf9
Sander Rijken <1292085+srijken@users.noreply.github.com>
2008-06-06 22:43:02 +0000

    Turn off ShowInTaskbar (again?)
    * src/Ankh.UI/MergeWizard/MergeWizardDialog.Designer.cs
        See above

--- src/Ankh.UI/MergeWizard/MergeWizardDialog.Designer.cs
+++ src/Ankh.UI/MergeWizard/MergeWizardDialog.Designer.cs
@@ -15,6 +15,7 @@ namespace Ankh.UI.MergeWizard
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(349, 317);
             this.Name = "MergeWizardDialog";
+            this.ShowInTaskbar = false;
             this.Text = "Merge Wizard";
             this.ResumeLayout(false);
             this.PerformLayout();
