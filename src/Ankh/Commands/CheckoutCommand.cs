f7a9294258d1ee286965416ce4e9fc12cb67283c
Bert Huijben <rhuijben@users.noreply.github.com>
2008-06-07 00:03:40 +0000

    Auto select item in repository browser in more cases and fixed the repository checkout command

--- src/Ankh/Commands/CheckoutCommand.cs
+++ src/Ankh/Commands/CheckoutCommand.cs
@@ -16,25 +16,68 @@ namespace Ankh.Commands
     [Command(AnkhCommand.Checkout)]
     public class CheckoutCommand : CommandBase
     {
-        #region Implementation of ICommand
+        public override void OnUpdate(CommandUpdateEventArgs e)
+        {
+            bool first = false;
+
+            foreach (Ankh.Scc.ISvnRepositoryItem i in e.Selection.GetSelection<Ankh.Scc.ISvnRepositoryItem>())
+            {
+                if (first)
+                {
+                    e.Enabled = false;
+                    return;
+                }
+                first = true;
+            }
+
+            if (!first)
+                e.Enabled = false;
+        }
+
 
         public override void OnExecute(CommandEventArgs e)
         {
+            Uri uri = null;
+            SharpSvn.SvnRevision rev = null;
+            string name = null;
+
+            foreach (Ankh.Scc.ISvnRepositoryItem i in e.Selection.GetSelection<Ankh.Scc.ISvnRepositoryItem>())
+            {
+                if (uri != null)
+                    return;
+
+                name = i.Name;
+                uri = i.Uri;
+                rev = i.Revision;               
+            }
+
+            Ankh.VS.IAnkhSolutionSettings ss = e.GetService<Ankh.VS.IAnkhSolutionSettings>();
+
+            if (uri == null)
+                return;
+
+
             IUIService ui = e.GetService<IUIService>();
 
             using (CheckoutDialog dlg = new CheckoutDialog())
             {
+                dlg.Uri = uri;
+                dlg.LocalPath = System.IO.Path.Combine(ss.NewProjectLocation, name);
+                
+
                 if (ui.ShowDialog(dlg) != DialogResult.OK)
                     return;
 
-                CheckoutRunner runner = new CheckoutRunner(
-                        dlg.LocalPath, dlg.Revision, new Uri(dlg.Url), dlg.Recursive ? SvnDepth.Infinity : SvnDepth.Empty);
+                e.GetService<IProgressRunner>().Run("Checking Out", 
+                    delegate(object sender, ProgressWorkerArgs a)
+                    {
+                        SvnCheckOutArgs args = new SvnCheckOutArgs();
+                        args.Revision = dlg.Revision;
+                        args.Depth = dlg.Recursive ? SvnDepth.Infinity : SvnDepth.Children;
 
-                e.GetService<IProgressRunner>().Run("Checking out", runner.Work);
-
+                        a.Client.CheckOut(dlg.Uri, dlg.LocalPath, args);
+                    });
             }
         }
-
-        #endregion
     }
 }
