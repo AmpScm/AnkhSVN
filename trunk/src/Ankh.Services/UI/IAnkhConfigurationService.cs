7bda707f9191499838a5d768b0b6c24c81230938
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-06-11 09:38:58 +0000

    Moved services to Services folder to clean up project root
    * src/Ankh/Ankh.csproj
      Updated for these changes
      
    * src/Ankh/Services/AnkhCommandService.cs
    * src/Ankh/Services/AnkhContext.cs
    * src/Ankh/Services/AnkhErrorHandler.cs
    * src/Ankh/Services/ConfigService.cs
    * src/Ankh/Services/IContext.cs
    * src/Ankh/Services/IUIShell.cs
    * src/Ankh/Services/ProgressRunner.cs
    * src/Ankh/Services/SolutionSettings.cs
    * src/Ankh/Services/UIShell.cs
      Moved here
      
    * src/Ankh/Commands/Runners/CommitOperation.cs
    * src/Ankh/CommitOperation.cs
      Moved to command runners (Only used by Commit item command now)
    
    * src/Ankh.Scc/ProjectNotifier.cs
    * src/Ankh/Services
    * src/Ankh/Config
    * src/Ankh/AnkhCommandService.cs
    * src/Ankh/UIShell.cs
    * src/Ankh/IUIShell.cs
    * src/Ankh/AnkhContext.cs
    * src/Ankh/IContext.cs
    * src/Ankh/AnkhErrorHandler.cs
    * src/Ankh/ProgressRunner.cs
    * src/Ankh/Settings/SolutionSettings.cs
    * src/Ankh/Settings
      Removed here
      
    * src/Ankh/Config/ConfigService.cs
      Added a few new public helpers to access the registry from other services

--- trunk/src/Ankh.Services/UI/IAnkhConfigurationService.cs
+++ trunk/src/Ankh.Services/UI/IAnkhConfigurationService.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using Ankh.Configuration;
+using Microsoft.Win32;
 
 namespace Ankh.UI
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IAnkhConfigurationService
     {
         Configuration.AnkhConfig Instance
@@ -14,10 +18,29 @@ namespace Ankh.UI
 
         void LoadConfig();
 
-        AnkhConfig GetNewConfigInstance();
-
         void SaveConfig(AnkhConfig config);
 
         void LoadDefaultConfig();
+
+        /// <summary>
+        /// Opens the user instance key (per hive + per user)
+        /// </summary>
+        /// <param name="subKey">The sub key.</param>
+        /// <returns></returns>
+        RegistryKey OpenUserInstanceKey(string subKey);
+
+        /// <summary>
+        /// Opens the instance key (per hive)
+        /// </summary>
+        /// <param name="subKey">The sub key.</param>
+        /// <returns></returns>
+        RegistryKey OpenInstanceKey(string subKey);
+
+        /// <summary>
+        /// Opens the global key (one hklm key)
+        /// </summary>
+        /// <param name="subKey">The sub key.</param>
+        /// <returns></returns>
+        RegistryKey OpenGlobalKey(string subKey);
     }
 }
