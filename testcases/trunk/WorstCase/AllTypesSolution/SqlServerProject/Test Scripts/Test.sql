69b29a53cc3f074378139017c3ce311d4992b788
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-06-13 10:10:53 +0000

    Reopening the solution added these files? Probably nice conflict test ;) (Or a bug we should resolve)
    * WorstCase/AllTypesSolution/CsWebApplication/Default.aspx.designer.cs
    * WorstCase/AllTypesSolution/SqlServerProject/Test Scripts/Test.sql
    * WorstCase/AllTypesSolution/ReportsApplication/Form1.Designer.cs
    * WorstCase/AllTypesSolution/ReportsApplication/Form1.resx
    * WorstCase/AllTypesSolution/VbWindowsApplication/Form1.Designer.vb
    * WorstCase/ExternalSite/Default.aspx.cs
    * WorstCase/AllTypesSolution/InternalSite/Default.aspx.cs
      Added generated files

---
+++ testcases/trunk/WorstCase/AllTypesSolution/SqlServerProject/Test Scripts/Test.sql
@@ -0,0 +1,35 @@
+﻿-- Examples for queries that exercise different SQL objects implemented by this assembly
+
+-----------------------------------------------------------------------------------------
+-- Stored procedure
+-----------------------------------------------------------------------------------------
+-- exec StoredProcedureName
+
+
+-----------------------------------------------------------------------------------------
+-- User defined function
+-----------------------------------------------------------------------------------------
+-- select dbo.FunctionName()
+
+
+-----------------------------------------------------------------------------------------
+-- User defined type
+-----------------------------------------------------------------------------------------
+-- CREATE TABLE test_table (col1 UserType)
+-- go
+--
+-- INSERT INTO test_table VALUES (convert(uri, 'Instantiation String 1'))
+-- INSERT INTO test_table VALUES (convert(uri, 'Instantiation String 2'))
+-- INSERT INTO test_table VALUES (convert(uri, 'Instantiation String 3'))
+--
+-- select col1::method1() from test_table
+
+
+
+-----------------------------------------------------------------------------------------
+-- User defined type
+-----------------------------------------------------------------------------------------
+-- select dbo.AggregateName(Column1) from Table1
+
+
+select 'To run your project, please edit the Test.sql file in your project. This file is located in the Test Scripts folder in the Solution Explorer.'
