0e8bf377e85e6d5ef29ad606fd343d06be042856
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-06-17 17:36:31 +0000

    Added secondary package 'Ankh.Trigger' which is (and will stay) very light. This package loads  when VS loads and is responsible for loading Ankh.Package only when it is really needed.
    * src/AnkhSvn.2008.sln
      Added project to the 2008 solution
    
    * src/Ankh.Trigger/Ankh.Trigger.csproj  
    * src/Ankh.Trigger/TriggerPackage.cs
    * src/Ankh.Trigger/Ankh.Trigger.snk
    * src/Ankh.Trigger/GlobalSuppressions.cs
    * src/Ankh.Trigger/Properties/AssemblyInfo.cs
    * src/Ankh.Trigger/Resources.resx
    * src/Ankh.Trigger/VSPackage.resx
    * src/Ankh.Trigger/Resources/Package.ico
    * src/Ankh.Trigger
    * src/Ankh.Trigger/Resources
    * src/Ankh.Trigger/Resources.Designer.cs
    * src/Ankh.Trigger/Hooks/SelectionFilter.cs
    * src/Ankh.Trigger/Ankh.Trigger.wxs
      Added new UI Context listening package which will trigger loading the real AnkhSVN
      package for new solution scenarios and for initial cleanup of the old AnkhSVN AddIn.
      And can check if other SCC providers are active to enable some menu commands if they
      are not.
      
    * src/Ankh.Ids/AnkhId.cs
      Added guids and identifiers of Ankh.Trigger package

---
+++ trunk/src/Ankh.Trigger/Hooks/SelectionFilter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio;
+
+namespace Ankh.Trigger
+{
+    sealed class SelectionFilter : IVsSelectionEvents, IDisposable
+    {
+        readonly IVsMonitorSelection _monitor;
+        uint _cookie;
+
+        public SelectionFilter(IVsMonitorSelection monitor)
+        {
+            if (monitor == null)
+                throw new ArgumentNullException("monitor");
+
+            _monitor = monitor;
+
+            if (!ErrorHandler.Succeeded(_monitor.AdviseSelectionEvents(this, out _cookie)))
+                _cookie = 0;
+        }
+
+        void IDisposable.Dispose()
+        {
+            if (_cookie != 0)
+            {
+                _monitor.UnadviseSelectionEvents(_cookie);
+                _cookie = 0;
+            }
+        }
+
+        int IVsSelectionEvents.OnCmdUIContextChanged(uint dwCmdUICookie, int fActive)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSelectionEvents.OnElementValueChanged(uint elementid, object varValueOld, object varValueNew)
+        {
+            return VSConstants.S_OK;
+        }
+
+        int IVsSelectionEvents.OnSelectionChanged(IVsHierarchy pHierOld, uint itemidOld, IVsMultiItemSelect pMISOld, ISelectionContainer pSCOld, IVsHierarchy pHierNew, uint itemidNew, IVsMultiItemSelect pMISNew, ISelectionContainer pSCNew)
+        {
+            return VSConstants.S_OK;
+        }
+    }
+}
