845eed29cec53cb6c8c2d7419ede0d2c3caf6bdf
Bert Huijben <rhuijben@users.noreply.github.com>
2008-06-17 17:36:31 +0000

    Added secondary package 'Ankh.Trigger' which is (and will stay) very light. This package loads  when VS loads and is responsible for loading Ankh.Package only when it is really needed.
    * src/AnkhSvn.2008.sln
      Added project to the 2008 solution
    
    * src/Ankh.Trigger/Ankh.Trigger.csproj  
    * src/Ankh.Trigger/TriggerPackage.cs
    * src/Ankh.Trigger/Ankh.Trigger.snk
    * src/Ankh.Trigger/GlobalSuppressions.cs
    * src/Ankh.Trigger/Properties/AssemblyInfo.cs
    * src/Ankh.Trigger/Resources.resx
    * src/Ankh.Trigger/VSPackage.resx
    * src/Ankh.Trigger/Resources/Package.ico
    * src/Ankh.Trigger
    * src/Ankh.Trigger/Resources
    * src/Ankh.Trigger/Resources.Designer.cs
    * src/Ankh.Trigger/Hooks/SelectionFilter.cs
    * src/Ankh.Trigger/Ankh.Trigger.wxs
      Added new UI Context listening package which will trigger loading the real AnkhSVN
      package for new solution scenarios and for initial cleanup of the old AnkhSVN AddIn.
      And can check if other SCC providers are active to enable some menu commands if they
      are not.
      
    * src/Ankh.Ids/AnkhId.cs
      Added guids and identifiers of Ankh.Trigger package

--- src/Ankh.Ids/AnkhId.cs
+++ src/Ankh.Ids/AnkhId.cs
@@ -20,6 +20,16 @@ namespace Ankh.Ids
         public const string PlkCompany = "AnkhSVN Core Team";
         //**********************************************************************************
 
+        //************ The Package Load Key Registration ***********************************
+        /// <summary>
+        /// The guid the Ankh Trigger package is registered with inside Visual Studio
+        /// </summary>
+        public const string TriggerPackageId = "2340124C-5DAE-4D72-84AA-4DEF3EFDFA1D";
+
+        public const string TriggerPlkVersion = PlkVersion;
+        public const string TriggerPlkProduct = PlkProduct;
+        public const string TriggerPlkCompany = PlkCompany;
+        //**********************************************************************************
 
         public const string SubversionSccName = "SubversionScc";
 
@@ -30,9 +40,20 @@ namespace Ankh.Ids
         /// <remarks>Must be changed when the PackageId changes</remarks>
         public const string CommandSet = "aa61c329-d559-468f-8f0f-4f03896f704d";
 
-
         public const string SccProviderId = "8770915b-b235-42ec-bbc6-8e93286e59b5";
         public const string SccServiceId = "d8c473d2-9634-4513-91d5-e1a671fe2df4";
+
+
+        //************ Special contexts managed by our Trigger package *********************        
+        /// <summary>
+        /// Context set by Trigger package when another Scc package is active
+        /// </summary>
+        public const string CtxNoOtherSccActive = "3eec2d0e-8224-4d68-9748-773e2ace8dda";
+        public const string CtxNoOtherSccManaging = "644c07df-048b-44c5-aeff-cf54ad82a209";
+        
+        // When this context is set the full package is loaded and the trigger package is deactivated
+        public const string CtxFullSccLoaded = "1142ad52-4c53-499a-afac-3f3694261f5d";
+        //**********************************************************************************
 
         public const string LogMessageLanguageServiceId = "1dfe69ce-7f9b-4cc5-b09b-e5bde95e9439";
         public const string LogMessageServiceName = "Log Messages (AnkhSVN)";
---
+++ src/Ankh.Trigger/TriggerPackage.cs
@@ -0,0 +1,178 @@
+﻿// VsPkg.cs : Implementation of Ankh_Trigger
+//
+
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using OLEConstants = Microsoft.VisualStudio.OLE.Interop.Constants;
+
+using Ankh.Ids;
+
+namespace Ankh.Trigger
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    ///
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the 
+    /// IVsPackage interface and uses the registration attributes defined in the framework to 
+    /// register itself and its components with the shell.
+    /// </summary>
+    // This attribute tells the registration utility (regpkg.exe) that this class needs
+    // to be registered as package.
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    // A Visual Studio component can be registered under different regitry roots; for instance
+    // when you debug your package you want to register it in the experimental hive. This
+    // attribute specifies the registry root to use if no one is provided to regpkg.exe with
+    // the /root switch.
+    [DefaultRegistryRoot("Software\\Microsoft\\VisualStudio\\9.0")]
+    //
+    [ProvideAutoLoad("adfc4e64-0397-11d1-9f4e-00a0c911004f")]   // Load on 'no solution'
+    [ProvideAutoLoad("f1536ef8-92ec-443c-9ed7-fdadf150da82")]   // Load on 'solution'
+    [ProvideAutoLoad(AnkhId.SccProviderId)] // Load on 'Scc active'
+    // In order be loaded inside Visual Studio in a machine that has not the VS SDK installed, 
+    // package needs to have a valid load key (it can be requested at 
+    // http://msdn.microsoft.com/vstudio/extend/). This attributes tells the shell that this 
+    // package has a load key embedded in its resources.
+    [ProvideLoadKey("Standard", AnkhId.TriggerPlkVersion, AnkhId.TriggerPlkProduct, AnkhId.TriggerPlkCompany, 1)]
+    [Guid(AnkhId.TriggerPackageId)]    
+    sealed class Ankh_TriggerPackage : Package, IVsShellPropertyEvents, IOleCommandTarget
+    {
+        /// <summary>
+        /// Default constructor of the package.
+        /// Inside this method you can place any initialization code that does not require 
+        /// any Visual Studio service because at this point the package object is created but 
+        /// not sited yet inside Visual Studio environment. The place to do all the other 
+        /// initialization is the Initialize method.
+        /// </summary>
+        public Ankh_TriggerPackage()
+        {
+        }
+
+        /////////////////////////////////////////////////////////////////////////////
+        // Overriden Package Implementation
+        #region Package Members
+
+        [DebuggerStepThrough]
+        public T GetService<T>()
+            where T : class
+        {
+            return GetService<T>(typeof(T));
+        }
+
+        [DebuggerStepThrough]
+        public T GetService<T>(Type serviceType)
+            where T : class
+        {
+            return GetService(typeof(T)) as T;
+        }
+
+
+        uint _shellCookie;
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initilaization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            IVsShell shell = GetService<IVsShell>(typeof(SVsShell));
+
+            if (shell != null)
+            {
+                shell.AdviseShellPropertyChanges(this, out _shellCookie);
+
+                EnsureHooks(); // Will unregister the advise if no longer needed
+            }
+        }
+        #endregion
+
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                ReleaseShellHook();
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
+
+
+        #region IVsShellPropertyEvents Members
+
+        int IVsShellPropertyEvents.OnShellPropertyChange(int propid, object var)
+        {
+            if (propid == (int)__VSSPROPID.VSSPROPID_Zombie)
+            {
+                bool value = (bool)var;
+
+                if (value == false)
+                {
+                    EnsureHooks();
+
+                    ReleaseShellHook();                    
+                }
+            }
+            return VSConstants.S_OK;
+        }
+
+        private void ReleaseShellHook()
+        {
+            if (_shellCookie != 0)
+            {
+                IVsShell shell = GetService<IVsShell>(typeof(SVsShell));
+
+                if (shell != null)
+                {
+                    shell.UnadviseShellPropertyChanges(_shellCookie);
+                    _shellCookie = 0;
+                }
+            }
+        }
+
+        #endregion
+
+        SelectionFilter _filter;
+
+        private void EnsureHooks()
+        {
+            if(_filter != null)
+                return;
+
+            IVsMonitorSelection monitorSelection = GetService<IVsMonitorSelection>();
+
+            if (monitorSelection != null)
+            {
+                _filter = new SelectionFilter(monitorSelection);
+                ReleaseShellHook();
+            }
+        }
+
+
+        #region IOleCommandTarget Members
+
+        int IOleCommandTarget.Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+            return (int)OLEConstants.OLECMDERR_E_NOTSUPPORTED;
+        }
+
+        int IOleCommandTarget.QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+        {
+            return (int)OLEConstants.OLECMDERR_E_NOTSUPPORTED;
+        }
+
+        #endregion
+    }
+}
