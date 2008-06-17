06d7943dc7238055cfe852a820285ab22611343c
Sander Rijken <1292085+srijken@users.noreply.github.com>
2008-06-17 21:33:21 +0000

    Add BusyOverlay when we're doing work
    * src/Ankh.UI/SvnLog/LogRevisionControl.cs
    * src/Ankh.UI/SvnLog/LogRevisionControl.Designer.cs
        See above

--- src/Ankh.UI/SvnLog/LogRevisionControl.Designer.cs
+++ src/Ankh.UI/SvnLog/LogRevisionControl.Designer.cs
@@ -28,65 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.logRevisionControl1 = new Ankh.UI.SvnLog.LogRevisionView(this.components);
-            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
-            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
-            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
-            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
-            this.SuspendLayout();
-            // 
-            // logRevisionControl1
-            // 
-            this.logRevisionControl1.AllowColumnReorder = true;
-            this.logRevisionControl1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			this.components = new System.ComponentModel.Container();
+			this.logRevisionControl1 = new Ankh.UI.SvnLog.LogRevisionView(this.components);
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+			this.SuspendLayout();
+			// 
+			// logRevisionControl1
+			// 
+			this.logRevisionControl1.AllowColumnReorder = true;
+			this.logRevisionControl1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3,
             this.columnHeader4});
-            this.logRevisionControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.logRevisionControl1.FullRowSelect = true;
-            this.logRevisionControl1.Location = new System.Drawing.Point(0, 0);
-            this.logRevisionControl1.Name = "logRevisionControl1";
-            this.logRevisionControl1.ProvideWholeListForSelection = false;
-            this.logRevisionControl1.Size = new System.Drawing.Size(552, 324);
-            this.logRevisionControl1.TabIndex = 0;
-            this.logRevisionControl1.UseCompatibleStateImageBehavior = false;
-            this.logRevisionControl1.View = System.Windows.Forms.View.Details;
-            this.logRevisionControl1.VirtualMode = true;
-            this.logRevisionControl1.ShowContextMenu += new System.EventHandler(this.logRevisionControl1_ShowContextMenu);
-            this.logRevisionControl1.SelectedIndexChanged += new System.EventHandler(this.logRevisionControl1_SelectedIndexChanged);
-            this.logRevisionControl1.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.logRevisionControl1_RetrieveVirtualItem);
-            this.logRevisionControl1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.logRevisionControl1_ItemSelectionChanged);
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Revision";
-            this.columnHeader1.Width = 75;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Author";
-            this.columnHeader2.Width = 73;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Date";
-            this.columnHeader3.Width = 118;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "Message";
-            this.columnHeader4.Width = 300;
-            // 
-            // LogRevisionControlNew
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.logRevisionControl1);
-            this.Name = "LogRevisionControlNew";
-            this.Size = new System.Drawing.Size(552, 324);
-            this.ResumeLayout(false);
+			this.logRevisionControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.logRevisionControl1.FullRowSelect = true;
+			this.logRevisionControl1.Location = new System.Drawing.Point(0, 0);
+			this.logRevisionControl1.Name = "logRevisionControl1";
+			this.logRevisionControl1.ProvideWholeListForSelection = false;
+			this.logRevisionControl1.Size = new System.Drawing.Size(552, 324);
+			this.logRevisionControl1.TabIndex = 0;
+			this.logRevisionControl1.UseCompatibleStateImageBehavior = false;
+			this.logRevisionControl1.View = System.Windows.Forms.View.Details;
+			this.logRevisionControl1.VirtualMode = true;
+			this.logRevisionControl1.ShowContextMenu += new System.EventHandler(this.logRevisionControl1_ShowContextMenu);
+			this.logRevisionControl1.SelectedIndexChanged += new System.EventHandler(this.logRevisionControl1_SelectedIndexChanged);
+			this.logRevisionControl1.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.logRevisionControl1_RetrieveVirtualItem);
+			this.logRevisionControl1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.logRevisionControl1_ItemSelectionChanged);
+			this.logRevisionControl1.CacheVirtualItems += new System.Windows.Forms.CacheVirtualItemsEventHandler(this.logRevisionControl1_CacheVirtualItems);
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Revision";
+			this.columnHeader1.Width = 75;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Author";
+			this.columnHeader2.Width = 73;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Date";
+			this.columnHeader3.Width = 118;
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "Message";
+			this.columnHeader4.Width = 300;
+			// 
+			// LogRevisionControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.logRevisionControl1);
+			this.Name = "LogRevisionControl";
+			this.Size = new System.Drawing.Size(552, 324);
+			this.ResumeLayout(false);
 
         }
 
