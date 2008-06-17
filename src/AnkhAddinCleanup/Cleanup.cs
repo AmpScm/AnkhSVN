381a9aa87b198e53d160a17f63f47502e2fd0476
Sander Rijken <1292085+srijken@users.noreply.github.com>
2008-06-17 20:15:13 +0000

    Hook up MigrateAnkhSettings command to reworked cleanup code
    * src/AnkhAddinCleanup/Cleanup.cs
        Refactor cleanup code, provide a fast, and fully recursive way to search for commands/UI
    * src/AnkhSvn.2008.sln
    * src/AnkhAddinCleanup/AnkhAddinCleanup.csproj
    * src/AnkhAddinCleanup/Ankh.Migrate.csproj
        Rename project (and resulting assembly)
    * src/AnkhAddinCleanup/Ankh.Migrate.snk
        And sign the assembly
    * src/Ankh/Ankh.csproj
        Add reference to Ankh.Migrate
    * src/Ankh/Commands/MigrateAnkhSettings.cs
        Hook up to cleanup code in Ankh.Migrate

--- src/AnkhAddinCleanup/Cleanup.cs
+++ src/AnkhAddinCleanup/Cleanup.cs
@@ -3,45 +3,76 @@ using System.Collections.Generic;
 using System.Text;
 using EnvDTE;
 using Microsoft.VisualStudio.CommandBars;
+using System.Diagnostics;
 
-namespace AnkhAddinCleanup
+namespace Ankh.Migrate
 {
-    public class Cleanup
+    public static class Cleanup
     {
-        public void Clean(DTE dte)
+        public static void RemoveOldUI(DTE dte, bool fullSearch)
         {
-			foreach (CommandBar bar in (CommandBars)dte.CommandBars)
-            {
-                DeleteControls(bar.Controls);
-            }
-            foreach (Command command in dte.Commands)
-            {
-                if (command.Name != null && command.Name.StartsWith("Ankh"))
-                {
-                    // TODO: Check command GUID
-                    command.Delete();
-                }
-            }
+			foreach (Command c in dte.Commands)
+			{
+				if (c.Guid == "{1E58696E-C90F-11D2-AAB2-00C04F688DDE}")
+					c.Delete();
+			}
+
+			foreach (CommandBar bar in ((CommandBars)dte.CommandBars))
+			{
+				string name = bar.Name;
+				if (name == "Context Menus")
+				{
+					foreach (CommandBarControl control in bar.Controls)
+					{
+						if (control.Type == MsoControlType.msoControlPopup)
+						{
+							string caption = control.Caption;
+							if (caption == "Project and Solution Context Menus" || caption == "Other Context Menus")
+								RecurseCommands(((CommandBarPopup)control).Controls);
+							else if (fullSearch)
+								RecurseCommands(((CommandBarPopup)control).Controls);
+						}
+					}
+				}
+				else if (name == "ReposExplorer")
+					bar.Delete();
+				else if (name == "WorkingCopyExplorer")
+					bar.Delete();
+				else if (name == "MenuBar")
+					RecurseCommands(bar.Controls);
+				else if (fullSearch)
+					RecurseCommands(bar.Controls);
+			}
         }
 
-        private void DeleteControls(CommandBarControls controls)
-        {
-            foreach (CommandBarControl control in controls)
-            {
-                if (control.accChildCount > 0 && control.Type == MsoControlType.msoControlPopup)
-                {
-                    CommandBarPopup popup = control as CommandBarPopup;
-                    if (popup != null)
-                        DeleteControls(popup.Controls);
-                }
+		static void RecurseCommands(CommandBarControls controls)
+		{
+			foreach (CommandBarControl control in controls)
+			{
 
-                if (control.Caption.StartsWith("Ankh"))
-                {
-                    // TODO: Check something else than Caption, maybe control.Creator ?
-                    // false to remove permanently
-                    control.Delete(false);
-                }
-            }
-        }
+				if (control.accChildCount > 0 && control.Type == MsoControlType.msoControlPopup)
+					RecurseCommands(((CommandBarPopup)control).Controls);
+
+				string caption = control.Caption;
+				if (caption == "An&kh" || caption == "An&khSVN" || caption == "WorkingCopyExplorer" || caption == "ReposExplorer")
+				{
+					control.Delete(false);
+					//Debug.WriteLine(GetPath(control));
+				}
+			}
+		}
+
+		/*static string GetPath(object control)
+		{
+			CommandBarControl barCtrl = control as CommandBarControl;
+			if (barCtrl != null)
+				return GetPath(barCtrl.Parent) + "->" + barCtrl.Caption;
+
+			CommandBar bar = control as CommandBar;
+			if (bar != null)
+				return GetPath(bar.Parent) + "->" + bar.Name;
+
+			return "DTE";
+		}*/
     }
 }
