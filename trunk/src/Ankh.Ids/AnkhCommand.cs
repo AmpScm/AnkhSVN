c517b55c37e45651bfe430bd8b9ecad8bdb533ff
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-07-04 11:45:08 +0000

    Remove + unused command
    * src/Ankh/Ankh.csproj
    * src/Ankh/Commands/AddSolutionToRepositoryCommand.cs
        Remove
    * src/Ankh.Ids/AnkhCommand.cs
        Mark command enum value obsolete

--- trunk/src/Ankh.Ids/AnkhCommand.cs
+++ trunk/src/Ankh.Ids/AnkhCommand.cs
@@ -47,6 +47,7 @@ namespace Ankh.Ids
         // Raw dump of old commands; to be sorted out
         AddItem,
         AddRepositoryRoot,
+        [Obsolete]
         AddSolutionToRepository,
         AddWorkingCopyExplorerRoot,
         Blame,
