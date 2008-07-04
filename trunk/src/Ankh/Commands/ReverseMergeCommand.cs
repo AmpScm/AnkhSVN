55de458ce7dcef590a51fabc885430276d12079c
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-07-04 11:14:56 +0000

    Remove unused code + commented code

--- trunk/src/Ankh/Commands/ReverseMergeCommand.cs
+++ trunk/src/Ankh/Commands/ReverseMergeCommand.cs
@@ -24,8 +24,6 @@ namespace Ankh.Commands
     [Command(AnkhCommand.RevertSolutionToRevision)]
     public class ReverseMergeCommand : CommandBase
     {
-        #region Implementation of ICommand
-
         public override void OnUpdate(CommandUpdateEventArgs e)
         {
             if (!e.State.SccProviderActive)
@@ -149,69 +147,7 @@ namespace Ankh.Commands
                     ee.Client.Merge(item.FullPath, new SvnPathTarget(item.FullPath), revisions);
                 });
             }
-            /*
-            IContext context = e.Context.GetService<IContext>();
-
-            SaveAllDirtyDocuments(context);
-
-            IList resources = context.Selection.GetSelectionResources(
-                true, new ResourceFilterCallback(SvnItem.VersionedFilter));
-            using (e.Context.BeginOperation("Merging"))
-            {
-                using (ReverseMergeDialog dlg = new ReverseMergeDialog())
-                {
-                    dlg.GetPathInfo += new EventHandler<ResolvingPathEventArgs>(GetPathInfo);
-                    dlg.Items = resources;
-                    dlg.CheckedItems = resources;
-                    dlg.Recursive = true;
-
-                    if (dlg.ShowDialog(e.Context.DialogOwner) != DialogResult.OK)
-                        return;
-
-                    ReverseMergeRunner runner = new ReverseMergeRunner(
-                        dlg.CheckedItems, dlg.Revision, dlg.Recursive ? SvnDepth.Infinity : SvnDepth.Empty,
-                        dlg.DryRun);
-                    context.UIShell.RunWithProgressDialog(runner, "Merging");
-                }
-            }*/
-        }
-
-
-        #endregion
-
-        /// <summary>
-        /// A progressrunner for reverse merges.
-        /// </summary>
-        private class ReverseMergeRunner
-        {
-            public ReverseMergeRunner(IList items, SvnRevision revision,
-                SvnDepth depth, bool dryRun)
-            {
-                this.items = items;
-                this.revision = revision;
-                this.depth = depth;
-                this.dryRun = dryRun;
-            }
-
-            public void Work(object sender, ProgressWorkerArgs e)
-            {
-                SvnMergeArgs args = new SvnMergeArgs();
-                args.Depth = depth;
-                args.DryRun = dryRun;
-                args.IgnoreAncestry = false;
-                args.Force = false;
-
-                foreach (SvnItem item in this.items)
-                {
-                    e.Client.Merge(item.FullPath, item.FullPath, new SvnRevisionRange(SvnRevision.Working, revision), args);
-                }
-
-            }
-
-            private IList items;
-            private SvnRevision revision;
-            private SvnDepth depth;
-            private bool dryRun;
+            
         }
     }
 }
