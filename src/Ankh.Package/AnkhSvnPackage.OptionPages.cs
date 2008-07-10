4030014b724c6b6150621df5f3a7e42f2813dcbb
Bert Huijben <rhuijben@users.noreply.github.com>
2008-07-10 20:15:56 +0000

    Fixed issue #443: Subversion property page always visible.
    * src/Ankh.Package/AnkhSvnPackage.cs
      Moved option page definition to AnkhSvnPackage.OptionPages.cs
    
    * src/Ankh.Package/Attributes/ProvideToolsOptionsPageVisibility.cs
      Implemented/Copy&Pasted attribute to set registry key for registering
      our SCC option pages as only visible when we are active.
      
    * src/Ankh.Package/AnkhSvnPackage.OptionPages.cs
      Moved ProvideOptionPage page to this new partial class.
      Added ProvideToolsOptionsPageVisibility property to mark our page
      invisible if we are not active.
    
    * src/Ankh.Package/Ankh.Package.csproj
      Added new files to project.

---
+++ src/Ankh.Package/AnkhSvnPackage.OptionPages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ankh.VSPackage.Attributes;
+using Ankh.Ids;
+using Microsoft.VisualStudio.Shell;
+
+namespace Ankh.VSPackage
+{
+    [ProvideOptionPage(typeof(AnkhSourceControlSettingsPage), "Source Control", "Subversion", 106, 107, false)]
+    [ProvideToolsOptionsPageVisibility("Source Control", "Subversion", AnkhId.SccProviderId)]
+    partial class AnkhSvnPackage
+    {
+    }
+}
