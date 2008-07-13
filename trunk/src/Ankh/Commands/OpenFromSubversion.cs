a9d50087d250b7de7441ca1ee74855f5fff703f3
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-07-13 02:04:44 +0000

    Hide the Subversion menus in File and Context when we are off. Fixing setting defaultInvisible on Properties (2.0) and Switch Solution (Post 2.0).
    * src/Ankh/Commands/OpenFromSubversion.cs
      Removed AlwaysAvailable from FileSccOpenFrom... and FileSccAddFrom.
      (Kept those on the FileFileOpen.. and FileFileAddFrom)
      
    * src/Ankh/Commands/ShowToolWindows.cs
      Marked ShowRepositoryExplorer as AlwaysAvailable
      
    * src/Ankh.Package/Gui/AnkhSvn.xml
      Fixed visibility settings on serveral commands

--- trunk/src/Ankh/Commands/OpenFromSubversion.cs
+++ trunk/src/Ankh/Commands/OpenFromSubversion.cs
@@ -16,8 +16,8 @@ namespace Ankh.Commands
 {
     [Command(AnkhCommand.FileFileOpenFromSubversion, AlwaysAvailable=true)]
     [Command(AnkhCommand.FileFileAddFromSubversion, AlwaysAvailable=true)]
-    [Command(AnkhCommand.FileSccOpenFromSubversion, AlwaysAvailable=true)]
-    [Command(AnkhCommand.FileSccAddFromSubversion, AlwaysAvailable=true)]
+    [Command(AnkhCommand.FileSccOpenFromSubversion)]
+    [Command(AnkhCommand.FileSccAddFromSubversion)]
     class OpenFromSubversion : CommandBase
     {
         public override void OnUpdate(CommandUpdateEventArgs e)
