2d31150ee3e00e7f82e7f3eee09945ac7def6e30
Bert Huijben <rhuijben@users.noreply.github.com>
2008-07-13 01:27:44 +0000

    Hide and disable the 'Open in .. Explorer' commands until implemented. (They showed up when another SCC provider was active)
    * src/Ankh/Ankh.csproj
    * src/Ankh/Commands/OpenInXExplorer.cs
      Added file with stubbed handler for context menu items

---
+++ src/Ankh/Commands/OpenInXExplorer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ankh.Ids;
+
+namespace Ankh.Commands
+{
+    [Command(AnkhCommand.ItemOpenFolderInWorkingCopyExplorer)]
+    [Command(AnkhCommand.ItemOpenFolderInRepositoryExplorer)]
+    class OpenInXExplorer : CommandBase
+    {
+        public override void OnUpdate(CommandUpdateEventArgs e)
+        {
+            e.Enabled = e.Visible = false;
+        }
+        public override void OnExecute(CommandEventArgs e)
+        {
+            
+        }
+    }
+}
