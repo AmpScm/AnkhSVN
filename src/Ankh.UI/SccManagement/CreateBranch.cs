08c262a2fabf671f03c549d5283b989a82a3511d
Bert Huijben <rhuijben@users.noreply.github.com>
2008-07-17 21:56:05 +0000

    Added work-in-progress CreateBranch dialog.
    * src/Ankh.UI/SccManagement/CreateBranch.cs
    * src/Ankh.UI/SccManagement/CreateBranch.resx
    * src/Ankh.UI/SccManagement/CreateBranch.Designer.cs
      Designed the UI for a simple branch command, reusing 
      the Create Directory dialog as start

---
+++ src/Ankh.UI/SccManagement/CreateBranch.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ankh.UI.SccManagement
+{
+    public partial class CreateBranch : VSContainerForm
+    {
+        public CreateBranch()
+        {
+            InitializeComponent();
+            ContainerMode = VSContainerMode.UseTextEditorScope | VSContainerMode.TranslateKeys;
+        }
+
+        protected override void OnContextChanged(EventArgs e)
+        {
+            base.OnContextChanged(e);
+
+            Initialize();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            Initialize();
+        }
+
+        bool _initialized, _hooked;
+        void Initialize()
+        {
+            if (!_initialized && Context != null)
+            {
+                logMessage.Init(Context, true);
+                _initialized = true;
+            }
+
+            if (!_hooked && _initialized && Context != null && IsHandleCreated)
+            {
+                AddCommandTarget(logMessage.CommandTarget);
+                AddWindowPane(logMessage.WindowPane);
+                _hooked = true;
+            }
+        }
+
+        public string NewDirectoryName
+        {
+            get { return toUrlBox.Text; }
+            set { toUrlBox.Text = value; }
+        }
+
+        public bool NewDirectoryReadonly
+        {
+            get { return !toUrlBox.Enabled; }
+            set { toUrlBox.Enabled = !value; }
+        }
+
+        public string LogMessage
+        {
+            get { return logMessage.Text; }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void versionBox_TextChanged(object sender, EventArgs e)
+        {
+            specificVersionRadio.Checked = true;
+        }
+    }
+}
