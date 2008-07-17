d36cfe8856dcd9c75db2b487e2796a3c2a057251
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-07-17 21:33:14 +0000

    Added UI definition for branch command on solution and project level.
    * src/Ankh.Ids/AnkhCommand.cs
      Added SolutionBranch and ProjectBranch commands
    
    * src/Ankh/Commands/BranchSolutionCommand.cs
      Added implementation that makes sure they are hidden until implemented
      
    * src/Ankh/Ankh.csproj
      Added file
    
    * src/Ankh.Package/Ankh.Package.csproj
    * src/Ankh.Package/Gui/PendingChanges/Branch.png
      Added image
      
    * src/Ankh.Package/Gui/AnkhSvn.xml
      Added Branch menu item under Solution->Subversion and Project->Subversion
      
    * src/Ankh/Commands/CommitItemCommand.cs
      Removed unused variable warning

---
+++ trunk/src/Ankh/Commands/BranchSolutionCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ankh.Ids;
+
+namespace Ankh.Commands
+{
+    [Command(AnkhCommand.ProjectBranch)]
+    [Command(AnkhCommand.SolutionBranch)]
+    class BranchSolutionCommand : CommandBase
+    {
+        public override void OnUpdate(CommandUpdateEventArgs e)
+        {
+            e.Enabled = false;
+        }
+        public override void OnExecute(CommandEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
