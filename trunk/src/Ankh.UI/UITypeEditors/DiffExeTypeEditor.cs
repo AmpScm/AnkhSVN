91f3ef2ad80afd134c270183026853460df8f49e
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-07-23 21:05:37 +0000

    Fixed diff tools template paths for X64 windows versions by retrieving the program files name. This should be further enhanced to find the real locations via a registry search.

--- trunk/src/Ankh.UI/UITypeEditors/DiffExeTypeEditor.cs
+++ trunk/src/Ankh.UI/UITypeEditors/DiffExeTypeEditor.cs
@@ -16,31 +16,33 @@ namespace Ankh.UI.UITypeEditors
 
         protected override StringEditorTemplate[] GetTemplates()
         {
+            string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
             return new StringEditorTemplate[]{
                         new StringEditorTemplate("%base", "Base version", "Base version (%base)"),
                         new StringEditorTemplate("%mine", "My version", "My version (%mine)"),
 
                         // TortoiseMerge
-                        new StringEditorTemplate(@"C:\Program Files\TortoiseSVN\bin\TortoiseMerge.exe " +
+                        new StringEditorTemplate("\"" + programFiles + @"\TortoiseSVN\bin\TortoiseMerge.exe"" " +
                                                  "/base:\"%base\" /mine:\"%mine\"",
                                                  "TortoiseMerge", "TortoiseMerge"),
 
                         // KDiff3
-                        new StringEditorTemplate(@"C:\Program Files\KDiff3\KDiff3.exe " + 
+                        new StringEditorTemplate("\"" + programFiles + @"\KDiff3\KDiff3.exe"" " + 
                                                  "\"%base\" --fname \"Base version\" \"%mine\" --fname \"My version\"", 
                                                  "KDiff3", "KDiff3"),
 
                         // SourceGear DiffMerge
-                        new StringEditorTemplate(@"C:\Program Files\SourceGear\DiffMerge\DiffMerge.exe " +
+                        new StringEditorTemplate("\"" + programFiles + @"\SourceGear\DiffMerge\DiffMerge.exe"" " +
                                                  "\"%base\" \"%mine\" " +
                                                  "/t1=\"Base version\" /t2=\"My version\"", "DiffMerge", "DiffMerge"),
 
                         // WinMerge
-                        new StringEditorTemplate(@"C:\Program Files\WinMerge\WinMergeU.exe -e -x -ub " +
+                        new StringEditorTemplate("\"" + programFiles + @"\WinMerge\WinMergeU.exe"" -e -x -ub " +
                                                  "-dl \"Base version\" -dr \"My version\" " +
                                                  "\"%base\" \"%mine\"", "WinMerge", "WinMerge"),
 
-                        new StringEditorTemplate(@"C:\Program Files\Araxis\Araxis Merge\Compare.exe " +
+                        new StringEditorTemplate("\"" + programFiles + @"\Araxis\Araxis Merge\Compare.exe"" " +
                                                 "/wait /2 \"%base\" \"%mine\"", "Araxis", "Araxis"),
                         };
         }
