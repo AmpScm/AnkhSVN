aa10dff60f9d92bf43b7aae43a12a0cadde3e60c
Bert Huijben <rhuijben@users.noreply.github.com>
2008-07-25 23:19:48 +0000

    Synchronize conflict handler requests back to the UI thread and check if interactive merging on conflict is enabled in the settings.
    * src/Ankh.UI/MergeWizard/InteractiveConflictService.cs
      Added Invoke to UI thread when InvokeRequired is true
      Check InteractiveMergeOnConflict property on global config instance

--- src/Ankh.UI/MergeWizard/InteractiveConflictService.cs
+++ src/Ankh.UI/MergeWizard/InteractiveConflictService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Ankh.Scc;
 using SharpSvn;
+using System.ComponentModel;
+using Ankh.Configuration;
 
 namespace Ankh.UI.MergeWizard
 {
@@ -16,28 +18,53 @@ namespace Ankh.UI.MergeWizard
 
         public void RegisterConflictHandler(SharpSvn.SvnClientArgsWithConflict args, System.ComponentModel.ISynchronizeInvoke synch)
         {
-            args.Conflict += new EventHandler<SvnConflictEventArgs>(OnConflict);
+            args.Conflict += new EventHandler<SvnConflictEventArgs>(new Handler(this, synch).OnConflict);
         }
 
         #endregion
 
-        MergeConflictHandler _currentMergeConflictHandler;
-        private void OnConflict(object sender, SvnConflictEventArgs args)
+        class Handler : AnkhService
         {
-            if (_currentMergeConflictHandler == null)
-            {
-                _currentMergeConflictHandler = CreateMergeConflictHandler();
-            }
-            
-            _currentMergeConflictHandler.OnConflict(args);
-        }
+            ISynchronizeInvoke _synchronizer;
+            MergeConflictHandler _currentMergeConflictHandler;
 
-        private MergeConflictHandler CreateMergeConflictHandler()
-        {
-            MergeConflictHandler mergeConflictHandler = new MergeConflictHandler();
-            mergeConflictHandler.PromptOnBinaryConflict = true;
-            mergeConflictHandler.PromptOnTextConflict = true;
-            return mergeConflictHandler;
+            public Handler(IAnkhServiceProvider context, ISynchronizeInvoke synchronizer)
+                : base(context)
+            {
+                _synchronizer = synchronizer;
+            }
+
+            public void OnConflict(object sender, SvnConflictEventArgs e)
+            {
+                if (_synchronizer != null && _synchronizer.InvokeRequired)
+                {
+                    // If needed marshall the call to the UI thread
+
+                    e.Detach(); // Make this instance thread safe!
+
+                    _synchronizer.Invoke(new EventHandler<SvnConflictEventArgs>(OnConflict), new object[] { sender, e });
+                    return;
+                }
+
+                AnkhConfig config = GetService<IAnkhConfigurationService>().Instance;
+
+                if (config.InteractiveMergeOnConflict)
+                {
+                    // Only call interactive merge if the user opted in on it
+                    if (_currentMergeConflictHandler == null)
+                        _currentMergeConflictHandler = CreateMergeConflictHandler();
+
+                    _currentMergeConflictHandler.OnConflict(e);
+                }
+            }
+
+            private MergeConflictHandler CreateMergeConflictHandler()
+            {
+                MergeConflictHandler mergeConflictHandler = new MergeConflictHandler();
+                mergeConflictHandler.PromptOnBinaryConflict = true;
+                mergeConflictHandler.PromptOnTextConflict = true;
+                return mergeConflictHandler;
+            }
         }
     }
 }
