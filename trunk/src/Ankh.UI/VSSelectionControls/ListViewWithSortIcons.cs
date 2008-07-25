26a75115095f65cf9b26bf13a8b5976d55868175
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-07-25 20:35:45 +0000

    Added ListViewWithSortIcons class, that allows to show Vista/XP style sort icons on the headers.
    * src/Ankh.UI/VSSelectionControls/ListViewWithSortIcons.cs
      Added SetSortIcon method that allows setting the sort glyphs to a list view header
      on XP and higher
      
    * src/Ankh.UI/VSSelectionControls/ListViewWithSelection.cs
      Inherit ListViewWithSortIcons instead of ListView
      
      
    * src/Ankh.UI/Ankh.UI.csproj
      Added file

---
+++ trunk/src/Ankh.UI/VSSelectionControls/ListViewWithSortIcons.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Ankh.UI.VSSelectionControls
+{
+    public enum SortIcon
+    {
+        None,
+        Up,
+        Down
+    }
+
+    public class ListViewWithSortIcons : ListView
+    {
+        #region SortIcons
+        static class NativeMethods
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct HDITEM
+            {
+                public Int32 mask;
+                public Int32 cxy;
+                [MarshalAs(UnmanagedType.LPTStr)]
+                public String pszText;
+                public IntPtr hbm;
+                public Int32 cchTextMax;
+                public Int32 fmt;
+                public IntPtr lParam;
+                public Int32 iImage;
+                public Int32 iOrder;
+            };
+
+            // Parameters for ListView-Headers
+            public const Int32 HDI_FORMAT = 0x0004;
+            public const Int32 HDF_LEFT = 0x0000;
+            public const Int32 HDF_STRING = 0x4000;
+            public const Int32 HDF_SORTUP = 0x0400;
+            public const Int32 HDF_SORTDOWN = 0x0200;
+            public const Int32 LVM_GETHEADER = 0x1000 + 31;  // LVM_FIRST + 31
+            public const Int32 HDM_GETITEM = 0x1200 + 11;  // HDM_FIRST + 11
+            public const Int32 HDM_SETITEM = 0x1200 + 12;  // HDM_FIRST + 12
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr SendMessage(IntPtr Handle, Int32 msg, IntPtr wParam, ref HDITEM lParam);
+        }
+
+        public void SetSortIcon(int column, SortIcon mode)
+        {
+            if (column < 0 || column > Columns.Count)
+                throw new ArgumentOutOfRangeException("column", column, "Invalid column number");
+
+            IntPtr hHeader = NativeMethods.SendMessage(Handle, NativeMethods.LVM_GETHEADER, IntPtr.Zero, IntPtr.Zero);
+            IntPtr col = new IntPtr(column);
+            NativeMethods.HDITEM hdItem = new NativeMethods.HDITEM();
+            IntPtr rtn;
+
+            // Only update the previous item if it existed and if it was a different one.
+            hdItem.mask = NativeMethods.HDI_FORMAT;
+            rtn = NativeMethods.SendMessage(hHeader, NativeMethods.HDM_GETITEM, col, ref hdItem);
+
+            hdItem.mask = NativeMethods.HDI_FORMAT;
+            hdItem.fmt &= ~(NativeMethods.HDF_SORTDOWN | NativeMethods.HDF_SORTUP);
+            switch (mode)
+            {
+                case SortIcon.Up:
+                    hdItem.fmt |= NativeMethods.HDF_SORTUP;
+                    break;
+                case SortIcon.Down:
+                    hdItem.fmt |= NativeMethods.HDF_SORTDOWN;
+                    break;
+            }
+
+            rtn = NativeMethods.SendMessage(hHeader, NativeMethods.HDM_SETITEM, col, ref hdItem);
+        }
+        #endregion
+    }
+}
