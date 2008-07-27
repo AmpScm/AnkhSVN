f522fb722a394e87f68f50791bfd9bca50171f55
Bert Huijben <rhuijben@users.noreply.github.com>
2008-07-27 23:37:09 +0000

    Enable the internal Diff window to show a real diff.
    * src/Ankh.UI/DiffWindow/DiffToolWindowControl.cs
      Enabled showing diff
      
    * src/Ankh.Package/AnkhSvnPackage.ToolWindows.cs
      Removed VS toolbar until one is defined for this window

--- src/Ankh.UI/DiffWindow/DiffToolWindowControl.cs
+++ src/Ankh.UI/DiffWindow/DiffToolWindowControl.cs
@@ -8,6 +8,9 @@ using System.Windows.Forms;
 using Ankh.Scc.UI;
 using Ankh.UI.Services;
 using Microsoft.VisualStudio.Shell.Interop;
+using System.Collections.ObjectModel;
+using System.IO;
+using Ankh.Diff.DiffUtils;
 
 namespace Ankh.UI.DiffWindow
 {
@@ -59,11 +62,29 @@ namespace Ankh.UI.DiffWindow
             //throw new NotImplementedException();
         }
 
+        private void GetFileLines(string strA, string strB, out Collection<string> A, out Collection<string> B)
+        {
+            A = Functions.GetFileTextLines(strA);
+            B = Functions.GetFileTextLines(strB);
+        }
 
         public void Reset(int n, AnkhDiffArgs args)
         {
             _nFrame = n;
             Clear();
+
+            Collection<string> A, B;
+            GetFileLines(args.BaseFile, args.MineFile, out A, out B);
+            TextDiff Diff = new TextDiff(HashType.HashCode, false, false);
+            EditScript Script = Diff.Execute(A, B);
+
+            string strCaptionA = "Base";
+            string strCaptionB = "Mine";
+            //Ankh.Diff.FileName fnA = new Ankh.Diff.FileName(mine);
+            //Ankh.Diff.FileName fnB = new Ankh.Diff.FileName(theirs);
+            diffControl1.SetData(A, B, Script, strCaptionA, strCaptionB);
+
+            ((IAnkhUISite)Site).Title = (args.MineTitle ?? Path.GetFileName(args.MineFile)) + " - Diff";
         }
     }
 }
