38a9ce781c7d89d86fe0ad8da53302c643b465bd
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-07-28 13:54:42 +0000

    Cleaned up the Diff and Merge templates to take advantage of the new label fields.
    * src/Ankh.UI/UITypeEditors/MergeExeTypeEditor.cs
    * src/Ankh.UI/UITypeEditors/DiffExeTypeEditor.cs
      Updated code: added titles and some missing parameters

--- trunk/src/Ankh.UI/UITypeEditors/MergeExeTypeEditor.cs
+++ trunk/src/Ankh.UI/UITypeEditors/MergeExeTypeEditor.cs
@@ -19,38 +19,45 @@ namespace Ankh.UI.UITypeEditors
             string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
 
             return new StringEditorTemplate[]{
-                        new StringEditorTemplate("%mine", "My version", "My version (%mine)"),
-                        new StringEditorTemplate("%base", "Base version", "Base version (%base)"),
-                        new StringEditorTemplate("%theirs", "Their version", "Their version (%theirs)"),
-                        new StringEditorTemplate("%merged", "Merged output", "Merged ouput (%merged)"),
+                        new StringEditorTemplate("%mine", "My version", "My Version (%mine)"),
+                        new StringEditorTemplate("%base", "Base version", "Base Version (%base)"),
+                        new StringEditorTemplate("%theirs", "Their version", "Their Version (%theirs)"),
+                        new StringEditorTemplate("%merged", "Merged output", "Merged Ouput (%merged)"),
+
+                        new StringEditorTemplate("%minename", "My Name", "My Name (%minename)"),
+                        new StringEditorTemplate("%basename", "Base Name", "Base Name (%basename)"),
+                        new StringEditorTemplate("%theirsname", "Their Name", "Their Name (%theirsname)"),
+                        new StringEditorTemplate("%mergedname", "Merged Name", "Merged Name (%mergedname)"),
 
                         // TortoiseMerge
-                        new StringEditorTemplate("\"" + programFiles + @"\TortoiseSVN\bin\TortoiseMerge.exe " +
-                                                 "/base:\"%base\" /theirs:\"%theirs\" /mine:\"%mine\" /merged:\"%merged\"",
+                        // BH: I use  %PROGRAMFILES% here, as that is /not/ mapped at X64!
+                        new StringEditorTemplate((@"'%PROGRAMFILES%\TortoiseSVN\bin\TortoiseMerge.exe' " +
+                                                 "/base:'%base' /theirs:'%theirs' /mine:'%mine' /merged:'%merged'").Replace('\'', '"'),
                                                  "TortoiseMerge", "TortoiseMerge"),
 
                         // KDiff3
-                        new StringEditorTemplate("\"" + programFiles + @"\KDiff3\KDiff3.exe " + 
-                                                 "\"%base\" --fname \"Base version\" \"%theirs\" --fname \"Their version\" " + 
-                                                 "\"%mine\" --fname \"My version\" -o \"%merged\"", "KDiff3", "KDiff3"),
+                        new StringEditorTemplate(("'" + programFiles + @"\KDiff3\KDiff3.exe' " + 
+                                                 "'%base' --fname '%basename' '%theirs' --fname '%theirsname' " + 
+                                                 "'%mine' --fname '%minename' -o '%merged'").Replace('\'', '"'), 
+                                                 "KDiff3", "KDiff3"),
 
                         // DiffMerge
                         new StringEditorTemplate(("'" + programFiles + @"\SourceGear\DiffMerge\DiffMerge.exe' " +
                                                  "/m '/r=%merged' '%base' '%mine' '%theirs' " +
-                                                 "'/t1=%bname' '/t2=%yname' '/t3=%tname' '/c=%mname'").Replace('\'', '\"'),
+                                                 "'/t1=%basename' '/t2=%minename' '/t3=%theirsname' '/c=%mergedname'").Replace('\'', '\"'),
                                                  "DiffMerge", "DiffMerge"),
                         // WinMerge
-                        new StringEditorTemplate("\"" + programFiles + @"\WinMerge\WinMergeU.exe -e -x -ub " +
-                                                 "-dl \"Base version\" -dr \"My version\" " +
-                                                 "\"%base\" \"%mine\" \"%theirs\"", "WinMerge", "WinMerge"),
-
+                        // BH: This one misses the %merged reference
+                        new StringEditorTemplate(("'" + programFiles + @"\WinMerge\WinMergeU.exe' -e -x -ub " +
+                                                 "-dl '%basename' -dr '%minename' " +
+                                                 "'%base' '%mine' '%theirs'").Replace('\'', '"'),
+                                                 "WinMerge", "WinMerge"),
                        
-
                         // Araxis merge
-                        new StringEditorTemplate( "\"" + programFiles + @"\Araxis\Araxis Merge\Compare.exe " +
-                            "/wait /swap /a3 /3 /title1:Base /title2:Theirs /title3:Merged " +
-                            "\"%base\" \"%theirs\" \"%mine\" \"%merged\"", "Araxis", "Araxis" ),
-
+                        new StringEditorTemplate(("'" + programFiles + @"\Araxis\Araxis Merge\Compare.exe' " +
+                                                "/wait /swap /a3 /3 /title1:'%basename' /title2:'%theirsname' " +
+                                                "/title3:'%minename' '%base' '%theirs' '%mine' '%merged'").Replace('\'', '"'), 
+                                                "Araxis", "Araxis" ),
                  };
 
 
