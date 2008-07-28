1936809fea47b81ba9847606ebb44c1e9bef4087
Bert Huijben <rhuijben@users.noreply.github.com>
2008-07-28 13:54:42 +0000

    Cleaned up the Diff and Merge templates to take advantage of the new label fields.
    * src/Ankh.UI/UITypeEditors/MergeExeTypeEditor.cs
    * src/Ankh.UI/UITypeEditors/DiffExeTypeEditor.cs
      Updated code: added titles and some missing parameters

--- src/Ankh.UI/UITypeEditors/DiffExeTypeEditor.cs
+++ src/Ankh.UI/UITypeEditors/DiffExeTypeEditor.cs
@@ -21,29 +21,35 @@ namespace Ankh.UI.UITypeEditors
             return new StringEditorTemplate[]{
                         new StringEditorTemplate("%base", "Base version", "Base version (%base)"),
                         new StringEditorTemplate("%mine", "My version", "My version (%mine)"),
+                        new StringEditorTemplate("%basename", "Base Name", "Base Name (%basename)"),
+                        new StringEditorTemplate("%minename", "Mine Name", "Mine Name (%minename)"),
 
                         // TortoiseMerge
-                        new StringEditorTemplate("\"" + programFiles + @"\TortoiseSVN\bin\TortoiseMerge.exe"" " +
-                                                 "/base:\"%base\" /mine:\"%mine\"",
+                        // BH: I use %PROGRAMFILES% here, as that is /not/ mapped at X64!
+                        new StringEditorTemplate((@"'%PROGRAMFILES%\TortoiseSVN\bin\TortoiseMerge.exe' " +
+                                                 "/base:'%base' /mine:'%mine'").Replace('\'', '"'),
                                                  "TortoiseMerge", "TortoiseMerge"),
 
                         // KDiff3
-                        new StringEditorTemplate("\"" + programFiles + @"\KDiff3\KDiff3.exe"" " + 
-                                                 "\"%base\" --fname \"Base version\" \"%mine\" --fname \"My version\"", 
+                        new StringEditorTemplate(("'" + programFiles + @"\KDiff3\KDiff3.exe' " + 
+                                                 "'%base' --fname '%basename' '%mine' --fname '%minename'").Replace('\'','"'), 
                                                  "KDiff3", "KDiff3"),
 
                         // SourceGear DiffMerge
-                        new StringEditorTemplate("\"" + programFiles + @"\SourceGear\DiffMerge\DiffMerge.exe"" " +
-                                                 "\"%base\" \"%mine\" " +
-                                                 "/t1=\"Base version\" /t2=\"My version\"", "DiffMerge", "DiffMerge"),
+                        new StringEditorTemplate(("'" + programFiles + @"\SourceGear\DiffMerge\DiffMerge.exe' " +
+                                                 "'%base' '%mine' " + "/t1='%basename' /t2='%minename'").Replace('\'', '"'), 
+                                                 "DiffMerge", "DiffMerge"),
 
                         // WinMerge
-                        new StringEditorTemplate("\"" + programFiles + @"\WinMerge\WinMergeU.exe"" -e -x -ub " +
-                                                 "-dl \"Base version\" -dr \"My version\" " +
-                                                 "\"%base\" \"%mine\"", "WinMerge", "WinMerge"),
+                        new StringEditorTemplate(("'" + programFiles + @"\WinMerge\WinMergeU.exe' -e -x -ub " +
+                                                 "-dl '%basename' -dr 'minename' " +
+                                                 "'%base' '%mine'").Replace('\'', '"'),
+                                                 "WinMerge", "WinMerge"),
 
-                        new StringEditorTemplate("\"" + programFiles + @"\Araxis\Araxis Merge\Compare.exe"" " +
-                                                "/wait /2 \"%base\" \"%mine\"", "Araxis", "Araxis"),
+                        new StringEditorTemplate(("'" + programFiles + @"\Araxis\Araxis Merge\Compare.exe' " +
+                                                "/wait /2 /title1:'%basename' /title2:'%minename' " +
+                                                "'%base' '%mine'").Replace('\'', '"'),
+                                                "Araxis", "Araxis"),
                         };
         }
     }
