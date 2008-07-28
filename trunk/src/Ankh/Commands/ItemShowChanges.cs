b8e71d87e909402d48af5cff4ebca263e6b6d5ef
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-07-28 00:13:05 +0000

    Cleanup unified diff support from diff support.
    * src/Ankh/Commands/ItemShowChanges.cs
    * src/Ankh/Commands/DiffLocalItemCommand.cs
      New name for the primary Show Changes class.
      
      Use external or internal visual diff. (No more unified diff)
       
    * src/Ankh/Commands/ItemUnifiedDiffCommand.cs
      Moved unified diff support here (Currently not connected to a command)
    
    * src/Ankh/Ankh.csproj
      Changed for file rename

---
+++ trunk/src/Ankh/Commands/ItemShowChanges.cs
@@ -0,0 +1,190 @@
+// $Id$
+using System.IO;
+using Ankh.UI;
+using Ankh.Ids;
+using Ankh.VS;
+using System;
+using Microsoft.VisualStudio.Shell.Interop;
+using SharpSvn;
+using System.Collections.Generic;
+using Ankh.Configuration;
+using Ankh.Scc.UI;
+
+namespace Ankh.Commands
+{
+    /// <summary>
+    /// Shows differences compared to local text base.
+    /// </summary>
+    [Command(AnkhCommand.DiffLocalItem)]
+    [Command(AnkhCommand.ItemCompareBase)]
+    [Command(AnkhCommand.ItemCompareCommitted)]
+    [Command(AnkhCommand.ItemCompareHead)]
+    [Command(AnkhCommand.ItemComparePrevious)]
+    [Command(AnkhCommand.ItemCompareSpecific)]
+    [Command(AnkhCommand.ItemShowChanges)]
+    public sealed class DiffLocalItem : CommandBase
+    {
+        public override void OnUpdate(CommandUpdateEventArgs e)
+        {
+            if (e.Command == AnkhCommand.DiffExternalLocalItem)
+            {
+                e.Enabled = false; // Unsupported for now
+                return;
+            }
+
+            bool noConflictDiff = e.Command == AnkhCommand.ItemShowChanges;
+
+            foreach (SvnItem item in e.Selection.GetSelectedSvnItems(false))
+            {
+                if (item.IsVersioned && (item.Status.CombinedStatus != SvnStatus.Added || item.Status.IsCopied))
+                {
+                    if (e.Command == AnkhCommand.ItemCompareBase || e.Command == AnkhCommand.ItemShowChanges)
+                    {
+                        if (!(item.IsModified || item.IsDocumentDirty))
+                            continue;
+                    }
+
+                    if (noConflictDiff && item.IsConflicted)
+                    {
+                        // Use advanced diff to get a diff, or 'Edit Conflict' to resolve it
+                        continue;
+                    }
+
+                    return;
+                }
+            }
+            e.Enabled = false;
+        }
+
+        public override void OnExecute(CommandEventArgs e)
+        {
+            List<SvnItem> selectedFiles = new List<SvnItem>();
+
+            foreach (SvnItem item in e.Selection.GetSelectedSvnItems(false))
+            {
+                if (!item.IsVersioned || (item.Status.CombinedStatus == SvnStatus.Added && !item.Status.IsCopied))
+                    continue;
+
+                if (e.Command == AnkhCommand.ItemCompareBase || e.Command == AnkhCommand.ItemShowChanges)
+                {
+                    if (!(item.IsModified || item.IsDocumentDirty))
+                        continue;
+                }
+
+                selectedFiles.Add(item);
+            }
+
+            SvnRevisionRange revRange = null;
+            switch (e.Command)
+            {
+                case AnkhCommand.DiffExternalLocalItem:
+                    goto case AnkhCommand.DiffLocalItem;
+                case AnkhCommand.DiffLocalItem:
+                case AnkhCommand.ItemCompareBase:
+                case AnkhCommand.ItemShowChanges:
+                    revRange = new SvnRevisionRange(SvnRevision.Base, SvnRevision.Working);
+                    break;
+                case AnkhCommand.ItemCompareCommitted:
+                    revRange = new SvnRevisionRange(SvnRevision.Committed, SvnRevision.Working);
+                    break;
+                case AnkhCommand.ItemCompareHead:
+                    revRange = new SvnRevisionRange(SvnRevision.Head, SvnRevision.Working);
+                    break;
+                case AnkhCommand.ItemComparePrevious:
+                    revRange = new SvnRevisionRange(SvnRevision.Previous, SvnRevision.Working);
+                    break;
+            }
+
+            if (selectedFiles.Count > 1 || revRange == null)
+            {
+                PathSelectorInfo info = new PathSelectorInfo("Select item for Diff", selectedFiles);
+                info.SingleSelection = false;
+                info.RevisionStart = revRange == null ? SvnRevision.Base : revRange.StartRevision;
+                info.RevisionEnd = revRange == null ? SvnRevision.Working : revRange.EndRevision;
+
+                info.EnableRecursive = false;
+                info.Depth = SvnDepth.Infinity;
+
+                PathSelectorResult result;
+                // should we show the path selector?
+                if (e.PromptUser && !CommandBase.Shift)
+                {
+                    IUIShell ui = e.GetService<IUIShell>();
+
+                    result = ui.ShowPathSelector(info);
+                    if (!result.Succeeded)
+                        return;
+
+                    if (info == null)
+                        return;
+                }
+                else
+                    result = info.DefaultResult;
+
+                selectedFiles.Clear();
+                selectedFiles.AddRange(result.Selection);
+            }
+
+            List<AnkhDiffArgs> diffArgs = new List<AnkhDiffArgs>();
+
+            IAnkhDiffHandler diff = e.GetService<IAnkhDiffHandler>();
+
+            string tempDir = e.GetService<IAnkhTempDirManager>().GetTempDir();
+
+            foreach (SvnItem item in selectedFiles)
+            {
+                AnkhDiffArgs da = new AnkhDiffArgs();
+
+                da.BaseFile = GetPath(e.Context, revRange.StartRevision, item, tempDir);
+                da.MineFile = GetPath(e.Context, revRange.EndRevision, item, tempDir);
+
+                da.MineTitle = item.Name + " - Mine";
+                da.BaseTitle = item.Name + " - Base";
+
+                diff.RunDiff(da);
+            }
+        }
+
+        private string GetPath(IAnkhServiceProvider context, SvnRevision revision, SvnItem item, string tempDir)
+        {
+            if (revision == SvnRevision.Working)
+            {
+                return item.FullPath;
+            }
+
+            string tempFile = Path.GetFileNameWithoutExtension(item.Name) + "." + revision.ToString() + Path.GetExtension(item.Name);
+            tempFile = Path.Combine(tempDir, tempFile);
+            // we need to get it from the repos
+            context.GetService<IProgressRunner>().Run("Retrieving file for diffing", delegate(object o, ProgressWorkerArgs ee)
+            {
+                SvnTarget target;
+
+                switch (revision.RevisionType)
+                {
+                    case SvnRevisionType.Head:
+                    case SvnRevisionType.Number:
+                    case SvnRevisionType.Time:
+                        target = new SvnUriTarget(item.Status.Uri);
+                        break;
+                    default:
+                        target = new SvnPathTarget(item.FullPath);
+                        break;
+                }
+                SvnWriteArgs args = new SvnWriteArgs();
+                args.Revision = revision;
+                args.SvnError += delegate(object sender, SvnErrorEventArgs eea)
+                {
+                    if (eea.Exception is SvnClientUnrelatedResourcesException)
+                        eea.Cancel = true;
+                };
+
+                using (FileStream stream = new FileStream(tempFile, FileMode.Create, FileAccess.Write))
+                {
+                    ee.Client.Write(target, stream, args);
+                }
+            });
+
+            return tempFile;
+        }
+    }
+}
---
+++ trunk/src/Ankh/Commands/ItemUnifiedDiffCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpSvn;
+using Ankh.Ids;
+using Ankh.UI;
+using System.IO;
+using Ankh.VS;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Ankh.Commands
+{
+    class ItemUnifiedDiffCommand : LocalDiffCommandBase
+    {
+        public override void OnExecute(CommandEventArgs e)
+        {
+            IContext context = e.Context.GetService<IContext>();
+            List<string> selectedFiles = new List<string>();
+            foreach (SvnItem i in e.Selection.GetSelectedSvnItems(false))
+            {
+                if (i.IsModified)
+                    selectedFiles.Add(i.FullPath);
+            }
+
+            SvnRevisionRange revRange = null;
+            bool forceExternal = false;
+            switch (e.Command)
+            {
+                case AnkhCommand.DiffExternalLocalItem:
+                    forceExternal = true;
+                    goto case AnkhCommand.DiffLocalItem;
+                case AnkhCommand.DiffLocalItem:
+                case AnkhCommand.ItemCompareBase:
+                case AnkhCommand.ItemShowChanges:
+                    revRange = new SvnRevisionRange(SvnRevision.Base, SvnRevision.Working);
+                    break;
+                case AnkhCommand.ItemCompareCommitted:
+                    revRange = new SvnRevisionRange(SvnRevision.Committed, SvnRevision.Working);
+                    break;
+                case AnkhCommand.ItemCompareHead:
+                    revRange = new SvnRevisionRange(SvnRevision.Head, SvnRevision.Working);
+                    break;
+                case AnkhCommand.ItemComparePrevious:
+                    revRange = new SvnRevisionRange(SvnRevision.Previous, SvnRevision.Working);
+                    break;
+            }
+            string diff = this.GetDiff(context, e.Selection, revRange, forceExternal);
+            if (diff != null)
+            {
+                // convert it to HTML and store in a temp file
+                DiffHtmlModel model = new DiffHtmlModel(diff);
+                string html = model.GetHtml();
+                string htmlFile = Path.GetTempFileName();
+                TempFileCollection.AddFile(htmlFile, false);
+                using (StreamWriter w = new StreamWriter(htmlFile, false, System.Text.Encoding.Default))
+                    w.Write(html);
+
+                IAnkhWebBrowser browser = e.Context.GetService<IAnkhWebBrowser>();
+                BrowserArgs args = new BrowserArgs();
+                args.CreateFlags = __VSCREATEWEBBROWSER.VSCWB_AutoShow |
+                    __VSCREATEWEBBROWSER.VSCWB_NoHistory |
+                    __VSCREATEWEBBROWSER.VSCWB_StartCustom |
+                    __VSCREATEWEBBROWSER.VSCWB_OptionDisableStatusBar;
+                args.BaseCaption = selectedFiles.Count == 1 ? Path.GetFileName(selectedFiles[0]) : "Subversion";
+                browser.Navigate(new Uri("file:///" + htmlFile), args);
+            }
+        }
+    }
+}
