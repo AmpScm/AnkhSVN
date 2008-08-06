66b142bc3f7d62b79852cf2f18e169c5d0492b7b
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-08-06 09:59:00 +0000

    Added GlobalServiceAttribute support to allow declaring services on service implementations instead of requiring registration from the module class.
    * src/Ankh.Services/Ankh.Services.csproj
    * src/Ankh.Services/GlobalServiceAttribute.cs
    * src/Ankh.Services/IAnkhServiceImplementation.cs
      Added attribute that is only valid on classes implementing
      the IAnkhServiceImplementation 'marker interface'.
      
    * src/Ankh.Services/AnkhRuntime.cs
      (LoadServices): Added method that allows initializing all services
      of an assembly in the specified container.
      
    * src/Ankh.Services/AnkhService.cs
      Implement IAnkhServiceImplementation

---
+++ trunk/src/Ankh.Services/IAnkhServiceImplementation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh
+{
+    public interface IAnkhServiceImplementation
+    {
+        /// <summary>
+        /// Called when the service is instantiated
+        /// </summary>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        void OnPreInitialize();
+
+        /// <summary>
+        /// Called after all modules and services received their OnPreInitialize
+        /// </summary>
+        /// <param name="e"></param>
+        void OnInitialize();
+    }
+}
