05f7ef0d39c0b934d469cd42ce2785de6130a58f
Bert Huijben <rhuijben@users.noreply.github.com>
2008-08-07 12:27:02 +0000

    Added OnShowContextMenu handler to TreeViewWithSelection and use .ShowContext to show the context menu in the working copy explorer.
    * src/Ankh.UI/WorkingCopyExplorer/WorkingCopyExplorerControl.cs
      Use .ShowContextMenu events instead of trusting on right click and keyboard
      actions.
      
    * src/Ankh.UI/VSSelectionControls/TreeViewWithSelection.cs
      Added WM_CONTEXT handler, routing to OnShowContextMenu()
      
    * src/Ankh.UI/WorkingCopyExplorer/FileSystemTreeView.cs
    * src/Ankh.UI/WorkingCopyExplorer/FileSystemDetailsView.cs
      Removed IWorkingCopyExplorerSubControl interface

--- src/Ankh.UI/WorkingCopyExplorer/FileSystemTreeView.cs
+++ src/Ankh.UI/WorkingCopyExplorer/FileSystemTreeView.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 
 namespace Ankh.UI.WorkingCopyExplorer
 {
-    class FileSystemTreeView : TreeViewWithSelection<FileSystemTreeNode>, IWorkingCopyExplorerSubControl
+    class FileSystemTreeView : TreeViewWithSelection<FileSystemTreeNode>
     {
         public event EventHandler SelectedItemChanged;
 
