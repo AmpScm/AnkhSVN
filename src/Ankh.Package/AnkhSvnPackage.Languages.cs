8618467b2893a5af3c3deeb71718cfc4d193f1e4
Bert Huijben <rhuijben@users.noreply.github.com>
2008-09-07 22:59:26 +0000

    Disable registration of text editor settings until we find a way to make the settings work and edittable. (Currently the options pages work until the PC Window is visible for the first time; but the settings are not applied).
    * src/Ankh.Package/AnkhSvnPackage.Languages.cs
      Disabled text editor setting registration attributes

--- src/Ankh.Package/AnkhSvnPackage.Languages.cs
+++ src/Ankh.Package/AnkhSvnPackage.Languages.cs
@@ -3,29 +3,31 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.Shell;
 using Ankh.UI.PendingChanges;
+using Ankh.Ids;
+using Ankh.VSPackage.Attributes;
 
 namespace Ankh.VSPackage
 {
-    [ProvideLanguageService(typeof(LogMessageLanguageService), LogMessageLanguageService.ServiceName, 301,
+    /*[ProvideLanguageService(typeof(LogMessageLanguageService), LogMessageLanguageService.ServiceName, 301,
         AutoOutlining = false,
         CodeSense = false,
         DefaultToInsertSpaces = true,
-        EnableAdvancedMembersOption=false,
-        EnableAsyncCompletion=false,
-        EnableCommenting=true,
-        EnableLineNumbers=false,
-        MatchBraces=true,
-        MatchBracesAtCaret=true,
-        MaxErrorMessages=10,
-        QuickInfo=false,
-        RequestStockColors=false,
-        ShowCompletion=false,
-        ShowHotURLs=true,
-        ShowMatchingBrace=true,
-        ShowSmartIndent=true,
-        SingleCodeWindowOnly=true)]
-    [ProvideService(typeof(LogMessageLanguageService))]
+        EnableAdvancedMembersOption = false,
+        EnableAsyncCompletion = false,
+        EnableCommenting = true,
+        EnableLineNumbers = false,
+        MatchBraces = true,
+        MatchBracesAtCaret = true,
+        MaxErrorMessages = 10,
+        QuickInfo = false,
+        RequestStockColors = false,
+        ShowCompletion = false,
+        ShowHotURLs = true,
+        ShowMatchingBrace = true,
+        SingleCodeWindowOnly = true)]
+    [ProvideLanguageSettings(typeof(LogMessageLanguageService), LogMessageLanguageService.ServiceName, LogMessageLanguageService.ServiceName, 301, 301)]*/
+    [ProvideService(typeof(LogMessageLanguageService), ServiceName = AnkhId.LogMessageServiceName)]
     partial class AnkhSvnPackage
-    {        
+    {
     }
 }
