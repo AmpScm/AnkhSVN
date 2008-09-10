bb73e76fb86605292e1ea0e8c53dd0a7f0ade8d0
Bert Huijben <rhuijben@users.noreply.github.com>
2008-09-10 16:17:03 +0000

    * src/Ankh.UI/PropertyEditors/EolStylePropertyEditor.Designer.cs * src/Ankh.UI/PropertyEditors/ExecutablePropertyEditor.Designer.cs * src/Ankh.UI/PropertyEditors/ExternalsPropertyEditor.Designer.cs * src/Ankh.UI/PropertyEditors/IgnorePropertyEditor.Designer.cs * src/Ankh.UI/PropertyEditors/MimeTypePropertyEditor.Designer.cs * src/Ankh.UI/PropertyEditors/PlainPropertyEditor.Designer.cs   Handle more resizing options

--- src/Ankh.UI/PropertyEditors/IgnorePropertyEditor.Designer.cs
+++ src/Ankh.UI/PropertyEditors/IgnorePropertyEditor.Designer.cs
@@ -15,60 +15,50 @@ namespace Ankh.UI.PropertyEditors
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.ignoreTextBox = new System.Windows.Forms.TextBox();
             this.ignoreGroupBox = new System.Windows.Forms.GroupBox();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.conflictToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.ignoreTextBox = new System.Windows.Forms.TextBox();
             this.ignoreGroupBox.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ignoreGroupBox
+            // 
+            this.ignoreGroupBox.Controls.Add(this.ignoreTextBox);
+            this.ignoreGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ignoreGroupBox.Location = new System.Drawing.Point(0, 0);
+            this.ignoreGroupBox.Name = "ignoreGroupBox";
+            this.ignoreGroupBox.Size = new System.Drawing.Size(348, 196);
+            this.ignoreGroupBox.TabIndex = 4;
+            this.ignoreGroupBox.TabStop = false;
+            this.ignoreGroupBox.Text = "Ignore the following lines or patterns";
             // 
             // ignoreTextBox
             // 
             this.ignoreTextBox.AcceptsReturn = true;
             this.ignoreTextBox.AcceptsTab = true;
-            this.ignoreTextBox.Location = new System.Drawing.Point(3, 3);
+            this.ignoreTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ignoreTextBox.Location = new System.Drawing.Point(3, 16);
             this.ignoreTextBox.Multiline = true;
             this.ignoreTextBox.Name = "ignoreTextBox";
-            this.ignoreTextBox.Size = new System.Drawing.Size(226, 113);
-            this.ignoreTextBox.TabIndex = 2;
-            this.ignoreTextBox.TextChanged += new System.EventHandler(this.ignoreTextBox_TextChanged);
-            // 
-            // ignoreGroupBox
-            // 
-            this.ignoreGroupBox.Controls.Add(this.panel1);
-            this.ignoreGroupBox.Location = new System.Drawing.Point(3, 3);
-            this.ignoreGroupBox.Name = "ignoreGroupBox";
-            this.ignoreGroupBox.Size = new System.Drawing.Size(244, 144);
-            this.ignoreGroupBox.TabIndex = 4;
-            this.ignoreGroupBox.TabStop = false;
-            this.ignoreGroupBox.Text = "Ignore the following lines or patterns";
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.ignoreTextBox);
-            this.panel1.Location = new System.Drawing.Point(6, 19);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(232, 119);
-            this.panel1.TabIndex = 3;
+            this.ignoreTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ignoreTextBox.Size = new System.Drawing.Size(342, 177);
+            this.ignoreTextBox.TabIndex = 3;
             // 
             // IgnorePropertyEditor
             // 
             this.Controls.Add(this.ignoreGroupBox);
             this.Name = "IgnorePropertyEditor";
             this.ignoreGroupBox.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.ignoreGroupBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
         #endregion
 
-        private System.Windows.Forms.TextBox ignoreTextBox;
         private System.Windows.Forms.GroupBox ignoreGroupBox;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ToolTip conflictToolTip;
         private System.ComponentModel.IContainer components;
+        private System.Windows.Forms.TextBox ignoreTextBox;
         
     }
 }
