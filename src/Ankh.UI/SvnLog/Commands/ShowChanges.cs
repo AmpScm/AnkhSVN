5f2e1f0c3e70a9bcb1116d034f173b120e5191b4
Sander Rijken <1292085+srijken@users.noreply.github.com>
2008-09-17 22:50:38 +0000

    Add Show Changes command to the 'changed paths' panel of the history viewer, to enable diffing arbitrary files in a single revision (REV-1:REV)
    * src/Ankh.Package/Gui/AnkhSvn.xml
      Add UI
    * src/Ankh.UI/SvnLog/Commands/ShowChanges.cs
      Add implementation for ChangedPaths

--- src/Ankh.UI/SvnLog/Commands/ShowChanges.cs
+++ src/Ankh.UI/SvnLog/Commands/ShowChanges.cs
@@ -27,6 +27,19 @@ namespace Ankh.UI.SvnLog.Commands
                 return;
             }
 
+            bool one = false;
+            foreach (ISvnLogChangedPathItem change in e.Selection.GetSelection<ISvnLogChangedPathItem>())
+            {
+                if (one)
+                {
+                    e.Enabled = false;
+                    break;
+                }
+                one = true;
+            }
+            if (one)
+                return;
+
             // TODO: Remove this code when we're able to handle directories
             SvnItem first = null;
             foreach (SvnItem i in logWindow.WorkingCopyItems)
@@ -68,7 +81,23 @@ namespace Ankh.UI.SvnLog.Commands
                 }
             }
             if (!touched)
+            {
+                ISvnLogChangedPathItem change = null;
+                foreach (ISvnLogChangedPathItem c in e.Selection.GetSelection<ISvnLogChangedPathItem>())
+                {
+                    change = c;
+                    touched = true;
+                    break;
+                }
+                if(change != null)
+                {
+                    ExecuteChangedPaths(e, change);
+                    return;
+                }
+            }
+            if(!touched)
                 return;
+
             if (min == max)
                 min--;
 
@@ -87,8 +116,35 @@ namespace Ankh.UI.SvnLog.Commands
                 return;
 
             SvnRevisionRange range = new SvnRevisionRange(min, max);
-
             SvnTarget diffTarget = new SvnPathTarget(workingCopyItem.FullPath);
+            ExecuteDiff(e, range, diffTarget);
+        }
+
+        void ExecuteChangedPaths(CommandEventArgs e, ISvnLogChangedPathItem change)
+        {
+            IWorkingCopyOperations wcOper = e.GetService<IWorkingCopyOperations>();
+            ILogControl logWindow = e.Selection.ActiveDialogOrFrameControl as ILogControl;
+            ISvnClientPool clientPool = e.GetService<ISvnClientPool>();
+
+            Uri repositoryRoot = null;
+            using(SvnClient client = clientPool.GetClient())
+            {
+                client.Info(new SvnPathTarget(logWindow.WorkingCopyItems[0].FullPath),
+                    delegate(object sender, SvnInfoEventArgs infoEventArgs)
+                    {
+                        repositoryRoot = infoEventArgs.RepositoryRoot;
+                    });
+            }
+            if (repositoryRoot == null)
+                return;
+
+            Uri target = new Uri(repositoryRoot, change.Path.TrimStart('/'));
+
+            ExecuteDiff(e, new SvnRevisionRange(change.Revision - 1, change.Revision), new SvnUriTarget(target, change.Revision));
+        }
+
+        void ExecuteDiff(CommandEventArgs e, SvnRevisionRange range, SvnTarget diffTarget)
+        {
             IAnkhDiffHandler diff = e.GetService<IAnkhDiffHandler>();
             AnkhDiffArgs da = new AnkhDiffArgs();
             da.BaseFile = diff.GetTempFile(diffTarget, range.StartRevision, false);
