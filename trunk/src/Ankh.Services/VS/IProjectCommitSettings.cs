5c75a70c91d824cc82e226c989e8c5d5b7b152b8
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-09-21 13:36:37 +0000

    Updated SharpSvn reference to enable using SvnPropertyNames as constants. (Also updates OpenSSL and compiles APR for Windows NT instead of '9X to optimize disk IO)
    * src/Ankh.Services/Ankh.Services.csproj
      Added new file
    * src/Ankh.Services/VS/IProjectCommitSettings.cs
      Added new service definition (Work in progress on real issue)

---
+++ trunk/src/Ankh.Services/VS/IProjectCommitSettings.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh.VS
+{
+    public interface IProjectCommitSettings
+    {        
+        /// <summary>
+        /// Gets the issue tracker URI for the specified issue
+        /// </summary>
+        /// <param name="issueId">The issue id.</param>
+        /// <returns></returns>
+        Uri GetIssueTrackerUri(string issueId);
+
+        /// <summary>
+        /// Gets the raw bug tracker URI.
+        /// </summary>
+        /// <value>The raw bug tracker URI.</value>
+        string RawIssueTrackerUri { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to warn if no issue is provided
+        /// </summary>
+        /// <value><c>true</c> if [warn if no issue]; otherwise, <c>false</c>.</value>
+        bool WarnIfNoIssue { get; }
+
+        /// <summary>
+        /// Gets the raw issue tracker message.
+        /// </summary>
+        /// <value>The raw issue tracker message.</value>
+        string RawIssueTrackerMessage { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether to append the issue tracker message. If false it should be
+        /// inserted before the message
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if [append issue tracker message]; otherwise, <c>false</c>.
+        /// </value>
+        bool AppendIssueTrackerMessage { get; }
+
+        /// <summary>
+        /// Gets the issue label. If null or empty a default label is used
+        /// </summary>
+        /// <value>The issue label.</value>
+        string IssueLabel { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether [show log message box].
+        /// </summary>
+        /// <value><c>true</c> if [show log message box]; otherwise, <c>false</c>.</value>
+        bool ShowIssueBox { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether issue id's are numeric only
+        /// </summary>
+        /// <value><c>true</c> if [nummeric issues]; otherwise, <c>false</c>.</value>
+        /// <remarks>You can always use ',' to specify multiple issues</remarks>
+        bool NummericIssueIds { get; }
+
+        /// <summary>
+        /// Gets the raw log issue regexes.
+        /// </summary>
+        /// <value>The raw log issue regexes.</value>
+        ICollection<string> RawLogIssueRegexes { get; }
+
+        /// <summary>
+        /// Gets the issue references from the specified logmessage
+        /// </summary>
+        /// <param name="logmessage">The logmessage.</param>
+        /// <returns></returns>
+        IEnumerable<IssueMarker> GetIssues(string logmessage);
+
+        /// <summary>
+        /// Builds a log message from the specified message and issueId
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="issueId">The issue id.</param>
+        /// <returns></returns>
+        string BuildLogMessage(string message, string issueId);
+    }
+
+    public class IssueMarker
+    {
+        readonly int _index;
+        readonly int _length;
+        readonly string _value;
+
+        public IssueMarker(int index, int length, string value)
+        {
+            _index = index;
+            _length = length;
+            _value = value;
+        }
+
+        public int Index
+        {
+            get { return _index; }
+        }
+
+        public int Length
+        {
+            get { return _length; }
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }
+    }
+}
