05be9f1ec8731b462fbff2c44025fec6005278e1
Bert Huijben <rhuijben@users.noreply.github.com>
2008-09-29 13:30:22 +0000

    * src/Ankh.Services/Ankh.Services.csproj * src/Ankh.Services/Scc/IAnkhProjectLayoutService.cs   Added currently unused IAnkhProjectLayoutService definition

---
+++ src/Ankh.Services/Scc/IAnkhProjectLayoutService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh
+{
+    public interface IAnkhProjectLayoutService
+    {
+        /// <summary>
+        /// Gets all update roots of the current open solution
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<SvnItem> GetUpdateRoots();
+    }    
+}
