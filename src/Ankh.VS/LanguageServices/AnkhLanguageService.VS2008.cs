84438d32df951131189c2d9ea668b2ce53fc740a
Sander Rijken <1292085+srijken@users.noreply.github.com>
2008-10-16 18:27:53 +0000

    Follow up on r5401: Add some missing files
    * src/Ankh.VS/LanguageServices/AnkhLanguageService.cs
    * src/Ankh.VS/LanguageServices/AnkhLanguageService.VS2008.cs
    * src/Ankh.VS/LanguageServices/UnifiedDiffLanguageService.cs

---
+++ src/Ankh.VS/LanguageServices/AnkhLanguageService.VS2008.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh.VS
+{
+	partial class AnkhLanguageService
+	{
+        public override string GetFormatFilterList()
+        {
+            return "";
+        }
+	}
+
+    partial class AnkhViewFilter
+    {
+        public override void ShowContextMenu(int menuId, Guid groupGuid, Microsoft.VisualStudio.OLE.Interop.IOleCommandTarget target, int x, int y)
+        {
+            PrepareContextMenu(ref menuId, ref groupGuid, ref target);
+
+            base.ShowContextMenu(menuId, groupGuid, target, x, y);
+        }
+    }
+}
---
+++ src/Ankh.VS/LanguageServices/AnkhLanguageService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Package;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System.ComponentModel.Design;
+
+namespace Ankh.VS
+{
+    [CLSCompliant(false)]
+    public abstract partial class AnkhLanguageService : LanguageService, IAnkhServiceImplementation, IAnkhServiceProvider
+    {
+        readonly IAnkhServiceProvider _context;
+
+        protected AnkhLanguageService(IAnkhServiceProvider context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+            _context = context;
+        }
+
+        public virtual void OnPreInitialize()
+        {
+            // Initialize the language service api
+            SetSite(GetService<IServiceContainer>());
+        }
+
+        public virtual void OnInitialize()
+        {
+
+        }
+
+        protected internal IAnkhServiceProvider Context
+        {
+            get { return _context; }
+        }
+
+        #region IAnkhServiceProvider Members
+
+        public T GetService<T>() where T : class
+        {
+            return Context.GetService<T>();
+        }
+
+        public T GetService<T>(Type serviceType) where T : class
+        {
+            return Context.GetService<T>(serviceType);
+        }
+
+        #endregion
+    }
+
+    abstract partial class AnkhViewFilter : ViewFilter
+    {
+        protected AnkhViewFilter(CodeWindowManager mgr, IVsTextView view)
+            : base(mgr, view)
+        {
+        }
+        public virtual void PrepareContextMenu(ref int menuId, ref Guid groupGuid, ref Microsoft.VisualStudio.OLE.Interop.IOleCommandTarget target)
+        {
+            
+        }
+    }
+}
