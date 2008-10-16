1b8c742eb85e6a28f3a365ff670aa0d0852b32bb
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-10-16 18:42:04 +0000

    Follow up to r4001, add another missing file

---
+++ trunk/src/Ankh.VS/LanguageServices/AnkhLanguageService.VS2005.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh.VS
+{
+    // VS2005 SDK only addition to the LogMessageLanguage class
+    // (The VS2008 SDK version compiles LogMessageLanguageService.VS2008.cs instead)
+    partial class AnkhLanguageService
+    {
+        // Intentionally left blank
+    }
+
+    partial class AnkhViewFilter
+    {
+        public override void ShowContextMenu(int menuId, Guid groupGuid, Microsoft.VisualStudio.OLE.Interop.IOleCommandTarget target)
+        {
+            PrepareContextMenu(ref menuId, ref groupGuid, ref target);
+
+            base.ShowContextMenu(menuId, groupGuid, target);
+        }
+    }
+}
