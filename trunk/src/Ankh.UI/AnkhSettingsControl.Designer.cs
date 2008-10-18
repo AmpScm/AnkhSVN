d1a50f6921491726c32abdc1f0838af2bf5d5cfc
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-10-18 10:04:29 +0000

    Added comboboxes instead of editboxes in the settings to make it easier to find custom diff, merge and patch tools. (The boxes are currently unfilled)
    * src/Ankh.UI/AnkhSettingsControl.cs
    * src/Ankh.UI/AnkhSettingsControl.Designer.cs
      Partial commit

--- trunk/src/Ankh.UI/AnkhSettingsControl.Designer.cs
+++ trunk/src/Ankh.UI/AnkhSettingsControl.Designer.cs
@@ -28,34 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cbDiffMergeManual = new System.Windows.Forms.CheckBox();
-            this.txtDiffExePath = new System.Windows.Forms.TextBox();
+            this.diffExeBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnDiffExePath = new System.Windows.Forms.Button();
+            this.diffBrowseBtn = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtMergeExePath = new System.Windows.Forms.TextBox();
-            this.btnMergeExePath = new System.Windows.Forms.Button();
+            this.mergeExeBox = new System.Windows.Forms.ComboBox();
+            this.mergeBrowseBtn = new System.Windows.Forms.Button();
             this.interactiveMergeOnConflict = new System.Windows.Forms.CheckBox();
+            this.patchBrowseBtn = new System.Windows.Forms.Button();
+            this.patchExeBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // cbDiffMergeManual
+            // diffExeBox
             // 
-            this.cbDiffMergeManual.AutoSize = true;
-            this.cbDiffMergeManual.Location = new System.Drawing.Point(3, 86);
-            this.cbDiffMergeManual.Name = "cbDiffMergeManual";
-            this.cbDiffMergeManual.Size = new System.Drawing.Size(269, 17);
-            this.cbDiffMergeManual.TabIndex = 4;
-            this.cbDiffMergeManual.Text = "Manually choose between Internal and External Diff";
-            this.cbDiffMergeManual.UseVisualStyleBackColor = true;
-            // 
-            // txtDiffExePath
-            // 
-            this.txtDiffExePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this.diffExeBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtDiffExePath.Location = new System.Drawing.Point(0, 15);
-            this.txtDiffExePath.Name = "txtDiffExePath";
-            this.txtDiffExePath.Size = new System.Drawing.Size(355, 20);
-            this.txtDiffExePath.TabIndex = 0;
+            this.diffExeBox.Location = new System.Drawing.Point(0, 15);
+            this.diffExeBox.Name = "diffExeBox";
+            this.diffExeBox.Size = new System.Drawing.Size(355, 21);
+            this.diffExeBox.TabIndex = 1;
             // 
             // label1
             // 
@@ -63,19 +55,19 @@
             this.label1.Location = new System.Drawing.Point(-3, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 13);
-            this.label1.TabIndex = 3;
+            this.label1.TabIndex = 0;
             this.label1.Text = "External &Diff Path:";
             // 
-            // btnDiffExePath
+            // diffBrowseBtn
             // 
-            this.btnDiffExePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnDiffExePath.Location = new System.Drawing.Point(366, 13);
-            this.btnDiffExePath.Name = "btnDiffExePath";
-            this.btnDiffExePath.Size = new System.Drawing.Size(26, 23);
-            this.btnDiffExePath.TabIndex = 1;
-            this.btnDiffExePath.Text = "...";
-            this.btnDiffExePath.UseVisualStyleBackColor = true;
-            this.btnDiffExePath.Click += new System.EventHandler(this.btnDiffExePath_Click);
+            this.diffBrowseBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.diffBrowseBtn.Location = new System.Drawing.Point(366, 13);
+            this.diffBrowseBtn.Name = "diffBrowseBtn";
+            this.diffBrowseBtn.Size = new System.Drawing.Size(26, 23);
+            this.diffBrowseBtn.TabIndex = 2;
+            this.diffBrowseBtn.Text = "...";
+            this.diffBrowseBtn.UseVisualStyleBackColor = true;
+            this.diffBrowseBtn.Click += new System.EventHandler(this.btnDiffExePath_Click);
             // 
             // label2
             // 
@@ -83,51 +75,81 @@
             this.label2.Location = new System.Drawing.Point(-3, 43);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 13);
-            this.label2.TabIndex = 5;
+            this.label2.TabIndex = 3;
             this.label2.Text = "External &Merge Path:";
             // 
-            // txtMergeExePath
+            // mergeExeBox
             // 
-            this.txtMergeExePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this.mergeExeBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtMergeExePath.Location = new System.Drawing.Point(0, 58);
-            this.txtMergeExePath.Name = "txtMergeExePath";
-            this.txtMergeExePath.Size = new System.Drawing.Size(355, 20);
-            this.txtMergeExePath.TabIndex = 2;
+            this.mergeExeBox.Location = new System.Drawing.Point(0, 58);
+            this.mergeExeBox.Name = "mergeExeBox";
+            this.mergeExeBox.Size = new System.Drawing.Size(355, 21);
+            this.mergeExeBox.TabIndex = 4;
             // 
-            // btnMergeExePath
+            // mergeBrowseBtn
             // 
-            this.btnMergeExePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMergeExePath.Location = new System.Drawing.Point(366, 55);
-            this.btnMergeExePath.Name = "btnMergeExePath";
-            this.btnMergeExePath.Size = new System.Drawing.Size(26, 23);
-            this.btnMergeExePath.TabIndex = 3;
-            this.btnMergeExePath.Text = "...";
-            this.btnMergeExePath.UseVisualStyleBackColor = true;
-            this.btnMergeExePath.Click += new System.EventHandler(this.btnMergePath_Click);
+            this.mergeBrowseBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.mergeBrowseBtn.Location = new System.Drawing.Point(366, 55);
+            this.mergeBrowseBtn.Name = "mergeBrowseBtn";
+            this.mergeBrowseBtn.Size = new System.Drawing.Size(26, 23);
+            this.mergeBrowseBtn.TabIndex = 5;
+            this.mergeBrowseBtn.Text = "...";
+            this.mergeBrowseBtn.UseVisualStyleBackColor = true;
+            this.mergeBrowseBtn.Click += new System.EventHandler(this.btnMergePath_Click);
             // 
             // interactiveMergeOnConflict
             // 
             this.interactiveMergeOnConflict.AutoSize = true;
-            this.interactiveMergeOnConflict.Location = new System.Drawing.Point(3, 109);
+            this.interactiveMergeOnConflict.Location = new System.Drawing.Point(0, 124);
             this.interactiveMergeOnConflict.Name = "interactiveMergeOnConflict";
             this.interactiveMergeOnConflict.Size = new System.Drawing.Size(218, 17);
-            this.interactiveMergeOnConflict.TabIndex = 6;
+            this.interactiveMergeOnConflict.TabIndex = 10;
             this.interactiveMergeOnConflict.Text = "Start Interactive Merge on &Conflict (Beta)";
             this.interactiveMergeOnConflict.UseVisualStyleBackColor = true;
+            // 
+            // patchBrowseBtn
+            // 
+            this.patchBrowseBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.patchBrowseBtn.Location = new System.Drawing.Point(366, 94);
+            this.patchBrowseBtn.Name = "patchBrowseBtn";
+            this.patchBrowseBtn.Size = new System.Drawing.Size(26, 23);
+            this.patchBrowseBtn.TabIndex = 8;
+            this.patchBrowseBtn.Text = "...";
+            this.patchBrowseBtn.UseVisualStyleBackColor = true;
+            // 
+            // patchExeBox
+            // 
+            this.patchExeBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.patchExeBox.Location = new System.Drawing.Point(0, 97);
+            this.patchExeBox.Name = "patchExeBox";
+            this.patchExeBox.Size = new System.Drawing.Size(355, 21);
+            this.patchExeBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-3, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "External &Patch Path:";
             // 
             // AnkhSettingsControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.patchBrowseBtn);
+            this.Controls.Add(this.patchExeBox);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.interactiveMergeOnConflict);
-            this.Controls.Add(this.btnMergeExePath);
-            this.Controls.Add(this.txtMergeExePath);
+            this.Controls.Add(this.mergeBrowseBtn);
+            this.Controls.Add(this.mergeExeBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.btnDiffExePath);
+            this.Controls.Add(this.diffBrowseBtn);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtDiffExePath);
-            this.Controls.Add(this.cbDiffMergeManual);
+            this.Controls.Add(this.diffExeBox);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "AnkhSettingsControl";
             this.Size = new System.Drawing.Size(400, 271);
@@ -138,13 +160,15 @@
 
         #endregion
 
-		private System.Windows.Forms.CheckBox cbDiffMergeManual;
-		private System.Windows.Forms.TextBox txtDiffExePath;
+        private System.Windows.Forms.ComboBox diffExeBox;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Button btnDiffExePath;
+		private System.Windows.Forms.Button diffBrowseBtn;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.TextBox txtMergeExePath;
-		private System.Windows.Forms.Button btnMergeExePath;
+		private System.Windows.Forms.ComboBox mergeExeBox;
+		private System.Windows.Forms.Button mergeBrowseBtn;
         private System.Windows.Forms.CheckBox interactiveMergeOnConflict;
+        private System.Windows.Forms.Button patchBrowseBtn;
+        private System.Windows.Forms.ComboBox patchExeBox;
+        private System.Windows.Forms.Label label3;
     }
 }
