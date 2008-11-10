1aff67dbd00413880c40c5720eaac5a3adae3ad7
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-11-10 18:53:59 +0000

    svn:needs-lock property editor

---
+++ trunk/src/Ankh.UI/PropertyEditors/NeedsLockPropertyEditor.Designer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh.UI.PropertyEditors
+{
+    partial class NeedsLockPropertyEditor
+    {
+
+        #region Component Designer generated code
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.needsLockToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.needsLockTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // needsLockTextBox
+            // 
+            this.needsLockTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.needsLockTextBox.Location = new System.Drawing.Point(0, 0);
+            this.needsLockTextBox.Multiline = true;
+            this.needsLockTextBox.Name = "needsLockTextBox";
+            this.needsLockTextBox.ReadOnly = true;
+            this.needsLockTextBox.Size = new System.Drawing.Size(348, 196);
+            this.needsLockTextBox.TabIndex = 0;
+            // 
+            // NeedsLockPropertyEditor
+            // 
+            this.Controls.Add(this.needsLockTextBox);
+            this.Name = "NeedsLockPropertyEditor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.ToolTip needsLockToolTip;
+        private System.ComponentModel.IContainer components;
+        private System.Windows.Forms.TextBox needsLockTextBox;
+    }
+}
---
+++ trunk/src/Ankh.UI/PropertyEditors/NeedsLockPropertyEditor.cs
@@ -0,0 +1,97 @@
+// $Id: NeedsLockPropertyEditor.cs 5127 2008-09-08 15:06:35Z rhuijben $
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Windows.Forms;
+using SharpSvn;
+
+namespace Ankh.UI.PropertyEditors
+{
+    /// <summary>
+    /// Property editor for executable properties.
+    /// </summary>
+    internal partial class NeedsLockPropertyEditor : PropertyEditControl, IPropertyEditor
+    {
+
+        public event EventHandler Changed;
+	
+        public NeedsLockPropertyEditor()
+        {
+            // This call is required by the Windows.Forms Form Designer.
+            InitializeComponent();
+
+            this.components = new System.ComponentModel.Container();
+            CreateMyToolTip();
+            Reset();
+        }
+
+        public void Reset() 
+        {
+            if (this.needsLockTextBox != null && !this.needsLockTextBox.IsDisposed)
+            {
+                this.needsLockTextBox.Text = FEEDBACK_TEXT;
+            }
+        }
+        
+        public bool Valid
+        {
+			
+            get { return true; }
+        }
+
+        public PropertyItem PropertyItem
+        {
+            get
+            {
+                if ( !this.Valid )
+                {
+                    throw new InvalidOperationException(
+                        "Can not get a property item when valid is false");
+                }
+				
+                return new TextPropertyItem(FEEDBACK_TEXT);
+            }
+
+            set { }
+        }
+
+        /// <summary>
+        /// File property
+        /// </summary>
+        public SvnNodeKind GetAllowedNodeKind()
+        {
+            return SvnNodeKind.File;
+        }
+
+        public override string ToString()
+        {
+            return SvnPropertyNames.SvnNeedsLock;
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing )
+            {
+                if(components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose( disposing );
+        }
+
+        private void CreateMyToolTip()
+        {
+            // Set up the ToolTip text for the Button and Textbox.
+            needsLockToolTip.SetToolTip( this.needsLockTextBox, FEEDBACK_TEXT);
+        }
+
+        private static readonly string FEEDBACK_TEXT = "File needs lock.";
+    }
+}
+
