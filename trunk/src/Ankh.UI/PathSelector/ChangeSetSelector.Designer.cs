25575b4faef774ba302308be4240ca6ae082f729
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-11-11 13:43:00 +0000

    * src/Ankh.UI/PathSelector/PathSelector.Designer.cs   Give version selectors some exta cpase
    * src/Ankh.UI/PathSelector/VersionSelector.cs
    * src/Ankh.UI/PathSelector/VersionSelector.Designer.cs
      Switch subselectors correctly
    
    * src/Ankh.UI/Ankh.UI.csproj
    * src/Ankh.UI/PathSelector/ChangeSetSelector.Designer.cs
    * src/Ankh.UI/PathSelector/DateSelector.Designer.cs
      Added some subcontrols

--- trunk/src/Ankh.UI/PathSelector/ChangeSetSelector.Designer.cs
+++ trunk/src/Ankh.UI/PathSelector/ChangeSetSelector.Designer.cs
@@ -28,19 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.browseButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browseButton.Location = new System.Drawing.Point(224, 2);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(28, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // ChangeSetSelector
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.browseButton);
             this.Name = "ChangeSetSelector";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Size = new System.Drawing.Size(237, 25);
+            this.Size = new System.Drawing.Size(252, 25);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- trunk/src/Ankh.UI/PathSelector/DateSelector.Designer.cs
+++ trunk/src/Ankh.UI/PathSelector/DateSelector.Designer.cs
@@ -28,18 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
+            // 
+            // datePicker
+            // 
+            this.datePicker.Location = new System.Drawing.Point(0, 3);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(171, 20);
+            this.datePicker.TabIndex = 0;
+            // 
+            // timePicker
+            // 
+            this.timePicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timePicker.Location = new System.Drawing.Point(177, 2);
+            this.timePicker.Name = "timePicker";
+            this.timePicker.Size = new System.Drawing.Size(73, 20);
+            this.timePicker.TabIndex = 1;
             // 
             // DateSelector
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.timePicker);
+            this.Controls.Add(this.datePicker);
             this.Name = "DateSelector";
-            this.Size = new System.Drawing.Size(237, 25);
+            this.Size = new System.Drawing.Size(252, 25);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.DateTimePicker timePicker;
     }
 }
