e711a5f2dfc4f84ec0b8c72da44bb66090caf36e
Bert Huijben <rhuijben@users.noreply.github.com>
2008-11-11 13:14:21 +0000

    Added user controls for the version control choices Date and ChangeSet.
    * src/Ankh.UI/Ankh.UI.csproj
      Added files
    * src/Ankh.UI/PathSelector/ChangeSetSelector.cs
    * src/Ankh.UI/PathSelector/ChangeSetSelector.Designer.cs
    * src/Ankh.UI/PathSelector/ChangeSetSelector.resx
      Added user control for explicit version selection
    * src/Ankh.UI/PathSelector/DateSelector.cs
    * src/Ankh.UI/PathSelector/DateSelector.Designer.cs
    * src/Ankh.UI/PathSelector/DateSelector.resx
      Added user control for date/time selection
    * src/Ankh.UI/PathSelector/VersionResolverService.cs
      Hook creating the user controls

---
+++ src/Ankh.UI/PathSelector/DateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ankh.UI.PathSelector
+{
+    public partial class DateSelector : UserControl
+    {
+        public DateSelector()
+        {
+            InitializeComponent();
+        }
+    }
+}
