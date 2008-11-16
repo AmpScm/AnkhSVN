f947dcbb91537e439fa5ea6945694daa9f4629ef
Bert Huijben <rhuijben@users.noreply.github.com>
2008-11-16 15:08:41 +0000

    Added Solution Update dialog and moved several command specific dialogs to the new Commands folder in the Ankh.UI project.
    * src/Ankh.UI/Ankh.UI.csproj
    * src/Ankh.UI/CheckoutDialog.cs
    * src/Ankh.UI/CheckoutDialog.Designer.cs
    * src/Ankh.UI/CheckoutDialog.resx
      Moved to Commands
    * src/Ankh.UI/Commands
    * src/Ankh.UI/Commands/CheckoutDialog.cs
    * src/Ankh.UI/Commands/CheckoutDialog.Designer.cs
    * src/Ankh.UI/Commands/CheckoutDialog.resx
    * src/Ankh.UI/Commands/ExportDialog.cs
    * src/Ankh.UI/Commands/ExportDialog.Designer.cs
    * src/Ankh.UI/Commands/ExportDialog.resx
    * src/Ankh.UI/Commands/SwitchDialog.cs
    * src/Ankh.UI/Commands/SwitchDialog.Designer.cs
    * src/Ankh.UI/Commands/SwitchDialog.resx
      Moved here
    * src/Ankh.UI/Commands/UpdateDialog.cs
    * src/Ankh.UI/Commands/UpdateDialog.Designer.cs
    * src/Ankh.UI/Commands/UpdateDialog.resx
      New dialog to allow updating to a specific revision and/or accepting
      unversioned obstructions.
      
    * src/Ankh.UI/ExportDialog.cs
    * src/Ankh.UI/ExportDialog.Designer.cs
    * src/Ankh.UI/ExportDialog.resx
    * src/Ankh.UI/SwitchDialog.cs
    * src/Ankh.UI/SwitchDialog.Designer.cs
    * src/Ankh.UI/SwitchDialog.resx
      Moved to Commands folder
    * src/Ankh.UI/PathSelector/VersionSelector.Designer.cs
      Moved label a few pixels to the left

---
+++ src/Ankh.UI/Commands/UpdateDialog.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Ankh.UI.Commands
+{
+    partial class UpdateDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.projectRootBox = new System.Windows.Forms.TextBox();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.versionBox = new Ankh.UI.PathSelector.VersionSelector();
+            this.updateExternals = new System.Windows.Forms.CheckBox();
+            this.allowObstructions = new System.Windows.Forms.CheckBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "&Project Root:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "&Url:";
+            // 
+            // projectRootBox
+            // 
+            this.projectRootBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.projectRootBox.Location = new System.Drawing.Point(87, 6);
+            this.projectRootBox.Name = "projectRootBox";
+            this.projectRootBox.ReadOnly = true;
+            this.projectRootBox.Size = new System.Drawing.Size(379, 20);
+            this.projectRootBox.TabIndex = 6;
+            // 
+            // urlBox
+            // 
+            this.urlBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlBox.Location = new System.Drawing.Point(87, 32);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.ReadOnly = true;
+            this.urlBox.Size = new System.Drawing.Size(379, 20);
+            this.urlBox.TabIndex = 8;
+            // 
+            // versionBox
+            // 
+            this.versionBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.versionBox.Location = new System.Drawing.Point(87, 58);
+            this.versionBox.Name = "versionBox";
+            this.versionBox.Size = new System.Drawing.Size(379, 25);
+            this.versionBox.SvnOrigin = null;
+            this.versionBox.TabIndex = 0;
+            // 
+            // updateExternals
+            // 
+            this.updateExternals.AutoSize = true;
+            this.updateExternals.Checked = true;
+            this.updateExternals.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.updateExternals.Location = new System.Drawing.Point(87, 93);
+            this.updateExternals.Name = "updateExternals";
+            this.updateExternals.Size = new System.Drawing.Size(107, 17);
+            this.updateExternals.TabIndex = 1;
+            this.updateExternals.Text = "Update E&xternals";
+            this.updateExternals.UseVisualStyleBackColor = true;
+            // 
+            // allowObstructions
+            // 
+            this.allowObstructions.AutoSize = true;
+            this.allowObstructions.Location = new System.Drawing.Point(87, 116);
+            this.allowObstructions.Name = "allowObstructions";
+            this.allowObstructions.Size = new System.Drawing.Size(176, 17);
+            this.allowObstructions.TabIndex = 2;
+            this.allowObstructions.Text = "Allow Unversioned O&bstructions";
+            this.allowObstructions.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(310, 146);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(391, 146);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // UpdateDialog
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(478, 181);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.allowObstructions);
+            this.Controls.Add(this.updateExternals);
+            this.Controls.Add(this.versionBox);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.projectRootBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "UpdateDialog";
+            this.Text = "Update Solution";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox projectRootBox;
+        private System.Windows.Forms.TextBox urlBox;
+        private Ankh.UI.PathSelector.VersionSelector versionBox;
+        private System.Windows.Forms.CheckBox updateExternals;
+        private System.Windows.Forms.CheckBox allowObstructions;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
