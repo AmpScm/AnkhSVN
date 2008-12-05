d957247b6a4476f8fe50c6452f262c23d52b937e
Bert Huijben <rhuijben@users.noreply.github.com>
2008-12-05 11:47:56 +0000

    When viewing repository files use their actual name as filename for the tempfile instead of just a guid with its extension
    * src/Ankh.Services/VS/IAnkhTempFileManager.cs
      Added GetTempFileNamed() method to get a temporary file with the specified name
      
    * src/Ankh.VS/TempFileManager.cs
      Implement GetTempFileNamed() and some refactoring
    * src/Ankh/Commands/ItemAnnotateCommand.cs
      Use GetTempFileNamed() for annotating repository files
    * src/Ankh/Commands/RepositoryExplorer/ViewInVSNetCommand.cs
      Use GetTempFileNamed() for saving repository files
    * src/Ankh/Commands/RepositoryExplorer/ViewRepositoryFileCommand.cs
      Save the actual file version selected (if available)

--- src/Ankh.VS/TempFileManager.cs
+++ src/Ankh.VS/TempFileManager.cs
@@ -27,23 +27,45 @@ namespace Ankh.VS
         {
             string name = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
 
-            using (FileStream fs = File.Create(name))
-            {
-            }
+            File.WriteAllBytes(name, new byte[0]);
             TempFileCollection.AddFile(name, false);
             return name;            
         }
 
         public string GetTempFile(string extension)
         {
+            if (string.IsNullOrEmpty(extension))
+                throw new ArgumentNullException("extension");
             string name = Path.ChangeExtension(
                 Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N")),
                 extension);
             using (FileStream fs = File.Create(name))
             {
             }
-            TempFileCollection.AddFile(name, false);
+
+            File.WriteAllBytes(name, new byte[0]);
             return name;            
+        }
+
+        string _lastDir;
+
+        public string GetTempFileNamed(string filename)
+        {
+            if (string.IsNullOrEmpty(filename))
+                throw new ArgumentNullException("filename");
+
+            filename = Path.GetFileName(filename); // Remove any paths
+
+            string name;
+            if(_lastDir == null || File.Exists(name = Path.Combine(_lastDir, filename)))
+            {
+                _lastDir = GetService<IAnkhTempDirManager>().GetTempDir();
+                name = Path.Combine(_lastDir, filename);
+            }
+
+            File.WriteAllBytes(name, new byte[0]);
+
+            return name;
         }
         #endregion
     }
