7d6a0002117f09b28fb4ea7117e1a226218779b6
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-12-12 13:27:01 +0000

    * src/Ankh.UI/PendingChanges/LogMessageEditor.cs   Remove MS copyright that doesn't apply to this file and several unused   usings.

--- trunk/src/Ankh.UI/PendingChanges/LogMessageEditor.cs
+++ trunk/src/Ankh.UI/PendingChanges/LogMessageEditor.cs
@@ -14,32 +14,11 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-//***************************************************************************
-//
-//    Copyright (c) Microsoft Corporation. All rights reserved.
-//    This code is licensed under the Visual Studio SDK license terms.
-//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-//***************************************************************************
-
 using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.OLE.Interop;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.TextManager.Interop;
 
-using IOleServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
-using OLEConstants = Microsoft.VisualStudio.OLE.Interop.Constants;
-using System.Security.Permissions;
-using Ankh.Scc.UI;
 using Ankh.Ids;
 using Ankh.UI.PendingChanges.Commits;
 
