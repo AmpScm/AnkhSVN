c6bbe6c6f25145e9676a15109db91f32eaa9e473
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2008-12-12 13:03:53 +0000

    Applied svn:eol-style and apache license header to most .cs files.
    */*
      Apply header scription (Follow up will fix the rest)

--- trunk/src/Ankh.Diff/DiffUtils/Controls/DiffView.cs
+++ trunk/src/Ankh.Diff/DiffUtils/Controls/DiffView.cs
@@ -1,3 +1,19 @@
+// $Id$
+//
+// Copyright 2008 The AnkhSVN Project
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 #region Copyright And Revision History
 
 /*---------------------------------------------------------------------------
@@ -1313,7 +1329,7 @@ namespace Ankh.Diff.DiffUtils.Controls
         {
             if (m_bShowWhitespace)
             {
-                strText = String.Format("{0}¶", strText.Replace(' ', '·'));
+                strText = String.Format("{0}", strText.Replace(' ', ''));
             }
 
             G.DrawString(strText, Font, B, x, y, m_StringFormat);
