AmpScm/AnkhSVN

commit 0b9bfb2b7066f0cd64ca4dd26e77bdec66bf8dae

Use SvnOrigin for blame command handling.
* src/Ankh.Services/Ankh.Services.csproj
* src/Ankh.Services/Scc/ISvnBlameSection.cs
* src/Ankh.Services/Scc/UI/IAnnotateSection.cs
* src/Ankh.Services/Scc/UI/IBlameControl.cs
  Removed IBlameControl and renamed ISvnBlameSection to IAnnotateSection.
  
* src/Ankh.UI/Annotate/AnnotateMarginControl.cs
* src/Ankh.UI/Annotate/AnnotateSection.cs
* src/Ankh.UI/Annotate/AnnotateToolWindowControl.cs
  Pass SvnOrigins instead of SvnTarget/SvnItem.
  
* src/Ankh/Commands/ItemAnnotateCommand.cs
* src/Ankh/Commands/LogCommand.cs
  Calculate SvnOrigins.


git-svn-id: https://ctf.open.collab.net/svn/repos/ankhsvn/trunk@5437 ecf7f2a6-7b29-9143-b1c6-df2d17ec8657

src/Ankh.Services/Scc/UI/IAnnotateSection.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Ankh.Scc
+namespace Ankh.Scc.UI
 {
-    public interface IBlameSection
+    public interface IAnnotateSection
     {
         string Author { get; }
         long Revision { get; }

src/Ankh/Commands/ItemAnnotateCommand.cs
@@ -34,20 +34,8 @@ namespace Ankh.Commands
             switch (e.Command)
             {
                 case AnkhCommand.BlameShowBlame:
-                    IBlameControl blameControl = e.Selection.ActiveDialogOrFrameControl as IBlameControl;
-                    if (blameControl == null || !blameControl.HasWorkingCopyItems)
-                    {
-                        e.Enabled = e.Visible = false;
-                        return;
-                    }
-
-                    int blameCount = 0;
-                    foreach (IBlameSection blameItem in e.Selection.GetSelection<IBlameSection>())
-                    {
-                        blameCount++;
-                    }
-                    if (blameCount == 1)
-                        return;
+                    if (null == EnumTools.GetSingle(e.Selection.GetSelection<IAnnotateSection>()))
+                        e.Enabled = false;
                     break;
                 case AnkhCommand.ItemAnnotate:
                     foreach (SvnItem item in e.Selection.GetSelectedSvnItems(false))
@@ -96,26 +84,12 @@ namespace Ankh.Commands
 
         void BlameBlame(CommandEventArgs e)
         {
-            IBlameControl blameControl = e.Selection.ActiveDialogOrFrameControl as IBlameControl;
+            IAnnotateSection blameSection = EnumTools.GetFirst(e.Selection.GetSelection<IAnnotateSection>());
 
-            IBlameSection blameSection = null;
-            foreach (IBlameSection blame in e.Selection.GetSelection<IBlameSection>())
-            {
-                blameSection = blame;
-                break;
-            }
-
-            SvnItem item = null;
-            foreach(SvnItem i in blameControl.WorkingCopyItems)
-            {
-                item = i;
-            }
-
-
-            SvnRevision revisionStart = SvnRevision.Zero;
+            SvnRevision revisionStart = SvnRevision.Zero; // Copy from original blame?
             SvnRevision revisionEnd = blameSection.Revision;
 
-            DoBlame(e, item, revisionStart, revisionEnd); 
+            DoBlame(e, blameSection.Origin, revisionStart, revisionEnd); 
         }
 
         void BlameRevision(CommandEventArgs e)
@@ -134,22 +108,10 @@ namespace Ankh.Commands
                 break;
             }
 
-            SvnItem svnItem = null;
-            throw new InvalidOperationException();
-            // BH: Intersecting to find a single item in a command that doesn't depend on a workingcopy?
-            /*IEnumerable<SvnItem> intersectedItems = LogHelper.IntersectWorkingCopyItemsWithChangedPaths(logControl.WorkingCopyItems, changedPaths);
-            foreach (SvnItem i in intersectedItems)
-            {
-                svnItem = i;
-                break;
-            }*/
-            if (svnItem == null)
-                return;
-
             SvnRevision revisionStart = SvnRevision.Zero;
             SvnRevision revisionEnd = item.Revision;
 
-            DoBlame(e, svnItem, revisionStart, revisionEnd);
+            DoBlame(e, item.Origin, revisionStart, revisionEnd);
         }
 
         void BlameItem(CommandEventArgs e)
@@ -203,10 +165,10 @@ namespace Ankh.Commands
             foreach (SvnItem i in result.Selection)
                 blameItem = i;
 
-            DoBlame(e, blameItem, result.RevisionStart, result.RevisionEnd);
+            DoBlame(e, new SvnOrigin(blameItem), result.RevisionStart, result.RevisionEnd);
         }
 
-        void DoBlame(CommandEventArgs e, SvnItem item, SvnRevision revisionStart, SvnRevision revisionEnd)
+        void DoBlame(CommandEventArgs e, SvnOrigin item, SvnRevision revisionStart, SvnRevision revisionEnd)
         {
             IAnkhPackage p = e.GetService<IAnkhPackage>();
             SvnExportArgs ea = new SvnExportArgs();
@@ -216,7 +178,7 @@ namespace Ankh.Commands
             ba.Start = revisionStart;
             ba.End = revisionEnd;
 
-            SvnTarget target = new SvnPathTarget(item.FullPath);
+            SvnTarget target = item.Target;
 
             IAnkhTempFileManager tempMgr = e.GetService<IAnkhTempFileManager>();
             string tempFile = tempMgr.GetTempFile();
@@ -224,7 +186,6 @@ namespace Ankh.Commands
             Collection<SvnBlameEventArgs> blameResult = null;
             e.GetService<IProgressRunner>().Run("Annotating", delegate(object sender, ProgressWorkerArgs ee)
             {
-
                 ee.Client.Export(target, tempFile, ea);
 
                 ee.Client.GetBlame(target, ba, out blameResult);
@@ -234,7 +195,17 @@ namespace Ankh.Commands
             BlameToolWindowControl blameToolControl = e.GetService<ISelectionContext>().ActiveFrameControl as BlameToolWindowControl;
             blameToolControl.Init();
 
-            blameToolControl.LoadFile(item.FullPath, tempFile);
+            SvnPathTarget pt = target as SvnPathTarget;
+
+            if (pt != null)
+            {
+                blameToolControl.LoadFile(pt.FullPath, tempFile);
+            }
+            else
+            {
+                blameToolControl.LoadFile(tempMgr.GetTempFile(Path.GetExtension(target.FileName)), tempFile);
+            }
+
             blameToolControl.AddLines(item, blameResult);
         }
     }

src/Ankh.Services/Ankh.Services.csproj
@@ -75,9 +75,8 @@
     <Compile Include="IAnkhServiceImplementation.cs" />
     <Compile Include="Scc\IAnkhProjectLayoutService.cs" />
     <Compile Include="Scc\IAnkhTextEditingTracker.cs" />
-    <Compile Include="Scc\ISvnBlameSection.cs" />
+    <Compile Include="Scc\UI\IAnnotateSection.cs" />
     <Compile Include="Scc\SvnOrigin.cs" />
-    <Compile Include="Scc\UI\IBlameControl.cs" />
     <Compile Include="UI\RegistryLifoList.cs" />
     <Compile Include="Scc\IPendingChangeHandler.cs" />
     <Compile Include="UI\IAnkhToolWindowHost.cs" />

src/Ankh.Services/Scc/UI/IBlameControl.cs
@@ -1,12 +1,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Ankh.Scc.UI
-{
-    public interface IBlameControl
-    {
-        bool HasWorkingCopyItems { get; }
-        SvnItem[] WorkingCopyItems { get; }
-    }
-}

src/Ankh.UI/Annotate/AnnotateMarginControl.cs
@@ -14,14 +14,14 @@ namespace Ankh.UI.Blame
 {
     class BlameMarginControl :Control
     {
-        List<BlameSection> _sections;
+        List<AnnotateSection> _sections;
         BlameToolWindowControl _control;
         int _firstLine;
         int _lastLine;
         IAnkhServiceProvider _context;
         
 
-        internal void Init(IAnkhServiceProvider context, BlameToolWindowControl control, List<BlameSection> sections)
+        internal void Init(IAnkhServiceProvider context, BlameToolWindowControl control, List<AnnotateSection> sections)
         {
             _context = context;
             _control = control;
@@ -43,7 +43,7 @@ namespace Ankh.UI.Blame
             }
         }
 
-        Rectangle GetRectangle(BlameSection section)
+        Rectangle GetRectangle(AnnotateSection section)
         {
             int top = (section.StartLine - _firstLine) * LineHeight;
             int height = (section.EndLine - section.StartLine + 1) * LineHeight;
@@ -51,9 +51,9 @@ namespace Ankh.UI.Blame
             return new Rectangle(0, top, Width, height);
         }
 
-        BlameSection GetSection(Point location)
+        AnnotateSection GetSection(Point location)
         {
-            foreach (BlameSection section in _sections)
+            foreach (AnnotateSection section in _sections)
             {
                 Rectangle rect = GetRectangle(section);
                 if (rect.Contains(location))
@@ -67,7 +67,7 @@ namespace Ankh.UI.Blame
             base.OnMouseMove(e);
 
             bool changed = false;
-            foreach (BlameSection section in _sections)
+            foreach (AnnotateSection section in _sections)
             {
                 Rectangle rect = GetRectangle(section);
                 bool hovered = rect.Contains(e.Location);
@@ -88,7 +88,7 @@ namespace Ankh.UI.Blame
             base.OnMouseLeave(e);
             bool changed = false;
 
-            foreach (BlameSection section in _sections)
+            foreach (AnnotateSection section in _sections)
             {
                 if (section.Hovered == true)
                     changed = true;
@@ -101,7 +101,7 @@ namespace Ankh.UI.Blame
 
         protected override void OnMouseClick(MouseEventArgs e)
         {
-            BlameSection section = GetSection(e.Location);
+            AnnotateSection section = GetSection(e.Location);
             _control.SetSelection(section);
             Invalidate();
 
@@ -143,7 +143,7 @@ namespace Ankh.UI.Blame
             using (Brush blueBg = new LinearGradientBrush(new Point(0, 0), new Point(Width, 0), BackColor, Color.LightBlue))
             using (Brush selectedBg = new SolidBrush(SystemColors.Highlight))
             {
-                foreach (BlameSection section in _sections.ToArray())
+                foreach (AnnotateSection section in _sections.ToArray())
                 {
                     if (section.EndLine < _firstLine)
                         continue;

src/Ankh.UI/Annotate/AnnotateSection.cs
@@ -4,20 +4,23 @@ using System.Text;
 using SharpSvn;
 using Ankh.Scc;
 using System.ComponentModel;
+using Ankh.Scc.UI;
 
 namespace Ankh.UI.Blame
 {
-    public class BlameSection : IBlameSection
+    class AnnotateSection : IAnnotateSection
     {
         readonly SvnBlameEventArgs _args;
         readonly SvnOrigin _origin;
         long _endLine;
 
+        // BH: TODO: We should copy the values to release the cached lines in the SvnBlameEventArgs
+
         internal bool Hovered;
 
-        public BlameSection(SvnBlameEventArgs e, SvnOrigin origin)
+        public AnnotateSection(SvnBlameEventArgs blameArgs, SvnOrigin origin)
         {
-            _args = e;
+            _args = blameArgs;
             _endLine = _args.LineNumber;
             _origin = origin;
         }

src/Ankh.UI/Annotate/AnnotateToolWindowControl.cs
@@ -11,14 +11,15 @@ using Ankh.UI.PendingChanges;
 using Ankh.UI.VSSelectionControls;
 using Ankh.Scc;
 using Ankh.Scc.UI;
+using System.Collections.ObjectModel;
 
 namespace Ankh.UI.Blame
 {
-    public partial class BlameToolWindowControl : AnkhToolWindowControl, ISelectionMapOwner<IBlameSection>, IBlameControl
+    public partial class BlameToolWindowControl : AnkhToolWindowControl, ISelectionMapOwner<IAnnotateSection>
     {
-        List<BlameSection> blameSections = new List<BlameSection>();
+        List<AnnotateSection> blameSections = new List<AnnotateSection>();
         SelectionItemMap _map;
-        SvnItem _wcItem;
+        SvnOrigin _origin;
 
         public BlameToolWindowControl()
         {
@@ -35,7 +36,7 @@ namespace Ankh.UI.Blame
         {
             base.OnFrameCreated(e);
 
-            _map = SelectionItemMap.Create<IBlameSection>(this);
+            _map = SelectionItemMap.Create<IAnnotateSection>(this);
             _map.Context = ToolWindowHost;
 
             if (SelectionChanged != null)
@@ -58,19 +59,18 @@ namespace Ankh.UI.Blame
         }
 
 
-        public void AddLines(SvnItem workingCopyItem, System.Collections.ObjectModel.Collection<SharpSvn.SvnBlameEventArgs> blameResult)
+        public void AddLines(SvnOrigin origin, Collection<SharpSvn.SvnBlameEventArgs> blameResult)
         {
-            _wcItem = workingCopyItem;
-            SvnOrigin origin = new SvnOrigin(workingCopyItem);
+            _origin = origin;
 
-            BlameSection section = null;
+            AnnotateSection section = null;
             blameSections.Clear();
 
             foreach (SvnBlameEventArgs e in blameResult)
             {
                 if (blameSections.Count == 0)
                 {
-                    section = new BlameSection(e, origin);
+                    section = new AnnotateSection(e, origin);
                     blameSections.Add(section);
                 }
                 else
@@ -79,12 +79,10 @@ namespace Ankh.UI.Blame
                         section.EndLine = (int)e.LineNumber;
                     else
                     {
-                        section = new BlameSection(e, origin);
+                        section = new AnnotateSection(e, origin);
                         blameSections.Add(section);
                     }
                 }
-
-                
             }
             blameMarginControl1.Invalidate();
         }
@@ -94,19 +92,19 @@ namespace Ankh.UI.Blame
             blameMarginControl1.NotifyScroll(e.MinUnit, e.MaxUnit, e.VisibleUnits, e.FirstVisibleUnit);
         }
 
-        BlameSection _selected;
-        internal BlameSection Selected
+        AnnotateSection _selected;
+        internal AnnotateSection Selected
         {
             get { return _selected; }
         }
 
-        internal void SetSelection(IBlameSection section)
+        internal void SetSelection(IAnnotateSection section)
         {
             // Check if necessary
             //Focus();
             //Select();
 
-            _selected = (BlameSection)section;
+            _selected = (AnnotateSection)section;
 
             if (SelectionChanged != null)
                 SelectionChanged(this, EventArgs.Empty);
@@ -123,9 +121,9 @@ namespace Ankh.UI.Blame
             get
             {
                 if (_selected == null)
-                    return new IBlameSection[] { };
+                    return new IAnnotateSection[] { };
 
-                return new IBlameSection[] { _selected };
+                return new IAnnotateSection[] { _selected };
             }
         }
 
@@ -139,54 +137,40 @@ namespace Ankh.UI.Blame
             return IntPtr.Zero;
         }
 
-        public int GetImageListIndex(IBlameSection item)
+        public int GetImageListIndex(IAnnotateSection item)
         {
             return 0;
         }
 
-        public string GetText(IBlameSection item)
+        public string GetText(IAnnotateSection item)
         {
             return item.Revision.ToString();
         }
 
-        public object GetSelectionObject(IBlameSection item)
+        public object GetSelectionObject(IAnnotateSection item)
         {
             return item;
         }
 
-        public IBlameSection GetItemFromSelectionObject(object item)
+        public IAnnotateSection GetItemFromSelectionObject(object item)
         {
-            return (IBlameSection)item;
+            return (IAnnotateSection)item;
         }
 
-        public void SetSelection(IBlameSection[] items)
+        public void SetSelection(IAnnotateSection[] items)
         {
             if (items.Length > 0)
                 SetSelection(items[0]);
             else
-                SetSelection((IBlameSection)null);
+                SetSelection((IAnnotateSection)null);
         }
 
-        public string GetCanonicalName(IBlameSection item)
+        public string GetCanonicalName(IAnnotateSection item)
         {
-            BlameSection section = (BlameSection)item;
+            AnnotateSection section = (AnnotateSection)item;
             return section.Author + section.StartLine + section.Revision;
         }
 
-        #endregion
-
-        #region IBlameControl Members
-
-        public bool HasWorkingCopyItems
-        {
-            get { return true; }
-        }
-
-        public SvnItem[] WorkingCopyItems
-        {
-            get { return new SvnItem[] { _wcItem }; }
-        }
-
-        #endregion
+        #endregion    
     }
 }

src/Ankh/Commands/LogCommand.cs
@@ -116,7 +116,7 @@ namespace Ankh.Commands
                     break;
                 case AnkhCommand.BlameShowLog:
                     i = 0;
-                    foreach (IBlameSection section in e.Selection.GetSelection<IBlameSection>())
+                    foreach (IAnnotateSection section in e.Selection.GetSelection<IAnnotateSection>())
                     {
                         if (section == null)
                             continue;
@@ -187,8 +187,8 @@ namespace Ankh.Commands
                         PerformLog(e.Context, new SvnOrigin[] { item.Origin }, null, null);
                     break;
                 case AnkhCommand.BlameShowLog:
-                    IBlameSection section = null;
-                    foreach (IBlameSection s in e.Selection.GetSelection<IBlameSection>())
+                    IAnnotateSection section = null;
+                    foreach (IAnnotateSection s in e.Selection.GetSelection<IAnnotateSection>())
                     {
                         section = s;
                         break;

commit 0f183e438f5ad3830763ee459a01750c83e95fcd

Enable the annotate command from the annotate viewer (broken since r5437?) and the repository explorer (new functionality).
* src/Ankh.Ids/AnkhCommand.cs
  Renamed BlameShowBlame to SvnNodeAnnotate
* src/Ankh.Package/Gui/AnkhSvn.xml
  Add Annotate to repository explorer context menu
* src/Ankh.Services/Scc/UI/IAnnotateSection.cs
  Always implement ISvnRepositoryItem
* src/Ankh/Commands/ItemAnnotateCommand.cs
  Use ISvnRepositoryItem instead of IAnnotateSection to make the command
  more generic

src/Ankh.Ids/AnkhCommand.cs
@@ -74,7 +74,7 @@ namespace Ankh.Ids
         /// <summary>
         /// Execute blame command from blame window
         /// </summary>
-        BlameShowBlame,
+        SvnNodeAnnotate,
         Checkout,
         CheckoutFolder,
         CheckoutSolution,

src/Ankh.Package/Gui/AnkhSvn.xml
@@ -384,6 +384,7 @@
                   toolTip="View History." iconFile ="PendingChanges/ViewHistory.png" dynamicVisibility="true"/>
           <Button id="Cmd_RenameRepositoryItem" name="Rename" priority="0x400"
                   text="&amp;Rename" dynamicVisibility="true" allowParams="true" />
+          <ButtonRef id="Cmd_SvnNodeAnnotate" priority="0x500" />
         </Group>
         <GroupRef id="guidSHLMainMenu:IDG_VS_CTXT_ITEM_PROPERTIES" priority="0xF000" />
       </Menu>
@@ -466,7 +467,7 @@
         <Group id="Grp_BlameContextMenu" priority="0x100">
           <Button id="Cmd_BlameShowLog" priority="0x100" name="BlameShowLog" text="View history"
                   iconFile="PendingChanges/ViewHistory.png" />
-          <Button id="Cmd_BlameShowBlame" priority="0x200" name="AnnotateShowAnnotate" text="&amp;Annotate this version"
+          <Button id="Cmd_SvnNodeAnnotate" priority="0x200" name="SvnNodeAnnotate" text="&amp;Annotate"
                   iconFile="PendingChanges/Annotate.png"/>
         </Group>
       </Menu>

src/Ankh.Services/Scc/UI/IAnnotateSection.cs
@@ -20,7 +20,7 @@ using System.Text;
 
 namespace Ankh.Scc.UI
 {
-    public interface IAnnotateSection
+    public interface IAnnotateSection : ISvnRepositoryItem
     {
         string Author { get; }
         long Revision { get; }

src/Ankh/Commands/ItemAnnotateCommand.cs
@@ -32,7 +32,7 @@ namespace Ankh.Commands
     /// </summary>
     [Command(AnkhCommand.ItemAnnotate)]
     [Command(AnkhCommand.LogAnnotateRevision)]
-    [Command(AnkhCommand.BlameShowBlame)]
+    [Command(AnkhCommand.SvnNodeAnnotate)]
     [Command(AnkhCommand.DocumentAnnotate)]
     class ItemAnnotateCommand : CommandBase
     {
@@ -40,9 +40,10 @@ namespace Ankh.Commands
         {
             switch (e.Command)
             {
-                case AnkhCommand.BlameShowBlame:
-                    if (null == EnumTools.GetSingle(e.Selection.GetSelection<IAnnotateSection>()))
-                        e.Enabled = false;
+                case AnkhCommand.SvnNodeAnnotate:
+                    ISvnRepositoryItem ri = EnumTools.GetSingle(e.Selection.GetSelection<ISvnRepositoryItem>());
+                    if (ri != null && ri.Origin != null)
+                        return;
                     break;
                 case AnkhCommand.ItemAnnotate:
                     foreach (SvnItem item in e.Selection.GetSelectedSvnItems(false))
@@ -87,8 +88,8 @@ namespace Ankh.Commands
                 case AnkhCommand.LogAnnotateRevision:
                     BlameRevision(e);
                     break;
-                case AnkhCommand.BlameShowBlame:
-                    BlameBlame(e);
+                case AnkhCommand.SvnNodeAnnotate:
+                    SvnNodeBlame(e);
                     break;
                 case AnkhCommand.DocumentAnnotate:
                     BlameDocument(e);
@@ -96,12 +97,14 @@ namespace Ankh.Commands
             }
         }
 
-        void BlameBlame(CommandEventArgs e)
+        void SvnNodeBlame(CommandEventArgs e)
         {
-            IAnnotateSection blameSection = EnumTools.GetFirst(e.Selection.GetSelection<IAnnotateSection>());
+            ISvnRepositoryItem blameSection = EnumTools.GetFirst(e.Selection.GetSelection<ISvnRepositoryItem>());
 
-            SvnRevision revisionStart = SvnRevision.Zero; // Copy from original blame?
+            SvnRevision revisionStart = SvnRevision.Zero;
             SvnRevision revisionEnd = blameSection.Revision;
+
+            // TODO: Confirm revisions?
 
             DoBlame(e, blameSection.Origin, revisionStart, revisionEnd);
         }