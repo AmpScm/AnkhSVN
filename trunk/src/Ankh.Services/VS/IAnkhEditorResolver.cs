8924bbf84745d2df23cce246cefb6833764e07e7
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-01-20 15:12:08 +0000

    Add AnkhEditor resolver service that allows us to translate a file extension in a language service guid.
    * src/Ankh.Services/Ankh.Services.csproj
    * src/Ankh.Services/VS/IAnkhEditorResolver.cs
      Add service definition
    * src/Ankh.VS/Ankh.VS.csproj
    * src/Ankh.VS/LanguageServices/AnkhEditorResolver.cs
      Implement AnkhEditorResolver via the LanguageService MPF code.

---
+++ trunk/src/Ankh.Services/VS/IAnkhEditorResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh.VS
+{
+    public interface IAnkhEditorResolver
+    {
+        /// <summary>
+        /// Tries to get the language service for the specified extension
+        /// </summary>
+        /// <param name="extension">The extension.</param>
+        /// <param name="languageService">The language service.</param>
+        /// <returns></returns>
+        bool TryGetLanguageService(string extension, out Guid languageService);
+    }
+}
---
+++ trunk/src/Ankh.VS/LanguageServices/AnkhEditorResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Package;
+
+namespace Ankh.VS.LanguageServices
+{
+    [GlobalService(typeof(IAnkhEditorResolver))]
+    class AnkhEditorResolver : AnkhService, IAnkhEditorResolver
+    {
+        readonly EditorFactory _factory;
+        public AnkhEditorResolver(IAnkhServiceProvider context)
+            : base(context)
+        {
+            _factory = new EditorFactory();
+        }
+        #region IAnkhEditorResolver Members
+
+        public bool TryGetLanguageService(string extension, out Guid languageService)
+        {
+            if (!string.IsNullOrEmpty(extension))
+            {
+                string value = _factory.GetLanguageService(extension);
+
+                if (!string.IsNullOrEmpty(value))
+                {
+                    languageService = new Guid(value);
+                    return true;
+                }
+            }
+            languageService = Guid.Empty;
+            return false;
+        }
+
+        #endregion
+    }
+}
