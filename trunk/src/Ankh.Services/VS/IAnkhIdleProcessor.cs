639a5c99c72b4bae079a6c16c8beb728b38653b6
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-01-22 15:27:53 +0000

    Enable the OnIdle processing of ourour language services on idle processing, this is required to enable background processing.
    * src/Ankh.Package/AnkhSvnPackage.OleComponent.cs
      Enable OnIdle events to our package
    * src/Ankh.Services/Ankh.Services.csproj
    * src/Ankh.Services/IAnkhPackage.cs
    * src/Ankh.Services/VS/IAnkhIdleProcessor.cs
      Provide OnIdle registration api.

---
+++ trunk/src/Ankh.Services/VS/IAnkhIdleProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh.VS
+{
+    public interface IAnkhIdleProcessor
+    {
+        bool OnIdle(bool periodic);
+    }
+}
