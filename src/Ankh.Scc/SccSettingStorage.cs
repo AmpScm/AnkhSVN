3b9b5facb86b764959776b1658c34bb6c43e3dbb
Bert Huijben <rhuijben@users.noreply.github.com>
2009-01-31 13:03:59 +0000

    * src/Ankh.Scc/Ankh.Scc.csproj * src/Ankh.Scc/Native * src/Ankh.Scc/Native/ICOMPropertyBag.cs * src/Ankh.Scc/SccSettingStorage.cs   More refactoring preparations

---
+++ src/Ankh.Scc/SccSettingStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ankh.Scc.ProjectMap;
+
+namespace Ankh.Scc
+{
+    /// <summary>
+    /// This class is responsible for mapping the settings stored in the solution file
+    /// to the real values used by the project system
+    /// </summary>
+    [GlobalService(typeof(ISccSettingsStore))]
+    sealed class SccSettingStorage : AnkhService
+    {
+        Dictionary<string, SccTranslateData> _solutionToData;
+        Dictionary<string, SccTranslateData> _userToData;
+
+        public SccSettingStorage(IAnkhServiceProvider context)
+            : base(context)
+        {
+        }
+
+
+    }
+}
