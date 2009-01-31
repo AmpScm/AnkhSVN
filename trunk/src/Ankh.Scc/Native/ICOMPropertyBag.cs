8d39cea585360147d5967d356832ca9b3cd63d3e
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-01-31 13:03:59 +0000

    * src/Ankh.Scc/Ankh.Scc.csproj * src/Ankh.Scc/Native * src/Ankh.Scc/Native/ICOMPropertyBag.cs * src/Ankh.Scc/SccSettingStorage.cs   More refactoring preparations

---
+++ trunk/src/Ankh.Scc/Native/ICOMPropertyBag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace Ankh.Scc.Native
+{
+    /// <summary>
+    /// IPropertyBag but then with parsable HResult
+    /// </summary>
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown), ComImport]
+    [Guid("55272A00-42CB-11CE-8135-00AA004BB851")]
+    interface ICOMPropertyBag
+    {
+        [PreserveSig, MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int Read(string pszPropName, out object pVar, IErrorLog pErrorLog, uint VARTYPE, object pUnkObj);
+
+        [PreserveSig, MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        int Write(string pszPropName, ref object pVar);
+    }
+}
