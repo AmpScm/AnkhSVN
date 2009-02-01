11251828cbd40275e84e3a25850c6dc5e48efd75
Bert Huijben <rhuijben@users.noreply.github.com>
2009-02-01 22:53:07 +0000

    Use the Keywords list provided by SharpSvn to prepare for upgrading to 1.6.
    * src/Ankh.Services/Ankh.Services.csproj
      Update SharpSvn reference
    * src/Ankh.UI/PropertyEditors/KeywordsPropertyEditor.cs
    * src/Ankh.UI/PropertyEditors/KeywordsPropertyEditor.Designer.cs
      Remove all but one keywords from the resources and use the SharpSvn provided
      list instead

--- src/Ankh.UI/PropertyEditors/KeywordsPropertyEditor.Designer.cs
+++ src/Ankh.UI/PropertyEditors/KeywordsPropertyEditor.Designer.cs
@@ -29,41 +29,32 @@ namespace Ankh.UI.PropertyEditors
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.keywordList = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
-            // checkedListBox1
+            // keywordList
             // 
-            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Items.AddRange(new object[] {
-            "Author",
-            "Date",
-            "HeadURL",
-            "Id",
-            "LastChangedBy",
-            "LastChangedDate",
-            "LastChangedRevision",
-            "Rev",
-            "Revision",
-            "URL"});
-            this.checkedListBox1.Location = new System.Drawing.Point(0, 0);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(348, 182);
-            this.checkedListBox1.TabIndex = 1;
-            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            this.keywordList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.keywordList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.keywordList.FormattingEnabled = true;
+            this.keywordList.Items.AddRange(new object[] {
+            "Id"});
+            this.keywordList.Location = new System.Drawing.Point(0, 0);
+            this.keywordList.Name = "keywordList";
+            this.keywordList.Size = new System.Drawing.Size(348, 182);
+            this.keywordList.Sorted = true;
+            this.keywordList.TabIndex = 1;
+            this.keywordList.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
             // 
             // KeywordsPropertyEditor
             // 
-            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.keywordList);
             this.Name = "KeywordsPropertyEditor";
             this.ResumeLayout(false);
 
         }
         #endregion
 
-        private System.ComponentModel.IContainer components;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox keywordList;
     }
 }
