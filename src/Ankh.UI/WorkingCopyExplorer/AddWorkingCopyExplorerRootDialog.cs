de8a4c42a844e37b7c12b4118fe76e3dbdb815fe
Bert Huijben <rhuijben@users.noreply.github.com>
2009-02-05 13:52:56 +0000

    Automatically update copyright years for all files editted in 2009

--- src/Ankh.UI/WorkingCopyExplorer/AddWorkingCopyExplorerRootDialog.cs
+++ src/Ankh.UI/WorkingCopyExplorer/AddWorkingCopyExplorerRootDialog.cs
@@ -1,6 +1,6 @@
 // $Id$
 //
-// Copyright 2006-2008 The AnkhSVN Project
+// Copyright 2006-2009 The AnkhSVN Project
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
