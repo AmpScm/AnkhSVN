74e37b1e95878c1809876c1788a10d24e3c28567
Sander Rijken <1292085+srijken@users.noreply.github.com>
2009-03-05 11:11:12 +0000

    Implement the QuerySaveFile(s) part of the EditQuerySave interface (hopefully) correctly. The means we also have to handle read-only files that cannot be made writeable by svn unlocking them.
    * src/Ankh.Scc/Ankh.Scc.csproj
    * src/Ankh.Scc/SccUI/SccManagementResources.Designer.cs
    * src/Ankh.Scc/SccUI/SccManagementResources.resx
    * src/Ankh.Scc/SccUI/SccQuerySaveReadonlyDialog.cs
    * src/Ankh.Scc/SccUI/SccQuerySaveReadonlyDialog.Designer.cs
    * src/Ankh.Scc/SccUI/SccQuerySaveReadonlyDialog.resx
      Add dialog + resource file for the dialog that asks the user wether to
        save as/overwrite/cancel
    * src/Ankh.Scc/AnkhSccProvider.QueryEditSave.cs
      Use the dialog when we detect a readonly file that cannot be unlocked by
        locking it with Subversion.
      Keep track of QuerySave batches, and handle the cancel case correctly.
        This means that we need to keep calling cancel when the user cancelled
        out of on of the dialogs.

---
+++ src/Ankh.Scc/SccUI/SccQuerySaveReadonlyDialog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Ankh.UI;
+
+namespace Ankh.Scc.SccUI
+{
+    public partial class SccQuerySaveReadonlyDialog : VSDialogForm
+    {
+        string _file;
+
+        public SccQuerySaveReadonlyDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string File
+        {
+            get { return _file; }
+            set { _file = value; }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            lblFile.Text = string.Format(SccManagementResources.ReadonlyFileXCannotBeSaved, Path.GetFileName(File));
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Yes;
+        }
+
+        private void btnOverwrite_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.No;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
