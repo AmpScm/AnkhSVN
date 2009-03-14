f720fa8fb289c3544171422bc21d01d56c260d34
Sander Rijken <1292085+srijken@users.noreply.github.com>
2009-03-14 21:16:55 +0000

    Add some more tests to the FileIconMapper

--- src/Ankh.VS.UnitTest/Services/FileIconMapperTest.cs
+++ src/Ankh.VS.UnitTest/Services/FileIconMapperTest.cs
@@ -17,13 +17,25 @@ namespace AnkhSvn_UnitTestProject.Services
     [TestFixture]
     public class FileIconMapperTest
     {
+        IFileIconMapper mapper;
+        IAnkhServiceProvider sp;
+        [SetUp]
+        public void SetUp()
+        {
+            sp = new AnkhServiceProvider();
+
+            mapper = new FileIconMapper(sp);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            mapper = null;
+        }
+
         [Test]
         public void TestNonExistingFileType()
         {
-            AnkhServiceProvider sp = new AnkhServiceProvider();
-            
-            IFileIconMapper mapper = new FileIconMapper(sp);
-
             Assert.That(mapper.GetFileType((string)null), Is.EqualTo(""));
             Assert.That(mapper.GetFileType(""), Is.EqualTo(""));
             Assert.That(mapper.GetFileType(".exe"), Is.EqualTo("Application"));
@@ -33,20 +45,15 @@ namespace AnkhSvn_UnitTestProject.Services
         [Test, ExpectedException(typeof(ArgumentNullException))]
         public void TestNullSvnItem()
         {
-            AnkhServiceProvider sp = new AnkhServiceProvider();
-            IFileIconMapper mapper = new FileIconMapper(sp);
-
             mapper.GetFileType((SvnItem)null);
         }
 
         [Test]
         public void TestExistingFileType()
         {
-            AnkhServiceProvider sp = new AnkhServiceProvider();
             var statusCache = new Mock<IFileStatusCache>();
-
-
             IFileIconMapper mapper = new FileIconMapper(sp);
+
             string tempFile = Path.GetTempFileName();
             string exeTempFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".exe");
             using (File.CreateText(exeTempFile))
@@ -67,6 +74,56 @@ namespace AnkhSvn_UnitTestProject.Services
             {
                 File.Delete(tempFile);
                 File.Delete(exeTempFile);
+            }
+        }
+
+        [Test]
+        public void TestIconForExtension()
+        {
+            Assert.That(mapper.DirectoryIcon, Is.GreaterThan(0));
+            Assert.That(mapper.FileIcon, Is.GreaterThan(0));
+
+            Assert.That(mapper.GetIconForExtension(null), Is.EqualTo(mapper.FileIcon));
+            Assert.That(mapper.GetIconForExtension(""), Is.EqualTo(mapper.FileIcon));
+            Assert.That(mapper.GetIconForExtension("qweqweqeqwe"), Is.EqualTo(mapper.FileIcon));
+            Assert.That(mapper.GetIconForExtension("exe"), Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void GetSpecialIcon()
+        {
+            foreach(Environment.SpecialFolder folder in Enum.GetValues(typeof(Environment.SpecialFolder)))
+            {
+                Assert.That(mapper.GetSpecialFolderIcon(folder), Is.GreaterThan(0), "Failed with value: {0}", folder);
+            }
+
+            foreach (WindowsSpecialFolder folder in Enum.GetValues(typeof(WindowsSpecialFolder)))
+            {
+                if (folder == WindowsSpecialFolder.MyDocuments)
+                    continue; // fails, find out why
+                if (folder == WindowsSpecialFolder.ResourcesLocalized)
+                    continue;
+                if (folder == WindowsSpecialFolder.CommonOemLinks)
+                    continue;
+                    
+                Assert.That(mapper.GetSpecialFolderIcon(folder), Is.GreaterThan(0), "Failed with value: {0}", folder);
+            }
+
+            foreach (SpecialIcon icon in Enum.GetValues(typeof(SpecialIcon)))
+            {
+                if (icon == SpecialIcon.Blank)
+                    Assert.That(mapper.GetSpecialIcon(icon), Is.EqualTo(0));
+                else
+                    Assert.That(mapper.GetSpecialIcon(icon), Is.GreaterThan(0), "Failed with value: {0}", icon);
+            }
+        }
+
+        [Test]
+        public void TestStateIcons()
+        {
+            foreach (StateIcon icon in Enum.GetValues(typeof(StateIcon)))
+            {
+                Assert.That(mapper.GetStateIcon(icon), Is.GreaterThanOrEqualTo(0), "Failed with value: {0}", icon);
             }
         }
     }
