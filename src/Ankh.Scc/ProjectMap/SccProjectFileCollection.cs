4efaf30ee4417d828aba072f9fc17aadc4524c0b
Bert Huijben <rhuijben@users.noreply.github.com>
2009-03-25 11:27:08 +0000

    Schedule an explicit glyph update when a file is added to a project to make sure it is shown in the Pending Changes window. Before r6268 this missed event was caught by the SvnItem changed broadcast.
    * src/Ankh.Scc/ProjectMap/SccProjectData.cs
      Wrap the GetService calls; use new TryGetValue on project file collection
      and do a glyph update on added files. Make sure project files are marked as
      project file if they first appear as member in another project.
    * src/Ankh.Scc/ProjectMap/SccProjectFileCollection.cs
      Add TryGetValue member mapping to the inner dictionary or a scan
      if there is no inner dictionary

--- src/Ankh.Scc/ProjectMap/SccProjectFileCollection.cs
+++ src/Ankh.Scc/ProjectMap/SccProjectFileCollection.cs
@@ -32,5 +32,29 @@ namespace Ankh.Scc.ProjectMap
         {
             return item.Filename;
         }
+
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="item">The item.</param>
+        /// <returns></returns>
+        public bool TryGetValue(string key, out SccProjectFileReference value)
+        {
+            if (Dictionary != null)
+                return Dictionary.TryGetValue(key, out value);
+
+            foreach (SccProjectFileReference p in Items)
+            {
+                if (Comparer.Equals(GetKeyForItem(p), key))
+                {
+                    value = p;
+                    return true;
+                }
+            }
+
+            value = null;
+            return false;
+        }
     }
 }
