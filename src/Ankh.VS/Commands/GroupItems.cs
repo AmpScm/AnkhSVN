bdfa357759fe045cf91356ec234a201205909d5e
Bert Huijben <rhuijben@users.noreply.github.com>
2009-03-31 17:09:17 +0000

    Prepare a new Group Items command that allows placing files below a parent file in C# and VB projects.
    * src/Ankh.Ids/AnkhCommand.cs
    * src/Ankh.Package/Gui/AnkhSvn.xml
      Add command id's
    * src/Ankh.VS/Ankh.VS.csproj
    * src/Ankh.VS/Commands/GroupItems.cs
    * src/Ankh.VS/Selection/SelectionContext.cs
    * src/Ankh.VS/Selection/SelectionContext.SelectionItem.cs
    * src/Ankh.VS/Selection/SelectionUtils.cs
      Detect when we probably can create/remove items to/from a group

---
+++ src/Ankh.VS/Commands/GroupItems.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ankh.VS.Selection;
+using Ankh.Selection;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio;
+using System.IO;
+
+namespace Ankh.Commands
+{
+    [Command(AnkhCommand.ProjectItemGroup, AlwaysAvailable = true)]
+    [Command(AnkhCommand.ProjectItemUngroup, AlwaysAvailable = true)]
+    class GroupItems : ICommandHandler
+    {
+        public void OnUpdate(CommandUpdateEventArgs e)
+        {
+            e.Enabled = false;
+            /*switch (e.Command)
+            {
+                case AnkhCommand.ProjectItemGroup:
+                    OnUpdateGroup(e);
+                    break;
+                case AnkhCommand.ProjectItemUngroup:
+                    OnUpdateUngroup(e);
+                    break;
+                default:
+                    e.Enabled = false;
+                    break;
+            }*/
+        }
+
+        private void OnUpdateGroup(CommandUpdateEventArgs e)
+        {
+            SelectionContext selection = e.Selection as SelectionContext;
+
+            if (selection == null || selection.IsSingleNodeSelection)
+            {
+                e.Enabled = false;
+                return;
+            }
+
+            IVsSccProject2 project = null;
+            IVsHierarchy hier = null;
+            uint parentId = VSConstants.VSITEMID_NIL;
+            bool enable = false;
+
+            try
+            {
+                foreach (SelectionItem item in selection.GetSelectedItems(false))
+                {
+                    if (project == null)
+                    {
+                        project = item.SccProject;
+
+                        if (project == null)
+                            return;
+
+                        hier = (IVsHierarchy)project;
+                        if (hier == null)
+                            return;
+                    }
+                    else if (item.SccProject != project || hier == null)
+                        return;
+
+                    object value;
+
+                    if (!ErrorHandler.Succeeded(hier.GetProperty(item.Id, (int)__VSHPROPID.VSHPROPID_Parent, out value)))
+                        return;
+
+                    if (parentId == VSConstants.VSITEMID_NIL)
+                    {
+                        parentId = SelectionContext.GetItemIdFromObject(value);
+
+                        if (parentId == VSConstants.VSITEMID_NIL)
+                            return;
+                    }
+                    else if (parentId != SelectionContext.GetItemIdFromObject(value))
+                        return;
+                }
+
+                enable = true; // All items are from the same project and have the same parent.. Ok!
+            }
+            catch
+            { /* Just disable on buggy projects */ }
+            finally
+            {
+                if (!enable)
+                    e.Enabled = false;
+            }
+        }
+
+        private void OnUpdateUngroup(CommandUpdateEventArgs e)
+        {
+            SelectionContext selection = e.Selection as SelectionContext;
+
+            if (selection == null)
+            {
+                e.Enabled = false;
+                return;
+            }
+
+            IVsSccProject2 project = null;
+            IVsHierarchy hier = null;
+            uint parentId = VSConstants.VSITEMID_NIL;
+            bool enable = false;
+
+            try
+            {
+                foreach (SelectionItem item in selection.GetSelectedItems(false))
+                {
+                    if (project == null)
+                    {
+                        project = item.SccProject;
+
+                        if (project == null)
+                            return;
+
+                        hier = (IVsHierarchy)project;
+                        if (hier == null)
+                            return;
+                    }
+                    else if (item.SccProject != project || hier == null)
+                        return;
+
+                    object value;
+
+                    if (!ErrorHandler.Succeeded(hier.GetProperty(item.Id, (int)__VSHPROPID.VSHPROPID_Parent, out value)))
+                        return;
+                    if (!ErrorHandler.Succeeded(hier.GetProperty(item.Id, (int)__VSHPROPID.VSHPROPID_Parent, out value)))
+                        return;
+
+                    if (parentId == VSConstants.VSITEMID_NIL)
+                    {
+                        parentId = SelectionContext.GetItemIdFromObject(value);
+
+                        if (parentId == VSConstants.VSITEMID_NIL)
+                            return;
+                    }
+                    else if (parentId != SelectionContext.GetItemIdFromObject(value))
+                        return;
+                }
+
+                // All nodes have the same parent. Check if their filenames match the parent
+                if (hier == null || parentId == VSConstants.VSITEMID_NIL || parentId == VSConstants.VSITEMID_ROOT)
+                    return;
+
+                IVsProject2 project2 = project as IVsProject2;
+                if (project2 == null)
+                    return;
+
+                string parentName;
+                if (!ErrorHandler.Succeeded(project2.GetMkDocument(parentId, out parentName)) || !SvnItem.IsValidPath(parentName))
+                    return;
+
+                parentName = Path.Combine(SharpSvn.SvnTools.GetNormalizedDirectoryName(parentName), Path.GetFileNameWithoutExtension(parentName) + ".");
+
+                foreach (string file in selection.GetSelectedFiles(false))
+                {
+                    if (!file.StartsWith(parentName, StringComparison.OrdinalIgnoreCase) && 0 > file.IndexOf(Path.DirectorySeparatorChar, parentName.Length))
+                        return; // File doesn't match parent name
+                }
+
+                enable = true; 
+            }
+            catch
+            { /* Just disable on buggy projects */ }
+            finally
+            {
+                if (!enable)
+                    e.Enabled = false;
+            }
+        }
+
+        public void OnExecute(CommandEventArgs e)
+        {
+            return;
+        }
+
+    }
+}
--- src/Ankh.VS/Selection/SelectionContext.cs
+++ src/Ankh.VS/Selection/SelectionContext.cs
@@ -226,82 +226,7 @@ namespace Ankh.VS.Selection
             }
         }
 
-        #endregion
-
-        internal sealed class SelectionItem : IEquatable<SelectionItem>
-        {
-            readonly IVsHierarchy _hierarchy;
-            IVsSccProject2 _sccProject;
-            readonly uint _id;
-            bool _checkedSccProject;
-
-            public SelectionItem(IVsHierarchy hierarchy, uint id)
-            {
-                if (hierarchy == null)
-                    throw new ArgumentNullException("hierarchy");
-
-                _hierarchy = hierarchy;
-                _id = id;
-            }
-
-            public SelectionItem(IVsHierarchy hierarchy, uint id, IVsSccProject2 project)
-                : this(hierarchy, id)
-            {
-                _sccProject = project;
-            }
-
-            public IVsHierarchy Hierarchy
-            {
-                [DebuggerStepThrough]
-                get { return _hierarchy; }
-            }
-
-            public IVsSccProject2 SccProject
-            {
-                get
-                {
-                    if (_sccProject == null && !_checkedSccProject)
-                    {
-                        _checkedSccProject = true;
-                        _sccProject = _hierarchy as IVsSccProject2;
-                    }
-
-                    return _sccProject;
-                }
-            }
-
-            public uint Id
-            {
-                get { return _id; }
-            }
-
-            public bool IsSolution
-            {
-                get { return (SccProject != null) && SelectionUtils.IsSolutionSccProject(SccProject); }
-            }
-
-            #region IEquatable<SelectionItem> Members
-
-            public bool Equals(SelectionItem other)
-            {
-                if (other == null)
-                    return false;
-
-                return (other.Id == Id) && (other.Hierarchy == Hierarchy);
-            }
-
-            public override bool Equals(object obj)
-            {
-                return Equals(obj as SelectionItem);
-            }
-
-            public override int GetHashCode()
-            {
-                return _hierarchy.GetHashCode() ^ _id.GetHashCode();
-            }
-
-            #endregion
-        }
+        #endregion        
 
         protected bool MightBeSolutionExplorerSelection
         {
@@ -379,7 +304,7 @@ namespace Ankh.VS.Selection
             }
         }
 
-        protected IEnumerable<SelectionItem> GetSelectedItems(bool recursive)
+        internal protected IEnumerable<SelectionItem> GetSelectedItems(bool recursive)
         {
             return recursive ? GetSelectedItemsRecursive() : GetSelectedItems();
         }
@@ -485,7 +410,7 @@ namespace Ankh.VS.Selection
             }
         }
 
-        static uint GetItemIdFromObject(object pvar)
+        internal static uint GetItemIdFromObject(object pvar)
         {
             if (pvar == null) return VSConstants.VSITEMID_NIL;
             if (pvar is int) return (uint)(int)pvar;
