bc0eb8e05c07b772bbb74eba8149c8be78b1ad12
Bert Huijben <rhuijben@users.noreply.github.com>
2009-04-08 17:09:19 +0000

    * src/Ankh.UI/Ankh.UI.csproj * src/Ankh.UI/Controls/Form1.cs * src/Ankh.UI/Controls/Form1.Designer.cs * src/Ankh.UI/Controls/Form1.resx * src/Ankh.UI/Controls/StatusContainer.cs * src/Ankh.UI/Controls/StatusContainerDesigner.cs * src/Ankh.UI/Controls/StatusPanel.cs * src/Ankh.UI/Controls/StatusPanelDesigner.cs   Checkpoint some GUI work (Buildaction = none for these files)

---
+++ src/Ankh.UI/Controls/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Ankh.UI.Controls
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.statusContainer = new Ankh.UI.Controls.StatusContainer();
+			this.statusPanel1 = new Ankh.UI.Controls.StatusPanel(this.components);
+			this.statusPanel2 = new Ankh.UI.Controls.StatusPanel(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			this.statusPanel3 = new Ankh.UI.Controls.StatusPanel(this.components);
+			this.statusPanel4 = new Ankh.UI.Controls.StatusPanel(this.components);
+			this.statusPanel5 = new Ankh.UI.Controls.StatusPanel(this.components);
+			this.statusContainer.SuspendLayout();
+			this.statusPanel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// statusContainer
+			// 
+			this.statusContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.statusContainer.AutoScroll = true;
+			this.statusContainer.Controls.Add(this.statusPanel1);
+			this.statusContainer.Controls.Add(this.statusPanel2);
+			this.statusContainer.Controls.Add(this.statusPanel3);
+			this.statusContainer.Controls.Add(this.statusPanel4);
+			this.statusContainer.Controls.Add(this.statusPanel5);
+			this.statusContainer.Location = new System.Drawing.Point(12, 24);
+			this.statusContainer.Name = "statusContainer";
+			this.statusContainer.Size = new System.Drawing.Size(640, 658);
+			this.statusContainer.TabIndex = 0;
+			this.statusContainer.Click += new System.EventHandler(this.statusContainer_Click);
+			// 
+			// statusPanel1
+			// 
+			this.statusPanel1.BackColor = System.Drawing.Color.Red;
+			this.statusPanel1.HeaderLeft = System.Drawing.Color.Red;
+			this.statusPanel1.HeaderRight = System.Drawing.SystemColors.Window;
+			this.statusPanel1.Height = 88;
+			this.statusPanel1.Name = "statusPanel1";
+			this.statusPanel1.Size = new System.Drawing.Size(626, 88);
+			this.statusPanel1.TabIndex = 0;
+			this.statusPanel1.Title = "Log Message";
+			// 
+			// statusPanel2
+			// 
+			this.statusPanel2.Controls.Add(this.button1);
+			this.statusPanel2.HeaderLeft = System.Drawing.Color.Red;
+			this.statusPanel2.HeaderRight = System.Drawing.SystemColors.Window;
+			this.statusPanel2.Height = 64;
+			this.statusPanel2.Name = "statusPanel2";
+			this.statusPanel2.Size = new System.Drawing.Size(626, 64);
+			this.statusPanel2.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.button1.Location = new System.Drawing.Point(548, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// statusPanel3
+			// 
+			this.statusPanel3.BackColor = System.Drawing.Color.Yellow;
+			this.statusPanel3.HeaderLeft = System.Drawing.Color.Red;
+			this.statusPanel3.HeaderRight = System.Drawing.SystemColors.Window;
+			this.statusPanel3.Height = 70;
+			this.statusPanel3.Name = "statusPanel3";
+			this.statusPanel3.Size = new System.Drawing.Size(626, 70);
+			this.statusPanel3.TabIndex = 0;
+			// 
+			// statusPanel4
+			// 
+			this.statusPanel4.BackColor = System.Drawing.Color.Lime;
+			this.statusPanel4.HeaderLeft = System.Drawing.Color.PaleGreen;
+			this.statusPanel4.HeaderRight = System.Drawing.SystemColors.Window;
+			this.statusPanel4.Height = 64;
+			this.statusPanel4.Name = "statusPanel4";
+			this.statusPanel4.Size = new System.Drawing.Size(626, 64);
+			this.statusPanel4.TabIndex = 0;
+			// 
+			// statusPanel5
+			// 
+			this.statusPanel5.BackColor = System.Drawing.Color.Maroon;
+			this.statusPanel5.HeaderLeft = System.Drawing.Color.Red;
+			this.statusPanel5.HeaderRight = System.Drawing.SystemColors.Window;
+			this.statusPanel5.Height = 64;
+			this.statusPanel5.Name = "statusPanel5";
+			this.statusPanel5.Size = new System.Drawing.Size(626, 64);
+			this.statusPanel5.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(664, 708);
+			this.Controls.Add(this.statusContainer);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.statusContainer.ResumeLayout(false);
+			this.statusPanel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private StatusContainer statusContainer;
+		private StatusPanel statusPanel1;
+		private StatusPanel statusPanel2;
+		private StatusPanel statusPanel3;
+		private StatusPanel statusPanel4;
+		private StatusPanel statusPanel5;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ src/Ankh.UI/Controls/StatusContainer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Ankh.UI.Controls
+{
+	[Designer(typeof(StatusContainerDesigner))]
+	public class StatusContainer : ContainerControl
+	{
+		public StatusContainer()
+		{
+			base.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+			base.SetStyle(ControlStyles.ResizeRedraw, true);
+			base.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+		}
+
+		protected override Control.ControlCollection CreateControlsInstance()
+		{
+			return new StatusContainerTypedControlCollection(this);
+		}
+
+		sealed class StatusContainerTypedControlCollection : ControlCollection
+		{
+			StatusContainer _owner;
+
+			public StatusContainerTypedControlCollection(StatusContainer owner)
+				: base(owner)
+			{
+				_owner = owner;
+			}
+			public override void Add(Control value)
+			{
+				StatusPanel sp = value as StatusPanel;
+				if (sp == null)
+					throw new ArgumentException("Not a status panel", "value");
+
+				if (!_owner.InsertingItem())
+				{
+					_owner.DoAddPanel(value);
+				}
+
+				base.Add(value);
+
+				ISite site = _owner.Site;
+				if (site != null && site.Container != null)
+				{
+					site.Container.Add(value);
+				}
+
+				_owner.ApplySizes(sp);
+			}
+		}
+
+		internal bool SetCollapsed(StatusPanel statusPanel, bool value)
+		{
+			return value;
+		}
+
+		protected override void OnSizeChanged(EventArgs e)
+		{
+			base.OnSizeChanged(e);
+			ApplySizes(null);
+		}
+
+		protected override void OnClientSizeChanged(EventArgs e)
+		{
+			base.OnClientSizeChanged(e);
+			ApplySizes(null);
+		}
+
+		protected override void AdjustFormScrollbars(bool displayScrollbars)
+		{
+			base.AdjustFormScrollbars(displayScrollbars);
+			ApplySizes(null);
+		}
+
+		protected override void OnCreateControl()
+		{
+			base.OnCreateControl();
+			ApplySizes(null);
+		}
+
+		internal void ApplySizes(StatusPanel first)
+		{
+			int i = 0;
+			Size clSize = ClientSize;
+			foreach (StatusPanel panel in Controls)
+			{
+				if (first != null && first != panel)
+					continue;
+				else
+					first = null;
+
+				if (!panel.Visible)
+					continue;
+
+				panel.PanelLocation = new Point(0, i);
+				int height = panel.HeaderHeight + panel.Height + panel.FooterHeight;
+				panel.PanelSize = new Size(clSize.Width - 2, height);
+
+				i += height + 4;
+			}
+		}
+
+		internal void DoAddPanel(Control value)
+		{
+			//throw new NotImplementedException();
+		}
+
+		internal bool InsertingItem()
+		{
+			return false;
+		}
+
+
+
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			int width = ClientSize.Width;
+			foreach (StatusPanel panel in Controls)
+			{
+				if (!panel.Visible)
+					continue;
+
+				Point pl = panel.PanelLocation;
+				Size pz = panel.PanelSize;
+				int hh = panel.HeaderHeight;
+
+				e.Graphics.DrawRectangle(SystemPens.ControlDark, new Rectangle(pl, pz));
+				e.Graphics.DrawLine(SystemPens.ControlDark, pl.X+1, pl.Y+hh-1, pl.X + pz.Width, pl.Y+hh-1);
+
+				using (Brush brush = new LinearGradientBrush(pl, pl+new Size(width,0), panel.HeaderLeft, panel.HeaderRight))
+					e.Graphics.FillRectangle(brush, new Rectangle(pl.X+1,pl.Y+1, pz.Width-2, panel.HeaderHeight-2));
+			}
+		}
+
+		private static GraphicsPath RoundedRectPath(Rectangle rect, int cornerRadius)
+		{
+			GraphicsPath roundedRect = new GraphicsPath();
+			roundedRect.AddArc(rect.X, rect.Y, cornerRadius * 2, cornerRadius * 2, 180, 90);
+			roundedRect.AddLine(rect.X + cornerRadius, rect.Y, rect.Right - cornerRadius * 2, rect.Y);
+			roundedRect.AddArc(rect.X + rect.Width - cornerRadius * 2, rect.Y, cornerRadius * 2, cornerRadius * 2, 270, 90);
+			roundedRect.AddLine(rect.Right, rect.Y + cornerRadius * 2, rect.Right, rect.Y + rect.Height - cornerRadius * 2);
+
+			roundedRect.AddArc(rect.X + rect.Width - cornerRadius * 2, rect.Y + rect.Height - cornerRadius * 2, cornerRadius * 2, cornerRadius * 2, 0, 90);
+
+			roundedRect.AddLine(rect.Right - cornerRadius * 2, rect.Bottom, rect.X + cornerRadius * 2, rect.Bottom);
+			roundedRect.AddArc(rect.X, rect.Bottom - cornerRadius * 2, cornerRadius * 2, cornerRadius * 2, 90, 90);
+			roundedRect.AddLine(rect.X, rect.Bottom - cornerRadius * 2, rect.X, rect.Y + cornerRadius * 2);
+			roundedRect.CloseFigure();
+			return roundedRect;
+		}
+	}
+}
