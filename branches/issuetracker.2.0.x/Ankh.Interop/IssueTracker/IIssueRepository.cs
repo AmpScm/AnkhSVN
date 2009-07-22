ab0ac3390adc46c7600e3ad0b857a9004d145f97
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-07-22 15:53:38 +0000

    added Ankh.Interop project to host the AnkhSvn's exposed extension points.

---
+++ branches/issuetracker.2.0.x/Ankh.Interop/IssueTracker/IIssueRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ankh.Interop.IssueTracker
+{
+    [System.Runtime.InteropServices.Guid("E1E1B50C-366C-4f3e-934A-6BCCF78C96E8")]
+    public interface IIssueRepository
+    {
+        /// <summary>
+        /// Gets the issue repository connector's registered name
+        /// </summary>
+        string ConnectorName { get; }
+
+        /// <summary>
+        /// Issue repository URL.
+        /// </summary>
+        Uri RepositoryUri { get; }
+
+        /// <summary>
+        /// Label for the issue repository.
+        /// </summary>
+        string Label { get; }
+
+        /// <summary>
+        /// Gets the control to be hosted in AnkhSVN PendingChanges window.
+        /// </summary>
+        Control Control { get; }
+
+        /// <summary>
+        /// Handles the request to open the issue.
+        /// </summary>
+        /// <param name="IssueId">Issue identifier</param>
+        void NavigateTo(string issueId);
+
+        /// <summary>
+        /// Called just before the changes are committed.
+        /// </summary>
+        void PreCommit(PreCommitArgs args);
+
+        /// <summary>
+        /// Called just after the changes are committed.
+        /// </summary>
+        /// <remarks>Provider is responsible for any post-commit issue related operations</remarks>
+        void PostCommit(PostCommitArgs args);
+
+        /// <summary>
+        /// Gets the RegEx pattern to recognize issues in a text.
+        /// </summary>
+        string IssueIdPattern { get; }
+    }
+}
---
+++ branches/issuetracker.2.0.x/Ankh.Interop/IssueTracker/IIssueRepositoryConnector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ankh.Interop.IssueTracker
+{
+    [System.Runtime.InteropServices.Guid("0E080D53-D4A1-4609-8AB1-45ABD217BA4B")]
+    public interface IIssueRepositoryConnector
+    {
+        string Name { get; }
+
+        bool TryGetRepository(Uri issueRepositoryUri, out IIssueRepository repository);
+
+        IIssueRepository Connect(Uri issueRepositoryUri, bool createNewConnection);
+    }
+}
