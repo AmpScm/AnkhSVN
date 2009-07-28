e036810ff7a0ecb8f2fe6413111b8745f134ba39
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-07-28 20:40:23 +0000

    Ankh.ExtensionPoints project

---
+++ trunk/src/Ankh.ExtensionPoints/IssueTracker/IIssueRepositoryConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ankh.ExtensionPoints.IssueTracker
+{
+    [System.Runtime.InteropServices.Guid("0E080D53-D4A1-4609-8AB1-45ABD217BA4B")]
+    public interface IIssueRepositoryConnector
+    {
+        /// <summary>
+        /// Gets the registered connector's unique name 
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Creates an Issue Repository based on the settings.
+        /// </summary>
+        IIssueRepository Create(IIssueRepositorySettings settings);
+
+        /// <summary>
+        /// Gets the IWin32Window (to edit/setup an issue repository)
+        /// </summary>
+        IIssueRepositoryConfigurationPage ConfigurationPage { get; }
+    }
+}
