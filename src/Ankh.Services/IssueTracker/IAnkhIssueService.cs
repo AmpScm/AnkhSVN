71f6c24b6401dda3fc789c2ec8370487eca61fa0
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-07-28 21:00:19 +0000

    IAnkhIssueService definition

---
+++ src/Ankh.Services/IssueTracker/IAnkhIssueService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Ankh.ExtensionPoints.IssueTracker;
+
+namespace Ankh
+{
+    [CLSCompliant(false)]
+    public interface IAnkhIssueService
+    {
+        /// <summary>
+        /// Gets all the registered Issue repository connectors.
+        /// </summary>
+        /// <remarks>This call DOES NOT trigger connector package initialization.</remarks>
+        ICollection<IIssueRepositoryConnector> Connectors { get; }
+
+        /// <summary>
+        /// Tries to find a registered connector with the given name.
+        /// </summary>
+        /// <remarks>This call DOES NOT trigger connector package initialization.</remarks>
+        bool TryGetConnector(string name, out IIssueRepositoryConnector connector);
+
+        /// <summary>
+        /// Gets or Sets the issue repository associated with the current solution.
+        /// </summary>
+        IIssueRepository CurrentIssueRepository { get; set; }
+
+        /// <summary>
+        /// Occurs when current solution's Issue Tracker Repository association settings are changed
+        /// </summary>
+        event EventHandler IssueRepositoryChanged;
+    }
+}
