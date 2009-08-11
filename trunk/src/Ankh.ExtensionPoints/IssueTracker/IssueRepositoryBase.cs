1e11388506ede45a7d2032be3fbe7d372cb3dc93
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-08-11 19:25:10 +0000

    convert interfaces to abstract base classes in Ankh.ExtensionPoints

---
+++ trunk/src/Ankh.ExtensionPoints/IssueTracker/IssueRepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows.Forms;
+
+namespace Ankh.ExtensionPoints.IssueTracker
+{
+    /// <summary>
+    /// Base class for IssueRepository implementation
+    /// </summary>
+    public abstract class IssueRepositoryBase : IssueRepositorySettingsBase
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="connectorName">Unique connector name(as registered with the registry)</param>
+        public IssueRepositoryBase(string connectorName)
+            : base(connectorName)
+        {
+        }
+
+        /// <summary>
+        /// Label for the issue repository.
+        /// </summary>
+        public abstract string Label { get; }
+
+        /// <summary>
+        /// Handles the request to open the issue.
+        /// </summary>
+        /// <param name="issueId">Issue identifier</param>
+        public virtual void NavigateTo(string issueId)
+        {
+        }
+
+        /// <summary>
+        /// Called just before the changes are committed.
+        /// </summary>
+        public virtual void PreCommit(PreCommitArgs args)
+        {
+            args.Cancel = false;
+        }
+
+        /// <summary>
+        /// Called just after the changes are committed.
+        /// </summary>
+        /// <remarks>Provider is responsible for any post-commit issue related operations</remarks>
+        public virtual void PostCommit(PostCommitArgs args)
+        {
+        }
+
+        /// <summary>
+        /// Gets the RegEx pattern to recognize issues in a text.
+        /// </summary>
+        public virtual string IssueIdPattern
+        {
+            get { return string.Empty; }
+        }
+
+        /// <summary>
+        /// Gets the IWin32Window instance for issue presentation
+        /// </summary>
+        /// <remarks>Default implementation returns this as IWin32Window.</remarks>
+        public virtual IWin32Window Window
+        {
+            get { return this as IWin32Window; }
+        }
+    }
+}
---
+++ trunk/src/Ankh.ExtensionPoints/IssueTracker/IssueRepositoryConnectorBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh.ExtensionPoints.IssueTracker
+{
+    /// <summary>
+    /// Base class for Issue Repository Connector service
+    /// </summary>
+    public abstract class IssueRepositoryConnectorBase
+    {
+        /// <summary>
+        /// Gets the registered connector's unique name 
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// Creates an Issue Repository based on the settings.
+        /// </summary>
+        public abstract IssueRepositoryBase Create(IssueRepositorySettingsBase settings);
+
+        /// <summary>
+        /// Gets the repository configuration page (to edit/setup an issue repository)
+        /// </summary>
+        public abstract IssueRepositoryConfigurationPageBase ConfigurationPage { get; }
+
+    }
+}
