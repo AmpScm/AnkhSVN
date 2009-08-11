8219cd565809b55ec99310fb8a9476c6302b45da
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-08-11 19:25:10 +0000

    convert interfaces to abstract base classes in Ankh.ExtensionPoints

---
+++ src/Ankh.ExtensionPoints/IssueTracker/IssueRepositoryConfigurationPageBase.cs
@@ -0,0 +1,96 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace Ankh.ExtensionPoints.IssueTracker
+{
+    /// <summary>
+    /// Base class for Issue Repository configuration page
+    /// </summary>
+    public abstract class IssueRepositoryConfigurationPageBase : IIssueRepositoryConfigurationPageEvents
+    {
+        private IssueRepositorySettingsBase _settings;
+
+        /// <summary>
+        /// Gets or sets the current repository settings.
+        /// </summary>
+        public virtual IssueRepositorySettingsBase Settings
+        {
+            get { return _settings; }
+            set { _settings = value; }
+        }
+
+        /// <summary>
+        /// Gets the IWin32Window instance for issue repository configuration UI.
+        /// </summary>
+        /// <remarks>Default implementation returns this as IWin32Window.</remarks>
+        public virtual IWin32Window Window
+        {
+            get
+            {
+                return this as IWin32Window;
+            }
+        }
+
+        /// <summary>
+        /// Notifies ragistered OnPageEvent handlers.
+        /// </summary>
+        public virtual void ConfigurationPageChanged(ConfigPageEventArgs e)
+        {
+            if (OnPageEvent != null)
+            {
+                OnPageEvent(this, e);
+            }
+        }
+
+        #region IIssueRepositoryConfigurationPageEvents Members
+
+        /// <summary>
+        /// Raised on a configuration page event 
+        /// </summary>
+        public event System.EventHandler<ConfigPageEventArgs> OnPageEvent;
+
+        #endregion
+    }
+
+    /// <summary>
+    /// This interface exposes the config page events raised by connector configuration logic.
+    /// </summary>
+    public interface IIssueRepositoryConfigurationPageEvents
+    {
+        /// <summary>
+        /// Raised on a config page event
+        /// </summary>
+        event EventHandler<ConfigPageEventArgs> OnPageEvent;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ConfigPageEventArgs : EventArgs
+    {
+        private bool _isComplete;
+        private Exception _exception;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is complete.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if this instance is complete; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsComplete
+        {
+            get { return _isComplete; }
+            set { _isComplete = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the exception.
+        /// </summary>
+        /// <value>The exception.</value>
+        public Exception Exception
+        {
+            get { return _exception; }
+            set { _exception = value; }
+        }
+    }
+}
---
+++ src/Ankh.ExtensionPoints/IssueTracker/IssueRepositorySettingsBase.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ankh.ExtensionPoints.IssueTracker
+{
+    /// <summary>
+    /// Base class for Issue Repository Settings
+    /// </summary>
+    public abstract class IssueRepositorySettingsBase
+    {
+        private string _connectorName;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="connectorName">Unique connector name(as registered with the registry)</param>
+        public IssueRepositorySettingsBase(string connectorName)
+        {
+            _connectorName = connectorName;
+        }
+
+        /// <summary>
+        /// Gets the issue repository connector's registered name
+        /// </summary>
+        public virtual string ConnectorName
+        {
+            get { return _connectorName; }
+        }
+
+        /// <summary>
+        /// Gets Issue Repository URI
+        /// </summary>
+        public abstract Uri RepositoryUri { get; }
+
+        /// <summary>
+        /// Gets the unique id of the repository.
+        /// </summary>
+        /// <remarks>This value can be used to identify a specific (sub) issue repository on RepositoryUri</remarks>
+        public virtual string RepositoryId
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Custom properties specific to the connector.
+        /// </summary>
+        public virtual IDictionary<string, object> CustomProperties
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the <paramref name="otherSettings"/> is different from this settings
+        /// </summary>
+        public virtual bool IsDifferentFrom(IssueRepositorySettingsBase otherSettings)
+        {
+            return !(true
+                && otherSettings != null
+                && string.Equals(ConnectorName, otherSettings.ConnectorName)
+                && Uri.Equals(RepositoryUri, otherSettings.RepositoryUri)
+                && string.Equals(RepositoryId, otherSettings.RepositoryId)
+                && HasSameProperties(CustomProperties, otherSettings.CustomProperties)
+                )
+                ;
+        }
+
+        static bool HasSameProperties(IDictionary<string, object> props1, IDictionary<string, object> props2)
+        {
+            if (props1 == null && props2 == null) { return true; }
+            int props1Count = props1 == null ? -1 : props1.Count;
+            int props2Count = props2 == null ? -1 : props2.Count;
+            if (props1Count != props2Count) { return false; }
+            foreach (string key in props1.Keys)
+            {
+                if (!props2.ContainsKey(key)) { return false; }
+                object value1 = props1[key];
+                object value2 = props2[key];
+                if (!object.Equals(value1, value2)) { return false; }
+            }
+            return true;
+        }
+    }
+}
