50cbc5fe73aee53272bedd1ec5749d35963856b6
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-09-08 21:56:40 +0000

    Add a very minimalistic pkgdef generator and use this to generate an Ankh.pkgdef that is now distributed with AnkhSVN. (We can't use the VS2008/VS2010 tools for this, because our releas binaries  are compiled against the VS2005 SDK)
    Isolated shell developers can use this file to register AnkhSVN in their
    shell. (Most likely by copying all our files in their common7 folder
    
    * src/Ankh.Votive.wixproj
    * src/Ankh.Votive/Ankh.Votive.wxs
      Add reference to Ankh.GenPkgDef
    
    * src/AnkhSvn.2008.sln
    * src/tools/Ankh.GenPkgDef/Ankh.GenPkgDef.csproj
    * src/tools/Ankh.GenPkgDef/PkgDefContext.cs
    * src/tools/Ankh.GenPkgDef/Program.cs
    * src/tools/Ankh.GenPkgDef/Properties/AssemblyInfo.cs
      Add pkgdef project

---
+++ trunk/src/tools/Ankh.GenPkgDef/PkgDefContext.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Shell;
+using System.IO;
+
+namespace Ankh.GenPkgDef
+{
+    class PkgDefContext : RegistrationAttribute.RegistrationContext, IDisposable
+    {
+        readonly TextWriter _writer;
+        readonly Type _componentType;
+
+        public PkgDefContext(TextWriter tw, Type type)
+        {
+            _writer = tw;
+            _componentType = type;
+        }
+
+        public override string CodeBase
+        {
+            get { return ""; }
+        }
+
+        public override string ComponentPath
+        {
+            get { return ""; }
+        }
+
+        public override Type ComponentType
+        {
+            get { return _componentType; }
+        }
+
+        public override RegistrationAttribute.Key CreateKey(string name)
+        {
+            return new PkgDefKey(this, name, null);
+        }
+
+        public override string EscapePath(string str)
+        {
+            return str;
+        }
+
+        public override string InprocServerPath
+        {
+            get { return "%windir%\\system32\\mscoree.dll"; }
+        }
+
+        StringWriter _sw;
+        public override System.IO.TextWriter Log
+        {
+            get { return _sw ?? (_sw = new StringWriter()); }
+        }
+
+        public override Microsoft.VisualStudio.Shell.RegistrationMethod RegistrationMethod
+        {
+            get { return RegistrationMethod.Default; }
+        }
+
+        public override void RemoveKey(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveKeyIfEmpty(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveValue(string keyname, string valuename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string RootFolder
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        #endregion
+
+        internal TextWriter Tw
+        {
+            get { return _writer; }
+        }
+
+        static string EscapeString(string value)
+        {
+            return (value ?? "").Replace("\\", "\\\\").Replace("\"", "\\\\");
+        }
+
+        internal string FormatValue(object value)
+        {
+            if (value is string)
+                return "\"" + EscapeString((string)value) + "\"";
+            else if (value is int || value is uint || value is short || value is ushort)
+                return string.Format("dword:{0:X8}", value);
+            else
+                return FormatValue(value.ToString());
+        }
+
+        class PkgDefKey : RegistrationAttribute.Key
+        {
+            readonly PkgDefContext _context;
+            string _name;
+            PkgDefKey _parent;
+            PkgDefKey _open;
+            bool _wasOpen;
+            public PkgDefKey(PkgDefContext context, string name, PkgDefKey parent)
+            {
+                if (parent != null)
+                {
+                    _parent = parent;
+                    _parent._open = parent;
+                }
+
+                _context = context;
+                _name = name;
+                context._writer.WriteLine(string.Format("[$RootKey$\\{0}]", name));
+            }
+
+            internal TextWriter Tw
+            {
+                get { return _context.Tw; }
+            }
+
+            public override void Close()
+            {
+                if(_parent != null)
+                    _parent._open = null;
+            }
+
+            public override RegistrationAttribute.Key CreateSubkey(string name)
+            {
+                if (_wasOpen)
+                    throw new InvalidOperationException();
+
+                _wasOpen = true;
+                return new PkgDefKey(_context, _name + "\\" + name, this);
+            }
+
+            public override void SetValue(string valueName, object value)
+            {
+                if (_wasOpen)
+                    throw new InvalidOperationException();
+                string name = string.IsNullOrEmpty(valueName) ? "@" : FormatValue(valueName);
+                Tw.WriteLine(string.Format("{0}={1}", name, FormatValue(value)));
+            }
+
+            private string FormatValue(object value)
+            {
+                return _context.FormatValue(value);
+            }
+        }
+
+        
+    }
+}
---
+++ trunk/src/tools/Ankh.GenPkgDef/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.Shell;
+
+namespace Ankh.GenPkgDef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Usage: Ankh.GenPkgDef <output> <assemblies....>");
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            using (StreamWriter pkgdef = File.CreateText(args[0]))
+            {
+                for (int i = 1; i < args.Length; i++)
+                {
+                    Assembly asm = Assembly.LoadFrom(args[i]);
+
+                    foreach (Type type in asm.GetTypes())
+                    {
+                        if (type.IsAbstract)
+                            continue;
+
+                        using(PkgDefContext ctx = new PkgDefContext(pkgdef, type))
+                        foreach (RegistrationAttribute ra in type.GetCustomAttributes(typeof(RegistrationAttribute), true))
+                        {
+                            ra.Register(ctx);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
