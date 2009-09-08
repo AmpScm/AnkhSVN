32af2ab8e3a29ac270289757721e6ebd57ad1dec
Bert Huijben <rhuijben@users.noreply.github.com>
2009-09-08 22:20:17 +0000

    Fix yet another difference between the VS2005 and VS2008 SDK.
    * src/tools/Ankh.GenPkgDef/Ankh.GenPkgDef.csproj
      Define a switching partial file, just like with the language service
    * src/tools/Ankh.GenPkgDef/PkgDefContext.cs
      Mark partial
    * src/tools/Ankh.GenPkgDef/PkgDefContext.VS2005.cs
      File for VS2005 (empty)
    * src/tools/Ankh.GenPkgDef/PkgDefContext.VS2008.cs
      File for VS2008 (Implement abstract method)

---
+++ src/tools/Ankh.GenPkgDef/PkgDefContext.VS2008.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh.GenPkgDef
+{
+    partial class PkgDefContext
+    {
+        public override string RootFolder
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
