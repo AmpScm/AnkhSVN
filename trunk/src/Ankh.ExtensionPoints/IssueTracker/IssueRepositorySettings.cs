d83ca124c299e25e3806ad65610ef7fbd94fde0f
sander <sander@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2009-10-24 20:50:31 +0000

    Resolve 2 possible NullReferenceException in HasSameProperties

--- trunk/src/Ankh.ExtensionPoints/IssueTracker/IssueRepositorySettings.cs
+++ trunk/src/Ankh.ExtensionPoints/IssueTracker/IssueRepositorySettings.cs
@@ -88,16 +88,24 @@ namespace Ankh.ExtensionPoints.IssueTracker
 
         static bool HasSameProperties(IDictionary<string, object> props1, IDictionary<string, object> props2)
         {
-            if (props1 == null && props2 == null) { return true; }
-            int props1Count = props1 == null ? -1 : props1.Count;
-            int props2Count = props2 == null ? -1 : props2.Count;
-            if (props1Count != props2Count) { return false; }
+            if (props1 == null && props2 == null) 
+                return true;
+
+            if(props1 == null || props2 == null)
+                return false; // if props1 == null, then props2 != null and the other way around
+            
+            if (props1.Count != props2.Count) 
+                return false;
+
             foreach (string key in props1.Keys)
             {
-                if (!props2.ContainsKey(key)) { return false; }
+                if (!props2.ContainsKey(key)) 
+                    return false;
+
                 object value1 = props1[key];
                 object value2 = props2[key];
-                if (!object.Equals(value1, value2)) { return false; }
+                if (!object.Equals(value1, value2)) 
+                    return false;
             }
             return true;
         }
