58f8e6599de21ef154fb16fcf2a00c29cb122bff
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2010-01-20 23:51:07 +0000

    Mark the OK and Cancel button in the issuetracker as accept and cancel button to enable the normal dialog handling (e.g. escape to close).
    * src/Ankh.UI/IssueTracker/IssueSelector.Designer.cs
    * src/Ankh.UI/IssueTracker/IssueSelector.resx
    * src/Ankh.UI/IssueTracker/IssueTrackerConfigDialog.Designer.cs
    * src/Ankh.UI/IssueTracker/IssueTrackerConfigDialog.resx
      Set accept and cancel buttons and mark localizable

--- trunk/src/Ankh.UI/IssueTracker/IssueTrackerConfigDialog.Designer.cs
+++ trunk/src/Ankh.UI/IssueTracker/IssueTrackerConfigDialog.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IssueTrackerConfigDialog));
             this.connectorLabel = new System.Windows.Forms.Label();
             this.connectorComboBox = new System.Windows.Forms.ComboBox();
             this.configPagePanel = new System.Windows.Forms.Panel();
@@ -37,71 +38,49 @@
             // 
             // connectorLabel
             // 
-            this.connectorLabel.AutoSize = true;
-            this.connectorLabel.Location = new System.Drawing.Point(12, 9);
+            resources.ApplyResources(this.connectorLabel, "connectorLabel");
             this.connectorLabel.Name = "connectorLabel";
-            this.connectorLabel.Size = new System.Drawing.Size(127, 13);
-            this.connectorLabel.TabIndex = 0;
-            this.connectorLabel.Text = "Issue Tracker Connector:";
             // 
             // connectorComboBox
             // 
-            this.connectorComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.connectorComboBox, "connectorComboBox");
             this.connectorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.connectorComboBox.FormattingEnabled = true;
-            this.connectorComboBox.Location = new System.Drawing.Point(15, 25);
             this.connectorComboBox.Name = "connectorComboBox";
-            this.connectorComboBox.Size = new System.Drawing.Size(421, 21);
-            this.connectorComboBox.TabIndex = 1;
             this.connectorComboBox.SelectedIndexChanged += new System.EventHandler(this.connectorComboBox_SelectedIndexChanged);
             // 
             // configPagePanel
             // 
-            this.configPagePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.configPagePanel, "configPagePanel");
             this.configPagePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.configPagePanel.Location = new System.Drawing.Point(15, 52);
             this.configPagePanel.Name = "configPagePanel";
-            this.configPagePanel.Size = new System.Drawing.Size(421, 273);
-            this.configPagePanel.TabIndex = 2;
             // 
             // okButton
             // 
-            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.okButton, "okButton");
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Enabled = false;
-            this.okButton.Location = new System.Drawing.Point(280, 331);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
-            this.okButton.TabIndex = 3;
-            this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
             // 
             // cancelButton
             // 
-            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.cancelButton, "cancelButton");
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(361, 331);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
-            this.cancelButton.TabIndex = 4;
-            this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
             // 
             // IssueTrackerConfigDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AcceptButton = this.cancelButton;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 366);
+            this.CancelButton = this.cancelButton;
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.configPagePanel);
             this.Controls.Add(this.connectorComboBox);
             this.Controls.Add(this.connectorLabel);
             this.Name = "IssueTrackerConfigDialog";
-            this.Text = "Issue Tracker Configuration";
             this.Load += new System.EventHandler(this.IssueTrackerConfigDialog_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
