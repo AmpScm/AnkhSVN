d07a095053fcf649f6b5c36cc3f9cf733bb9c95b
Bert Huijben <rhuijben@users.noreply.github.com>
2010-01-20 23:51:07 +0000

    Mark the OK and Cancel button in the issuetracker as accept and cancel button to enable the normal dialog handling (e.g. escape to close).
    * src/Ankh.UI/IssueTracker/IssueSelector.Designer.cs
    * src/Ankh.UI/IssueTracker/IssueSelector.resx
    * src/Ankh.UI/IssueTracker/IssueTrackerConfigDialog.Designer.cs
    * src/Ankh.UI/IssueTracker/IssueTrackerConfigDialog.resx
      Set accept and cancel buttons and mark localizable

--- src/Ankh.UI/IssueTracker/IssueSelector.Designer.cs
+++ src/Ankh.UI/IssueTracker/IssueSelector.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IssueSelector));
             this.issuesView1 = new Ankh.UI.IssueTracker.IssuesView(this.components);
             this.okButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
@@ -36,50 +37,35 @@
             // 
             // issuesView1
             // 
-            this.issuesView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.issuesView1.Location = new System.Drawing.Point(12, 12);
+            resources.ApplyResources(this.issuesView1, "issuesView1");
             this.issuesView1.MultiSelect = false;
             this.issuesView1.Name = "issuesView1";
-            this.issuesView1.Size = new System.Drawing.Size(156, 97);
-            this.issuesView1.TabIndex = 0;
             this.issuesView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.issuesView1_ItemSelectionChanged);
             // 
             // okButton
             // 
-            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.okButton, "okButton");
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Enabled = false;
-            this.okButton.Location = new System.Drawing.Point(12, 121);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
-            this.okButton.TabIndex = 1;
-            this.okButton.Text = "&OK";
             this.okButton.UseVisualStyleBackColor = true;
             // 
             // cancelButton
             // 
-            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.cancelButton, "cancelButton");
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(93, 121);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
-            this.cancelButton.TabIndex = 2;
-            this.cancelButton.Text = "&Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
             // 
             // IssueSelector
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AcceptButton = this.okButton;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(184, 156);
+            this.CancelButton = this.cancelButton;
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.issuesView1);
-            this.MinimumSize = new System.Drawing.Size(192, 190);
             this.Name = "IssueSelector";
-            this.Text = "Issue Selector";
             this.ResumeLayout(false);
 
         }
