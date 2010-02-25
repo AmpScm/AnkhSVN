dda56fc9d96b0e9264831dc925026951fcd991a2
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2010-02-25 23:59:05 +0000

    * src/Ankh.ExtensionPoints/Properties/AssemblyInfo.cs   Fix assemblyinfo file compilation

--- trunk/src/Ankh.ExtensionPoints/Properties/AssemblyInfo.cs
+++ trunk/src/Ankh.ExtensionPoints/Properties/AssemblyInfo.cs
@@ -52,5 +52,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: NeutralResourcesLanguageAttribute("en")]
 [assembly: AssemblyVersion("2.1.*")]
