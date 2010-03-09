a6dc97f8cd327d2a9ff2737d9826ecd8bdc5e6b7
Bert Huijben <rhuijben@users.noreply.github.com>
2010-03-09 17:36:00 +0000

    * src/Ankh.UI/Ankh.UI.csproj * src/Ankh.UI/Commands/AnnotateDialog.cs * src/Ankh.UI/Commands/AnnotateDialog.Designer.cs * src/Ankh.UI/Commands/AnnotateDialog.resx   Add work in progress annotate dialog

---
+++ src/Ankh.UI/Commands/AnnotateDialog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ankh.UI.Commands
+{
+	public partial class AnnotateDialog : VSDialogForm
+	{
+		public AnnotateDialog()
+		{
+			InitializeComponent();
+		}
+
+		private void AnnotateDialog_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
