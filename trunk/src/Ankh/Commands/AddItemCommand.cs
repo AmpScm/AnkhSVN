22de7d6bd2d2cc2007c9dc3369e8dde56bd89d14
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2010-03-12 16:38:58 +0000

    * src/Ankh/Commands/AddItemCommand.cs   Implement the .IsInAutomation property to actually return the intended   value.

--- trunk/src/Ankh/Commands/AddItemCommand.cs
+++ trunk/src/Ankh/Commands/AddItemCommand.cs
@@ -31,6 +31,7 @@ namespace Ankh.Commands
     {
         public override void OnUpdate(CommandUpdateEventArgs e)
         {
+			System.GC.KeepAlive(e.IsInAutomation);
             foreach (SvnItem item in e.Selection.GetSelectedSvnItems(true))
             {
                 if (item.IsVersioned)
