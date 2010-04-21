93d8a479a6d94d6144bcbda8cfa6cde3855e0529
Sander Rijken <1292085+srijken@users.noreply.github.com>
2010-04-21 17:09:18 +0000

    Normalize the solution file name, to the true path
    * src/Ankh.VS/Selection/SelectionContext.cs
      Make sure that the solution filename doesn't look changed in SolutionSettings.IsDirty

--- src/Ankh.VS/Selection/SelectionContext.cs
+++ src/Ankh.VS/Selection/SelectionContext.cs
@@ -217,7 +217,7 @@ namespace Ankh.VS.Selection
                         string solutionDir, solutionFile, solutionUserFile;
                         if (ErrorHandler.Succeeded(Solution.GetSolutionInfo(out solutionDir, out solutionFile, out solutionUserFile)))
                         {
-                            _solutionFilename = solutionFile;
+                            _solutionFilename = SvnItem.IsValidPath(solutionFile) ? SvnTools.GetTruePath(solutionFile, true) : solutionFile;
                         }
                     }
                 }
