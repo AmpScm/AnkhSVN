1bcfcc259ae44c12d83c7fb92f97c178f37544c7
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2010-07-20 10:39:09 +0000

    * src/Ankh.Tests/Properties/AssemblyInfo.cs * src/Ankh.VS.IntegrationTest/Ankh.VS.IntegrationTest.csproj * src/Ankh.VS.IntegrationTest/Properties/AssemblyInfo.cs * src/Ankh.VS.UnitTest/Ankh.VS.UnitTest.csproj * src/Ankh.VS.UnitTest/Properties/AssemblyInfo.cs * src/tools/Ankh.GenerateVSIXManifest/Properties/AssemblyInfo.cs   Update AssemblyInfo.cs files to make some sense

--- trunk/src/Ankh.VS.IntegrationTest/Properties/AssemblyInfo.cs
+++ trunk/src/Ankh.VS.IntegrationTest/Properties/AssemblyInfo.cs
@@ -1,16 +1,18 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ankh.GenerateVSIXManifest")]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Ankh.GenerateVSIXManifest")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,9 +21,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a98fba9e-27de-4c1f-a60f-ac6ee712409e")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -29,7 +28,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
