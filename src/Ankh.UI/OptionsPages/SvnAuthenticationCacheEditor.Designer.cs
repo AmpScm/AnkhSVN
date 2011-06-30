f5547946b5d77faf08e18ea612df711da899f3c9
Bert Huijben <rhuijben@users.noreply.github.com>
2011-06-30 19:26:43 +0000

    Mark a few controls localizable.
    * src/Ankh.UI/OptionsPages/SvnAuthenticationCacheEditor.Designer.cs
    * src/Ankh.UI/OptionsPages/SvnAuthenticationCacheEditor.resx
    * src/Ankh.UI/OptionsPages/SvnProxyEditor.Designer.cs
    * src/Ankh.UI/OptionsPages/SvnProxyEditor.resx
    * src/Ankh.UI/OptionsPages/ToolArgumentDialog.Designer.cs
    * src/Ankh.UI/OptionsPages/ToolArgumentDialog.resx
      Mark localizable

--- src/Ankh.UI/OptionsPages/SvnAuthenticationCacheEditor.Designer.cs
+++ src/Ankh.UI/OptionsPages/SvnAuthenticationCacheEditor.Designer.cs
@@ -58,95 +58,64 @@ namespace Ankh.UI.OptionsPages
             // 
             // credentialList
             // 
-            this.credentialList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.credentialList, "credentialList");
             this.credentialList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.serverHeader,
             this.realmHeader,
             this.cachedHeader});
             this.credentialList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.credentialList.HideSelection = false;
-            this.credentialList.Location = new System.Drawing.Point(6, 19);
             this.credentialList.Name = "credentialList";
-            this.credentialList.Size = new System.Drawing.Size(505, 162);
-            this.credentialList.TabIndex = 0;
             this.credentialList.SelectedIndexChanged += new System.EventHandler(this.credentialList_SelectedIndexChanged);
             // 
             // serverHeader
             // 
-            this.serverHeader.Text = "Server";
-            this.serverHeader.Width = 200;
+            resources.ApplyResources(this.serverHeader, "serverHeader");
             // 
             // realmHeader
             // 
-            this.realmHeader.Text = "Realm";
-            this.realmHeader.Width = 140;
+            resources.ApplyResources(this.realmHeader, "realmHeader");
             // 
             // cachedHeader
             // 
-            this.cachedHeader.Text = "Cached";
-            this.cachedHeader.Width = 140;
+            resources.ApplyResources(this.cachedHeader, "cachedHeader");
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.removeButton);
             this.groupBox1.Controls.Add(this.credentialList);
-            this.groupBox1.Location = new System.Drawing.Point(12, 58);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(517, 216);
-            this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Repository Servers";
             // 
             // removeButton
             // 
-            this.removeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.removeButton.Enabled = false;
-            this.removeButton.Location = new System.Drawing.Point(6, 187);
+            resources.ApplyResources(this.removeButton, "removeButton");
             this.removeButton.Name = "removeButton";
-            this.removeButton.Size = new System.Drawing.Size(75, 23);
-            this.removeButton.TabIndex = 1;
-            this.removeButton.Text = "&Remove";
             this.removeButton.UseVisualStyleBackColor = true;
             this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(517, 46);
-            this.label1.TabIndex = 2;
-            this.label1.Text = resources.GetString("label1.Text");
             // 
             // closeButton
             // 
-            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.closeButton, "closeButton");
             this.closeButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.closeButton.Location = new System.Drawing.Point(454, 280);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(75, 23);
-            this.closeButton.TabIndex = 3;
-            this.closeButton.Text = "Close";
             this.closeButton.UseVisualStyleBackColor = true;
             // 
             // SvnAuthenticationCacheEditor
             // 
             this.AcceptButton = this.closeButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            resources.ApplyResources(this, "$this");
             this.CancelButton = this.closeButton;
-            this.ClientSize = new System.Drawing.Size(541, 316);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Name = "SvnAuthenticationCacheEditor";
-            this.Text = "Stored Credentials";
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
