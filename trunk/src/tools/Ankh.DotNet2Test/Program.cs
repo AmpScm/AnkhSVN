617c98c603ed1e76e72b5f1b480dc974eca53902
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2011-07-14 09:34:51 +0000

    Add a test project that loads all resources and constructs controls to make sure they can be loaded in .Net 2.0. This should catch some errors introduced by editting AnkhSVN with Visual Studio 2010.
    * src/AnkhSvn.2008.sln
    * src/tools/Ankh.DotNet2Test/Ankh.DotNet2Test.csproj
    * src/tools/Ankh.DotNet2Test/Program.cs
    * src/tools/Ankh.DotNet2Test/Properties/AssemblyInfo.cs
      Add new files

---
+++ trunk/src/tools/Ankh.DotNet2Test/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Resources;
+using System.ComponentModel;
+
+namespace Ankh.DotNet2Test
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Dictionary<string, Assembly> handled = new Dictionary<string, Assembly>();
+            VerifyCompatibility(handled, typeof(Ankh.VSPackage.AnkhSvnPackage).Assembly);
+        }
+
+        private static void VerifyCompatibility(Dictionary<string, Assembly> handled, Assembly asm)
+        {
+            AssemblyName name = new AssemblyName(asm.FullName);
+            handled.Add(name.Name, asm);
+
+            Console.WriteLine("Verifying assembly {0}", name.Name);
+
+            foreach(string resourceName in asm.GetManifestResourceNames())
+            {
+                if (!".resources".Equals(Path.GetExtension(resourceName)))
+                    continue;
+
+                Console.WriteLine("  Loading resource {0}", Path.GetFileNameWithoutExtension(resourceName));
+                using(Stream s = asm.GetManifestResourceStream(resourceName))
+                using (ResourceReader rr = new ResourceReader(s))
+                {
+                    foreach (object o in rr)
+                    {
+                        GC.KeepAlive(o);
+                    }
+                }
+            }
+
+            Type[] assemblyTypes = null;
+
+            try
+            {
+                assemblyTypes = asm.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                foreach (Exception ee in e.LoaderExceptions)
+                {
+                    Console.WriteLine(ee.ToString());
+                }
+            }
+
+            if (assemblyTypes != null)
+            {
+                foreach (Type t in assemblyTypes)
+                {
+                    if (!typeof(IComponent).IsAssignableFrom(t))
+                        continue;
+                    else if (t.ContainsGenericParameters)
+                        continue;
+                    else if (null == t.GetConstructor(BindingFlags.Instance | BindingFlags.Public | BindingFlags.CreateInstance | BindingFlags.ExactBinding, null, new Type[0], null))
+                        continue;
+
+                    Console.WriteLine("  Instantiating {0}", t.FullName);
+                    Activator.CreateInstance(t);
+                }
+            }
+
+            foreach (AssemblyName an in asm.GetReferencedAssemblies())
+            {
+                if (!an.Name.StartsWith("Ankh."))
+                    continue;
+                else if (handled.ContainsKey(an.Name))
+                    continue;
+
+                Assembly a = Assembly.Load(an);
+                VerifyCompatibility(handled, a);
+            }
+        }
+    }
+}
---
+++ trunk/src/tools/Ankh.DotNet2Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("Ankh.DotNet2Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Ankh.DotNet2Test")]
+[assembly: AssemblyCopyright("Copyright © AnkhSVN Project 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cdf8840a-01df-4f4a-8514-f9e964e59e9a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
