a5c9a24127db9040e0ea08f733ba9f5d1d5a0c26
Bert Huijben <rhuijben@users.noreply.github.com>
2011-12-21 15:38:00 +0000

    * src/Ankh.UI/SvnInfoGrid/InfoPropertyGrid.cs * src/Ankh.UI/SvnInfoGrid/SvnInfoGridControl.cs   Remove toolstrip rendering handling as we don't use the toolstrip.   (We use a VS toolbar instead)

--- src/Ankh.UI/SvnInfoGrid/InfoPropertyGrid.cs
+++ src/Ankh.UI/SvnInfoGrid/InfoPropertyGrid.cs
@@ -8,11 +8,5 @@ namespace Ankh.UI.SvnInfoGrid
 {
     class InfoPropertyGrid : PropertyGrid
     {
-        [Localizable(false), Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public new ToolStripRenderer ToolStripRenderer
-        {
-            get { return base.ToolStripRenderer; }
-            set { base.ToolStripRenderer = value; }
-        }
     }
 }
