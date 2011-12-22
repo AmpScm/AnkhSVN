2a15050de2e19e9d525fea06d7ad986a18e477f8
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2011-12-22 19:14:59 +0000

    When the pending changes view is in vertical mode don't display the spacer before the commit button.

---
+++ trunk/src/Ankh.UI/PendingChanges/Commands/SpacerHider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ankh.Commands;
+
+namespace Ankh.UI.PendingChanges.Commands
+{
+    [Command(AnkhCommand.PendingChangesSpacer, AlwaysAvailable=true, HideWhenDisabled=false)]
+    sealed class SpacerHider : ICommandHandler
+    {
+        public void OnUpdate(CommandUpdateEventArgs e)
+        {
+            e.Enabled = false;
+            if (PendingChangesToolControl.ShownVertically)
+                e.Visible = false;
+        }
+
+        public void OnExecute(CommandEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
