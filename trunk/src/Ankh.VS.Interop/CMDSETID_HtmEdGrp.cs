6040b2756ef0a320e8b06ae2335e2229e1d112e4
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2012-01-26 15:15:36 +0000

    Add the Subversion context menu to all the editor context menus of the html editor package.
    * src/Ankh.Package/Gui/AnkhSvn.xml
      Import Ids and use those to include subversion item.
    
    * src/Ankh.VS.Interop/Ankh.VS.Interop.csproj
    * src/Ankh.VS.Interop/CMDSETID_HtmEdGrp.cs
      Add helper class to add Ids that aren't part of the SDK.
      (Thanks VSCT Powertoy)

---
+++ trunk/src/Ankh.VS.Interop/CMDSETID_HtmEdGrp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Ankh
+{
+    [Guid("D7E8C5E1-BDB8-11d0-9C88-0000F8040A53")]
+    public enum CMDSETID_HtmEdGrp
+    {
+        IDMX_HTM_DESIGN_BASE = 0x30,
+        IDMX_HTM_DESIGN_TABLE = 0x31,
+        IDMX_HTM_SOURCE_BASIC = 0x32,
+        IDMX_HTM_SOURCE_HTML = 0x33,
+        IDMX_HTM_SOURCE_SCRIPT = 0x34,
+        IDMX_HTM_SOURCE_ASPX = 0x35,
+        IDMX_HTM_SOURCE_ASPX_CODE = 0x36,
+        IDMX_HTM_SOURCE_ASPX_CODE_VB = 0x37,
+        IDMX_HTM_SOURCE_ASMX = 0x38,
+        IDMX_HTM_SOURCE_ASMX_CODE = 0x39,
+        IDMX_HTM_SOURCE_ASMX_CODE_VB = 0x3A,
+        IDMX_HTM_SOURCE_ASAX = 0x3B,
+        IDMX_HTM_SOURCE_ASAX_CODE = 0x3C,
+        IDMX_HTM_SOURCE_ASAX_CODE_VB = 0x3D,
+    }
+}
