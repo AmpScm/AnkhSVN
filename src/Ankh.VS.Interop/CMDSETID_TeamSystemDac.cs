387b1f839808012fa46a846ce126835ae82d4cdf
Bert Huijben <rhuijben@users.noreply.github.com>
2012-01-26 15:32:38 +0000

    Following up on r10742, perform the same trick on the sql editor window.
    * src/Ankh.Package/Gui/AnkhSvn.xml
      Define Subversion menu in sql editor.
    
    * src/Ankh.VS.Interop/Ankh.VS.Interop.csproj
    * src/Ankh.VS.Interop/CMDSETID_TeamSystemDac.cs
      Add necessary definitions.
    
    * src/Ankh.VS.Interop/CMDSETID_HtmEdGrp.cs
      Remove unneeded using.

---
+++ src/Ankh.VS.Interop/CMDSETID_TeamSystemDac.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Ankh
+{
+    [Guid("09e21035-245e-4ffe-ae74-105773c29cc7")]
+    public enum CMDSETID_TeamSystemDac
+    {
+        mnuIdTeamSystemSqlEditorContext = 0x5200,
+    }
+}
