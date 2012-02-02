598ef3132b8f0a60a5e2c5c06bc599dd55f5c10b
Bert Huijben <rhuijben@users.noreply.github.com>
2012-02-02 19:49:16 +0000

    * src/Ankh.ExtensionPoints/Ankh.ExtensionPoints.csproj * src/Ankh.ExtensionPoints/Items/ISvnUriItem.cs   Add ISvnUriItem.

---
+++ src/Ankh.ExtensionPoints/Items/ISvnUriItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ankh.ExtensionPoints.Items
+{
+    /// <summary>
+    /// Interface to allow Subversion Info integration
+    /// </summary>
+    public interface ISvnUriItem
+    {
+        /// <summary>
+        /// The absolute path to the item on disk or <c>null</c> if the item has no local representation
+        /// </summary>
+        /// <remarks>Can be <c>null</c> if <see cref="Uri"/> is not null</remarks>
+        string FullPath { get; }
+
+        /// <summary>
+        /// The uri to the item in the repository
+        /// </summary>
+        /// <remarks>Can be <c>null</c> if <see cref="FullPath"/> is a valid path</remarks>
+        Uri Uri { get; }
+
+        /// <summary>
+        /// The repository root of <see cref="Uri"/> or <c>null</c>
+        /// </summary>
+        /// <remarks>It's better to provide <c>null</c> than a potentially invalid value</remarks>
+        Uri RepositoryRoot { get; }
+    }
+}
