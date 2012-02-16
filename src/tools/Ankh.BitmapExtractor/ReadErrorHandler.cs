0ba1fde3169ac210cfc1e49f458c28ef1603614d
Bert Huijben <rhuijben@users.noreply.github.com>
2012-02-16 17:16:02 +0000

    * src/AnkhSvn.2011.sln   Add a simple command bitmap extractor tool to allow extracting menu icons   from resource assemblies that contain a .cto resource.
    * src/tools/Ankh.BitmapExtractor/Ankh.BitmapExtractor.csproj
    * src/tools/Ankh.BitmapExtractor/App.config
    * src/tools/Ankh.BitmapExtractor/Program.cs
    * src/tools/Ankh.BitmapExtractor/Properties/AssemblyInfo.cs
    * src/tools/Ankh.BitmapExtractor/ReadErrorHandler.cs
      Add simple tool.

---
+++ src/tools/Ankh.BitmapExtractor/ReadErrorHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.CommandTable;
+
+namespace Ankh.BitmapExtractor
+{
+    class ReadErrorHandler : IMessageProcessor
+    {
+        public void Dependency(string file)
+        {
+            Console.WriteLine(" Dependency {0}", file);
+        }
+
+        public void Error(int error, string file, int line, int pos, string message)
+        {
+            Console.Error.WriteLine(" {2}({3},{4}: Error {0}: {5}", error, file, line, pos, message);
+        }
+
+        public bool VerboseOutput()
+        {
+            return true;
+        }
+
+        public void Warning(int error, string file, int line, int pos, string message)
+        {
+            Console.Error.WriteLine(" {2}({3},{4}: Warning {0}: {5}", error, file, line, pos, message);
+        }
+
+        public void WriteLine(string format, params object[] arg)
+        {
+            Console.WriteLine(" " + format, arg);
+        }
+    }
+}
