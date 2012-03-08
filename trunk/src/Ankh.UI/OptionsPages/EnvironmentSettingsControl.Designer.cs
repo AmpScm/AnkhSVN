0b6662d8a761504d7363ece7e3df7331fa935b68
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2012-03-08 20:53:07 +0000

    Add a checkbox in the settings control for enabling the client-side hook support.
    * src/Ankh.UI/OptionsPages/EnvironmentSettingsControl.cs
    * src/Ankh.UI/OptionsPages/EnvironmentSettingsControl.Designer.cs
    * src/Ankh.UI/OptionsPages/EnvironmentSettingsControl.resx
      Reposition controls and add boolean for client-side hooks.

--- trunk/src/Ankh.UI/OptionsPages/EnvironmentSettingsControl.Designer.cs
+++ trunk/src/Ankh.UI/OptionsPages/EnvironmentSettingsControl.Designer.cs
@@ -44,110 +44,118 @@ namespace Ankh.UI.OptionsPages
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnvironmentSettingsControl));
-			this.authenticationEdit = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.proxyEdit = new System.Windows.Forms.Button();
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.interactiveMergeOnConflict = new System.Windows.Forms.CheckBox();
-			this.autoAddFiles = new System.Windows.Forms.CheckBox();
-			this.flashWindowAfterOperation = new System.Windows.Forms.CheckBox();
-			this.autoLockFiles = new System.Windows.Forms.CheckBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.pcDefaultDoubleClick = new System.Windows.Forms.ComboBox();
-			this.groupBox1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// authenticationEdit
-			// 
-			resources.ApplyResources(this.authenticationEdit, "authenticationEdit");
-			this.authenticationEdit.Name = "authenticationEdit";
-			this.authenticationEdit.UseVisualStyleBackColor = true;
-			this.authenticationEdit.Click += new System.EventHandler(this.authenticationEdit_Click);
-			// 
-			// label1
-			// 
-			resources.ApplyResources(this.label1, "label1");
-			this.label1.Name = "label1";
-			// 
-			// label2
-			// 
-			resources.ApplyResources(this.label2, "label2");
-			this.label2.Name = "label2";
-			// 
-			// proxyEdit
-			// 
-			resources.ApplyResources(this.proxyEdit, "proxyEdit");
-			this.proxyEdit.Name = "proxyEdit";
-			this.proxyEdit.UseVisualStyleBackColor = true;
-			this.proxyEdit.Click += new System.EventHandler(this.proxyEdit_Click);
-			// 
-			// groupBox1
-			// 
-			resources.ApplyResources(this.groupBox1, "groupBox1");
-			this.groupBox1.Controls.Add(this.label2);
-			this.groupBox1.Controls.Add(this.proxyEdit);
-			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Controls.Add(this.authenticationEdit);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.TabStop = false;
-			// 
-			// interactiveMergeOnConflict
-			// 
-			resources.ApplyResources(this.interactiveMergeOnConflict, "interactiveMergeOnConflict");
-			this.interactiveMergeOnConflict.Name = "interactiveMergeOnConflict";
-			this.interactiveMergeOnConflict.UseVisualStyleBackColor = true;
-			// 
-			// autoAddFiles
-			// 
-			resources.ApplyResources(this.autoAddFiles, "autoAddFiles");
-			this.autoAddFiles.Name = "autoAddFiles";
-			this.autoAddFiles.UseVisualStyleBackColor = true;
-			// 
-			// flashWindowAfterOperation
-			// 
-			resources.ApplyResources(this.flashWindowAfterOperation, "flashWindowAfterOperation");
-			this.flashWindowAfterOperation.Name = "flashWindowAfterOperation";
-			this.flashWindowAfterOperation.UseVisualStyleBackColor = true;
-			// 
-			// autoLockFiles
-			// 
-			resources.ApplyResources(this.autoLockFiles, "autoLockFiles");
-			this.autoLockFiles.Name = "autoLockFiles";
-			this.autoLockFiles.UseVisualStyleBackColor = true;
-			// 
-			// label3
-			// 
-			resources.ApplyResources(this.label3, "label3");
-			this.label3.Name = "label3";
-			// 
-			// pcDefaultDoubleClick
-			// 
-			resources.ApplyResources(this.pcDefaultDoubleClick, "pcDefaultDoubleClick");
-			this.pcDefaultDoubleClick.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.pcDefaultDoubleClick.FormattingEnabled = true;
-			this.pcDefaultDoubleClick.Items.AddRange(new object[] {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnvironmentSettingsControl));
+            this.authenticationEdit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.proxyEdit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.interactiveMergeOnConflict = new System.Windows.Forms.CheckBox();
+            this.autoAddFiles = new System.Windows.Forms.CheckBox();
+            this.flashWindowAfterOperation = new System.Windows.Forms.CheckBox();
+            this.autoLockFiles = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pcDefaultDoubleClick = new System.Windows.Forms.ComboBox();
+            this.clientSideHooks = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // authenticationEdit
+            // 
+            resources.ApplyResources(this.authenticationEdit, "authenticationEdit");
+            this.authenticationEdit.Name = "authenticationEdit";
+            this.authenticationEdit.UseVisualStyleBackColor = true;
+            this.authenticationEdit.Click += new System.EventHandler(this.authenticationEdit_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // proxyEdit
+            // 
+            resources.ApplyResources(this.proxyEdit, "proxyEdit");
+            this.proxyEdit.Name = "proxyEdit";
+            this.proxyEdit.UseVisualStyleBackColor = true;
+            this.proxyEdit.Click += new System.EventHandler(this.proxyEdit_Click);
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.clientSideHooks);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.proxyEdit);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.authenticationEdit);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // interactiveMergeOnConflict
+            // 
+            resources.ApplyResources(this.interactiveMergeOnConflict, "interactiveMergeOnConflict");
+            this.interactiveMergeOnConflict.Name = "interactiveMergeOnConflict";
+            this.interactiveMergeOnConflict.UseVisualStyleBackColor = true;
+            // 
+            // autoAddFiles
+            // 
+            resources.ApplyResources(this.autoAddFiles, "autoAddFiles");
+            this.autoAddFiles.Name = "autoAddFiles";
+            this.autoAddFiles.UseVisualStyleBackColor = true;
+            // 
+            // flashWindowAfterOperation
+            // 
+            resources.ApplyResources(this.flashWindowAfterOperation, "flashWindowAfterOperation");
+            this.flashWindowAfterOperation.Name = "flashWindowAfterOperation";
+            this.flashWindowAfterOperation.UseVisualStyleBackColor = true;
+            // 
+            // autoLockFiles
+            // 
+            resources.ApplyResources(this.autoLockFiles, "autoLockFiles");
+            this.autoLockFiles.Name = "autoLockFiles";
+            this.autoLockFiles.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // pcDefaultDoubleClick
+            // 
+            resources.ApplyResources(this.pcDefaultDoubleClick, "pcDefaultDoubleClick");
+            this.pcDefaultDoubleClick.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.pcDefaultDoubleClick.FormattingEnabled = true;
+            this.pcDefaultDoubleClick.Items.AddRange(new object[] {
             resources.GetString("pcDefaultDoubleClick.Items"),
             resources.GetString("pcDefaultDoubleClick.Items1")});
-			this.pcDefaultDoubleClick.Name = "pcDefaultDoubleClick";
-			// 
-			// EnvironmentSettingsControl
-			// 
-			resources.ApplyResources(this, "$this");
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.autoAddFiles);
-			this.Controls.Add(this.autoLockFiles);
-			this.Controls.Add(this.interactiveMergeOnConflict);
-			this.Controls.Add(this.flashWindowAfterOperation);
-			this.Controls.Add(this.groupBox1);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.pcDefaultDoubleClick);
-			this.Name = "EnvironmentSettingsControl";
-			this.groupBox1.ResumeLayout(false);
-			this.groupBox1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.pcDefaultDoubleClick.Name = "pcDefaultDoubleClick";
+            // 
+            // clientSideHooks
+            // 
+            resources.ApplyResources(this.clientSideHooks, "clientSideHooks");
+            this.clientSideHooks.Name = "clientSideHooks";
+            this.clientSideHooks.UseVisualStyleBackColor = true;
+            // 
+            // EnvironmentSettingsControl
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.autoAddFiles);
+            this.Controls.Add(this.autoLockFiles);
+            this.Controls.Add(this.interactiveMergeOnConflict);
+            this.Controls.Add(this.flashWindowAfterOperation);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pcDefaultDoubleClick);
+            this.Name = "EnvironmentSettingsControl";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -164,5 +172,6 @@ namespace Ankh.UI.OptionsPages
         private System.Windows.Forms.CheckBox autoLockFiles;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox pcDefaultDoubleClick;
+        private System.Windows.Forms.CheckBox clientSideHooks;
     }
 }
