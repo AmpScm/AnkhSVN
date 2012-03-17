2b1df9bf8d0a7d156a3cb3224995b18384ce26bd
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2012-03-17 11:06:50 +0000

    Checkpoint the code to enable the navigation bar of the Annotate Editor in Visual Studio 2010 and later. This might break compilation on Visual Studio 2010.
    * src/Ankh.Services/Ankh.Services.csproj
    * src/Ankh.Services/VS/IGetWpfEditorInfo.cs
      Add wpf editor service to hide VS2010+ details from VS2005, VS2008 compatible
      code.
    
    * src/Ankh.UI/Annotate/AnnotateEditorControl.cs
      Always enable the header.
    
    * src/Ankh.VS/TextEditor/TheVSTextEditor.cs
      Call the helper service if available.
    
    * src/Ankh.WpfEditor.Package/Services/WpfEditorInfo.cs
      Add helper service to VS2010+ code.

---
+++ trunk/src/Ankh.Services/VS/IGetWpfEditorInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace Ankh.VS
+{
+    public interface IGetWpfEditorInfo
+    {
+        WpfEditorInfo GetWpfInfo(IVsTextView textView);
+    }
+
+    public abstract class WpfEditorInfo
+    {
+        public abstract Point GetTopLeft();
+
+        public abstract int GetLineHeight();
+    }
+}
---
+++ trunk/src/Ankh.WpfEditor.Package/Services/WpfEditorInfo.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using SystemPoint = System.Drawing.Point;
+using System.Windows.Media;
+using Ankh.VS;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Ankh.WpfPackage.Services
+{
+    [GlobalService(typeof(IGetWpfEditorInfo))]
+    sealed class WpfEditorInfoService : AnkhService, IGetWpfEditorInfo
+    {
+        public WpfEditorInfoService(IAnkhServiceProvider context)
+            : base(context)
+        {
+        }
+
+        sealed class TheWpfEditorInfo : WpfEditorInfo
+        {
+            readonly IAnkhServiceProvider _context;
+            readonly IComponentModel _componentModel;
+            readonly IVsEditorAdaptersFactoryService _adapterFactory;
+            readonly IVsTextView _textView;
+
+            public TheWpfEditorInfo(IAnkhServiceProvider context, IComponentModel componentModel, IVsEditorAdaptersFactoryService adapterFactory, IVsTextView textView)
+            {
+                if (context == null)
+                    throw new ArgumentNullException("context");
+                else if (componentModel == null)
+                    throw new ArgumentNullException("componentModel");
+                else if (adapterFactory == null)
+                    throw new ArgumentNullException("adapterFactory");
+                else if (textView == null)
+                    throw new ArgumentNullException("textView");
+
+                _context = context;
+                _componentModel = componentModel;
+                _textView = textView;
+                _adapterFactory = adapterFactory;
+            }
+
+            IWpfTextView _wpfTextView;
+            PresentationSource _source;
+
+            IWpfTextView WpfTextView
+            {
+                get
+                {
+                    Refresh();
+
+                    return _wpfTextView;
+                }
+            }
+
+            public PresentationSource Source
+            {
+                get
+                {
+                    Refresh();
+
+                    return _source;
+                }
+            }
+
+            void Refresh()
+            {
+                if (_wpfTextView != null && !_wpfTextView.IsClosed)
+                    return;
+
+                _wpfTextView = null;
+                _source = null;
+                    
+                IWpfTextView wpfTextView = _adapterFactory.GetWpfTextView(_textView);
+                if (wpfTextView.IsClosed)
+                    return;
+
+                _wpfTextView = wpfTextView;
+                _source = PresentationSource.FromVisual(wpfTextView.VisualElement);
+            }
+
+            public override System.Drawing.Point GetTopLeft()
+            {
+                IWpfTextView textView = WpfTextView;
+                PresentationSource source = Source;
+
+                if (textView == null || source == null)
+                    return new SystemPoint();
+
+                FrameworkElement el = textView.VisualElement;
+
+                GeneralTransform toRoot = el.TransformToAncestor(source.RootVisual);
+
+                Point p = toRoot.Transform(new Point(0, 0));
+
+                return new SystemPoint((int)p.X, (int)p.Y);
+            }
+
+            public override int GetLineHeight()
+            {
+                IWpfTextView wpfTextView = WpfTextView;
+
+                if (wpfTextView != null)
+                    return (int)wpfTextView.LineHeight;
+
+                return 1;
+            }
+        }
+
+        public WpfEditorInfo GetWpfInfo(IVsTextView textView)
+        {
+            if (textView == null)
+                throw new ArgumentNullException("textView");
+
+            IComponentModel componentModel = GetService<IComponentModel>(typeof(SComponentModel));
+            IVsEditorAdaptersFactoryService factory = componentModel.GetService<IVsEditorAdaptersFactoryService>();
+
+            if (factory != null)
+            {
+                if (factory.GetWpfTextView(textView) != null)
+                    return new TheWpfEditorInfo(this, componentModel, factory, textView);
+            }
+
+            return null;
+        }
+    }
+
+}
