5d7990d4b88b73337ce35f9baccbffbf59a4e27b
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2012-03-29 13:53:29 +0000

    Following up on r11043, add threaded wait dialog support for VS2005/8.
    * src/Ankh.VS/Ankh.VS.csproj
    * src/Ankh.VS/Diff
    * src/Ankh.VS/Diff/InternalDiff.cs
    * src/Ankh.VS/Services
    * src/Ankh.VS/Services/InternalDiff.cs
    * src/Ankh.VS/Services/ThreadedWaitService.cs
      Rename namespace and add similar VS2005 compatible service which
      handles the 2 second timeout itself.

---
+++ trunk/src/Ankh.VS/Services/ThreadedWaitService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ankh.UI;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Ankh.VS.Services
+{
+    [GlobalService(typeof(IAnkhThreadedWaitService), MaxVersion=VSInstance.VS2008)]
+    sealed class ThreadedWaitService : AnkhService, IAnkhThreadedWaitService
+    {
+        public ThreadedWaitService(IAnkhServiceProvider context)
+            : base(context)
+        {
+        }
+
+        sealed class ThreadedWaitWrapper : IAnkhThreadedWaitDialog
+        {
+            readonly DateTime _start;
+            readonly IAnkhServiceProvider _context;
+            string _caption;
+            string _message;
+            IVsThreadedWaitDialog _dlg;
+
+            public ThreadedWaitWrapper(IAnkhServiceProvider context, string caption, string message)
+            {
+                if (context == null)
+                    throw new ArgumentNullException("context");
+
+                _context = context;
+                _caption = caption;
+                _message = message;
+                _start = DateTime.UtcNow + new TimeSpan(0,0,2);
+            }
+
+            public void Tick()
+            {
+                if (_dlg == null)
+                {
+                    if (_start > DateTime.UtcNow)
+                        return;
+
+                    IVsThreadedWaitDialog dlg = _context.GetService<IVsThreadedWaitDialog>(typeof(SVsThreadedWaitDialog));
+
+                    if (dlg != null)
+                    {
+                        if (ErrorHandler.Succeeded(dlg.StartWaitDialog(_caption, _message, null, (uint)__VSTWDFLAGS.VSTWDFLAGS_TOPMOST, null, null)))
+                            _dlg = dlg;
+                    }
+                }
+                else
+                {
+                    int canceled;
+                    _dlg.GiveTimeSlice(null, null, 0, out canceled);
+                }
+            }
+
+            public void Dispose()
+            {
+                
+                if (_dlg != null)
+                    try
+                    {
+                        int canceled = 0;
+                        _dlg.EndWaitDialog(ref canceled);
+                    }
+                    finally
+                    {
+                        _dlg = null;
+                    }
+            }
+        }
+
+        public Ankh.UI.IAnkhThreadedWaitDialog Start(string caption, string message)
+        {
+            return new ThreadedWaitWrapper(this, caption, message);
+        }
+    }
+}
