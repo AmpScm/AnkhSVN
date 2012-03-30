f25e34638380a807cd3dd3e451f4d7c7663d8f93
Bert Huijben <rhuijben@users.noreply.github.com>
2012-03-30 12:45:02 +0000

    * src/Ankh.Services/VS/IGetWpfEditorInfo.cs   Resolve compiletime warning.

--- src/Ankh.Services/VS/IGetWpfEditorInfo.cs
+++ src/Ankh.Services/VS/IGetWpfEditorInfo.cs
@@ -6,6 +6,7 @@ using Microsoft.VisualStudio.TextManager.Interop;
 
 namespace Ankh.VS
 {
+    [CLSCompliant(false)]
     public interface IGetWpfEditorInfo
     {
         WpfEditorInfo GetWpfInfo(IVsTextView textView);
