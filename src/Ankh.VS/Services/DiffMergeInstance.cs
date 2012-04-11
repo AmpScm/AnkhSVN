9f5f078cea3b63f95e699a9099efdd7c62ee01a0
Bert Huijben <rhuijben@users.noreply.github.com>
2012-04-11 10:49:58 +0000

    * src/Ankh.VS/Services/DiffMergeInstance.cs   Remove unused usings to unbreak the (.Net 2.0) build.

--- src/Ankh.VS/Services/DiffMergeInstance.cs
+++ src/Ankh.VS/Services/DiffMergeInstance.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell.Interop;
 using UnregisterHandler = Ankh.VS.Services.InternalDiff.Merge_UnregisterMergeWindow;
