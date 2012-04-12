080a321202b683f9a210a02112cce1281d9fde91
Bert Huijben <rhuijben@users.noreply.github.com>
2012-04-12 11:54:21 +0000

    Allow registering some per project type flags to use a common system to work around glyph update bugs.
    * src/Ankh.Package/AnkhSvnPackage.Scc.cs
    * src/Ankh.Package/Attributes/ProvideProjectTypeSettings.cs
      Provide a project type flag for the SSIS project.

---
+++ src/Ankh.Package/AnkhSvnPackage.Scc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ankh.VSPackage.Attributes;
+using Ankh.Scc.ProjectMap;
+
+namespace Ankh.VSPackage
+{
+	[ProvideProjectTypeSettings("{d2abab84-bf74-430a-b69e-9dc6d40dda17}", SccProjectFlags.ForceSccGlyphChange)]
+	partial class AnkhSvnPackage
+	{
+	}
+}
---
+++ src/Ankh.Package/Attributes/ProvideProjectTypeSettings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Shell;
+using Ankh.Scc.ProjectMap;
+
+namespace Ankh.VSPackage.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+	sealed class ProvideProjectTypeSettingsAttribute : RegistrationAttribute
+	{
+		readonly Guid _projectType;
+		SccProjectFlags _flags;
+
+		public ProvideProjectTypeSettingsAttribute(string projectType, SccProjectFlags flags)
+		{
+			_projectType = new Guid(projectType);
+			_flags = flags;
+		}
+
+		public Guid ProjectType
+		{
+			get { return _projectType; }
+		}
+
+		string KeyName
+		{
+			get { return string.Format("Extensions\\AnkhSVN\\ProjectHandling\\{0}", ProjectType.ToString("B")); }
+		}
+
+		public override void Register(RegistrationAttribute.RegistrationContext context)
+		{
+			using (Key key = context.CreateKey(KeyName))
+			{
+				key.SetValue("flags", (int)_flags);
+			}
+		}
+
+		public override void Unregister(RegistrationAttribute.RegistrationContext context)
+		{
+			context.RemoveKey(KeyName);
+		}
+	}
+}
