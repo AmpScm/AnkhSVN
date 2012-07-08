dbabdc666bf3a1de799f6aa2649a15becbf2c190
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2012-07-08 11:24:21 +0000

    Remove Linq using

--- trunk/src/Ankh.Package/Attributes/ProvideThemeInformation.cs
+++ trunk/src/Ankh.Package/Attributes/ProvideThemeInformation.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.VisualStudio.Shell;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
 
 namespace Ankh.VSPackage.Attributes
 {
