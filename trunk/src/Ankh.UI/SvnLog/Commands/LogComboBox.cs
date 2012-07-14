13fa9049ee7be6d0418f9f3d74eebe098c731368
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2012-07-14 18:03:13 +0000

    Enable the log viewer combo box. Selecting an item restarts the log for the specific target.
    * src/Ankh.Package/Gui/AnkhSvn.xml
      Use a smaller default
    
    * src/Ankh.UI/SvnLog/Commands/LogComboBox.cs
      Add implementation.
    
    * src/Ankh.UI/SvnLog/LogToolWindowControl.cs
      Provide current revision range.

--- trunk/src/Ankh.UI/SvnLog/Commands/LogComboBox.cs
+++ trunk/src/Ankh.UI/SvnLog/Commands/LogComboBox.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Ankh.Commands;
+using Ankh.Scc;
+using Ankh.UI.SvnInfoGrid;
 
 namespace Ankh.UI.SvnLog.Commands
 {
@@ -16,20 +18,77 @@ namespace Ankh.UI.SvnLog.Commands
             if (_ctrl == null)
                 _ctrl = e.GetService<LogToolWindowControl>();
 
-            //if (_ctrl == null)
+            if (_ctrl == null)
             {
                 e.Enabled = e.Visible = false;
+                return;
             }
+
+            IList<SvnOrigin> origins = _ctrl.Origins;
+            if (origins == null || origins.Count == 0)
+            {
+                e.Enabled = e.Visible = false;
+                return;
+            }
+
+            // Enable something like this?
+            /*if (e.Command == AnkhCommand.SvnLogComboBox
+                && origins.Count == 1)
+            {
+                e.Enabled = false;
+            }*/
+        }
+
+        object _lastOrigins;
+        string[] _lastNames;
+        string[] GetNames()
+        {
+            IList<SvnOrigin> origins = _ctrl.Origins;
+            if (ReferenceEquals(_lastOrigins, origins))
+                return _lastNames;
+
+            List<string> names = new List<string>();
+
+            foreach (SvnOrigin o in origins)
+            {
+                names.Add(o.Target.FileName);
+            }
+
+            _lastOrigins = origins;
+            return _lastNames = names.ToArray();
         }
 
         public void OnExecute(CommandEventArgs e)
         {
             if (e.Command == AnkhCommand.SvnLogComboBoxFill)
             {
-                e.Result = new string[] { "1 item" };
+                e.Result = GetNames();
                 return;
             }
-            
+
+            IList<SvnOrigin> origins = _ctrl.Origins;
+
+            if (origins == null || origins.Count == 0)
+                return;
+
+            if (e.Argument != null)
+            {
+                string selected = e.Argument as string;
+
+                if (selected != null)
+                    foreach (SvnOrigin o in origins)
+                    {
+                        if (selected == o.Target.FileName)
+                        {
+                            _ctrl.StartLog(o, _ctrl.LastStartRevision, _ctrl.LastEndRevision);
+                            return;
+                        }
+                    }
+            }
+            else if (origins.Count > 1)
+                e.Result = string.Format(SvnInfoStrings.NrOfFileInfo, origins.Count);
+            else if (origins.Count == 1)
+                e.Result = _ctrl.Origins[0].Target.FileName;
         }
     }
 }
