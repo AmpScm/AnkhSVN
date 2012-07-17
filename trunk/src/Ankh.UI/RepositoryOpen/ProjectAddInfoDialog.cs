9491687fcb6fcba718457fd1236e62f2ea75290d
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2012-07-17 20:35:56 +0000

    Enable several options for how to add an existing project.
    * src/Ankh.UI/RepositoryOpen/ProjectAddInfoDialog.cs
    * src/Ankh.UI/RepositoryOpen/ProjectAddInfoDialog.Designer.cs
    * src/Ankh.UI/RepositoryOpen/ProjectAddInfoDialog.resx
    * src/Ankh/Commands/OpenFromSubversion.cs
      Enable External, Copy, Unconnected and Unversioned options.

--- trunk/src/Ankh.UI/RepositoryOpen/ProjectAddInfoDialog.cs
+++ trunk/src/Ankh.UI/RepositoryOpen/ProjectAddInfoDialog.cs
@@ -10,6 +10,14 @@ using System.Windows.Forms;
 
 namespace Ankh.UI.RepositoryOpen
 {
+    public enum ProjectAddMode
+    {
+        External,
+        Copy,
+        SlnConnection,
+        Unconnected,
+        Unversioned
+    }
     public partial class ProjectAddInfoDialog : VSDialogForm
     {
         public ProjectAddInfoDialog()
@@ -20,6 +28,94 @@ namespace Ankh.UI.RepositoryOpen
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        public bool EnableExternal 
+        {
+            get { return externalRadio.Enabled; }
+            set
+            {
+                externalRadio.Enabled = value;
+                externalBlock.Enabled = value;
+                if (externalRadio.Checked && !value)
+                {
+                    if (EnableCopy)
+                        copyRadio.Checked = true;
+                    else
+                        unconnectedRadio.Checked = true;
+                }
+                externalDefInfo.Visible =!value;
+            }
+        }
+
+        public bool EnableCopy
+        {
+            get { return copyRadio.Enabled; }
+            set
+            {
+                copyRadio.Enabled = value;
+                copyBlock.Enabled = value;
+                if (copyRadio.Checked && !value)
+                {
+                    if (EnableExternal)
+                        externalRadio.Checked = true;
+                    else
+                        unconnectedRadio.Checked = true;
+                }
+                copyDefInfo.Visible = !value;
+            }
+        }
+
+        public bool EnableSlnConnection
+        {
+            get { return slnRadio.Enabled; }
+            set
+            {
+                slnRadio.Enabled = value;
+                slnBlock.Enabled = value;
+                if (slnRadio.Checked)
+                    unconnectedRadio.Checked = true;
+            }
+        }
+
+        public string ExternalLocation
+        {
+            get { return (string)externalPropLocations.SelectedItem; }
+        }
+
+        public bool ExternalLocked
+        {
+            get { return lockExternal.Checked; }
+        }
+
+        public void SetExternalDirs(IEnumerable<string> dirs)
+        {
+            externalPropLocations.Items.Clear();
+            foreach (string path in dirs)
+            {
+                externalPropLocations.Items.Add(path);
+            }
+            if (externalPropLocations.Items.Count > 0)
+                externalPropLocations.SelectedIndex = 0;
+        }
+
+        public ProjectAddMode SelectedMode
+        {
+            get
+            {
+                if (externalRadio.Checked)
+                    return ProjectAddMode.External;
+                if (copyRadio.Checked)
+                    return ProjectAddMode.Copy;
+                if (slnRadio.Checked)
+                    return ProjectAddMode.SlnConnection;
+                if (unconnectedRadio.Checked)
+                    return ProjectAddMode.Unconnected;
+                if (notVersionedRadio.Checked)
+                    return ProjectAddMode.Unversioned;
+
+                return ProjectAddMode.Unconnected;
+            }
         }
     }
 }
