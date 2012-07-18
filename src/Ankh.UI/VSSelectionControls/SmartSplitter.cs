777fb650cd629a52e72d218456176a0d4bd40ce4
Bert Huijben <rhuijben@users.noreply.github.com>
2012-07-18 17:59:50 +0000

    * src/Ankh.Services/UI/VSDialogForm.cs * src/Ankh.UI/VSSelectionControls/SmartSplitter.cs   In our dialogs handle Ctrl+(Shift+)Tab as (Shift+)Tab and Ctrl+Return   as Return (=click default button)

--- src/Ankh.UI/VSSelectionControls/SmartSplitter.cs
+++ src/Ankh.UI/VSSelectionControls/SmartSplitter.cs
@@ -24,5 +24,20 @@ namespace Ankh.UI.VSSelectionControls
                 GetType().InvokeMember("EndInit", BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance, null, this, null);
             }
         }
+
+        // Handle Ctrl+(Shift+)Tab and Ctrl+Return as without Ctrl
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            Keys key = keyData & Keys.KeyCode;
+            if (key == Keys.Tab && (keyData & Keys.Alt) == Keys.None)
+            {
+                keyData = keyData & ~Keys.Control;
+            }
+            else if (key == Keys.Return && (keyData & Keys.Modifiers) == Keys.Control)
+            {
+                keyData = keyData & ~Keys.Control;
+            }
+            return base.ProcessDialogKey(keyData);
+        }
     }
 }
