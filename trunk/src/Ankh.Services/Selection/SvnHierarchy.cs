6f629486592c824a363e72d60727d1e16568bc7b
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2012-07-28 23:52:31 +0000

    * src/Ankh.Services/Selection/SvnHierarchy.cs   And fix another 4.0+ build issue.

--- trunk/src/Ankh.Services/Selection/SvnHierarchy.cs
+++ trunk/src/Ankh.Services/Selection/SvnHierarchy.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.VisualStudio.Shell.Interop;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Ankh.Selection
 {
