2c840896d8c0da60c9d9c6f5c80351951b48602e
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2013-05-03 10:27:38 +0000

    Handle the blue theme as a light theme for our ListView support.

--- trunk/src/Ankh.Package/AnkhSvnPackage.Scc.cs
+++ trunk/src/Ankh.Package/AnkhSvnPackage.Scc.cs
@@ -13,8 +13,6 @@ namespace Ankh.VSPackage
     [ProvideProjectTypeSettings("{159641d6-6404-4a2a-ae62-294de0fe8301}", SccProjectFlags.ForceSccGlyphChange, Name = ".dtproj (SQL2012) project")]
     [ProvideProjectTypeSettings("{f14b399a-7131-4c87-9e4b-1186c45ef12d}", SccProjectFlags.ForceSccGlyphChange, Name = ".rptproj")]
     [ProvideProjectTypeSettings("{999d2cb9-9277-4465-a902-1604ed3686a3}", SccProjectFlags.ForceSccGlyphChange, Name = ".smdlproj")]
-    [ProvideThemeInformation("{de3dbbcd-f642-433c-8353-8f1df4370aba}", true)]
-    [ProvideThemeInformation("{1ded0138-47ce-435e-84ef-9ec1f439b749}", false)]
     partial class AnkhSvnPackage
     {
     }
---
+++ trunk/src/Ankh.Package/AnkhSvnPackage.Themes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ankh.VSPackage.Attributes;
+using Ankh.Scc.ProjectMap;
+
+namespace Ankh.VSPackage
+{
+    [ProvideThemeInformation("{de3dbbcd-f642-433c-8353-8f1df4370aba}", true)] // Light
+    [ProvideThemeInformation("{1ded0138-47ce-435e-84ef-9ec1f439b749}", false)] // Dark
+    [ProvideThemeInformation("{a4d6a176-b948-4b29-8c66-53c97a1ed7d0}", true)] // Blue
+    partial class AnkhSvnPackage
+    {
+    }
+}
