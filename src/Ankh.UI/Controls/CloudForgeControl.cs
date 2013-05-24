f25a1534c4bc09d2d2f86fd245b73286129dfc5d
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2013-05-24 21:32:52 +0000

    user control for sponsor (CloudForge) promo

---
+++ src/Ankh.UI/Controls/CloudForgeControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ankh.UI.Controls
+{
+    /// <summary>
+    /// Provides a link to sign-up for CollabNet's CloudForge services
+    /// </summary>
+    public partial class CloudForgeControl : UserControl
+    {
+        private static readonly string CF_SIGNUP_URL = "https://app.cloudforge.com/subscriptions/new/?product=Free&source=ankhsvn";
+
+        public CloudForgeControl()
+        {
+            InitializeComponent();
+        }
+
+        private void cloudForgePictureBox_Click(object sender, EventArgs e)
+        {
+            IAnkhServiceProvider sp = Parent as IAnkhServiceProvider;
+            if (sp != null)
+            {
+                Ankh.VS.IAnkhWebBrowser wb = sp.GetService<Ankh.VS.IAnkhWebBrowser>();
+                if (wb != null)
+                {
+                    Ankh.VS.AnkhBrowserArgs args = new Ankh.VS.AnkhBrowserArgs();
+                    args.External = true;
+                    Uri cfSignUpUrl = new Uri(CF_SIGNUP_URL);
+                    wb.Navigate(cfSignUpUrl, args);
+                }
+            }
+        }
+    }
+}
