9e060bc187ec1b2d5318cf6daf650c3ba0c975ce
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2013-05-29 15:31:14 +0000

    Ankh service interface for SCM repository provider extension point

---
+++ trunk/src/Ankh.Services/RepositoryProvider/IAnkhRepositoryProviderService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Ankh.ExtensionPoints.RepositoryProvider;
+
+namespace Ankh
+{
+    public interface IAnkhRepositoryProviderService
+    {
+        /// <summary>
+        /// Gets all the registered SCM repository providers.
+        /// </summary>
+        /// <remarks>This call DOES NOT trigger provider package initialization.</remarks>
+        ICollection<ScmRepositoryProvider> RepositoryProviders { get; }
+
+        /// <summary>
+        /// Gets all the registered SCM repository providers for the given SCM type(svn, git).
+        /// </summary>
+        /// <remarks>This call DOES NOT trigger provider package initialization.</remarks>
+        ICollection<ScmRepositoryProvider> GetRepositoryProviders(string type);
+
+        /// <summary>
+        /// Tries to find a registered provider with the given name.
+        /// </summary>
+        /// <remarks>This call DOES NOT trigger provider package initialization.</remarks>
+        bool TryGetRepositoryProvider(string id, out ScmRepositoryProvider repoProvider);
+
+        /// <summary>
+        /// Shows the help for developing SCM repository provider
+        /// </summary>
+        void ShowProvideHelp();
+    }
+}
