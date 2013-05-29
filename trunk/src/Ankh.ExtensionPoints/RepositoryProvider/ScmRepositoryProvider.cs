7719a1a10db41c8fc07bf1697c425c4c5268f2c9
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2013-05-29 13:53:48 +0000

    RepositoryProvider extension point (work in progress)

---
+++ trunk/src/Ankh.ExtensionPoints/RepositoryProvider/ScmRepositoryProvider.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Ankh.ExtensionPoints.RepositoryProvider
+{
+    /// <summary>
+    /// Base class for SCM Repository Provider service
+    /// </summary>
+    public abstract class ScmRepositoryProvider
+    {
+        public static readonly string TYPE_SVN = "svn";
+        public static readonly string TYPE_GIT = "git";
+
+        private string scmType;
+
+        /// <summary>
+        /// Gets the identifier of this provider
+        /// </summary>
+        public abstract string Id { get; }
+
+        /// <summary>
+        /// Gets the user-friendly name of this provider.
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// Gets the SCM repository selection control which provides the UI to select  a repository
+        /// </summary>
+        public abstract ScmRepositorySelectionControl RepositorySelectionControl { get; }
+
+        /// <summary>
+        /// Gets or sets the type of the repositories that this provider supplies.
+        /// </summary>
+        public virtual string ScmType
+        {
+            get { return this.scmType; }
+            set { this.scmType = value; }
+        }
+    }
+}
