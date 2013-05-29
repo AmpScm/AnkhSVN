bbaf064abaf97168a5adc6716750ef3505877867
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2013-05-29 13:53:48 +0000

    RepositoryProvider extension point (work in progress)

---
+++ src/Ankh.ExtensionPoints/RepositoryProvider/ScmRepositorySelectionControl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ankh.ExtensionPoints.RepositoryProvider
+{
+    /// <summary>
+    /// Base class for SCM repository selection pages contributed by the repository providers.
+    /// </summary>
+    public abstract class ScmRepositorySelectionControl
+    {
+        /// <summary>
+        /// Raised when this control needs to notify containers, such as when selection changes, or when an exception is thrown.
+        /// </summary>
+        public event EventHandler<ScmRepositorySelectionControlEventArgs> ScmRepositorySelectionControlEvent;
+
+        /// <summary>
+        /// Gets the string representation of the selected SCM repository URI
+        /// </summary>
+        public abstract string SelectedUri { get; }
+
+        /// <summary>
+        /// Gets the IWin32Window instance which provides the UI for listing and selecting a SCM repository
+        /// </summary>
+        /// <remarks>Default implementation returns this as IWin32Window.</remarks>        
+        public virtual IWin32Window Window
+        {
+            get { return this as IWin32Window; }
+        }
+
+        /// <summary>
+        /// Raises ScmRepositorySelectionControlEvent when this control needs to notify containers, such as when selection changes, or when an exception is thrown.
+        /// </summary>
+        public virtual void OnScmRepositorySelectionControlEvent(ScmRepositorySelectionControlEventArgs e)
+        {
+            if (ScmRepositorySelectionControlEvent != null)
+            {
+                ScmRepositorySelectionControlEvent(this, e);
+            }
+        }
+    }
+
+    public class ScmRepositorySelectionControlEventArgs : EventArgs
+    {
+        // TODO
+        private string message;
+        private Exception exception;
+    }
+}
