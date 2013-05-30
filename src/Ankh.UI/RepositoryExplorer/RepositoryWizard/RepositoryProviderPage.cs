2e10a4a9e7e71b562246a6a5943ecd43d4da025c
baybora <baybora@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2013-05-30 17:52:50 +0000

    Repository selection wizard

---
+++ src/Ankh.UI/RepositoryExplorer/RepositoryWizard/RepositoryProviderPage.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Ankh.ExtensionPoints.RepositoryProvider;
+using Ankh.UI.VSSelectionControls;
+using Ankh.Configuration;
+
+namespace Ankh.UI.RepositoryExplorer.RepositoryWizard
+{
+    public partial class RepositoryProviderPage : WizardFramework.WizardPage
+    {
+        public RepositoryProviderPage()
+        {
+            Text = RepositoryWizardResources.RepoProviderPageHeaderTitle;
+            Description = RepositoryWizardResources.RepoProviderPageHeaderMessage;
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The URL entered in the text box.
+        /// </summary>
+        public Uri Uri
+        {
+            get
+            {
+                Uri uri = null;
+                if (this.urlRadioButton.Checked)
+                {
+                    if (string.IsNullOrEmpty(urlComboBox.Text) || !Uri.TryCreate(urlComboBox.Text, UriKind.Absolute, out uri))
+                        return null;
+                }
+                return uri;
+            }
+        }
+
+        public ScmRepositoryProvider SelectedRepositoryProvider
+        {
+            get
+            {
+                if (this.providerRadioButton.Checked)
+                {
+                    if (this.providerListView.SelectedItems.Count == 1)
+                    {
+                        RepositoryProviderListViewItem lvi = this.providerListView.SelectedItems[0] as RepositoryProviderListViewItem;
+                        if (lvi != null)
+                        {
+                            return lvi.RepositoryProvider;
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            this.urlRadioButton.Checked = true;
+            this.providerRadioButton.Checked = false;
+            if (Context != null)
+            {
+                foreach (string url in Context.GetService<IAnkhConfigurationService>().GetRecentReposUrls())
+                {
+                    this.urlComboBox.Items.Add(url);
+                }
+            }
+            this.urlComboBox.Select();
+            AnkhAction populateProviders = new AnkhAction(InitializeProviders);
+            BeginInvoke(populateProviders);
+        }
+
+        private void InitializeProviders()
+        {
+            ICollection<ScmRepositoryProvider> providers = GetRepositoryProviders();
+            if (InvokeRequired)
+            {
+                Action<ICollection<ScmRepositoryProvider>> initProviders = new Action<ICollection<ScmRepositoryProvider>>(InitializeProviders);
+                Invoke(initProviders, providers);
+            }
+            else
+            {
+                InitializeProviders(providers);
+            }
+        }
+
+        private void InitializeProviders(ICollection<ScmRepositoryProvider> providers)
+        {
+            Debug.Assert(!InvokeRequired);
+            this.providerRadioButton.Enabled 
+                = this.providerRadioButton.Visible 
+                = this.providerListView.Enabled 
+                = this.providerListView.Visible
+                = providers != null && providers.Count > 0;
+            // show link to the WIKI that lists the available third party repository providers
+            // only if there are not providers installed
+            this.wikiLinkLabel.Enabled = this.wikiLinkLabel.Visible = providers == null || providers.Count == 0;
+            if (providers != null && providers.Count > 0)
+            {
+                this.providerListView.BeginUpdate();
+                try
+                {
+                    this.providerListView.Items.Clear();
+                    List<RepositoryProviderListViewItem> items = new List<RepositoryProviderListViewItem>();
+
+                    foreach (ScmRepositoryProvider i in providers)
+                    {
+                        RepositoryProviderListViewItem lvi = new RepositoryProviderListViewItem(this.providerListView, i);
+                        items.Add(lvi);
+                    }
+                    this.providerListView.Items.AddRange(items.ToArray());
+                }
+                finally
+                {
+                    this.providerListView.EndUpdate();
+                }
+            }
+        }
+
+        private ICollection<ScmRepositoryProvider> GetRepositoryProviders()
+        {
+            if (Wizard != null && Wizard.Context != null)
+            {
+                IAnkhRepositoryProviderService repoProviderSvc = Wizard.Context.GetService<IAnkhRepositoryProviderService>();
+                if (repoProviderSvc != null)
+                {
+                    return repoProviderSvc.GetRepositoryProviders(ScmRepositoryProvider.TYPE_SVN);
+                }
+            }
+            return new ScmRepositoryProvider[] { };
+        }
+
+        private void EvaluatePage()
+        {
+            bool isPageComplete = false
+                || Uri != null
+                || SelectedRepositoryProvider != null;
+            IsPageComplete = isPageComplete;
+        }
+
+        private void providerListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.providerRadioButton.Checked = true;
+            EvaluatePage();
+        }
+
+        private void urlComboBox_TextChanged(object sender, EventArgs e)
+        {
+            this.urlRadioButton.Checked = true;
+            EvaluatePage();
+        }
+    }
+
+    sealed class RepositoryProviderListViewItem : SmartListViewItem
+    {
+        public RepositoryProviderListViewItem(SmartListView view, ScmRepositoryProvider repoProvider)
+            : base(view)
+        {
+            Tag = repoProvider;
+            Refresh();
+        }
+
+        public ScmRepositoryProvider RepositoryProvider
+        {
+            get { return Tag as ScmRepositoryProvider; }
+        }
+
+        public string RepositoryProviderName
+        {
+            get { return RepositoryProvider == null ? string.Empty : RepositoryProvider.Name; }
+        }
+
+        void Refresh()
+        {
+            SetValues(RepositoryProviderName);
+        }
+    }
+}
---
+++ src/Ankh.UI/RepositoryExplorer/RepositoryWizard/RepositorySelectionPage.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Ankh.UI.RepositoryExplorer.RepositoryWizard
+{
+    partial class RepositorySelectionPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cloudForgeControl1 = new Ankh.UI.Controls.CloudForgeControl();
+            this.repoProviderControlPanel = new System.Windows.Forms.Panel();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.repoProviderControlPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cloudForgeControl1
+            // 
+            this.cloudForgeControl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cloudForgeControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.cloudForgeControl1.Location = new System.Drawing.Point(10, 270);
+            this.cloudForgeControl1.Name = "cloudForgeControl1";
+            this.cloudForgeControl1.Size = new System.Drawing.Size(455, 70);
+            this.cloudForgeControl1.TabIndex = 0;
+            // 
+            // repoProviderControlPanel
+            // 
+            this.repoProviderControlPanel.Controls.Add(this.errorLabel);
+            this.repoProviderControlPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.repoProviderControlPanel.Location = new System.Drawing.Point(10, 10);
+            this.repoProviderControlPanel.Name = "repoProviderControlPanel";
+            this.repoProviderControlPanel.Size = new System.Drawing.Size(455, 260);
+            this.repoProviderControlPanel.TabIndex = 1;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.errorLabel.Location = new System.Drawing.Point(0, 0);
+            this.errorLabel.Margin = new System.Windows.Forms.Padding(3);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(455, 260);
+            this.errorLabel.TabIndex = 0;
+            this.errorLabel.Text = "Selected provider does not provide UI. Please contact your repository provider.";
+            this.errorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // RepositorySelectionPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.repoProviderControlPanel);
+            this.Controls.Add(this.cloudForgeControl1);
+            this.Name = "RepositorySelectionPage";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Size = new System.Drawing.Size(475, 350);
+            this.repoProviderControlPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Ankh.UI.Controls.CloudForgeControl cloudForgeControl1;
+        private System.Windows.Forms.Panel repoProviderControlPanel;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
