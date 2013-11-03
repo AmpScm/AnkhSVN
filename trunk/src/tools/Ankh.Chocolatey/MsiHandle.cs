3a9a900879fe4f85a78cb97269bd741ccc7ffb9f
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2013-11-03 13:09:55 +0000

    Add a very simple Chocolatey package generator that can generate a chocolatey manifest by just accessing our MSI.

---
+++ trunk/src/tools/Ankh.Chocolatey/MsiHandle.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace Ankh.Chocolatey
+{
+    class MsiHandle : SafeHandleMinusOneIsInvalid
+    {
+        public MsiHandle()
+            : base(true)
+        { }
+
+        protected override bool ReleaseHandle()
+        {
+            return NativeMethods.MsiCloseHandle(handle) == 0;
+        }
+    }
+}
