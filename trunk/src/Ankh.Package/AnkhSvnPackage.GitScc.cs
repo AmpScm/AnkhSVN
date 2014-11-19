fc2f846ce4f583bbbb1576bc6cae49596979adfc
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2014-11-19 12:13:44 +0000

    Add Ankh.Git assembly to project, and the first bits of plumbing.
    * src/Ankh.GitScc
    * src/Ankh.GitScc/Ankh.GitScc.csproj
    * src/Ankh.GitScc/Ankh.GitScc.snk
    * src/Ankh.GitScc/Ankh.GitScc.wxs
    * src/Ankh.GitScc/Properties/AssemblyInfo.cs
    * src/Ankh.Package/Ankh.Package.csproj
    * src/Ankh.Package/Ankh.Package.wxs
    * src/Ankh.Package/AnkhSvnPackage.GitScc.cs
    * src/Ankh.Package/Properties/AssemblyInfo.cs
    * src/Ankh.Votive.wixproj
    * src/AnkhSvn.sln
      Add project to build

---
+++ trunk/src/Ankh.Package/AnkhSvnPackage.GitScc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.Shell;
+
+using Ankh.VSPackage.Attributes;
+using Ankh.Scc;
+using Ankh.Scc.ProjectMap;
+
+namespace Ankh.VSPackage
+{
+#if NO
+    [ProvideService(typeof(ITheAnkhSvnSccProvider), ServiceName = AnkhId.SubversionSccName)]
+    [ProvideSourceControlProvider(AnkhId.SccProviderId, AnkhId.SccProviderTitle, "#100", typeof(ITheAnkhSvnSccProvider))]
+    [ProvideSourceControlCommand(AnkhId.SccProviderId, SccProviderCommand.Open, AnkhCommand.FileFileOpenFromSubversion)]
+    [ProvideSourceControlCommand(AnkhId.SccProviderId, SccProviderCommand.Share, AnkhCommand.FileSccAddSolutionToSubversion)]
+#endif
+    partial class AnkhSvnPackage
+    {
+    }
+}
