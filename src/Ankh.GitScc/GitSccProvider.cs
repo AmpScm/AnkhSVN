bffd8507ac4218a199168955beb08dce5e743ac6
Bert Huijben <rhuijben@users.noreply.github.com>
2014-11-19 13:45:40 +0000

    Following up on r12483, more git scc provider plumbing.

---
+++ src/Ankh.GitScc/GitSccProvider.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Ankh.Scc;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Ankh.GitScc
+{
+    [GuidAttribute(AnkhId.SccServiceId), ComVisible(true), CLSCompliant(false)]
+    public interface ITheAnkhGitSccProvider : IVsSccProvider
+    {
+    }
+
+    [GlobalService(typeof(GitSccProvider))]
+    [GlobalService(typeof(IAnkhGitSccService))]
+    [GlobalService(typeof(ITheAnkhGitSccProvider), true)]
+    partial class GitSccProvider : AnkhService, ITheAnkhGitSccProvider, IVsSccProvider, IVsSccControlNewSolution, IAnkhGitSccService, IVsSccEnlistmentPathTranslation
+    {
+        public GitSccProvider(IAnkhServiceProvider context)
+            : base(context)
+        {
+
+        }
+        #region STUB
+        public int AnyItemsUnderSourceControl(out int pfResult)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SetActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SetInactive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddNewSolutionToSourceControl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDisplayStringForAction(out string pbstrActionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsActive
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsSolutionDirty
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void LoadingManagedSolution(bool asPrimarySccProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetProjectManaged(Selection.SvnProject project, bool managed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsProjectManaged(Selection.SvnProject project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSolutionManaged
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void RegisterAsPrimarySccProvider()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AnkhGlyph GetPathGlyph(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SerializeSccTranslateData(System.IO.Stream store, bool writeData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SerializeSccExcludeData(System.IO.Stream store, bool writeData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IgnoreEnumerationSideEffects(IVsSccProject2 sccProject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnsureCheckOutReference(Selection.SvnProject project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnsureLoaded()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasProjectProperties(IVsHierarchy pHierarchy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoreProjectProperties(IVsHierarchy pHierarchy, IPropertyMap map)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadProjectProperties(IVsHierarchy pHierarchy, string pszProjectName, string pszProjectMk, IPropertyMap map)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProjectLoadFailed(string pszProjectMk)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TranslateEnlistmentPathToProjectPath(string lpszEnlistmentPath, out string pbstrProjectPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TranslateProjectPathToEnlistmentPath(string lpszProjectPath, out string pbstrEnlistmentPath, out string pbstrEnlistmentPathUNC)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ src/Ankh.Services/Scc/IAnkhGitSccService.cs
@@ -0,0 +1,126 @@
+// $Id$
+//
+// Copyright 2008-2009 The AnkhSVN Project
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using Ankh.Selection;
+using System.IO;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Ankh.Scc
+{
+	/// <summary>
+	/// 
+	/// </summary>
+    [CLSCompliant(false)]
+	public interface IAnkhGitSccService
+	{
+		/// <summary>
+		/// Gets a value indicating whether the Ankh Scc service is active
+		/// </summary>
+		/// <value><c>true</c> if this instance is active; otherwise, <c>false</c>.</value>
+		bool IsActive { get; }
+
+
+        /// <summary>
+        /// Gets or sets a boolean indicating whether te solution should be saved for changed scc settings
+        /// </summary>
+        bool IsSolutionDirty { get; set; }
+
+        /// <summary>
+        /// Called by the package when loading a managed solution
+        /// </summary>
+        /// <param name="asPrimarySccProvider">if set to <c>true</c> Ankh is marked as the primary SCC provider; otherwise it is running as second chair</param>
+        void LoadingManagedSolution(bool asPrimarySccProvider);
+
+        /// <summary>
+        /// Marks the specified project as managed by the Scc provider
+        /// </summary>
+        /// <param name="project">A reference to the project or null for the solution</param>
+        /// <param name="managed"></param>
+        void SetProjectManaged(SvnProject project, bool managed);    
+
+        /// <summary>
+        /// Gets a boolean indicating whether the specified project (or the solution) is 
+        /// managed by the Subversion Scc provider
+        /// </summary>
+        /// <param name="project">A reference to the project or null for the solution</param>
+        /// <returns><c>true</c> if the solution is managed by the scc provider, otherwise <c>false</c></returns>
+        bool IsProjectManaged(SvnProject project);
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is solution managed.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if this instance is solution managed; otherwise, <c>false</c>.
+        /// </value>
+        bool IsSolutionManaged { get; }
+
+        /// <summary>
+        /// Register the scc provider as primary scc provider in Visual Studio
+        /// </summary>
+        void RegisterAsPrimarySccProvider();
+
+
+        /// <summary>
+        /// Gets the glyph for a specific path
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        AnkhGlyph GetPathGlyph(string path);
+
+        /// <summary>
+        /// Serializes the enlist data.
+        /// </summary>
+        /// <param name="store">The store.</param>
+        /// <param name="writeData">if set to <c>true</c> [write data].</param>
+        void SerializeSccTranslateData(Stream store, bool writeData);
+
+        /// <summary>
+        /// Serializes the scc-exclude data.
+        /// </summary>
+        /// <param name="store">The store.</param>
+        /// <param name="writeData">if set to <c>true</c> [write data].</param>
+        void SerializeSccExcludeData(Stream store, bool writeData);
+
+        /// <summary>
+        /// Gets a boolean indicating whether to ignores the enumeration side effects flag on this project
+        /// </summary>
+        /// <param name="sccProject">The SCC project.</param>
+        /// <returns></returns>
+        bool IgnoreEnumerationSideEffects(Microsoft.VisualStudio.Shell.Interop.IVsSccProject2 sccProject);
+
+        /// <summary>
+        /// Ensures the check out reference for the specified project
+        /// </summary>
+        /// <param name="project">The project.</param>
+        void EnsureCheckOutReference(SvnProject project);
+
+        /// <summary>
+        /// Ensure that all project state is loaded
+        /// </summary>
+        /// <remarks>Only used after enabling ourself as SCC provider</remarks>
+        void EnsureLoaded();
+
+        bool HasProjectProperties(IVsHierarchy pHierarchy);
+
+        void StoreProjectProperties(IVsHierarchy pHierarchy, IPropertyMap map);
+
+        void ReadProjectProperties(IVsHierarchy pHierarchy, string pszProjectName, string pszProjectMk, IPropertyMap map);
+
+        void ProjectLoadFailed(string pszProjectMk);
+    }
+}
