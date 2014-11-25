5561175e4374eee86c5f6ec8df77eaa357ae37f4
Bert Huijben <rhuijben@users.noreply.github.com>
2014-11-25 10:26:28 +0000

    Automatically enable git support in debug mode.

--- src/Ankh.Package/AnkhSvnPackage.GitScc.cs
+++ src/Ankh.Package/AnkhSvnPackage.GitScc.cs
@@ -6,15 +6,16 @@ using Microsoft.VisualStudio.Shell;
 using Ankh.VSPackage.Attributes;
 using Ankh.Scc;
 using Ankh.Scc.ProjectMap;
+using Ankh.GitScc;
 
 namespace Ankh.VSPackage
 {
-#if NO
-    [ProvideService(typeof(ITheAnkhSvnSccProvider), ServiceName = AnkhId.SubversionSccName)]
-    [ProvideSourceControlProvider(AnkhId.SccProviderId, AnkhId.SccProviderTitle, "#100", typeof(ITheAnkhSvnSccProvider))]
-    [ProvideSourceControlCommand(AnkhId.SccProviderId, SccProviderCommand.Open, AnkhCommand.FileFileOpenFromSubversion)]
-    [ProvideSourceControlCommand(AnkhId.SccProviderId, SccProviderCommand.Share, AnkhCommand.FileSccAddSolutionToSubversion)]
+#if DEBUG
+    [ProvideSourceControlProvider(AnkhId.GitSccProviderId, AnkhId.GitSccProviderTitle, "#101", typeof(ITheAnkhGitSccProvider))]
 #endif
+    [ProvideService(typeof(ITheAnkhGitSccProvider), ServiceName = AnkhId.GitSccName)]
+    //[ProvideSourceControlCommand(AnkhId.GitSccProviderId, SccProviderCommand.Open, AnkhCommand.FileFileOpenFromSubversion)]
+    //[ProvideSourceControlCommand(AnkhId.GitSccProviderId, SccProviderCommand.Share, AnkhCommand.FileSccAddSolutionToSubversion)]
     partial class AnkhSvnPackage
     {
     }
