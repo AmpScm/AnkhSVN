df67a99606878c26bf49a4550ee2c446a7d0d5d3
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2015-01-08 20:31:59 +0000

    Bump SharpGit version to obtain latest OpenSSL and to get the final missing pieces to get some status reported.
    * src/Ankh.Package/AnkhSvnPackage.GitScc.cs
      Remove debug only check.
    
    * src/Ankh.Services/Ankh.Services.csproj
      Bump SharpGit.
    
    * src/Ankh.Services/Scc/Git/GitItem.cs
    * src/Ankh.VS/SolutionExplorer/StatusImageMapper.cs
      Tweak image processing.

--- trunk/src/Ankh.Package/AnkhSvnPackage.GitScc.cs
+++ trunk/src/Ankh.Package/AnkhSvnPackage.GitScc.cs
@@ -10,9 +10,7 @@ using Ankh.GitScc;
 
 namespace Ankh.VSPackage
 {
-#if DEBUG
     [ProvideSourceControlProvider(AnkhId.GitSccProviderId, AnkhId.GitSccProviderTitle, "#101", typeof(ITheAnkhGitSccProvider))]
-#endif
     [ProvideService(typeof(ITheAnkhGitSccProvider), ServiceName = AnkhId.GitSccName)]
     //[ProvideSourceControlCommand(AnkhId.GitSccProviderId, SccProviderCommand.Open, AnkhCommand.FileFileOpenFromSubversion)]
     //[ProvideSourceControlCommand(AnkhId.GitSccProviderId, SccProviderCommand.Share, AnkhCommand.FileSccAddSolutionToSubversion)]
