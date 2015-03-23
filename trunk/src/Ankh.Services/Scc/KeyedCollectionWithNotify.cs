b93b6a24e00d1fa575dca5695ecb9525d09b0a09
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2015-03-23 14:35:58 +0000

    Add some generic collections that allow external observers to track their changes. This is plumbing for generalizing the pending change handling towards WPF and Git.
    This implements +- the standard .Net 4.0 ObservableCollection<T> class on
    a collection and keyedcollection.
    
    * src/Ankh.Services/Ankh.Services.csproj
      Add classes.
    
    * src/Ankh.Services/Scc/CollectionWithNotify.cs
    * src/Ankh.Services/Scc/ISupportsCollectionChange.cs
    * src/Ankh.Services/Scc/KeyedCollectionWithNotify.cs
      Implement support.

---
+++ trunk/src/Ankh.Services/Scc/KeyedCollectionWithNotify.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Ankh
+{
+    public abstract class KeyedCollectionWithNotify<TKey, TItem> : KeyedCollection<TKey, TItem>, ISupportsCollectionChanged, INotifyPropertyChanged
+    {
+        CollectionChangedEventArgs.SimpleMonitor _monitor = new CollectionChangedEventArgs.SimpleMonitor();
+        protected KeyedCollectionWithNotify()
+            : base()
+        { }
+
+        protected KeyedCollectionWithNotify(IEqualityComparer<TKey> comparer)
+            : base(comparer)
+        { }
+
+        protected KeyedCollectionWithNotify(IEqualityComparer<TKey> comparer, int dictionaryCreationThreshold)
+            : base(comparer, dictionaryCreationThreshold)
+        { }
+
+        protected override void ClearItems()
+        {
+            _monitor.CheckReentrancy(CollectionChanged);
+            base.ClearItems();
+            OnPropertyChanged("Count");
+            OnPropertyChanged("Item[]");
+            OnCollectionChanged(new CollectionChangedEventArgs(CollectionChange.Reset));
+        }
+
+        protected override void InsertItem(int index, TItem item)
+        {
+            _monitor.CheckReentrancy(CollectionChanged);
+            base.InsertItem(index, item);
+            this.OnPropertyChanged("Count");
+            this.OnPropertyChanged("Item[]");
+            this.OnCollectionChanged(new CollectionChangedEventArgs(CollectionChange.Add, item, index));
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            _monitor.CheckReentrancy(CollectionChanged);
+            TItem t = base[index];
+            base.RemoveItem(index);
+            this.OnPropertyChanged("Count");
+            this.OnPropertyChanged("Item[]");
+            this.OnCollectionChanged(new CollectionChangedEventArgs(CollectionChange.Remove, t, index));
+        }
+
+        protected override void SetItem(int index, TItem item)
+        {
+            _monitor.CheckReentrancy(CollectionChanged);
+            TItem t = base[index];
+            base.SetItem(index, item);
+            this.OnPropertyChanged("Item[]");
+            this.OnCollectionChanged(new CollectionChangedEventArgs(CollectionChange.Replace, t, item, index));
+        }
+
+        public event EventHandler<CollectionChangedEventArgs> CollectionChanged;
+
+        protected void OnCollectionChanged(CollectionChangedEventArgs e)
+        {
+            if (CollectionChanged != null)
+                CollectionChanged(this, e);
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
