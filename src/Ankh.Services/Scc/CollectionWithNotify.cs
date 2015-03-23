d322a8d8a6b63e7857d5cd7ac2249fa54ca966f0
Bert Huijben <rhuijben@users.noreply.github.com>
2015-03-23 14:35:58 +0000

    Add some generic collections that allow external observers to track their changes. This is plumbing for generalizing the pending change handling towards WPF and Git.
    This implements +- the standard .Net 4.0 ObservableCollection<T> class on
    a collection and keyedcollection.
    
    * src/Ankh.Services/Ankh.Services.csproj
      Add classes.
    
    * src/Ankh.Services/Scc/CollectionWithNotify.cs
    * src/Ankh.Services/Scc/ISupportsCollectionChange.cs
    * src/Ankh.Services/Scc/KeyedCollectionWithNotify.cs
      Implement support.

---
+++ src/Ankh.Services/Scc/CollectionWithNotify.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Ankh
+{
+    /// <summary>
+    /// Our own implementation of ObservableCollection, compatible with .Net 2.0
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CollectionWithNotify<T> : Collection<T>, ISupportsCollectionChanged, INotifyPropertyChanged
+    {
+        CollectionChangedEventArgs.SimpleMonitor _monitor = new CollectionChangedEventArgs.SimpleMonitor();
+
+        public CollectionWithNotify()
+            : base()
+        { }
+
+        public CollectionWithNotify(IList<T> list)
+            : base(list)
+        { }
+
+        protected override void ClearItems()
+        {
+            _monitor.CheckReentrancy(CollectionChanged);
+            base.ClearItems();
+            OnPropertyChanged("Count");
+            OnPropertyChanged("Item[]");
+            OnCollectionChanged(new CollectionChangedEventArgs(CollectionChange.Reset));
+        }
+
+        protected override void InsertItem(int index, T item)
+        {
+            _monitor.CheckReentrancy(CollectionChanged);
+            base.InsertItem(index, item);
+            this.OnPropertyChanged("Count");
+            this.OnPropertyChanged("Item[]");
+            this.OnCollectionChanged(new CollectionChangedEventArgs(CollectionChange.Add, item, index));
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            _monitor.CheckReentrancy(CollectionChanged);
+            T t = base[index];
+            base.RemoveItem(index);
+            this.OnPropertyChanged("Count");
+            this.OnPropertyChanged("Item[]");
+            this.OnCollectionChanged(new CollectionChangedEventArgs(CollectionChange.Remove, t, index));
+        }
+
+        protected override void SetItem(int index, T item)
+        {
+            _monitor.CheckReentrancy(CollectionChanged);
+            T t = base[index];
+            base.SetItem(index, item);
+            this.OnPropertyChanged("Item[]");
+            this.OnCollectionChanged(new CollectionChangedEventArgs(CollectionChange.Replace, t, item, index));
+        }
+
+        public event EventHandler<CollectionChangedEventArgs> CollectionChanged;
+
+        protected void OnCollectionChanged(CollectionChangedEventArgs e)
+        {
+            if (CollectionChanged != null)
+                using (_monitor.Enter())
+                {
+                    CollectionChanged(this, e);
+                }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+
+        void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ src/Ankh.Services/Scc/ISupportsCollectionChange.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Ankh
+{
+    public interface ISupportsCollectionChanged
+    {
+        event EventHandler<CollectionChangedEventArgs> CollectionChanged;
+    }
+
+    public enum CollectionChange
+    {
+        /// <summary>One or more items were added to the collection.</summary>
+        Add = 0 /* = NotifyCollectionChangedAction.Add */,
+        /// <summary>One or more items were removed from the collection.</summary>
+        Remove = 1 /* = NotifyCollectionChangedAction.Remove */,
+        /// <summary>One or more items were replaced in the collection.</summary>
+        Replace = 2 /* = NotifyCollectionChangedAction.Replace */,
+        /// <summary>One or more items were moved within the collection.</summary>
+        Move = 3 /* = NotifyCollectionChangedAction.Move */,
+        /// <summary>The content of the collection changed dramatically.</summary>
+        Reset = 4 /* = NotifyCollectionChangedAction.Reset */,
+    }
+
+    /* Our own clone of NotifyCollectionChangedEventArgs */
+    public class CollectionChangedEventArgs : EventArgs
+    {
+        readonly CollectionChange _action;
+        readonly IList _newItems;
+        readonly IList _oldItems;
+        readonly int _newStartIndex = -1;
+        readonly int _oldStartIndex = -1;
+
+        public CollectionChangedEventArgs(CollectionChange action)
+        {
+            if (action != CollectionChange.Reset)
+                throw new ArgumentException();
+            _action = action;
+        }
+
+        public CollectionChangedEventArgs(CollectionChange action, object changedItem)
+            : this(action, changedItem, -1)
+        {
+        }
+
+        public CollectionChangedEventArgs(CollectionChange action, object changedItem, int index)
+        {
+            if (action != CollectionChange.Reset
+                && action != CollectionChange.Add
+                && action != CollectionChange.Remove)
+            {
+                throw new ArgumentException();
+            }
+            else if (action != CollectionChange.Reset && changedItem == null)
+                throw new ArgumentNullException();
+
+            _action = action;
+            if (action == CollectionChange.Add)
+            {
+                _newItems = new object[] { changedItem };
+                _newStartIndex = index;
+            }
+            else if (action == CollectionChange.Remove)
+            {
+                _oldItems = new object[] { changedItem };
+                _oldStartIndex = index;
+            }
+        }
+
+        public CollectionChangedEventArgs(CollectionChange action, object newItem, object oldItem)
+            : this(action, newItem, oldItem, -1)
+        {
+
+        }
+
+        public CollectionChangedEventArgs(CollectionChange action, object newItem, object oldItem, int startingIndex)
+        {
+            if (action != CollectionChange.Replace)
+                throw new ArgumentException();
+            else if (newItem == null)
+                throw new ArgumentNullException("newItem");
+            else if (oldItem == null)
+                throw new ArgumentNullException("newItem");
+
+            _action = action;
+            if (action == CollectionChange.Add)
+            {
+                _newItems = new object[] { newItem };
+                _newStartIndex = startingIndex;
+            }
+            else if (action == CollectionChange.Remove)
+            {
+                _oldItems = new object[] { oldItem };
+                _oldStartIndex = startingIndex;
+            }
+        }
+
+        public CollectionChangedEventArgs(CollectionChange action, IList newItems, IList oldItems)
+            : this(action, newItems, oldItems, -1)
+        {
+
+        }
+
+        public CollectionChangedEventArgs(CollectionChange action, IList newItems, IList oldItems, int startingIndex)
+        {
+            if (action != CollectionChange.Replace)
+                throw new ArgumentException();
+            else if (newItems == null)
+                throw new ArgumentNullException("newItems");
+            else if (oldItems == null)
+                throw new ArgumentNullException("oldItems");
+            _action = action;
+            _newItems = ArrayList.ReadOnly(newItems);
+            _oldItems = ArrayList.ReadOnly(oldItems);
+            _oldStartIndex = _newStartIndex = startingIndex;
+        }
+
+        public CollectionChangedEventArgs(CollectionChange action, object changedItem, int index, int oldIndex)
+        {
+            if (action != CollectionChange.Move)
+                throw new ArgumentException();
+            else if (changedItem == null)
+                throw new ArgumentNullException("changedItem");
+            else if (index < 0 || oldIndex < 0)
+                throw new ArgumentException();
+            _action = action;
+            _oldItems = _newItems = new object[] { changedItem };
+            _newStartIndex = index;
+            _oldStartIndex = oldIndex;
+        }
+
+        public CollectionChangedEventArgs(CollectionChange action, IList changedItems, int index, int oldIndex)
+        {
+            if (action != CollectionChange.Move)
+                throw new ArgumentException();
+            else if (changedItems == null)
+                throw new ArgumentNullException("changedItems");
+            else if (index < 0 || oldIndex < 0)
+                throw new ArgumentException();
+            _action = action;
+            _oldItems = _newItems = ArrayList.ReadOnly(changedItems);
+            _newStartIndex = index;
+            _oldStartIndex = oldIndex;
+        }
+
+        public CollectionChange Action
+        {
+            get { return _action; }
+        }
+
+        public IList NewItems
+        {
+            get { return _newItems; }
+        }
+
+        public IList OldItems
+        {
+            get { return _oldItems; }
+        }
+
+        public int NewStartingIndex
+        {
+            get { return _newStartIndex; }
+        }
+
+        public int OldStartingIndex
+        {
+            get { return _oldStartIndex; }
+        }
+
+        internal class SimpleMonitor : IDisposable
+        {
+            private int _busyCount;
+            public bool Busy
+            {
+                get
+                {
+                    return this._busyCount > 0;
+                }
+            }
+            public IDisposable Enter()
+            {
+                this._busyCount++;
+                return this;
+            }
+            public void Dispose()
+            {
+                this._busyCount--;
+            }
+            public SimpleMonitor()
+            {
+            }
+
+            internal void CheckReentrancy(EventHandler<CollectionChangedEventArgs> collectionChange)
+            {
+                if (Busy && collectionChange != null)
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
