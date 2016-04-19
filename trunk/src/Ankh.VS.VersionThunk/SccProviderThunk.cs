cd369df068ddbe0f0ae81ad5e0f00c678eb77ff5
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2016-04-19 11:05:58 +0000

    Introduce a VersionThunk assembly to allow (after a few more patches) to hook up the new in VS 2015 Update 2 SCC status bar.
    Add initial plumbing to test on the buildbot.

---
+++ trunk/src/Ankh.VS.VersionThunk/SccProviderThunk.cs
@@ -0,0 +1,16 @@
+﻿using Ankh.Scc;
+using System;
+using System.Text;
+
+namespace Ankh.Scc
+{
+    public abstract partial class SccProviderThunk : SccProvider
+    {
+        [CLSCompliant(false)]
+        protected SccProviderThunk(IAnkhServiceProvider context, SccProjectMap projectMap)
+            : base(context, projectMap)
+        {
+
+        }
+    }
+}
