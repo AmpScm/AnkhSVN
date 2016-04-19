3a1530d62380f6264923dd6a62b0ea64c764aa7b
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2016-04-19 12:14:30 +0000

    * src/Ankh.Package/Attributes/ProvideAnkhVersionThunkRedirectAttribute.cs * src/Ankh.VS.VersionThunk/Ankh.VS.VersionThunk.csproj * src/Ankh.VS.VersionThunk/VSVersionThunk.cs   Add stub class to hook up without requiring dependencies.

---
+++ trunk/src/Ankh.VS.VersionThunk/VSVersionThunk.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ankh.VS
+{
+    public static class VSVersionThunk
+    {
+    }
+}
