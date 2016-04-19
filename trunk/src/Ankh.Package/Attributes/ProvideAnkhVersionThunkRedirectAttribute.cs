0941b62d4416ab3dfc8015b0485ade060a33c6ce
rhuijben <rhuijben@ecf7f2a6-7b29-9143-b1c6-df2d17ec8657>
2016-04-19 12:08:25 +0000

    Now that the files are there, setup the version thunking on recent VS versions using the package definition (shadowed to the registry)
    * src/Ankh.Ids/AnkhId.cs
    * src/Ankh.Package/Ankh.Package.csproj
    * src/Ankh.Package/AnkhSvnPackage.cs
    * src/Ankh.Package/Attributes/ProvideAnkhVersionThunkRedirectAttribute.cs
      Setup redirect.

---
+++ trunk/src/Ankh.Package/Attributes/ProvideAnkhVersionThunkRedirectAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.VisualStudio.Shell;
+
+namespace Ankh.VSPackage.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    sealed class ProvideAnkhVersionThunkRedirectAttribute : RegistrationAttribute
+    {
+        public override void Register(RegistrationAttribute.RegistrationContext context)
+        {
+            AssemblyName name = typeof(Ankh.Scc.SccProviderThunk).Assembly.GetName();
+            using (Key key = context.CreateKey(GetKey()))
+            {
+                key.SetValue("name", name.Name);
+                key.SetValue("culture", "neutral");
+                key.SetValue("publicKeyToken", TokenToString(name.GetPublicKeyToken()));
+                key.SetValue("version", name.Version);
+                if (context.GetType().Name.ToUpperInvariant().Contains("PKGDEF"))
+                {
+                    string dllName = name.Name + "-V4.dll";
+
+                    if (!context.GetType().Name.Contains("Ankh"))
+                        dllName = name.Name + ".dll"; // Doesn't work in debug mode at this time
+
+                    key.SetValue("codeBase", Path.Combine("$PackageFolder$", dllName));
+                }
+                else
+                    key.SetValue("codeBase", "[#CF_" + name.Name + ".V4.dll" + "]");
+            }
+        }
+
+        private string TokenToString(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder(16);
+
+            foreach (byte b in bytes)
+                sb.AppendFormat("{0:x2}", b);
+
+            return sb.ToString();
+        }
+
+        public override void Unregister(RegistrationAttribute.RegistrationContext context)
+        {
+            context.RemoveKey(GetKey());
+        }
+
+        private string GetKey()
+        {
+            return @"RuntimeConfiguration\dependentAssembly\codeBase\" + new Guid(AnkhId.VersionThunkId).ToString("B");
+        }
+
+    }
+}
