bc04bf9c11a04b83a64a835aaf7bfbac4e1e8c4d
Alex Moir <alexrmoir@gmail.com>
2026-09-24 13:07:12 -0400

    Fix SelectionItemMap test VSItemId reference

--- src/Ankh.VS.UnitTest/Selection/SelectionItemMapContractTests.cs
+++ src/Ankh.VS.UnitTest/Selection/SelectionItemMapContractTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using Ankh;
 using Ankh.Selection;
 using Ankh.Services;
 using Microsoft.VisualStudio;
