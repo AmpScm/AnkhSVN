83df81781985f2fb69e5b412bbd5a7a78f254bae
Alex Moir <alexrmoir@gmail.com>
2026-09-23 22:10:59 -0400

    Keep Pending Changes selection behavior theme-neutral

--- src/Ankh.UI/PendingChanges/Commits/PendingCommitsView.cs
+++ src/Ankh.UI/PendingChanges/Commits/PendingCommitsView.cs
@@ -324,13 +324,6 @@ namespace Ankh.UI.PendingChanges.Commits
         {
             IWinFormsThemingService themer = sender.GetService<IWinFormsThemingService>();
 
-            if (themer != null)
-            {
-                HideSelection = PendingCommitsThemeLogic.ShouldHideInactiveSelection(
-                    themer.ThemePalette.IsDarkSurface,
-                    SystemInformation.HighContrast);
-            }
-
             if (VSVersion.VS2012OrLater
                 && themer != null
                 && PendingCommitsThemeLogic.ShouldCancelVsHeaderTheming(
