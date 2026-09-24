be32e7dcfa7f0a8046e24a7e5b24ba69faad3d63
Alex Moir <alexrmoir@gmail.com>
2026-09-23 22:10:54 -0400

    Use semantic selection color without luminance assumptions

--- src/Ankh.UI/VSSelectionControls/SmartListView.cs
+++ src/Ankh.UI/VSSelectionControls/SmartListView.cs
@@ -1653,10 +1653,7 @@ namespace Ankh.UI.VSSelectionControls
                 _headerForeColor = palette.SurfaceForeground;
                 _headerBorderColor = palette.Border;
                 _selectionBackColor = palette.SelectionBackground;
-                _selectionForeColor = SmartListViewThemeLogic.ResolveSelectionForeground(
-                    palette.IsDarkSurface,
-                    palette.SurfaceForeground,
-                    palette.SelectionForeground);
+                _selectionForeColor = palette.SelectionForeground;
                 _usePaletteSelectionColors = !SystemInformation.HighContrast;
             }
             else
