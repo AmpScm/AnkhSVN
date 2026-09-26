0ed1069adc27f13d477a47720c9598d0de413b42
Alex Moir <alexrmoir@gmail.com>
2026-09-25 21:52:51 -0400

    Fix issue navigation test compile

--- src/Ankh.VS.UnitTest/Dialogs/PendingIssuesPageTests.cs
+++ src/Ankh.VS.UnitTest/Dialogs/PendingIssuesPageTests.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Ankh;
 using Ankh.UI.IssueTracker;
 using Ankh.UI.PendingChanges;
 using System;
